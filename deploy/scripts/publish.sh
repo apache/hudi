e59dd480190e5b1ba305e6c633e977f71408c4d9
Prasanna Rajaperumal <prasanna@uber.com>
2016-12-21 12:47:01 -0800

    Add ossrh profile to publish maven artifacts to oss.sonatype.org (synced with maven central)

--- deploy/scripts/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-SCRIPT_DIR=`dirname $0`
-DEPLOY_DIR=$SCRIPT_DIR/..
-openssl aes-256-cbc -pass pass:$gpg.passphrase -in $DEPLOY_DIR/pubring.gpg.enc -out pubring.gpg -d
-openssl aes-256-cbc -pass pass:$gpg.passphrase -in $DEPLOY_DIR/secring.gpg.enc -out secring.gpg -d
