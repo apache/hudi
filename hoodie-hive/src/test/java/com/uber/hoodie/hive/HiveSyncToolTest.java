36a1947e7999ffd135e13a38cb46f3f17284e690
Nishith Agarwal <nagarwal@uber.com>
2017-06-22 20:33:33 -0700

    Sync Tool registers 2 tables, RO and RT Tables

--- hoodie-hive/src/test/java/com/uber/hoodie/hive/HiveSyncToolTest.java
+++ hoodie-hive/src/test/java/com/uber/hoodie/hive/HiveSyncToolTest.java
@@ -305,4 +305,59 @@ public class HiveSyncToolTest {
         hiveClient.getLastCommitTimeSynced().get());
   }
 
+  @Test
+  public void testSyncMergeOnReadRT()
+          throws IOException, InitializationError, URISyntaxException, TException, InterruptedException {
+    String commitTime = "100";
+    String deltaCommitTime = "101";
+    String roTablename = TestUtil.hiveSyncConfig.tableName;
+    TestUtil.hiveSyncConfig.tableName = TestUtil.hiveSyncConfig.tableName + HiveSyncTool.SUFFIX_REALTIME_TABLE;
+    TestUtil.createMORDataset(commitTime, deltaCommitTime, 5);
+    HoodieHiveClient hiveClientRT = new HoodieHiveClient(TestUtil.hiveSyncConfig,
+            TestUtil.getHiveConf(), TestUtil.fileSystem);
+
+    assertFalse("Table " + TestUtil.hiveSyncConfig.tableName + HiveSyncTool.SUFFIX_REALTIME_TABLE + " should not exist initially",
+            hiveClientRT.doesTableExist());
+
+    // Lets do the sync
+    HiveSyncTool tool = new HiveSyncTool(TestUtil.hiveSyncConfig, TestUtil.getHiveConf(),
+            TestUtil.fileSystem);
+    tool.syncHoodieTable();
+
+    assertTrue("Table " + TestUtil.hiveSyncConfig.tableName + HiveSyncTool.SUFFIX_REALTIME_TABLE + " should exist after sync completes",
+            hiveClientRT.doesTableExist());
+
+    assertEquals("Hive Schema should match the dataset schema + partition field",
+            hiveClientRT.getTableSchema().size(), SchemaTestUtil.getSimpleSchema().getFields().size() + 1);
+    assertEquals("Table partitions should match the number of partitions we wrote", 5,
+            hiveClientRT.scanTablePartitions().size());
+    assertEquals("The last commit that was sycned should be updated in the TBLPROPERTIES",
+            deltaCommitTime,
+            hiveClientRT.getLastCommitTimeSynced().get());
+
+    // Now lets create more parititions and these are the only ones which needs to be synced
+    DateTime dateTime = DateTime.now().plusDays(6);
+    String commitTime2 = "102";
+    String deltaCommitTime2 = "103";
+
+    TestUtil.addCOWPartitions(1, true, dateTime, commitTime2);
+    TestUtil.addMORPartitions(1, true, false, dateTime, commitTime2, deltaCommitTime2);
+    // Lets do the sync
+    tool = new HiveSyncTool(TestUtil.hiveSyncConfig, TestUtil.getHiveConf(),
+            TestUtil.fileSystem);
+    tool.syncHoodieTable();
+    hiveClientRT = new HoodieHiveClient(TestUtil.hiveSyncConfig,
+            TestUtil.getHiveConf(), TestUtil.fileSystem);
+
+    assertEquals("Hive Schema should match the evolved dataset schema + partition field",
+            hiveClientRT.getTableSchema().size(), SchemaTestUtil.getEvolvedSchema().getFields().size() + 1);
+    // Sync should add the one partition
+    assertEquals("The 2 partitions we wrote should be added to hive", 6,
+            hiveClientRT.scanTablePartitions().size());
+    assertEquals("The last commit that was sycned should be 103",
+            deltaCommitTime2,
+            hiveClientRT.getLastCommitTimeSynced().get());
+    TestUtil.hiveSyncConfig.tableName = roTablename;
+  }
+
 }
