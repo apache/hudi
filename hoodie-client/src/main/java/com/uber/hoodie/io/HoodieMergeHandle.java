bdd77dc4b4a2812d7fbec11760001f6ca4a018db
Sunil Ramaiah <sramaiah@uber.com>
2018-04-23 15:23:42 -0700

    Fix for updating duplicate records in same/different files in same parition

--- hoodie-client/src/main/java/com/uber/hoodie/io/HoodieMergeHandle.java
+++ hoodie-client/src/main/java/com/uber/hoodie/io/HoodieMergeHandle.java
@@ -35,9 +35,11 @@ import com.uber.hoodie.io.storage.HoodieStorageWriter;
 import com.uber.hoodie.io.storage.HoodieStorageWriterFactory;
 import com.uber.hoodie.table.HoodieTable;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.IndexedRecord;
 import org.apache.hadoop.fs.Path;
@@ -52,6 +54,7 @@ public class HoodieMergeHandle<T extends HoodieRecordPayload> extends HoodieIOHa
 
   private WriteStatus writeStatus;
   private Map<String, HoodieRecord<T>> keyToNewRecords;
+  private Set<String> writtenRecordKeys;
   private HoodieStorageWriter<IndexedRecord> storageWriter;
   private TableFileSystemView.ReadOptimizedView fileSystemView;
   private Path newFilePath;
@@ -81,6 +84,8 @@ public class HoodieMergeHandle<T extends HoodieRecordPayload> extends HoodieIOHa
    * Extract old file path, initialize StorageWriter and WriteStatus
    */
   private void init(String fileId, String partitionPath) {
+    this.writtenRecordKeys = new HashSet<>();
+
     WriteStatus writeStatus = ReflectionUtils.loadClass(config.getWriteStatusClassName());
     writeStatus.setStat(new HoodieWriteStat());
     this.writeStatus = writeStatus;
@@ -205,7 +210,7 @@ public class HoodieMergeHandle<T extends HoodieRecordPayload> extends HoodieIOHa
            */
           copyOldRecord = false;
         }
-        keyToNewRecords.remove(key);
+        writtenRecordKeys.add(key);
       } catch (Exception e) {
         throw new HoodieUpsertException(
             "Failed to combine/merge new record with old value in storage, for new record {"
@@ -239,10 +244,13 @@ public class HoodieMergeHandle<T extends HoodieRecordPayload> extends HoodieIOHa
       Iterator<String> pendingRecordsItr = keyToNewRecords.keySet().iterator();
       while (pendingRecordsItr.hasNext()) {
         String key = pendingRecordsItr.next();
-        HoodieRecord<T> hoodieRecord = keyToNewRecords.get(key);
-        writeUpdateRecord(hoodieRecord, hoodieRecord.getData().getInsertValue(schema));
+        if (!writtenRecordKeys.contains(key)) {
+          HoodieRecord<T> hoodieRecord = keyToNewRecords.get(key);
+          writeUpdateRecord(hoodieRecord, hoodieRecord.getData().getInsertValue(schema));
+        }
       }
       keyToNewRecords.clear();
+      writtenRecordKeys.clear();
 
       if (storageWriter != null) {
         storageWriter.close();
