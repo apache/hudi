083170dd2915352ee8dc6b6b4ade79e1a474cdef
Balaji Varadarajan <varadarb@uber.com>
2018-09-16 08:06:30 -0700

    Hive Sync handling must work for datasets with multi-partition keys

--- hoodie-hive/src/main/java/com/uber/hoodie/hive/HiveSyncConfig.java
+++ hoodie-hive/src/main/java/com/uber/hoodie/hive/HiveSyncConfig.java
@@ -85,4 +85,18 @@ public class HiveSyncConfig implements Serializable {
         + ", help=" + help
         + '}';
   }
+
+  public static HiveSyncConfig copy(HiveSyncConfig cfg) {
+    HiveSyncConfig newConfig = new HiveSyncConfig();
+    newConfig.basePath = cfg.basePath;
+    newConfig.assumeDatePartitioning = cfg.assumeDatePartitioning;
+    newConfig.databaseName = cfg.databaseName;
+    newConfig.hivePass = cfg.hivePass;
+    newConfig.hiveUser = cfg.hiveUser;
+    newConfig.partitionFields = cfg.partitionFields;
+    newConfig.partitionValueExtractorClass = cfg.partitionValueExtractorClass;
+    newConfig.jdbcUrl = cfg.jdbcUrl;
+    newConfig.tableName = cfg.tableName;
+    return newConfig;
+  }
 }
