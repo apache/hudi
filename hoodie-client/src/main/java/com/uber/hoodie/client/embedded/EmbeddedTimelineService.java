2c40e8419e209767653e2741e653a71bb429d31d
Balaji Varadarajan <varadarb@uber.com>
2019-06-20 08:34:56 -0700

    Ensure TableMetaClient and FileSystem instances have exclusive copy of Configuration

--- hoodie-client/src/main/java/com/uber/hoodie/client/embedded/EmbeddedTimelineService.java
+++ hoodie-client/src/main/java/com/uber/hoodie/client/embedded/EmbeddedTimelineService.java
@@ -68,7 +68,7 @@ public class EmbeddedTimelineService {
   }
 
   public void startServer() throws IOException {
-    server = new TimelineService(0, viewManager);
+    server = new TimelineService(0, viewManager, hadoopConf.get());
     serverPort = server.startService();
     logger.info("Started embedded timeline server at " + hostAddr + ":" + serverPort);
   }
