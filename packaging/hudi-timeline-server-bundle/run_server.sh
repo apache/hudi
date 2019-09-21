6e8a28bcaee173f703132f52f525d1568596ac3d
Balaji Varadarajan <varadarb@uber.com>
2019-09-20 23:01:32 -0700

    HUDI-121 : Address comments during RC2 voting
    1. Remove dnl utils jar from git
    2. Add LICENSE Headers in missing files
    3. Fix NOTICE and LICENSE in all HUDI packages and in top-level
    4. Fix License wording in certain HUDI source files
    5. Include non java/scala code in RAT licensing check
    6. Use whitelist to include dependencies as part of timeline-server bundling

--- packaging/hudi-timeline-server-bundle/run_server.sh
+++ packaging/hudi-timeline-server-bundle/run_server.sh
@@ -19,13 +19,35 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #Ensure we pick the right jar even for hive11 builds
-HOODIE_JAR=`ls -c $DIR/target/hudi-timeline-*.jar | grep -v test | head -1`
+HOODIE_JAR=`ls -c $DIR/target/hudi-timeline-server-bundle-*.jar | grep -v test | head -1`
+
+if [ -z "$HADOOP_HOME" ]; then
+  echo "HADOOP_HOME not set. It must be set"
+  exit -1
+fi
 
 if [ -z "$HADOOP_CONF_DIR" ]; then
   echo "setting hadoop conf dir"
   HADOOP_CONF_DIR="/etc/hadoop/conf"
 fi
 
-OTHER_JARS=`ls -1 $DIR/target/lib/*jar | grep -v '*avro*-1.' | tr '\n' ':'`
-echo "Running command : java -cp $DIR/target/:${HADOOP_CONF_DIR}:$HOODIE_JAR:$OTHER_JARS org.apache.hudi.timeline.service.TimelineService $@"
-java -Xmx4G -cp $DIR/target/test-classes/:${HADOOP_CONF_DIR}:$HOODIE_JAR:$OTHER_JARS org.apache.hudi.timeline.service.TimelineService "$@"
+if [ ! -f "${DIR}/javax.servlet-api-3.1.0.jar" ]; then
+ echo "Downloading runtime servlet jar first :"
+ wget https://repo1.maven.org/maven2/javax/servlet/javax.servlet-api/3.1.0/javax.servlet-api-3.1.0.jar
+fi
+
+if [ ! -f "${DIR}/javax.servlet-api-3.1.0.jar" ]; then
+ echo "Servlet API Jar not found. "
+ exit -1
+fi
+
+HADOOP_COMMON_JARS=`ls -c ${HADOOP_HOME}/share/hadoop/common/*.jar | grep -v test | grep -v source | grep -v servlet | grep -v 'jetty-' | grep -v 'jersey-' | grep -v 'jsp-' | tr '\n' ':'`
+HADOOP_COMMON_LIB_JARS=`ls -c ${HADOOP_HOME}/share/hadoop/common/lib/*.jar | grep -v test | grep -v source | grep -v servlet | grep -v 'jetty-' | grep -v 'jersey-' | grep -v 'jsp-' | tr '\n' ':'`
+HADOOP_HDFS_JARS=`ls -c ${HADOOP_HOME}/share/hadoop/hdfs/*.jar | grep -v test | grep -v source | grep -v servlet | grep -v 'jetty-' | grep -v 'jersey-' | grep -v 'jsp-' | tr '\n' ':'`
+HADOOP_HDFS_LIB_JARS=`ls -c ${HADOOP_HOME}/share/hadoop/hdfs/lib/*.jar | grep -v test | grep -v source | grep -v servlet | grep -v 'jetty-' | grep -v 'jersey-' | grep -v 'jsp-' | tr '\n' ':'`
+HADOOP_JARS=${HADOOP_HOME}/share/hadoop/common/*:${HADOOP_HOME}/share/hadoop/mapreduce/*:${HADOOP_HOME}/share/hadoop/hdfs/*:${HADOOP_HOME}/share/hadoop/common/lib/*:${HADOOP_HOME}/share/hadoop/hdfs/lib/*
+HADOOP_JARS=${HADOOP_COMMON_JARS}:${HADOOP_COMMON_LIB_JARS}:${HADOOP_HDFS_JARS}:${HADOOP_HDFS_LIB_JARS}
+
+SERVLET_JAR=${DIR}/javax.servlet-api-3.1.0.jar
+echo "Running command : java -cp ${SERVLET_JAR}:${HADOOP_JARS}:${HADOOP_CONF_DIR}:$HOODIE_JAR org.apache.hudi.timeline.service.TimelineService $@"
+java -Xmx4G -cp ${SERVLET_JAR}:${HADOOP_JARS}:${HADOOP_CONF_DIR}:$HOODIE_JAR org.apache.hudi.timeline.service.TimelineService "$@"
