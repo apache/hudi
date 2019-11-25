723e6b6ff49136013bcde9389598d37564a432d9
hongdd <jn_hdd@163.com>
2019-11-25 16:34:16 +0800

    test checks

--- hudi-cli/hudi-cli.sh
+++ hudi-cli/hudi-cli.sh
@@ -25,7 +25,6 @@ HOODIE_JAR=`ls $DIR/target/hudi-cli-*.jar | grep -v source | grep -v javadoc`
 
 if [ -z "$CLIENT_JAR" ]; then
   echo "Client jar location not set, please set it in conf/hudi-env.sh"
-  exit
 fi
 
 java -cp ${HADOOP_CONF_DIR}:${SPARK_CONF_DIR}:$DIR/target/lib/*:$HOODIE_JAR:${CLIENT_JAR} -DSPARK_CONF_DIR=${SPARK_CONF_DIR} -DHADOOP_CONF_DIR=${HADOOP_CONF_DIR} org.springframework.shell.Bootstrap $@
