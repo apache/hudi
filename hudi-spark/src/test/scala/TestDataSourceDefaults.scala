23ff366eb258d842b1941ec37334e198efacb8cf
Brandon Scheller <bschelle@amazon.com>
2019-11-25 23:23:53 +0000

    Fix typo in merge commit

--- hudi-spark/src/test/scala/TestDataSourceDefaults.scala
+++ hudi-spark/src/test/scala/TestDataSourceDefaults.scala
@@ -19,7 +19,7 @@ import org.apache.avro.generic.GenericRecord
 import org.apache.hudi.common.model.EmptyHoodieRecordPayload
 import org.apache.hudi.common.util.{Option, SchemaTestUtil, TypedProperties}
 import org.apache.hudi.exception.{HoodieException, HoodieKeyException}
-import org.apache.hudi.{ComplexKeyGenerator, DataSourceWriteOptions, EmptyHoodieRecordPayload, OverwriteWithLatestAvroPayload, SimpleKeyGenerator}
+import org.apache.hudi.{ComplexKeyGenerator, DataSourceWriteOptions, OverwriteWithLatestAvroPayload, SimpleKeyGenerator}
 import org.junit.Assert._
 import org.junit.{Before, Test}
 import org.scalatest.junit.AssertionsForJUnit
