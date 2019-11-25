65b45bac155f23dc60a591ded04f466cb34c78d7
fbalicchia <fbalicchia@cuebiq.com>
2019-11-25 09:22:51 +0100

    remove unused Callable import

--- hudi-spark/src/test/java/HoodieJavaStreamingApp.java
+++ hudi-spark/src/test/java/HoodieJavaStreamingApp.java
@@ -19,7 +19,6 @@
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
