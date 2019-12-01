c3c87ca2c526d1a97b7cff73f3fe5523ed11fc50
XuQianJin-Stars <x1q1j1@163.com>
2019-12-01 15:29:58 +0800

    [HUDI-209] Implement JMX metrics reporter

--- hudi-client/src/main/java/org/apache/hudi/config/HoodieWriteConfig.java
+++ hudi-client/src/main/java/org/apache/hudi/config/HoodieWriteConfig.java
@@ -478,8 +478,8 @@ public class HoodieWriteConfig extends DefaultHoodieConfig {
     return props.getProperty(HoodieMetricsConfig.JMX_HOST);
   }
 
-  public int getJmxPort() {
-    return Integer.parseInt(props.getProperty(HoodieMetricsConfig.JMX_PORT));
+  public String getJmxPorts() {
+    return props.getProperty(HoodieMetricsConfig.JMX_PORT);
   }
 
   /**
--- hudi-client/src/main/java/org/apache/hudi/metrics/JmxMetricsReporter.java
+++ hudi-client/src/main/java/org/apache/hudi/metrics/JmxMetricsReporter.java
@@ -20,11 +20,14 @@ package org.apache.hudi.metrics;
 
 import org.apache.hudi.config.HoodieWriteConfig;
 import org.apache.hudi.exception.HoodieException;
-
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jmx.JmxReporter;
 import com.google.common.base.Preconditions;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import java.util.Iterator;
+import javax.management.MBeanServer;
 import javax.management.remote.JMXConnectorServer;
 import javax.management.remote.JMXConnectorServerFactory;
 import javax.management.remote.JMXServiceURL;
@@ -32,33 +35,50 @@ import javax.management.remote.JMXServiceURL;
 import java.io.Closeable;
 import java.lang.management.ManagementFactory;
 import java.rmi.registry.LocateRegistry;
+import org.apache.hudi.client.utils.NetUtils;
 
 /**
  * Implementation of Jmx reporter, which used to report jmx metric.
  */
 public class JmxMetricsReporter extends MetricsReporter {
-
   private static final Logger LOG = LogManager.getLogger(JmxMetricsReporter.class);
   private final JMXConnectorServer connector;
+  private final JmxReporter reporter;
   private String host;
-  private int port;
 
-  public JmxMetricsReporter(HoodieWriteConfig config) {
+  public JmxMetricsReporter(HoodieWriteConfig config, MetricRegistry registry) {
     try {
       // Check the host and port here
       this.host = config.getJmxHost();
-      this.port = config.getJmxPort();
-      if (host == null || port == 0) {
+      String portsConfig = config.getJmxPorts();
+      if (host == null || portsConfig == null) {
         throw new RuntimeException(
             String.format("Jmx cannot be initialized with host[%s] and port[%s].",
-                host, port));
+                host, portsConfig));
       }
-      LocateRegistry.createRegistry(port);
-      String serviceUrl =
-          "service:jmx:rmi://" + host + ":" + port + "/jndi/rmi://" + host + ":" + port + "/jmxrmi";
-      JMXServiceURL url = new JMXServiceURL(serviceUrl);
-      this.connector = JMXConnectorServerFactory
-          .newJMXConnectorServer(url, null, ManagementFactory.getPlatformMBeanServer());
+
+      Iterator<Integer> ports = NetUtils.getPortRangeFromString(portsConfig);
+      JMXConnectorServer successConnectorServer = null;
+      JmxReporter successReporter = null;
+      while (ports.hasNext() && successConnectorServer == null) {
+        int port = ports.next();
+        LocateRegistry.createRegistry(port);
+        String serviceUrl =
+            "service:jmx:rmi://" + host + ":" + port + "/jndi/rmi://" + host + ":" + port
+                + "/jmxrmi";
+        JMXServiceURL url = new JMXServiceURL(serviceUrl);
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        successConnectorServer = JMXConnectorServerFactory
+            .newJMXConnectorServer(url, null, mBeanServer);
+        successReporter = JmxReporter.forRegistry(registry).registerWith(mBeanServer).build();
+      }
+      if (successConnectorServer == null || successReporter == null) {
+        throw new RuntimeException(
+            "Could not start JMX server on any configured port. Ports: " + portsConfig);
+      }
+      this.connector = successConnectorServer;
+      this.reporter = successReporter;
+      LOG.info("Configured JMXReporter with {port:" + portsConfig + "}");
     } catch (Exception e) {
       String msg = "Jmx initialize failed: ";
       LOG.error(msg, e);
@@ -71,6 +91,7 @@ public class JmxMetricsReporter extends MetricsReporter {
     try {
       Preconditions.checkNotNull(connector, "Cannot start as the jmxReporter is null.");
       connector.start();
+      reporter.start();
     } catch (Exception e) {
       throw new HoodieException(e);
     }
@@ -82,6 +103,6 @@ public class JmxMetricsReporter extends MetricsReporter {
 
   @Override
   public Closeable getReporter() {
-    return null;
+    return reporter;
   }
 }
