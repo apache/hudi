e637d9ed26fea1a336f2fd6139cde0dd192c429d
hejinbiao123 <38057507+hejinbiao123@users.noreply.github.com>
2019-12-31 13:49:34 +0800

    [HUDI-455] Redo hudi-client log statements using SLF4J (#1145)
    * [HUDI-455] Redo hudi-client log statements using SLF4J

--- hudi-client/src/main/java/org/apache/hudi/index/hbase/DefaultHBaseQPSResourceAllocator.java
+++ hudi-client/src/main/java/org/apache/hudi/index/hbase/DefaultHBaseQPSResourceAllocator.java
@@ -20,12 +20,12 @@ package org.apache.hudi.index.hbase;
 
 import org.apache.hudi.config.HoodieWriteConfig;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DefaultHBaseQPSResourceAllocator implements HBaseIndexQPSResourceAllocator {
   private HoodieWriteConfig hoodieWriteConfig;
-  private static final Logger LOG = LogManager.getLogger(DefaultHBaseQPSResourceAllocator.class);
+  private static final Logger LOG = LoggerFactory.getLogger(DefaultHBaseQPSResourceAllocator.class);
 
   public DefaultHBaseQPSResourceAllocator(HoodieWriteConfig hoodieWriteConfig) {
     this.hoodieWriteConfig = hoodieWriteConfig;
@@ -46,7 +46,7 @@ public class DefaultHBaseQPSResourceAllocator implements HBaseIndexQPSResourceAl
   @Override
   public void releaseQPSResources() {
     // Do nothing, as there are no resources locked in default implementation
-    LOG.info(String.format("Release QPS resources called for %s with default implementation, do nothing",
-        this.hoodieWriteConfig.getHbaseTableName()));
+    LOG.info("Release QPS resources called for {} with default implementation, do nothing",
+        this.hoodieWriteConfig.getHbaseTableName());
   }
 }
--- hudi-client/src/main/java/org/apache/hudi/io/HoodieCommitArchiveLog.java
+++ hudi-client/src/main/java/org/apache/hudi/io/HoodieCommitArchiveLog.java
@@ -54,9 +54,9 @@ import com.google.common.collect.Sets;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.IndexedRecord;
 import org.apache.hadoop.fs.Path;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -71,7 +71,7 @@ import java.util.stream.Stream;
  */
 public class HoodieCommitArchiveLog {
 
-  private static final Logger LOG = LogManager.getLogger(HoodieCommitArchiveLog.class);
+  private static final Logger LOG = LoggerFactory.getLogger(HoodieCommitArchiveLog.class);
 
   private final Path archiveFilePath;
   private final HoodieTableMetaClient metaClient;
@@ -118,9 +118,9 @@ public class HoodieCommitArchiveLog {
       boolean success = true;
       if (!instantsToArchive.isEmpty()) {
         this.writer = openWriter();
-        LOG.info("Archiving instants " + instantsToArchive);
+        LOG.info("Archiving instants {}", instantsToArchive);
         archive(instantsToArchive);
-        LOG.info("Deleting archived instants " + instantsToArchive);
+        LOG.info("Deleting archived instants {}", instantsToArchive);
         success = deleteArchivedInstants(instantsToArchive);
       } else {
         LOG.info("No Instants to archive");
@@ -189,14 +189,14 @@ public class HoodieCommitArchiveLog {
   }
 
   private boolean deleteArchivedInstants(List<HoodieInstant> archivedInstants) throws IOException {
-    LOG.info("Deleting instants " + archivedInstants);
+    LOG.info("Deleting instants {}", archivedInstants);
     boolean success = true;
     for (HoodieInstant archivedInstant : archivedInstants) {
       Path commitFile = new Path(metaClient.getMetaPath(), archivedInstant.getFileName());
       try {
         if (metaClient.getFs().exists(commitFile)) {
           success &= metaClient.getFs().delete(commitFile, false);
-          LOG.info("Archived and deleted instant file " + commitFile);
+          LOG.info("Archived and deleted instant file {}", commitFile);
         }
       } catch (IOException e) {
         throw new HoodieIOException("Failed to delete archived instant " + archivedInstant, e);
@@ -208,7 +208,7 @@ public class HoodieCommitArchiveLog {
       return i.isCompleted() && (i.getAction().equals(HoodieTimeline.COMMIT_ACTION)
           || (i.getAction().equals(HoodieTimeline.DELTA_COMMIT_ACTION)));
     }).max(Comparator.comparing(HoodieInstant::getTimestamp)));
-    LOG.info("Latest Committed Instant=" + latestCommitted);
+    LOG.info("Latest Committed Instant={}", latestCommitted);
     if (latestCommitted.isPresent()) {
       success &= deleteAllInstantsOlderorEqualsInAuxMetaFolder(latestCommitted.get());
     }
@@ -236,7 +236,7 @@ public class HoodieCommitArchiveLog {
       Path metaFile = new Path(metaClient.getMetaAuxiliaryPath(), deleteInstant.getFileName());
       if (metaClient.getFs().exists(metaFile)) {
         success &= metaClient.getFs().delete(metaFile, false);
-        LOG.info("Deleted instant file in auxiliary metapath : " + metaFile);
+        LOG.info("Deleted instant file in auxiliary metapath : {}", metaFile);
       }
     }
     return success;
@@ -246,7 +246,7 @@ public class HoodieCommitArchiveLog {
     try {
       HoodieTimeline commitTimeline = metaClient.getActiveTimeline().getAllCommitsTimeline().filterCompletedInstants();
       Schema wrapperSchema = HoodieArchivedMetaEntry.getClassSchema();
-      LOG.info("Wrapper schema " + wrapperSchema.toString());
+      LOG.info("Wrapper schema {}", wrapperSchema.toString());
       List<IndexedRecord> records = new ArrayList<>();
       for (HoodieInstant hoodieInstant : instants) {
         try {
@@ -255,7 +255,7 @@ public class HoodieCommitArchiveLog {
             writeToFile(wrapperSchema, records);
           }
         } catch (Exception e) {
-          LOG.error("Failed to archive commits, .commit file: " + hoodieInstant.getFileName(), e);
+          LOG.error("Failed to archive commits, commit file: {}", hoodieInstant.getFileName(), e);
           if (this.config.isFailOnTimelineArchivingEnabled()) {
             throw e;
           }
