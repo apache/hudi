963ef18fbdeb029125a41ed6fcef315d33977a40
OpenOpened <a261049174@outlook.com>
2020-01-07 14:22:24 +0800

    fix check style

--- hudi-utilities/src/test/java/org/apache/hudi/utilities/TestTimestampBasedKeyGenerator.java
+++ hudi-utilities/src/test/java/org/apache/hudi/utilities/TestTimestampBasedKeyGenerator.java
@@ -15,60 +15,57 @@ import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 
 public class TestTimestampBasedKeyGenerator {
-    private Schema schema = SchemaTestUtil.getTimestampEvolvedSchema();
-    private GenericRecord baseRecord = null;
+  private Schema schema = SchemaTestUtil.getTimestampEvolvedSchema();
+  private GenericRecord baseRecord = null;
 
-    public TestTimestampBasedKeyGenerator() throws IOException {
-    }
+  public TestTimestampBasedKeyGenerator() throws IOException {
+  }
 
-    @Before
-    public void initialize() throws IOException {
-        baseRecord = SchemaTestUtil
-                .generateAvroRecordFromJson(schema, 1, "001", "f1");
-    }
+  @Before
+  public void initialize() throws IOException {
+    baseRecord = SchemaTestUtil
+        .generateAvroRecordFromJson(schema, 1, "001", "f1");
+  }
 
-    private TypedProperties getKeyConfig(String recordKeyFieldName, String partitionPathField, String hiveStylePartitioning) {
-        TypedProperties props = new TypedProperties();
-        props.setProperty(DataSourceWriteOptions.RECORDKEY_FIELD_OPT_KEY(), recordKeyFieldName);
-        props.setProperty(DataSourceWriteOptions.PARTITIONPATH_FIELD_OPT_KEY(), partitionPathField);
-        props.setProperty(DataSourceWriteOptions.HIVE_STYLE_PARTITIONING_OPT_KEY(), hiveStylePartitioning);
-        props.setProperty("hoodie.deltastreamer.keygen.timebased.timestamp.type", "EPOCHMILLISECONDS");
-        props.setProperty("hoodie.deltastreamer.keygen.timebased.output.dateformat", "yyyy-MM-dd hh");
-        props.setProperty("hoodie.deltastreamer.keygen.timebased.timezone", "GMT+8:00");
-        return props;
-    }
+  private TypedProperties getKeyConfig(String recordKeyFieldName, String partitionPathField, String hiveStylePartitioning) {
+    TypedProperties props = new TypedProperties();
+    props.setProperty(DataSourceWriteOptions.RECORDKEY_FIELD_OPT_KEY(), recordKeyFieldName);
+    props.setProperty(DataSourceWriteOptions.PARTITIONPATH_FIELD_OPT_KEY(), partitionPathField);
+    props.setProperty(DataSourceWriteOptions.HIVE_STYLE_PARTITIONING_OPT_KEY(), hiveStylePartitioning);
+    props.setProperty("hoodie.deltastreamer.keygen.timebased.timestamp.type", "EPOCHMILLISECONDS");
+    props.setProperty("hoodie.deltastreamer.keygen.timebased.output.dateformat", "yyyy-MM-dd hh");
+    props.setProperty("hoodie.deltastreamer.keygen.timebased.timezone", "GMT+8:00");
+    return props;
+  }
 
-    private TypedProperties getKeyConfig2(String recordKeyFieldName, String partitionPathField, String hiveStylePartitioning) {
-        TypedProperties props = getKeyConfig(recordKeyFieldName, partitionPathField, hiveStylePartitioning);
-        props.setProperty("hoodie.deltastreamer.keygen.timebased.timezone", "GMT");
-        return props;
-    }
+  private TypedProperties getKeyConfig2(String recordKeyFieldName, String partitionPathField, String hiveStylePartitioning) {
+    TypedProperties props = getKeyConfig(recordKeyFieldName, partitionPathField, hiveStylePartitioning);
+    props.setProperty("hoodie.deltastreamer.keygen.timebased.timezone", "GMT");
+    return props;
+  }
 
-    private TypedProperties getKeyConfig3(String recordKeyFieldName, String partitionPathField, String hiveStylePartitioning) {
-        TypedProperties props = getKeyConfig(recordKeyFieldName, partitionPathField, hiveStylePartitioning);
-        props.setProperty("hoodie.deltastreamer.keygen.timebased.timestamp.type", "DATE_STRING");
-        props.setProperty("hoodie.deltastreamer.keygen.timebased.input.dateformat", "yyyy-MM-dd hh:mm:ss");
-        props.setProperty("hoodie.deltastreamer.keygen.timebased.timezone", "GMT+8:00");
-        return props;
-    }
+  private TypedProperties getKeyConfig3(String recordKeyFieldName, String partitionPathField, String hiveStylePartitioning) {
+    TypedProperties props = getKeyConfig(recordKeyFieldName, partitionPathField, hiveStylePartitioning);
+    props.setProperty("hoodie.deltastreamer.keygen.timebased.timestamp.type", "DATE_STRING");
+    props.setProperty("hoodie.deltastreamer.keygen.timebased.input.dateformat", "yyyy-MM-dd hh:mm:ss");
+    props.setProperty("hoodie.deltastreamer.keygen.timebased.timezone", "GMT+8:00");
+    return props;
+  }
 
+  @Test
+  public void testTimestampBasedKeyGenerator() {
+    // if timezone is GMT+8:00
+    baseRecord.put("createTime", 1578283932000L);
+    HoodieKey hk1 = new TimestampBasedKeyGenerator(getKeyConfig("field1", "createTime", "false")).getKey(baseRecord);
+    assertEquals(hk1.getPartitionPath(), "2020-01-06 12");
 
-    @Test
-    public void testTimestampBasedKeyGenerator() {
-        // if timezone is GMT+8:00
-        baseRecord.put("createTime",1578283932000L);
-        HoodieKey hk1 = new TimestampBasedKeyGenerator(getKeyConfig("field1", "createTime", "false")).getKey(baseRecord);
-        assertEquals(hk1.getPartitionPath(), "2020-01-06 12");
+    // if timezone is GMT
+    HoodieKey hk2 = new TimestampBasedKeyGenerator(getKeyConfig2("field1", "createTime", "false")).getKey(baseRecord);
+    assertEquals(hk2.getPartitionPath(), "2020-01-06 04");
 
-        // if timezone is GMT
-        HoodieKey hk2 = new TimestampBasedKeyGenerator(getKeyConfig2("field1", "createTime", "false")).getKey(baseRecord);
-        assertEquals(hk2.getPartitionPath(), "2020-01-06 04");
-
-        // if timestamp is DATE_STRING
-        baseRecord.put("createTime","2020-01-06 12:12:12");
-        HoodieKey hk3 = new TimestampBasedKeyGenerator(getKeyConfig3("field1", "createTime", "false")).getKey(baseRecord);
-        assertEquals(hk3.getPartitionPath(), "2020-01-06 12");
-    }
-
-
+    // if timestamp is DATE_STRING
+    baseRecord.put("createTime", "2020-01-06 12:12:12");
+    HoodieKey hk3 = new TimestampBasedKeyGenerator(getKeyConfig3("field1", "createTime", "false")).getKey(baseRecord);
+    assertEquals(hk3.getPartitionPath(), "2020-01-06 12");
+  }
 }
