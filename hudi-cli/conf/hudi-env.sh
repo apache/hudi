11fb2c26145f672d36a3491366c05caf3a23fde8
lamber-ken <lamberken@163.com>
2020-02-25 21:43:20 +0800

    [HUDI-580] Fix incorrect license header in files

--- hudi-cli/conf/hudi-env.sh
+++ hudi-cli/conf/hudi-env.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-################################################################################
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
 #  distributed with this work for additional information
@@ -16,7 +15,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 # limitations under the License.
-################################################################################
 
 # Set the necessary environment variables
 export HADOOP_CONF_DIR=${HADOOP_CONF_DIR:-"/etc/hadoop/conf"}
