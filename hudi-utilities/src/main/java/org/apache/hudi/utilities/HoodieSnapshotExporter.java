d8d9de1bcf387de8b782b2e6cea4acd677ea2cf9
OpenOpened <a261049174@outlook.com>
2020-02-26 18:00:45 +0800

    export and copier unite

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/HoodieSnapshotExporter.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/HoodieSnapshotExporter.java
@@ -3,11 +3,13 @@ package org.apache.hudi.utilities;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
-import org.apache.hudi.AvroConversionUtils;
-import org.apache.hudi.common.model.HoodieBaseFile;
-import org.apache.hudi.common.model.HoodieCommitMetadata;
+import org.apache.hudi.common.SerializableConfiguration;
+import org.apache.hudi.common.model.HoodiePartitionMetadata;
+import org.apache.hudi.common.table.HoodieTableConfig;
 import org.apache.hudi.common.table.HoodieTableMetaClient;
 import org.apache.hudi.common.table.HoodieTimeline;
 import org.apache.hudi.common.table.TableFileSystemView;
@@ -15,35 +17,34 @@ import org.apache.hudi.common.table.timeline.HoodieInstant;
 import org.apache.hudi.common.table.view.HoodieTableFileSystemView;
 import org.apache.hudi.common.util.FSUtils;
 import org.apache.hudi.common.util.Option;
-import org.apache.hudi.common.util.ParquetUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.apache.spark.SparkContext;
-import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Column;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.StructType;
+import scala.Tuple2;
 import scala.collection.JavaConversions;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+/**
+ * export the latest records of Hudi dataset to a set of external files (e.g., plain parquet files).
+ */
 
 public class HoodieSnapshotExporter {
   private static final Logger LOG = LogManager.getLogger(HoodieSnapshotExporter.class);
 
   public static class Config implements Serializable {
     @Parameter(names = {"--source-base-path", "-sbp"}, description = "Base path for the source Hudi dataset to be snapshotted", required = true)
-    String sourceBasePath = null;
+    String basePath = null;
 
     @Parameter(names = {"--target-base-path", "-tbp"}, description = "Base path for the target output files (snapshots)", required = true)
-    String targetBasePath = null;
+    String outputPath = null;
 
     @Parameter(names = {"--snapshot-prefix", "-sp"}, description = "Snapshot prefix or directory under the target base path in order to segregate different snapshots")
     String snapshotPrefix;
@@ -59,36 +60,16 @@ public class HoodieSnapshotExporter {
   }
 
   public void export(SparkSession spark, Config cfg) throws IOException {
-    String sourceBasePath = cfg.sourceBasePath;
-    String targetBasePath = cfg.targetBasePath;
+    String sourceBasePath = cfg.basePath;
+    String targetBasePath = cfg.outputPath;
     String snapshotPrefix = cfg.snapshotPrefix;
     String outputFormat = cfg.outputFormat;
     String outputPartitionField = cfg.outputPartitionField;
     String outputPartitioner = cfg.outputPartitioner;
-    SparkContext jc = spark.sparkContext();
-    FileSystem fs = FSUtils.getFs(sourceBasePath, jc.hadoopConfiguration());
-//    final HoodieTableMetaClient tableMetadata = new HoodieTableMetaClient(fs.getConf(), sourceBasePath);
-//    HoodieTimeline commitTimeline = tableMetadata.getActiveTimeline().getCommitsTimeline().filterCompletedInstants();
-//    HoodieInstant hoodieInstant = commitTimeline.lastInstant().get();
-//    Option<byte[]> instant = commitTimeline.getInstantDetails(hoodieInstant);
-//    HoodieCommitMetadata latestMeta = HoodieCommitMetadata
-//        .fromBytes(instant.get(), HoodieCommitMetadata.class);
-//    HashMap<String, String> fileIdAndFullPaths = latestMeta.getFileIdAndFullPaths(sourceBasePath);
-//
-//    if (fileIdAndFullPaths.size() <= 0) {
-//      spark.stop();
-//      System.exit(-1);
-//    }
-//
-//    List<String> files = fileIdAndFullPaths.values().stream().collect(Collectors.toList());
-//    if (snapshotPrefix != null && !snapshotPrefix.isEmpty()) {
-//      files = fileIdAndFullPaths.values().stream().filter(f -> f.contains(snapshotPrefix)).collect(Collectors.toList());
-//    }
-//
-//    spark.read().parquet(JavaConversions.asScalaIterator(files.iterator()).toSeq()).show();
+    JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+    FileSystem fs = FSUtils.getFs(sourceBasePath, jsc.hadoopConfiguration());
 
-//    spark.read().parquet(JavaConversions.asScalaIterator(files.iterator()).toSeq()).write().format(outputFormat).mode(SaveMode.Overwrite).save(targetBasePath);
-
+    final SerializableConfiguration serConf = new SerializableConfiguration(jsc.hadoopConfiguration());
     final HoodieTableMetaClient tableMetadata = new HoodieTableMetaClient(fs.getConf(), sourceBasePath);
     final TableFileSystemView.BaseFileOnlyView fsView = new HoodieTableFileSystemView(tableMetadata,
         tableMetadata.getActiveTimeline().getCommitsTimeline().filterCompletedInstants());
@@ -104,30 +85,94 @@ public class HoodieSnapshotExporter {
         latestCommitTimestamp));
 
     List<String> partitions = FSUtils.getAllPartitionPaths(fs, sourceBasePath, false);
-    List<String> dataFiles = new ArrayList<>();
-    if (StringUtils.isNotBlank(snapshotPrefix)) {
-      for (String partition : partitions) {
-        if (partition.contains(snapshotPrefix)) {
+    if (partitions.size() > 0) {
+      List<String> dataFiles = new ArrayList<>();
+
+      if (StringUtils.isNotBlank(snapshotPrefix)) {
+        for (String partition : partitions) {
+          if (partition.contains(snapshotPrefix)) {
+            dataFiles.addAll(fsView.getLatestBaseFilesBeforeOrOn(partition, latestCommitTimestamp).map(f -> f.getPath()).collect(Collectors.toList()));
+          }
+        }
+      } else {
+        for (String partition : partitions) {
           dataFiles.addAll(fsView.getLatestBaseFilesBeforeOrOn(partition, latestCommitTimestamp).map(f -> f.getPath()).collect(Collectors.toList()));
         }
       }
-    } else {
-      for (String partition : partitions) {
-        dataFiles.addAll(fsView.getLatestBaseFilesBeforeOrOn(partition, latestCommitTimestamp).map(f -> f.getPath()).collect(Collectors.toList()));
+
+      if (!outputFormat.equalsIgnoreCase("hudi")) {
+        // Do transformation
+        if (StringUtils.isNotBlank(outputPartitionField)) {
+          // A field to do simple Spark repartitioning
+          spark.read().parquet(JavaConversions.asScalaIterator(dataFiles.iterator()).toSeq()).repartition(new Column(outputPartitionField)).write().format(outputFormat).mode(SaveMode.Overwrite).save(targetBasePath);
+        } else {
+          spark.read().parquet(JavaConversions.asScalaIterator(dataFiles.iterator()).toSeq()).write().format(outputFormat).mode(SaveMode.Overwrite).save(targetBasePath);
+        }
+      } else {
+        // No transformation is needed for output format "HUDI", just copy the original files.
+
+        // Make sure the output directory is empty
+        Path outputPath = new Path(targetBasePath);
+        if (fs.exists(outputPath)) {
+          LOG.warn(String.format("The output path %s targetBasePath already exists, deleting", outputPath));
+          fs.delete(new Path(targetBasePath), true);
+        }
+
+        jsc.parallelize(partitions, partitions.size()).flatMap(partition -> {
+          // Only take latest version files <= latestCommit.
+          FileSystem fs1 = FSUtils.getFs(sourceBasePath, serConf.newCopy());
+          List<Tuple2<String, String>> filePaths = new ArrayList<>();
+          dataFiles.forEach(hoodieDataFile -> filePaths.add(new Tuple2<>(partition, hoodieDataFile)));
+
+          // also need to copy over partition metadata
+          Path partitionMetaFile =
+              new Path(new Path(sourceBasePath, partition), HoodiePartitionMetadata.HOODIE_PARTITION_METAFILE);
+          if (fs1.exists(partitionMetaFile)) {
+            filePaths.add(new Tuple2<>(partition, partitionMetaFile.toString()));
+          }
+
+          return filePaths.iterator();
+        }).foreach(tuple -> {
+          String partition = tuple._1();
+          Path sourceFilePath = new Path(tuple._2());
+          Path toPartitionPath = new Path(targetBasePath, partition);
+          FileSystem ifs = FSUtils.getFs(targetBasePath, serConf.newCopy());
+
+          if (!ifs.exists(toPartitionPath)) {
+            ifs.mkdirs(toPartitionPath);
+          }
+          FileUtil.copy(ifs, sourceFilePath, ifs, new Path(toPartitionPath, sourceFilePath.getName()), false,
+              ifs.getConf());
+        });
+
+        // Also copy the .commit files
+        LOG.info(String.format("Copying .commit files which are no-late-than %s.", latestCommitTimestamp));
+        FileStatus[] commitFilesToCopy =
+            fs.listStatus(new Path(sourceBasePath + "/" + HoodieTableMetaClient.METAFOLDER_NAME), (commitFilePath) -> {
+              if (commitFilePath.getName().equals(HoodieTableConfig.HOODIE_PROPERTIES_FILE)) {
+                return true;
+              } else {
+                String commitTime = FSUtils.getCommitFromCommitFile(commitFilePath.getName());
+                return HoodieTimeline.compareTimestamps(commitTime, latestCommitTimestamp,
+                    HoodieTimeline.LESSER_OR_EQUAL);
+              }
+            });
+        for (FileStatus commitStatus : commitFilesToCopy) {
+          Path targetFilePath =
+              new Path(targetBasePath + "/" + HoodieTableMetaClient.METAFOLDER_NAME + "/" + commitStatus.getPath().getName());
+          if (!fs.exists(targetFilePath.getParent())) {
+            fs.mkdirs(targetFilePath.getParent());
+          }
+          if (fs.exists(targetFilePath)) {
+            LOG.error(
+                String.format("The target output commit file (%s targetBasePath) already exists.", targetFilePath));
+          }
+          FileUtil.copy(fs, commitStatus.getPath(), fs, targetFilePath, false, fs.getConf());
+        }
       }
-    }
-
-
-    if (StringUtils.isNotBlank(outputPartitionField)) {
-//      spark.read().parquet(JavaConversions.asScalaIterator(dataFiles.iterator()).toSeq()).repartition(new Column(outputPartitionField)).show(100);
-      spark.read().parquet(JavaConversions.asScalaIterator(dataFiles.iterator()).toSeq()).repartition(new Column(outputPartitionField)).write().format(outputFormat).mode(SaveMode.Overwrite).save(targetBasePath);
-    } else if (StringUtils.isNotBlank(outputPartitioner)) {
-      spark.read().parquet(JavaConversions.asScalaIterator(dataFiles.iterator()).toSeq());
     } else {
-//      spark.read().parquet(JavaConversions.asScalaIterator(dataFiles.iterator()).toSeq()).show(100);
-      spark.read().parquet(JavaConversions.asScalaIterator(dataFiles.iterator()).toSeq()).write().format(outputFormat).mode(SaveMode.Overwrite).save(targetBasePath);
+      LOG.info("The job has 0 partition to copy.");
     }
-
   }
 
   public static void main(String[] args) throws IOException {
@@ -135,7 +180,7 @@ public class HoodieSnapshotExporter {
     final Config cfg = new Config();
     new JCommander(cfg, null, args);
 
-    // Create a spark job to do the snapshot copy
+    // Create a spark job to do the snapshot export
     SparkSession spark = SparkSession.builder().appName("Hoodie-snapshot-exporter").master("local[2]")
         .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer").getOrCreate();
     LOG.info("Initializing spark job.");
--- hudi-utilities/src/test/java/org/apache/hudi/utilities/TestHoodieSnapshotExporter.java
+++ hudi-utilities/src/test/java/org/apache/hudi/utilities/TestHoodieSnapshotExporter.java
@@ -1,8 +1,11 @@
 package org.apache.hudi.utilities;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hudi.DataSourceWriteOptions;
 import org.apache.hudi.common.HoodieTestDataGenerator;
+import org.apache.hudi.common.model.HoodieTestUtils;
 import org.apache.hudi.common.util.FSUtils;
 import org.apache.hudi.config.HoodieWriteConfig;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -15,20 +18,26 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TestHoodieSnapshotExporter {
+  private static String TEST_WRITE_TOKEN = "1-0-1";
+
   public SparkSession spark = null;
   public HoodieTestDataGenerator dataGen = null;
   public String basePath = null;
+  public String outputPath = null;
+  public String rootPath = null;
   public FileSystem fs = null;
   public Map commonOpts;
   public HoodieSnapshotExporter.Config cfg;
+  private JavaSparkContext jsc = null;
 
   @Before
   public void initialize() throws IOException {
@@ -37,6 +46,7 @@ public class TestHoodieSnapshotExporter {
         .master("local[2]")
         .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
         .getOrCreate();
+    jsc = new JavaSparkContext(spark.sparkContext());
     dataGen = new HoodieTestDataGenerator();
     TemporaryFolder folder = new TemporaryFolder();
     folder.create();
@@ -54,42 +64,22 @@ public class TestHoodieSnapshotExporter {
 
     cfg = new HoodieSnapshotExporter.Config();
     {
-      cfg.sourceBasePath = basePath;
-      cfg.targetBasePath = basePath + "/target";
+      cfg.basePath = basePath;
+      cfg.outputPath = outputPath = basePath + "/target";
       cfg.outputFormat = "json";
       cfg.outputPartitionField = "partition";
     }
   }
 
   @After
-  public void teardown() throws Exception {
+  public void cleanup() throws Exception {
     if (spark != null) {
       spark.stop();
     }
   }
 
   @Test
-  public void testExporter() throws IOException {
-    // Insert Operation
-    List<String> records = DataSourceTestUtils.convertToStringList(dataGen.generateInserts("000", 100));
-    Dataset<Row> inputDF = spark.read().json(new JavaSparkContext(spark.sparkContext()).parallelize(records, 2));
-    inputDF.write().format("hudi")
-        .options(commonOpts)
-        .option(DataSourceWriteOptions.OPERATION_OPT_KEY(), DataSourceWriteOptions.INSERT_OPERATION_OPT_VAL())
-        .mode(SaveMode.Overwrite)
-        .save(basePath);
-    long sourceCount = inputDF.count();
-
-    HoodieSnapshotExporter hoodieSnapshotExporter = new HoodieSnapshotExporter();
-    hoodieSnapshotExporter.export(spark, cfg);
-
-    long targetCount = spark.read().json(cfg.targetBasePath).count();
-
-    assertTrue(sourceCount == targetCount);
-  }
-
-  @Test
-  public void testConsumerPartitioner() throws IOException {
+  public void testSnapshotExporter() throws IOException {
     cfg.outputPartitioner = "";
     // Insert Operation
     List<String> records = DataSourceTestUtils.convertToStringList(dataGen.generateInserts("000", 100));
@@ -104,10 +94,106 @@ public class TestHoodieSnapshotExporter {
     HoodieSnapshotExporter hoodieSnapshotExporter = new HoodieSnapshotExporter();
     hoodieSnapshotExporter.export(spark, cfg);
 
-    long targetCount = spark.read().json(cfg.targetBasePath).count();
+    long targetCount = spark.read().json(outputPath).count();
 
     assertTrue(sourceCount == targetCount);
   }
 
+  // for testEmptySnapshotCopy
+  public void init() throws IOException {
+    TemporaryFolder folder = new TemporaryFolder();
+    folder.create();
+    rootPath = "file://" + folder.getRoot().getAbsolutePath();
+    basePath = rootPath + "/" + HoodieTestUtils.RAW_TRIPS_TEST_NAME;
+    outputPath = rootPath + "/output";
+
+    final Configuration hadoopConf = HoodieTestUtils.getDefaultHadoopConf();
+    fs = FSUtils.getFs(basePath, hadoopConf);
+    HoodieTestUtils.init(hadoopConf, basePath);
+  }
+
+  @Test
+  public void testEmptySnapshotCopy() throws IOException {
+    init();
+    // There is no real data (only .hoodie directory)
+    assertEquals(fs.listStatus(new Path(basePath)).length, 1);
+    assertFalse(fs.exists(new Path(outputPath)));
+
+    // Do the snapshot
+    HoodieSnapshotCopier copier = new HoodieSnapshotCopier();
+    copier.snapshot(jsc, basePath, outputPath, true);
+
+    // Nothing changed; we just bail out
+    assertEquals(fs.listStatus(new Path(basePath)).length, 1);
+    assertFalse(fs.exists(new Path(outputPath + "/_SUCCESS")));
+  }
+
+  // TODO - uncomment this after fixing test failures
+  // @Test
+  public void testSnapshotCopy() throws Exception {
+    // Generate some commits and corresponding parquets
+    String commitTime1 = "20160501010101";
+    String commitTime2 = "20160502020601";
+    String commitTime3 = "20160506030611";
+    new File(basePath + "/.hoodie").mkdirs();
+    new File(basePath + "/.hoodie/hoodie.properties").createNewFile();
+    // Only first two have commit files
+    new File(basePath + "/.hoodie/" + commitTime1 + ".commit").createNewFile();
+    new File(basePath + "/.hoodie/" + commitTime2 + ".commit").createNewFile();
+    new File(basePath + "/.hoodie/" + commitTime3 + ".inflight").createNewFile();
+
+    // Some parquet files
+    new File(basePath + "/2016/05/01/").mkdirs();
+    new File(basePath + "/2016/05/02/").mkdirs();
+    new File(basePath + "/2016/05/06/").mkdirs();
+    HoodieTestDataGenerator.writePartitionMetadata(fs, new String[]{"2016/05/01", "2016/05/02", "2016/05/06"},
+        basePath);
+    // Make commit1
+    File file11 = new File(basePath + "/2016/05/01/" + FSUtils.makeDataFileName(commitTime1, TEST_WRITE_TOKEN, "id11"));
+    file11.createNewFile();
+    File file12 = new File(basePath + "/2016/05/02/" + FSUtils.makeDataFileName(commitTime1, TEST_WRITE_TOKEN, "id12"));
+    file12.createNewFile();
+    File file13 = new File(basePath + "/2016/05/06/" + FSUtils.makeDataFileName(commitTime1, TEST_WRITE_TOKEN, "id13"));
+    file13.createNewFile();
+
+    // Make commit2
+    File file21 = new File(basePath + "/2016/05/01/" + FSUtils.makeDataFileName(commitTime2, TEST_WRITE_TOKEN, "id21"));
+    file21.createNewFile();
+    File file22 = new File(basePath + "/2016/05/02/" + FSUtils.makeDataFileName(commitTime2, TEST_WRITE_TOKEN, "id22"));
+    file22.createNewFile();
+    File file23 = new File(basePath + "/2016/05/06/" + FSUtils.makeDataFileName(commitTime2, TEST_WRITE_TOKEN, "id23"));
+    file23.createNewFile();
+
+    // Make commit3
+    File file31 = new File(basePath + "/2016/05/01/" + FSUtils.makeDataFileName(commitTime3, TEST_WRITE_TOKEN, "id31"));
+    file31.createNewFile();
+    File file32 = new File(basePath + "/2016/05/02/" + FSUtils.makeDataFileName(commitTime3, TEST_WRITE_TOKEN, "id32"));
+    file32.createNewFile();
+    File file33 = new File(basePath + "/2016/05/06/" + FSUtils.makeDataFileName(commitTime3, TEST_WRITE_TOKEN, "id33"));
+    file33.createNewFile();
+
+    // Do a snapshot copy
+    HoodieSnapshotCopier copier = new HoodieSnapshotCopier();
+    copier.snapshot(jsc, basePath, outputPath, false);
+
+    // Check results
+    assertTrue(fs.exists(new Path(outputPath + "/2016/05/01/" + file11.getName())));
+    assertTrue(fs.exists(new Path(outputPath + "/2016/05/02/" + file12.getName())));
+    assertTrue(fs.exists(new Path(outputPath + "/2016/05/06/" + file13.getName())));
+    assertTrue(fs.exists(new Path(outputPath + "/2016/05/01/" + file21.getName())));
+    assertTrue(fs.exists(new Path(outputPath + "/2016/05/02/" + file22.getName())));
+    assertTrue(fs.exists(new Path(outputPath + "/2016/05/06/" + file23.getName())));
+    assertFalse(fs.exists(new Path(outputPath + "/2016/05/01/" + file31.getName())));
+    assertFalse(fs.exists(new Path(outputPath + "/2016/05/02/" + file32.getName())));
+    assertFalse(fs.exists(new Path(outputPath + "/2016/05/06/" + file33.getName())));
+
+    assertTrue(fs.exists(new Path(outputPath + "/.hoodie/" + commitTime1 + ".commit")));
+    assertTrue(fs.exists(new Path(outputPath + "/.hoodie/" + commitTime2 + ".commit")));
+    assertFalse(fs.exists(new Path(outputPath + "/.hoodie/" + commitTime3 + ".commit")));
+    assertFalse(fs.exists(new Path(outputPath + "/.hoodie/" + commitTime3 + ".inflight")));
+    assertTrue(fs.exists(new Path(outputPath + "/.hoodie/hoodie.properties")));
+
+    assertTrue(fs.exists(new Path(outputPath + "/_SUCCESS")));
+  }
 
 }
