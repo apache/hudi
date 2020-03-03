e34024fbf0fedc6fc0ca84a50cacab92710e93bf
hongdd <jn_hdd@163.com>
2020-03-03 16:55:58 +0800

    Update TestHoodieCommitArchiveLog.java

--- hudi-client/src/test/java/org/apache/hudi/io/TestHoodieCommitArchiveLog.java
+++ hudi-client/src/test/java/org/apache/hudi/io/TestHoodieCommitArchiveLog.java
@@ -43,8 +43,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
