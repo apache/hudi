90cd096fe13610a232c0c350b92c41df47a59153
Satish Kotha <satishkotha@uber.com>
2020-03-03 12:28:17 -0800

    remove unused import

--- hudi-client/src/test/java/org/apache/hudi/client/TestUpdateSchemaEvolution.java
+++ hudi-client/src/test/java/org/apache/hudi/client/TestUpdateSchemaEvolution.java
@@ -40,7 +40,6 @@ import org.apache.parquet.avro.AvroReadSupport;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 //import org.junit.Test;
 
 import java.util.ArrayList;
