288bc4dd809521af896d0538ec2ae87bc034df7b
AakashPradeep <email2aakash@gmail.com>
2020-05-03 23:09:17 -0700

    [HUDI-852] adding check for table name for Append Save mode  (#1580)
    * adding check for table name for Append Save mode
    
    * adding existing table validation for delete and upsert operation
    
    Co-authored-by: Aakash Pradeep <apradeep@twilio.com>

--- hudi-spark/src/main/scala/org/apache/hudi/HoodieSparkSqlWriter.scala
+++ hudi-spark/src/main/scala/org/apache/hudi/HoodieSparkSqlWriter.scala
@@ -82,6 +82,13 @@ private[hudi] object HoodieSparkSqlWriter {
     val fs = basePath.getFileSystem(sparkContext.hadoopConfiguration)
     var exists = fs.exists(new Path(basePath, HoodieTableMetaClient.METAFOLDER_NAME))
 
+    if (exists && mode == SaveMode.Append) {
+      val existingTableName = new HoodieTableMetaClient(sparkContext.hadoopConfiguration, path.get).getTableConfig.getTableName
+      if (!existingTableName.equals(tblName.get)) {
+        throw new HoodieException(s"hoodie table with name $existingTableName already exist at $basePath")
+      }
+    }
+
     val (writeStatuses, writeClient: HoodieWriteClient[HoodieRecordPayload[Nothing]]) =
       if (!operation.equalsIgnoreCase(DELETE_OPERATION_OPT_VAL)) {
       // register classes & schemas
