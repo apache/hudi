236af3d5ec162a29c9d0f54317cfc31973d04261
UZi5136225 <965147871@qq.com>
2020-05-12 21:19:07 +0800

    Fix hudi-alluxio irregular

--- hudi-common/src/test/java/org/apache/hudi/common/storage/TestStorageSchemes.java
+++ hudi-common/src/test/java/org/apache/hudi/common/storage/TestStorageSchemes.java
@@ -42,6 +42,7 @@ public class TestStorageSchemes {
     assertFalse(StorageSchemes.isAppendSupported("abfs"));
     assertFalse(StorageSchemes.isAppendSupported("oss"));
     assertTrue(StorageSchemes.isAppendSupported("viewfs"));
+    assertFalse(StorageSchemes.isAppendSupported("alluxio"));
     assertThrows(IllegalArgumentException.class, () -> {
       StorageSchemes.isAppendSupported("s2");
     }, "Should throw exception for unsupported schemes");
