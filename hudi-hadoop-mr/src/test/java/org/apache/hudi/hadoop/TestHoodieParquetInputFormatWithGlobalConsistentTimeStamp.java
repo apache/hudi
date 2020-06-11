3c04db9057ad98334fab27164dc9752ece80c1bd
Sanjay Sundaresan <ssanjay@uber.com>
2020-06-12 02:35:51 +0530

    [HoodieParquetInputFormat] add a way to simulate consistent reads in xDC setup by truncating the hudi timeline with the commit timestamp property that is stored in the hivemetastore.
    Summary:
    Hive calls listStatus per table involved in the hive query and the jobConf is populated each time with the
    table specific properties so only those tables which have this property is queried.
    
    Note that unlike session property which are global for the session the table properties are specific to a table
    and therefore they don't need to be namespaced with dbname.tablename
    
    Refer the POC PRs
    https://code.uberinternal.com/D4440969
    https://code.uberinternal.com/D4440961
    
    Test Plan:
    Added unit tests
    Also tested in staging environment
    see code review for diff logs
    
    Reviewers: satishd, akhilb, sahiln, satishkotha, nagarwal, O955 Project Hoodie Project Reviewer: Add blocking reviewers, PHID-PROJ-pxfpotkfgkanblb3detq
    
    Reviewed By: satishkotha, nagarwal, O955 Project Hoodie Project Reviewer: Add blocking reviewers, PHID-PROJ-pxfpotkfgkanblb3detq
    
    Subscribers: varadarb
    
    Differential Revision: https://code.uberinternal.com/D4514221

---
+++ hudi-hadoop-mr/src/test/java/org/apache/hudi/hadoop/TestHoodieParquetInputFormatWithGlobalConsistentTimeStamp.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hudi.hadoop;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.apache.hudi.common.table.HoodieTableGloballyConsistentMetaClient;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class TestHoodieParquetInputFormatWithGlobalConsistentTimeStamp
+    extends TestHoodieParquetInputFormat {
+
+  // Test parquet input format using both session property and timestamp property
+  private String timeStampAndSession;
+
+  public TestHoodieParquetInputFormatWithGlobalConsistentTimeStamp(String timeStampAndSession) {
+    this.timeStampAndSession = timeStampAndSession;
+  }
+
+  @Parameterized.Parameters(name = "timeStampAndSession")
+  public static Collection<String[]> data() {
+    // 00 not needed thats the parent class unit test itself
+    return Arrays.asList(new String[][] {{"01"}, {"10"}, {"11"}});
+  }
+
+  @Override
+  public void setUp() {
+    super.setUp();
+    switch (timeStampAndSession) {
+      case "01":
+        jobConf.setBoolean(HoodieTableGloballyConsistentMetaClient.DISABLE_HOODIE_GLOBALLY_CONSISTENT_READS,
+            true);
+        break;
+      case "10":
+        jobConf.set(HoodieTableGloballyConsistentMetaClient.GLOBALLY_CONSISTENT_READ_TIMESTAMP,
+            String.valueOf(Long.MAX_VALUE));
+        break;
+      case "11":
+        // set to 0 attempting to hide everything but this won't work due to session property
+        jobConf.set(HoodieTableGloballyConsistentMetaClient.GLOBALLY_CONSISTENT_READ_TIMESTAMP, "0");
+        jobConf.setBoolean(HoodieTableGloballyConsistentMetaClient.DISABLE_HOODIE_GLOBALLY_CONSISTENT_READS,
+            true);
+        break;
+      default:
+        throw new RuntimeException(
+            String.format("unexpected timestampAndSession value: %s", timeStampAndSession));
+
+    }
+  }
+}
