2e7dc4d10458ad851488a53214d2a44c8cc4dbce
UZI5136225 <965147871@qq.com>
2020-06-13 16:51:11 +0800

    hudi_support_prometheus_test

--- hudi-client/src/test/java/org/apache/hudi/metrics/prometheus/TestPrometheusReporter.java
+++ hudi-client/src/test/java/org/apache/hudi/metrics/prometheus/TestPrometheusReporter.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hudi.metrics.prometheus;
 
 import org.apache.hudi.config.HoodieWriteConfig;
@@ -13,20 +30,17 @@ import static org.mockito.Mockito.when;
 
 public class TestPrometheusReporter {
 
-    HoodieWriteConfig config = mock(HoodieWriteConfig.class);
+  HoodieWriteConfig config = mock(HoodieWriteConfig.class);
 
-    @Test
-    public void testRegisterGauge() {
-        when(config.isMetricsOn()).thenReturn(true);
-        when(config.getMetricsReporterType()).thenReturn(MetricsReporterType.PROMETHEUS);
-        when(config.getPrometheusHost()).thenReturn("localhost");
-        when(config.getPrometheusPort()).thenReturn(9090);
-        new HoodieMetrics(config, "raw_table");
-        registerGauge("prometheusReporter_metric", 123L);
-        assertEquals("123", Metrics.getInstance().getRegistry().getGauges()
-                .get("prometheusReporter_metric_metric").getValue().toString());
-
-
-
+  @Test
+  public void testRegisterGauge() {
+    when(config.isMetricsOn()).thenReturn(true);
+    when(config.getMetricsReporterType()).thenReturn(MetricsReporterType.PROMETHEUS);
+    when(config.getPrometheusHost()).thenReturn("172.16.16.80");
+    when(config.getPrometheusPort()).thenReturn(9090);
+    new HoodieMetrics(config, "raw_table");
+    registerGauge("prometheusReporter_metric", 123L);
+    assertEquals("123", Metrics.getInstance().getRegistry().getGauges()
+            .get("prometheusReporter_metric_metric").getValue().toString());
     }
 }
