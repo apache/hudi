ff89f74c65de8d5bcf0e22e7f0755591c8cf34e7
Raymond Xu <xu.shiyan.raymond@gmail.com>
2020-06-17 19:08:18 -0700

    [HUDI-996] Add functional test suite in hudi-utilities
    - Share resources for functional tests
    - Add suite for functional test classes from hudi-utilities

--- hudi-utilities/src/test/java/org/apache/hudi/utilities/checkpointing/TestKafkaConnectHdfsProvider.java
+++ hudi-utilities/src/test/java/org/apache/hudi/utilities/checkpointing/TestKafkaConnectHdfsProvider.java
@@ -23,33 +23,23 @@ import org.apache.hudi.common.testutils.HoodieCommonTestHarness;
 import org.apache.hudi.common.testutils.HoodieTestUtils;
 import org.apache.hudi.exception.HoodieException;
 
-import org.apache.hadoop.conf.Configuration;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestKafkaConnectHdfsProvider extends HoodieCommonTestHarness {
 
-  private String topicPath = null;
-  private Configuration hadoopConf = null;
-
-  @BeforeEach
-  public void init() {
-    // Prepare directories
-    initPath();
-    hadoopConf = HoodieTestUtils.getDefaultHadoopConf();
-  }
-
   @Test
   public void testValidKafkaConnectPath() throws Exception {
     // a standard format(time based partition) of the files managed by kafka connect is:
     // topic/year=xxx/month=xxx/day=xxx/topic+partition+lowerOffset+upperOffset.file
-    topicPath = basePath + "/topic1";
-    new File(topicPath).mkdirs();
+    Path topicPath = tempDir.resolve("topic1");
+    Files.createDirectories(topicPath);
     // create regular kafka connect hdfs dirs
     new File(topicPath + "/year=2016/month=05/day=01/").mkdirs();
     new File(topicPath + "/year=2016/month=05/day=02/").mkdirs();
@@ -70,16 +60,16 @@ public class TestKafkaConnectHdfsProvider extends HoodieCommonTestHarness {
     new File(topicPath + "/year=2016/month=05/day=02/"
         + "random_snappy_2.parquet").createNewFile();
     final TypedProperties props = new TypedProperties();
-    props.put("hoodie.deltastreamer.checkpoint.provider.path", topicPath);
+    props.put("hoodie.deltastreamer.checkpoint.provider.path", topicPath.toString());
     final InitialCheckPointProvider provider = new KafkaConnectHdfsProvider(props);
-    provider.init(hadoopConf);
+    provider.init(HoodieTestUtils.getDefaultHadoopConf());
     assertEquals("topic1,0:300,1:200", provider.getCheckpoint());
   }
 
   @Test
   public void testMissingPartition() throws Exception {
-    topicPath = basePath + "/topic2";
-    new File(topicPath).mkdirs();
+    Path topicPath = tempDir.resolve("topic2");
+    Files.createDirectories(topicPath);
     // create regular kafka connect hdfs dirs
     new File(topicPath + "/year=2016/month=05/day=01/").mkdirs();
     new File(topicPath + "/year=2016/month=05/day=02/").mkdirs();
@@ -91,9 +81,9 @@ public class TestKafkaConnectHdfsProvider extends HoodieCommonTestHarness {
     new File(topicPath + "/year=2016/month=05/day=02/"
         + "topic1+0+201+300.parquet").createNewFile();
     final TypedProperties props = new TypedProperties();
-    props.put("hoodie.deltastreamer.checkpoint.provider.path", topicPath);
+    props.put("hoodie.deltastreamer.checkpoint.provider.path", topicPath.toString());
     final InitialCheckPointProvider provider = new KafkaConnectHdfsProvider(props);
-    provider.init(hadoopConf);
+    provider.init(HoodieTestUtils.getDefaultHadoopConf());
     assertThrows(HoodieException.class, provider::getCheckpoint);
   }
 }
