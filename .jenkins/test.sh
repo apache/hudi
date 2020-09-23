1cf076078d6f0a83f2d035c2c4c0e52c2a574c88
Prashant Wason <pwason@uber.com>
2020-09-22 18:03:07 -0700

    [UBER] Running unit tests in parallel.
    Reviewers: O955 Project Hoodie Project Reviewer: Add blocking reviewers, PHID-PROJ-pxfpotkfgkanblb3detq, #ldap_hadoop-platform, nagarwal, balajee
    
    Reviewed By: O955 Project Hoodie Project Reviewer: Add blocking reviewers, PHID-PROJ-pxfpotkfgkanblb3detq, #ldap_hadoop-platform, nagarwal
    
    Differential Revision: https://code.uberinternal.com/D4942137

--- .jenkins/test.sh
+++ .jenkins/test.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 
-set -xe
+set -x
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=/usr/lib/jvm/java-8-openjdk-amd64/bin:$PATH
 
 export HUDI_QUIETER_LOGGING=1
 
-# Run unit tests
+# Clean and build
 mvn clean compile test-compile install -DskipTests -DskipITs
-mvn test -Punit-tests -B
-mvn test -Pfunctional-tests -B
+
+# Run unit tests in parallel
+mvn test -Punit-tests -pl !hudi-client -B >log1.txt 2>&1 &
+PIDS[0]=$!
+mvn test -Punit-tests -pl hudi-client -B >log2.txt 2>&1 &
+PIDS[1]=$!
+mvn test -Pfunctional-tests -B >log3.txt 2>&1 &
+PIDS[2]=$!
+
+# Wait for completion
+ERR=0
+for pid in ${PIDS[*]}; do
+  wait $pid
+  if [ $? -ne 0 ]; then
+    echo "FAILED"
+    ERR=1 
+  fi
+done
+
+# Test output
+cat log1.txt  log2.txt log3.txt
 
 # Convert jacoco coverage report to cobertura format (recognized by https://github.com/uber/phabricator-jenkins-plugin)
 .jenkins/jacoco2cobertura.sh
+
+exit $ERR
 
