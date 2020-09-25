aa4b7b200905d73a6f6d2746068868b23bc5fd50
Brandon Scheller <bschelle@amazon.com>
2020-09-25 20:25:49 +0000

    fix tests

--- scripts/run_travis_tests.sh
+++ scripts/run_travis_tests.sh
@@ -22,12 +22,12 @@ sparkVersion=2.4.4
 hadoopVersion=2.7
 
 if [ "$mode" = "unit" ]; then
-  mvn clean install -DskipTests -q
+  mvn clean install -DskipTests -q -Dscala-2.12
   echo "Running Unit Tests"
-  mvn test -Punit-tests -pl "$modules" -B
+  mvn test -Punit-tests -pl "$modules" -B -Dscala-2.12 
 elif [ "$mode" = "functional" ]; then
   echo "Running Functional Tests"
-  mvn test -Pfunctional-tests -B
+  mvn test -Pfunctional-tests -B -Dscala-2.12
 elif [ "$mode" = "integration" ]; then
   echo "Downloading Apache Spark-${sparkVersion}-bin-hadoop${hadoopVersion}"
   wget http://archive.apache.org/dist/spark/spark-${sparkVersion}/spark-${sparkVersion}-bin-hadoop${hadoopVersion}.tgz -O /tmp/spark-${sparkVersion}.tgz
@@ -35,7 +35,7 @@ elif [ "$mode" = "integration" ]; then
   export SPARK_HOME=$PWD/spark-${sparkVersion}-bin-hadoop${hadoopVersion}
   mkdir /tmp/spark-events/
   echo "Running Integration Tests"
-  mvn verify -Pintegration-tests -B
+  mvn verify -Pintegration-tests -B -Dscala-2.12
 else
   echo "Unknown mode $mode"
   exit 1
