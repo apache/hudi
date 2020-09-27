63b88a9c2cc1a32156ff301b3b749509b89cf8b6
liujinhui1994 <965147871@qq.com>
2020-09-27 16:11:06 +0800

    HUDI-1274

--- hudi-sync/hudi-hive-sync/src/main/java/org/apache/hudi/hive/SlashEncodedDayPartitionValueExtractor.java
+++ hudi-sync/hudi-hive-sync/src/main/java/org/apache/hudi/hive/SlashEncodedDayPartitionValueExtractor.java
@@ -49,16 +49,28 @@ public class SlashEncodedDayPartitionValueExtractor implements PartitionValueExt
 
   @Override
   public List<String> extractPartitionValuesInPath(String partitionPath) {
+
+    int year = 0;
+    int mm = 0;
+    int dd = 0;
+    int hh = 0;
     // partition path is expected to be in this format yyyy/mm/dd
     String[] splits = partitionPath.split("/");
-    if (splits.length != 3) {
-      throw new IllegalArgumentException("Partition path " + partitionPath + " is not in the form yyyy/mm/dd ");
+    if (splits.length != 3 || splits.length !=4) {
+      throw new IllegalArgumentException("Partition path " + partitionPath + " is not in the form yyyy/mm/dd or yyyy/mm/dd/HH");
     }
     // Get the partition part and remove the / as well at the end
-    int year = Integer.parseInt(splits[0].contains("=") ? splits[0].split("=")[1] : splits[0]);
-    int mm = Integer.parseInt(splits[1].contains("=") ? splits[1].split("=")[1] : splits[1]);
-    int dd = Integer.parseInt(splits[2].contains("=") ? splits[2].split("=")[1] : splits[2]);
-    DateTime dateTime = new DateTime(year, mm, dd, 0, 0);
+    if (splits.length == 3 || splits.length == 4) {
+      year = Integer.parseInt(splits[0].contains("=") ? splits[0].split("=")[1] : splits[0]);
+      mm = Integer.parseInt(splits[1].contains("=") ? splits[1].split("=")[1] : splits[1]);
+      dd = Integer.parseInt(splits[2].contains("=") ? splits[2].split("=")[1] : splits[2]);
+    }
+
+    if (splits.length == 4) {
+      hh = Integer.parseInt(splits[3].contains("=") ? splits[3].split("=")[1] : splits[3]);
+    }
+
+    DateTime dateTime = new DateTime(year, mm, dd, hh, 0);
 
     return Collections.singletonList(getDtfOut().print(dateTime));
   }
