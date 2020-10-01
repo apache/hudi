11323b228fcafe4207e70f73f2265f6e73d94c34
Karl_Wang <wskqing@gmail.com>
2020-10-01 16:43:58 +0800

    Update DataSourceUtils.java

--- hudi-spark/src/main/java/org/apache/hudi/DataSourceUtils.java
+++ hudi-spark/src/main/java/org/apache/hudi/DataSourceUtils.java
@@ -52,8 +52,6 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import scala.Some;
-import scala.Some$;
 
 import java.io.IOException;
 import java.util.ArrayList;
