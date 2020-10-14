02a2dfc5bbf106ade2be415e900cabc7dc025785
Karl-WangSK <wskqing@gmail.com>
2020-10-14 10:25:10 +0800

    update

--- hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/table/action/commit/SparkWriteHelper.java
+++ hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/table/action/commit/SparkWriteHelper.java
@@ -60,8 +60,8 @@ public class SparkWriteHelper<T extends HoodieRecordPayload,R> extends AbstractW
       return new Tuple2<>(key, record);
     }).reduceByKey((rec1, rec2) -> {
       @SuppressWarnings("unchecked")
-      T reducedData = !schema.get().isEmpty() ? (T) rec1.getData().preCombine(rec2.getData(), new Schema.Parser().parse(schema.get()))
-                      : (T) rec1.getData().preCombine(rec2.getData());
+      T reducedData = schema != null && !schema.get().isEmpty() ? (T) rec1.getData().preCombine(rec2.getData(), new Schema.Parser().parse(schema.get()))
+              : (T) rec1.getData().preCombine(rec2.getData());
       // we cannot allow the user to change the key or partitionPath, since that will affect
       // everything
       // so pick it from one of the records.
