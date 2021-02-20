024bd41f6966946ccc527f4a99744ac1e9d40a22
liujinhui1994 <965147871@qq.com>
2021-02-20 10:10:20 +0800

    Fix CI error

--- hudi-integ-test/src/main/java/org/apache/hudi/integ/testsuite/HoodieTestSuiteWriter.java
+++ hudi-integ-test/src/main/java/org/apache/hudi/integ/testsuite/HoodieTestSuiteWriter.java
@@ -127,7 +127,7 @@ public class HoodieTestSuiteWriter {
     return false;
   }
 
-  public Pair<SchemaProvider, Pair<String, JavaRDD<HoodieRecord>>> fetchSource() throws Exception {
+  public Pair<Pair<SchemaProvider, JavaRDD<HoodieRecord>>, Pair<String, String>> fetchSource() throws Exception {
     return this.deltaStreamerWrapper.fetchSource();
   }
 
@@ -143,9 +143,9 @@ public class HoodieTestSuiteWriter {
     if (cfg.useDeltaStreamer) {
       return deltaStreamerWrapper.upsert(WriteOperationType.UPSERT);
     } else {
-      Pair<SchemaProvider, Pair<String, JavaRDD<HoodieRecord>>> nextBatch = fetchSource();
-      lastCheckpoint = Option.of(nextBatch.getValue().getLeft());
-      return writeClient.upsert(nextBatch.getRight().getRight(), instantTime.get());
+      Pair<Pair<SchemaProvider, JavaRDD<HoodieRecord>>, Pair<String, String>> nextBatch = fetchSource();
+      lastCheckpoint = Option.of(nextBatch.getRight().getLeft());
+      return writeClient.upsert(nextBatch.getLeft().getRight(), instantTime.get());
     }
   }
 
@@ -153,9 +153,9 @@ public class HoodieTestSuiteWriter {
     if (cfg.useDeltaStreamer) {
       return deltaStreamerWrapper.insert();
     } else {
-      Pair<SchemaProvider, Pair<String, JavaRDD<HoodieRecord>>> nextBatch = fetchSource();
-      lastCheckpoint = Option.of(nextBatch.getValue().getLeft());
-      return writeClient.insert(nextBatch.getRight().getRight(), instantTime.get());
+      Pair<Pair<SchemaProvider, JavaRDD<HoodieRecord>>, Pair<String, String>> nextBatch = fetchSource();
+      lastCheckpoint = Option.of(nextBatch.getRight().getLeft());
+      return writeClient.insert(nextBatch.getLeft().getRight(), instantTime.get());
     }
   }
 
@@ -163,9 +163,9 @@ public class HoodieTestSuiteWriter {
     if (cfg.useDeltaStreamer) {
       return deltaStreamerWrapper.bulkInsert();
     } else {
-      Pair<SchemaProvider, Pair<String, JavaRDD<HoodieRecord>>> nextBatch = fetchSource();
-      lastCheckpoint = Option.of(nextBatch.getValue().getLeft());
-      return writeClient.bulkInsert(nextBatch.getRight().getRight(), instantTime.get());
+      Pair<Pair<SchemaProvider, JavaRDD<HoodieRecord>>, Pair<String, String>> nextBatch = fetchSource();
+      lastCheckpoint = Option.of(nextBatch.getRight().getLeft());
+      return writeClient.bulkInsert(nextBatch.getLeft().getRight(), instantTime.get());
     }
   }
 
