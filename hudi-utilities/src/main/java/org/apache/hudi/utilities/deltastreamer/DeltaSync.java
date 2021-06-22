48404f0214abfc089b24d9f66022258bea45c978
Sebastian Bernauer <bernauerse@web.de>
2021-06-22 14:26:29 +0200

    [HUDI-2055] Added metric for time of lastSync

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/deltastreamer/DeltaSync.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/deltastreamer/DeltaSync.java
@@ -289,6 +289,8 @@ public class DeltaSync implements Serializable {
           srcRecordsWithCkpt.getRight().getLeft(), metrics, overallTimerContext);
     }
 
+    metrics.updateDeltaStreamerSyncMetrics(System.currentTimeMillis());
+
     // Clear persistent RDDs
     jssc.getPersistentRDDs().values().forEach(JavaRDD::unpersist);
     return result;
