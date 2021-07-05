10ded8447f9f188adec7ba4b43d06944c3d9edc2
rboyle <randal.boyle@bglgroup.co.uk>
2021-07-06 00:05:35 +0100

    setting confleunt version on parent pom and correcting test class name to align with other test classes

--- hudi-utilities/src/test/java/org/apache/hudi/utilities/schema/TestSchemaRegistryProvider.java
+++ hudi-utilities/src/test/java/org/apache/hudi/utilities/schema/TestSchemaRegistryProvider.java
@@ -37,7 +37,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-class SchemaRegistryProviderTest {
+class TestSchemaRegistryProvider {
 
   private final String basicAuth = "foo:bar";
 
