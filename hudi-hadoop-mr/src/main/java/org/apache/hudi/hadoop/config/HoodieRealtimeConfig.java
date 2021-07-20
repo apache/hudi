915dccba0a3cf0fcaaf290dfb7f839cb44696fef
xuedongluan <luanxuedong2009@gmail.com>
2021-07-20 21:06:33 +0800

    fix code style check

--- hudi-hadoop-mr/src/main/java/org/apache/hudi/hadoop/config/HoodieRealtimeConfig.java
+++ hudi-hadoop-mr/src/main/java/org/apache/hudi/hadoop/config/HoodieRealtimeConfig.java
@@ -22,15 +22,15 @@ import org.apache.hudi.common.config.HoodieConfig;
 
 public class HoodieRealtimeConfig extends HoodieConfig {
 
-    public static final ConfigProperty<Double> COMPACTION_MEMORY_FRACTION_PROP = ConfigProperty
-        .key("compaction.memory.fraction")
-        .defaultValue(0.75)
-        .withDocumentation("Fraction of mapper/reducer task memory used for compaction of log files");
+  public static final ConfigProperty<Double> COMPACTION_MEMORY_FRACTION_PROP = ConfigProperty
+      .key("compaction.memory.fraction")
+      .defaultValue(0.75)
+      .withDocumentation("Fraction of mapper/reducer task memory used for compaction of log files");
 
-    public static final ConfigProperty<Boolean> COMPACTION_LAZY_BLOCK_READ_ENABLED_PROP = ConfigProperty
-        .key("compaction.lazy.block.read.enabled")
-        .defaultValue(true)
-        .withDocumentation("used to choose a trade off between IO vs Memory when performing compaction process. "
-            + "Depending on outputfile size and memory provided, "
-            + "choose true to avoid OOM for large file size + small memory");
+  public static final ConfigProperty<Boolean> COMPACTION_LAZY_BLOCK_READ_ENABLED_PROP = ConfigProperty
+      .key("compaction.lazy.block.read.enabled")
+      .defaultValue(true)
+      .withDocumentation("used to choose a trade off between IO vs Memory when performing compaction process. "
+          + "Depending on outputfile size and memory provided, "
+          + "choose true to avoid OOM for large file size + small memory");
 }
