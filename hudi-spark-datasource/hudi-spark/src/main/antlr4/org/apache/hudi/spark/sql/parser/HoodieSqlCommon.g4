uber/hoodie

[HUDI-2182] Support Compaction Command For Spark Sql

hudi-spark-datasource/hudi-spark/src/main/antlr4/org/apache/hudi/spark/sql/parser/HoodieSqlCommon.g4
@@ -1,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+grammar HoodieSqlCommon;
+
+ singleStatement
+    : statement EOF
+    ;
+
+statement
+    : compactionStatement                                              #compactionCommand
+    | .*?                                                              #passThrough
+    ;
+
+ compactionStatement
+    : operation = (RUN | SCHEDULE) COMPACTION  ON tableIdentifier (AT timestamp = NUMBER)?    #compactionOnTable
+    | operation = (RUN | SCHEDULE) COMPACTION  ON path = STRING   (AT timestamp = NUMBER)?    #compactionOnPath
+    | SHOW COMPACTION  ON tableIdentifier (LIMIT limit = NUMBER)?                             #showCompactionOnTable
+    | SHOW COMPACTION  ON path = STRING (LIMIT limit = NUMBER)?                               #showCompactionOnPath
+    ;
+
+ tableIdentifier
+    : (db=IDENTIFIER '.')? table=IDENTIFIER
+    ;
+
+ ALL: 'ALL';
+ AT: 'AT';
+ COMPACTION: 'COMPACTION';
+ RUN: 'RUN';
+ SCHEDULE: 'SCHEDULE';
+ ON: 'ON';
+ SHOW: 'SHOW';
+ LIMIT: 'LIMIT';
+
+ NUMBER
+    : DIGIT+
+    ;
+
+ IDENTIFIER
+     : (LETTER | DIGIT | '_')+
+     ;
+
+STRING
+    : '\'' ( ~('\''|'\\') | ('\\' .) )* '\''
+    | '"' ( ~('"'|'\\') | ('\\' .) )* '"'
+    ;
+
+
+
+ fragment DIGIT
+     : [0-9]
+     ;
+
+ fragment LETTER
+     : [A-Z]
+     ;
+
+ SIMPLE_COMMENT
+     : '--' ~[\r\n]* '\r'? '\n'? -> channel(HIDDEN)
+     ;
+
+ BRACKETED_COMMENT
+     : '/*' .*? '*/' -> channel(HIDDEN)
+     ;
+
+ WS  : [ \r\n\t]+ -> channel(HIDDEN)
+     ;
+
+ // Catch-all for anything we can't recognize.
+ // We use this to be able to ignore and recover all the text
+ // when splitting statements with DelimiterLexer
+ UNRECOGNIZED
+     : .
+     ;

hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/Compaction.scala
@@ -1,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.catalyst.plans.logical
+
+import org.apache.spark.sql.catalyst.plans.logical.CompactionOperation.CompactionOperation
+
+case class CompactionTable(table: LogicalPlan, operation: CompactionOperation, timestamp: Option[Long])
+  extends Command {
+  override def children: Seq[LogicalPlan] = Seq(table)
+}
+
+case class CompactionPath(path: String, operation: CompactionOperation, timestamp: Option[Long])
+  extends Command
+
+case class CompactionShowOnTable(table: LogicalPlan, limit: Int = 20)
+  extends Command {
+  override def children: Seq[LogicalPlan] = Seq(table)
+}
+
+case class CompactionShowOnPath(path: String, limit: Int = 20) extends Command
+
+object CompactionOperation extends Enumeration {
+  type CompactionOperation = Value
+  val SCHEDULE, RUN = Value
+}

hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/hudi/command/CompactionHoodieTableCommand.scala
@@ -1,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.hudi.command
+
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.catalyst.catalog.CatalogTable
+import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference}
+import org.apache.spark.sql.catalyst.plans.logical.CompactionOperation.{CompactionOperation, RUN, SCHEDULE}
+import org.apache.spark.sql.execution.command.RunnableCommand
+import org.apache.spark.sql.hudi.HoodieSqlUtils.getTableLocation
+import org.apache.spark.sql.types.StringType
+
+case class CompactionHoodieTableCommand(table: CatalogTable,
+  operation: CompactionOperation, timestamp: Option[Long]) extends RunnableCommand {
+
+  override def run(sparkSession: SparkSession): Seq[Row] = {
+    val basePath = getTableLocation(table, sparkSession)
+      .getOrElse(s"missing location for ${table.identifier}")
+    CompactionHoodiePathCommand(basePath, operation, timestamp).run(sparkSession)
+  }
+
+  override val output: Seq[Attribute] = {
+    operation match {
+      case RUN => Seq.empty
+      case SCHEDULE => Seq(AttributeReference("timestamp", StringType, nullable = false)())
+    }
+  }
+}

hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/parser/HoodieSqlCommonAstBuilder.scala
@@ -1,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.parser
+
+import org.apache.hudi.SparkAdapterSupport
+import org.apache.hudi.spark.sql.parser.{HoodieSqlCommonBaseVisitor, HoodieSqlCommonParser}
+import org.apache.hudi.spark.sql.parser.HoodieSqlCommonParser.{CompactionOnPathContext, CompactionOnTableContext, ShowCompactionOnPathContext, ShowCompactionOnTableContext, SingleStatementContext, TableIdentifierContext}
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.TableIdentifier
+import org.apache.spark.sql.catalyst.analysis.UnresolvedRelation
+import org.apache.spark.sql.catalyst.parser.ParserUtils.withOrigin
+import org.apache.spark.sql.catalyst.parser.{ParserInterface, ParserUtils}
+import org.apache.spark.sql.catalyst.plans.logical.{CompactionOperation, CompactionPath, CompactionShowOnPath, CompactionShowOnTable, CompactionTable, LogicalPlan}
+
+class HoodieSqlCommonAstBuilder(session: SparkSession, delegate: ParserInterface)
+  extends HoodieSqlCommonBaseVisitor[AnyRef] with Logging with SparkAdapterSupport {
+
+  import ParserUtils._
+
+  override def visitSingleStatement(ctx: SingleStatementContext): LogicalPlan = withOrigin(ctx) {
+    ctx.statement().accept(this).asInstanceOf[LogicalPlan]
+  }
+
+  override def visitCompactionOnTable(ctx: CompactionOnTableContext): LogicalPlan = withOrigin(ctx) {
+    val table = ctx.tableIdentifier().accept(this).asInstanceOf[LogicalPlan]
+    val operation = CompactionOperation.withName(ctx.operation.getText.toUpperCase)
+    val timestamp = if (ctx.timestamp != null) Some(ctx.timestamp.getText.toLong) else None
+    CompactionTable(table, operation, timestamp)
+  }
+
+  override def visitCompactionOnPath (ctx: CompactionOnPathContext): LogicalPlan = withOrigin(ctx) {
+    val path = string(ctx.path)
+    val operation = CompactionOperation.withName(ctx.operation.getText.toUpperCase)
+    val timestamp = if (ctx.timestamp != null) Some(ctx.timestamp.getText.toLong) else None
+    CompactionPath(path, operation, timestamp)
+  }
+
+  override def visitShowCompactionOnTable (ctx: ShowCompactionOnTableContext): LogicalPlan = withOrigin(ctx) {
+    val table = ctx.tableIdentifier().accept(this).asInstanceOf[LogicalPlan]
+   if (ctx.limit != null) {
+     CompactionShowOnTable(table, ctx.limit.getText.toInt)
+   } else {
+     CompactionShowOnTable(table)
+   }
+  }
+
+  override def visitShowCompactionOnPath(ctx: ShowCompactionOnPathContext): LogicalPlan = withOrigin(ctx) {
+    val path = string(ctx.path)
+    if (ctx.limit != null) {
+      CompactionShowOnPath(path, ctx.limit.getText.toInt)
+    } else {
+      CompactionShowOnPath(path)
+    }
+  }
+
+  override def visitTableIdentifier(ctx: TableIdentifierContext): LogicalPlan = withOrigin(ctx) {
+    UnresolvedRelation(TableIdentifier(ctx.table.getText, Option(ctx.db).map(_.getText)))
+  }
+}

apache/hudi

hudi-spark-datasource/hudi-spark/src/main/antlr4/org/apache/hudi/spark/sql/parser/HoodieSqlCommon.g4
@@ -1,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+grammar HoodieSqlCommon;
+
+ singleStatement
+    : statement EOF
+    ;
+
+statement
+    : compactionStatement                                              #compactionCommand
+    | .*?                                                              #passThrough
+    ;
+
+ compactionStatement
+    : operation = (RUN | SCHEDULE) COMPACTION  ON tableIdentifier (AT instantTimestamp = NUMBER)?    #compactionOnTable
+    | operation = (RUN | SCHEDULE) COMPACTION  ON path = STRING   (AT instantTimestamp = NUMBER)?    #compactionOnPath
+    | SHOW COMPACTION  ON tableIdentifier (LIMIT limit = NUMBER)?                             #showCompactionOnTable
+    | SHOW COMPACTION  ON path = STRING (LIMIT limit = NUMBER)?                               #showCompactionOnPath
+    ;
+
+ tableIdentifier
+    : (db=IDENTIFIER '.')? table=IDENTIFIER
+    ;
+
+ ALL: 'ALL';
+ AT: 'AT';
+ COMPACTION: 'COMPACTION';
+ RUN: 'RUN';
+ SCHEDULE: 'SCHEDULE';
+ ON: 'ON';
+ SHOW: 'SHOW';
+ LIMIT: 'LIMIT';
+
+ NUMBER
+    : DIGIT+
+    ;
+
+ IDENTIFIER
+     : (LETTER | DIGIT | '_')+
+     ;
+
+STRING
+    : '\'' ( ~('\''|'\\') | ('\\' .) )* '\''
+    | '"' ( ~('"'|'\\') | ('\\' .) )* '"'
+    ;
+
+
+
+ fragment DIGIT
+     : [0-9]
+     ;
+
+ fragment LETTER
+     : [A-Z]
+     ;
+
+ SIMPLE_COMMENT
+     : '--' ~[\r\n]* '\r'? '\n'? -> channel(HIDDEN)
+     ;
+
+ BRACKETED_COMMENT
+     : '/*' .*? '*/' -> channel(HIDDEN)
+     ;
+
+ WS  : [ \r\n\t]+ -> channel(HIDDEN)
+     ;
+
+ // Catch-all for anything we can't recognize.
+ // We use this to be able to ignore and recover all the text
+ // when splitting statements with DelimiterLexer
+ UNRECOGNIZED
+     : .
+     ;

hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/Compaction.scala
@@ -1,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.catalyst.plans.logical
+
+import org.apache.spark.sql.catalyst.plans.logical.CompactionOperation.CompactionOperation
+
+case class CompactionTable(table: LogicalPlan, operation: CompactionOperation, instantTimestamp: Option[Long])
+  extends Command {
+  override def children: Seq[LogicalPlan] = Seq(table)
+}
+
+case class CompactionPath(path: String, operation: CompactionOperation, instantTimestamp: Option[Long])
+  extends Command
+
+case class CompactionShowOnTable(table: LogicalPlan, limit: Int = 20)
+  extends Command {
+  override def children: Seq[LogicalPlan] = Seq(table)
+}
+
+case class CompactionShowOnPath(path: String, limit: Int = 20) extends Command
+
+object CompactionOperation extends Enumeration {
+  type CompactionOperation = Value
+  val SCHEDULE, RUN = Value
+}

hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/hudi/command/CompactionHoodieTableCommand.scala
@@ -1,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.hudi.command
+
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.catalyst.catalog.CatalogTable
+import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference}
+import org.apache.spark.sql.catalyst.plans.logical.CompactionOperation.{CompactionOperation, RUN, SCHEDULE}
+import org.apache.spark.sql.execution.command.RunnableCommand
+import org.apache.spark.sql.hudi.HoodieSqlUtils.getTableLocation
+import org.apache.spark.sql.types.StringType
+
+case class CompactionHoodieTableCommand(table: CatalogTable,
+  operation: CompactionOperation, instantTimestamp: Option[Long])
+  extends RunnableCommand {
+
+  override def run(sparkSession: SparkSession): Seq[Row] = {
+    val basePath = getTableLocation(table, sparkSession)
+      .getOrElse(s"missing location for ${table.identifier}")
+    CompactionHoodiePathCommand(basePath, operation, instantTimestamp).run(sparkSession)
+  }
+
+  override val output: Seq[Attribute] = {
+    operation match {
+      case RUN => Seq.empty
+      case SCHEDULE => Seq(AttributeReference("instant", StringType, nullable = false)())
+    }
+  }
+}

hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/parser/HoodieSqlCommonAstBuilder.scala
@@ -1,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.parser
+
+import org.apache.hudi.SparkAdapterSupport
+import org.apache.hudi.spark.sql.parser.{HoodieSqlCommonBaseVisitor, HoodieSqlCommonParser}
+import org.apache.hudi.spark.sql.parser.HoodieSqlCommonParser.{CompactionOnPathContext, CompactionOnTableContext, ShowCompactionOnPathContext, ShowCompactionOnTableContext, SingleStatementContext, TableIdentifierContext}
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.TableIdentifier
+import org.apache.spark.sql.catalyst.analysis.UnresolvedRelation
+import org.apache.spark.sql.catalyst.parser.ParserUtils.withOrigin
+import org.apache.spark.sql.catalyst.parser.{ParserInterface, ParserUtils}
+import org.apache.spark.sql.catalyst.plans.logical.{CompactionOperation, CompactionPath, CompactionShowOnPath, CompactionShowOnTable, CompactionTable, LogicalPlan}
+
+class HoodieSqlCommonAstBuilder(session: SparkSession, delegate: ParserInterface)
+  extends HoodieSqlCommonBaseVisitor[AnyRef] with Logging with SparkAdapterSupport {
+
+  import ParserUtils._
+
+  override def visitSingleStatement(ctx: SingleStatementContext): LogicalPlan = withOrigin(ctx) {
+    ctx.statement().accept(this).asInstanceOf[LogicalPlan]
+  }
+
+  override def visitCompactionOnTable(ctx: CompactionOnTableContext): LogicalPlan = withOrigin(ctx) {
+    val table = ctx.tableIdentifier().accept(this).asInstanceOf[LogicalPlan]
+    val operation = CompactionOperation.withName(ctx.operation.getText.toUpperCase)
+    val timestamp = if (ctx.instantTimestamp != null) Some(ctx.instantTimestamp.getText.toLong) else None
+    CompactionTable(table, operation, timestamp)
+  }
+
+  override def visitCompactionOnPath (ctx: CompactionOnPathContext): LogicalPlan = withOrigin(ctx) {
+    val path = string(ctx.path)
+    val operation = CompactionOperation.withName(ctx.operation.getText.toUpperCase)
+    val timestamp = if (ctx.instantTimestamp != null) Some(ctx.instantTimestamp.getText.toLong) else None
+    CompactionPath(path, operation, timestamp)
+  }
+
+  override def visitShowCompactionOnTable (ctx: ShowCompactionOnTableContext): LogicalPlan = withOrigin(ctx) {
+    val table = ctx.tableIdentifier().accept(this).asInstanceOf[LogicalPlan]
+   if (ctx.limit != null) {
+     CompactionShowOnTable(table, ctx.limit.getText.toInt)
+   } else {
+     CompactionShowOnTable(table)
+   }
+  }
+
+  override def visitShowCompactionOnPath(ctx: ShowCompactionOnPathContext): LogicalPlan = withOrigin(ctx) {
+    val path = string(ctx.path)
+    if (ctx.limit != null) {
+      CompactionShowOnPath(path, ctx.limit.getText.toInt)
+    } else {
+      CompactionShowOnPath(path)
+    }
+  }
+
+  override def visitTableIdentifier(ctx: TableIdentifierContext): LogicalPlan = withOrigin(ctx) {
+    UnresolvedRelation(TableIdentifier(ctx.table.getText, Option(ctx.db).map(_.getText)))
+  }
+}