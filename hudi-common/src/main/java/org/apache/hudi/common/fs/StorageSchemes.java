dcfb2ee71133f1f6426e45216287f4a029efd0ee
xuzifu666 <1206332514@qq.com>
2021-08-10 23:18:12 +0800

    [HUDI-2288] Support storage on ks3 for hudi (#3434)
    Co-authored-by: xuzifu <xuzifu.com>

--- hudi-common/src/main/java/org/apache/hudi/common/fs/StorageSchemes.java
+++ hudi-common/src/main/java/org/apache/hudi/common/fs/StorageSchemes.java
@@ -60,6 +60,8 @@ public enum StorageSchemes {
   COS("cos", false),
   // Huawei Cloud Object Storage
   OBS("obs", false),
+  // Kingsoft Standard Storage ks3
+  KS3("ks3", false),
   // Baidu Object Storage
   BOS("bos", false);
 
