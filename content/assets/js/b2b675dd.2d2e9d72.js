17618c3946919fa9305c4a409b780dbaac902892
CI <cibot@hudi.apache.org>
2021-08-27 18:23:55 +0000

    Travis CI build asf-site

---
+++ content/assets/js/b2b675dd.2d2e9d72.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhudi=self.webpackChunkhudi||[]).push([[533],{8017:function(e){e.exports=JSON.parse('{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":3,"totalCount":27,"previousPage":null,"nextPage":"/blog/page/2","blogDescription":"Blog","blogTitle":"Blog - Apache Hudi: User-Facing Analytics"}')}}]);
