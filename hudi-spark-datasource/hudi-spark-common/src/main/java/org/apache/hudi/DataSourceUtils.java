13c3eea1e993c1987d94bce7c4f212fb9c116fd0
test-wangxiaoyu <90183930+test-wangxiaoyu@users.noreply.github.com>
2021-09-08 13:41:46 +0800

    Update DataSourceUtils.java

--- hudi-spark-datasource/hudi-spark-common/src/main/java/org/apache/hudi/DataSourceUtils.java
+++ hudi-spark-datasource/hudi-spark-common/src/main/java/org/apache/hudi/DataSourceUtils.java
@@ -304,6 +304,11 @@ public class DataSourceUtils {
         DataSourceWriteOptions.HIVE_SKIP_RO_SUFFIX_FOR_READ_OPTIMIZED_TABLE().defaultValue()));
     hiveSyncConfig.supportTimestamp = Boolean.valueOf(props.getString(DataSourceWriteOptions.HIVE_SUPPORT_TIMESTAMP_TYPE().key(),
         DataSourceWriteOptions.HIVE_SUPPORT_TIMESTAMP_TYPE().defaultValue()));
+    hiveSyncConfig.useKerberos = Boolean.valueOf(props.getString(DataSourceWriteOptions.HIVE_SYNC_USE_KERBEROS().key(),
+        DataSourceWriteOptions.HIVE_SYNC_USE_KERBEROS().defaultValue()));
+    hiveSyncConfig.kerberosPrincipal =
+        props.getString(DataSourceWriteOptions.HIVE_SYNC_KERBEROS_PRINCIPAL().key(), DataSourceWriteOptions.HIVE_SYNC_KERBEROS_PRINCIPAL().defaultValue());
+    
     return hiveSyncConfig;
   }
 }
