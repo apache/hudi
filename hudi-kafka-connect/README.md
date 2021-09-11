e528dd798ab8ce6e4d444d2d771c107c503e8f25
rmahindra123 <76502047+rmahindra123@users.noreply.github.com>
2021-09-10 18:20:26 -0700

    [HUDI-2394] Implement Kafka Sink Protocol for Hudi for Ingesting Immutable Data (#3592)
     - Fixing packaging, naming of classes
     - Use of log4j over slf4j for uniformity
    - More follow-on fixes
     - Added a version to control/coordinator events.
     - Eliminated the config added to write config
     - Fixed fetching of checkpoints based on table type
     - Clean up of naming, code placement
    
    Co-authored-by: Rajesh Mahindra <rmahindra@Rajeshs-MacBook-Pro.local>
    Co-authored-by: Vinoth Chandar <vinoth@apache.org>

---
+++ hudi-kafka-connect/README.md
@@ -0,0 +1,94 @@
+<!--
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+-->
+
+# Quick Start guide for Kafka Connect Sink for Hudi
+
+This repo contains a sample project that can be used to start off your own source connector for Kafka Connect.
+
+## Building the connector
+
+The first thing you need to do to start using this connector is building it. In order to do that, you need to install the following dependencies:
+
+- [Java 1.8+](https://openjdk.java.net/)
+- [Apache Maven](https://maven.apache.org/)
+
+After installing these dependencies, execute the following command:
+
+```bash
+cd $HUDI_DIR
+mvn clean package
+```
+
+## Incremental Builds
+
+```bash
+mvn clean -pl hudi-kafka-connect install -DskipTests
+mvn clean -pl packaging/hudi-kafka-connect-bundle install
+```
+
+## Put hudi connector in Kafka Connect classpath
+
+```bash
+cp $HUDI_DIR/packaging/hudi-kafka-connect-bundle/target/hudi-kafka-connect-bundle-0.10.0-SNAPSHOT.jar /usr/local/share/java/hudi-kafka-connect/
+```
+
+## Trying the connector
+
+After building the package, we need to install the Apache Kafka
+
+### 1 - Starting the environment
+
+Start the ZK and Kafka:
+
+```bash
+./bin/zookeeper-server-start.sh ./config/zookeeper.properties
+./bin/kafka-server-start.sh ./config/server.properties
+```
+
+Wait until the kafka cluster is up and running.
+
+### 2 - Create the Hudi Control Topic for Coordination of the transactions
+
+The control topic should only have `1` partition
+
+```bash
+./bin/kafka-topics.sh --delete --topic hudi-control-topic --bootstrap-server localhost:9092
+./bin/kafka-topics.sh --create --topic hudi-control-topic --partitions 1 --replication-factor 1 --bootstrap-server localhost:9092
+```
+
+### 3 - Create the Hudi Topic for the Sink and insert data into the topic
+
+Open a terminal to execute the following command:
+
+```bash
+bash runKafkaTrafficGenerator.sh <total_messages>
+```
+
+### 4 - Run the Sink connector worker (multiple workers can be run)
+
+Open a terminal to execute the following command:
+
+```bash
+./bin/connect-distributed.sh ../hudi-kafka-connect/configs/connect-distributed.properties
+```
+
+### 5- To add the Hudi Sink to the Connector (delete it if you want to re-configure)
+
+```bash
+curl -X DELETE http://localhost:8083/connectors/hudi-sink
+curl -X POST -H "Content-Type:application/json" -d @$HUDI-DIR/hudi-kafka-connect/configs/config-sink.json http://localhost:8083/connectors
+```
---
+++ hudi-kafka-connect/src/test/java/org/apache/hudi/connect/TestConnectTransactionParticipant.java
@@ -0,0 +1,273 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hudi.connect;
+
+import org.apache.hudi.common.util.Option;
+import org.apache.hudi.connect.kafka.KafkaControlAgent;
+import org.apache.hudi.connect.transaction.ConnectTransactionParticipant;
+import org.apache.hudi.connect.transaction.ControlEvent;
+import org.apache.hudi.connect.transaction.TransactionCoordinator;
+import org.apache.hudi.connect.writers.KafkaConnectConfigs;
+import org.apache.hudi.exception.HoodieException;
+import org.apache.hudi.helper.MockKafkaControlAgent;
+import org.apache.hudi.helper.TestHudiWriterProvider;
+import org.apache.hudi.helper.TestKafkaConnect;
+
+import org.apache.kafka.common.TopicPartition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestConnectTransactionParticipant {
+
+  private static final String TOPIC_NAME = "kafka-connect-test-topic";
+  private static final int PARTITION_NUMBER = 4;
+
+  private ConnectTransactionParticipant participant;
+  private MockCoordinator coordinator;
+  private TopicPartition partition;
+  private KafkaConnectConfigs configs;
+  private KafkaControlAgent kafkaControlAgent;
+  private TestHudiWriterProvider testHudiWriterProvider;
+  private TestKafkaConnect testKafkaConnect;
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    partition = new TopicPartition(TOPIC_NAME, PARTITION_NUMBER);
+    kafkaControlAgent = new MockKafkaControlAgent();
+    testKafkaConnect = new TestKafkaConnect(partition);
+    coordinator = new MockCoordinator(kafkaControlAgent);
+    coordinator.start();
+    configs = KafkaConnectConfigs.newBuilder()
+        .build();
+    initializeParticipant();
+  }
+
+  @ParameterizedTest
+  @EnumSource(value = CoordinatorFailureTestScenarios.class)
+  public void testAllCoordinatorFailureScenarios(CoordinatorFailureTestScenarios testScenario) {
+    int expectedRecordsWritten = 0;
+    switch (testScenario) {
+      case REGULAR_SCENARIO:
+        expectedRecordsWritten += testKafkaConnect.putRecordsToParticipant();
+        assertTrue(testKafkaConnect.isPaused());
+        break;
+      case COORDINATOR_FAILED_AFTER_START_COMMIT:
+        testKafkaConnect.putRecordsToParticipant();
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.START_COMMIT);
+        testKafkaConnect.putRecordsToParticipant();
+        // Coordinator Failed
+        initializeCoordinator();
+        break;
+      case COORDINATOR_FAILED_AFTER_END_COMMIT:
+        testKafkaConnect.putRecordsToParticipant();
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.START_COMMIT);
+        testKafkaConnect.putRecordsToParticipant();
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.END_COMMIT);
+        expectedRecordsWritten += testKafkaConnect.putRecordsToParticipant();
+        // Coordinator Failed
+        initializeCoordinator();
+        break;
+      default:
+        throw new HoodieException("Unknown test scenario " + testScenario);
+    }
+
+    // Regular Case or Coordinator Recovery Case
+    coordinator.sendEventFromCoordinator(ControlEvent.MsgType.START_COMMIT);
+    expectedRecordsWritten += testKafkaConnect.putRecordsToParticipant();
+    assertTrue(testKafkaConnect.isResumed());
+    coordinator.sendEventFromCoordinator(ControlEvent.MsgType.END_COMMIT);
+    testKafkaConnect.putRecordsToParticipant();
+    assertTrue(testKafkaConnect.isPaused());
+    coordinator.sendEventFromCoordinator(ControlEvent.MsgType.ACK_COMMIT);
+    testKafkaConnect.putRecordsToParticipant();
+    assertEquals(testHudiWriterProvider.getLatestNumberWrites(), expectedRecordsWritten);
+    // Ensure Coordinator and participant are in sync in the kafka offsets
+    assertEquals(participant.getLastKafkaCommittedOffset(), coordinator.getCommittedKafkaOffset());
+
+    participant.stop();
+  }
+
+  @ParameterizedTest
+  @EnumSource(value = ParticipantFailureTestScenarios.class)
+  public void testAllParticipantFailureScenarios(ParticipantFailureTestScenarios testScenario) {
+    int expectedRecordsWritten = 0;
+    switch (testScenario) {
+      case FAILURE_BEFORE_START_COMMIT:
+        testKafkaConnect.putRecordsToParticipant();
+        // Participant fails
+        initializeParticipant();
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.START_COMMIT);
+        expectedRecordsWritten += testKafkaConnect.putRecordsToParticipant();
+        assertTrue(testKafkaConnect.isResumed());
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.END_COMMIT);
+        testKafkaConnect.putRecordsToParticipant();
+        assertTrue(testKafkaConnect.isPaused());
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.ACK_COMMIT);
+        testKafkaConnect.putRecordsToParticipant();
+        assertEquals(testHudiWriterProvider.getLatestNumberWrites(), expectedRecordsWritten);
+        // Ensure Coordinator and participant are in sync in the kafka offsets
+        assertEquals(participant.getLastKafkaCommittedOffset(), coordinator.getCommittedKafkaOffset());
+        break;
+      case FAILURE_AFTER_START_COMMIT:
+        testKafkaConnect.putRecordsToParticipant();
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.START_COMMIT);
+        testKafkaConnect.putRecordsToParticipant();
+        // Participant fails
+        initializeParticipant();
+        testKafkaConnect.putRecordsToParticipant();
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.END_COMMIT);
+        testKafkaConnect.putRecordsToParticipant();
+        assertTrue(testKafkaConnect.isPaused());
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.ACK_COMMIT);
+        testKafkaConnect.putRecordsToParticipant();
+        assertEquals(testHudiWriterProvider.getLatestNumberWrites(), expectedRecordsWritten);
+        // Ensure Coordinator and participant are in sync in the kafka offsets
+        assertEquals(participant.getLastKafkaCommittedOffset(), coordinator.getCommittedKafkaOffset());
+        break;
+      case FAILURE_AFTER_END_COMMIT:
+        testKafkaConnect.putRecordsToParticipant();
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.START_COMMIT);
+        testKafkaConnect.putRecordsToParticipant();
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.END_COMMIT);
+        testKafkaConnect.putRecordsToParticipant();
+        // Participant fails
+        initializeParticipant();
+        testKafkaConnect.putRecordsToParticipant();
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.END_COMMIT);
+        testKafkaConnect.putRecordsToParticipant();
+        assertTrue(testKafkaConnect.isPaused());
+        coordinator.sendEventFromCoordinator(ControlEvent.MsgType.ACK_COMMIT);
+        testKafkaConnect.putRecordsToParticipant();
+        assertEquals(testHudiWriterProvider.getLatestNumberWrites(), expectedRecordsWritten);
+        // Ensure Coordinator and participant are in sync in the kafka offsets
+        assertEquals(participant.getLastKafkaCommittedOffset(), coordinator.getCommittedKafkaOffset());
+        break;
+      default:
+        throw new HoodieException("Unknown test scenario " + testScenario);
+    }
+  }
+
+  private void initializeParticipant() {
+    testHudiWriterProvider = new TestHudiWriterProvider();
+    participant = new ConnectTransactionParticipant(
+        partition,
+        kafkaControlAgent,
+        testKafkaConnect,
+        testHudiWriterProvider);
+    testKafkaConnect.setParticipant(participant);
+    participant.start();
+  }
+
+  private void initializeCoordinator() {
+    coordinator = new MockCoordinator(kafkaControlAgent);
+    coordinator.start();
+  }
+
+  private static class MockCoordinator implements TransactionCoordinator {
+
+    private static int currentCommitTime;
+
+    static {
+      currentCommitTime = 101;
+    }
+
+    private final KafkaControlAgent kafkaControlAgent;
+    private final TopicPartition partition;
+
+    private Option<ControlEvent> lastReceivedWriteStatusEvent;
+    private long committedKafkaOffset;
+
+    public MockCoordinator(KafkaControlAgent kafkaControlAgent) {
+      this.kafkaControlAgent = kafkaControlAgent;
+      partition = new TopicPartition(TOPIC_NAME, 0);
+      lastReceivedWriteStatusEvent = Option.empty();
+      committedKafkaOffset = 0L;
+    }
+
+    public void sendEventFromCoordinator(
+        ControlEvent.MsgType type) {
+      try {
+        if (type.equals(ControlEvent.MsgType.START_COMMIT)) {
+          ++currentCommitTime;
+        }
+        kafkaControlAgent.publishMessage(new ControlEvent.Builder(
+            type,
+            ControlEvent.SenderType.COORDINATOR,
+            String.valueOf(currentCommitTime),
+            partition)
+            .setCoordinatorInfo(new ControlEvent.CoordinatorInfo(
+                Collections.singletonMap(PARTITION_NUMBER, committedKafkaOffset)))
+            .build());
+      } catch (Exception exception) {
+        throw new HoodieException("Fatal error sending control event to Participant");
+      }
+    }
+
+    public Option<ControlEvent> getLastReceivedWriteStatusEvent() {
+      return lastReceivedWriteStatusEvent;
+    }
+
+    public long getCommittedKafkaOffset() {
+      return committedKafkaOffset;
+    }
+
+    @Override
+    public void start() {
+      kafkaControlAgent.registerTransactionCoordinator(this);
+    }
+
+    @Override
+    public void stop() {
+      kafkaControlAgent.deregisterTransactionCoordinator(this);
+    }
+
+    @Override
+    public TopicPartition getPartition() {
+      return partition;
+    }
+
+    @Override
+    public void processControlEvent(ControlEvent message) {
+      if (message.getMsgType().equals(ControlEvent.MsgType.WRITE_STATUS)) {
+        lastReceivedWriteStatusEvent = Option.of(message);
+        assertTrue(message.getParticipantInfo().getKafkaCommitOffset() >= committedKafkaOffset);
+        committedKafkaOffset = message.getParticipantInfo().getKafkaCommitOffset();
+      }
+    }
+  }
+
+  private enum CoordinatorFailureTestScenarios {
+    REGULAR_SCENARIO,
+    COORDINATOR_FAILED_AFTER_START_COMMIT,
+    COORDINATOR_FAILED_AFTER_END_COMMIT,
+  }
+
+  private enum ParticipantFailureTestScenarios {
+    FAILURE_BEFORE_START_COMMIT,
+    FAILURE_AFTER_START_COMMIT,
+    FAILURE_AFTER_END_COMMIT,
+  }
+
+}
