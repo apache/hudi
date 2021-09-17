0312ca5d38be2a3385d4910a70b6055e85faa1cd
jian.feng <fengjian428@gmial.com>
2021-09-18 02:13:33 +0800

    [HUDI-2418] add HiveSchemaProvider

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/schema/HiveSchemaProvider.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/schema/HiveSchemaProvider.java
@@ -19,71 +19,73 @@ import java.util.List;
 
 public class HiveSchemaProvider extends SchemaProvider {
 
-    /**
-     * Configs supported.
-     */
-    public static class Config {
-        private static final String SOURCE_SCHEMA_DATABASE_PROP = "hoodie.deltastreamer.schemaprovider.source.schema.hive.database";
-        private static final String SOURCE_SCHEMA_TABLE_PROP = "hoodie.deltastreamer.schemaprovider.source.schema.hive.table";
-        private static final String TARGET_SCHEMA_DATABASE_PROP = "hoodie.deltastreamer.schemaprovider.target.schema.hive.database";
-        private static final String TARGET_SCHEMA_TABLE_PROP = "hoodie.deltastreamer.schemaprovider.target.schema.hive.table";
+  /**
+   * Configs supported.
+   */
+  public static class Config {
+    private static final String SOURCE_SCHEMA_DATABASE_PROP = "hoodie.deltastreamer.schemaprovider.source.schema.hive.database";
+    private static final String SOURCE_SCHEMA_TABLE_PROP = "hoodie.deltastreamer.schemaprovider.source.schema.hive.table";
+    private static final String TARGET_SCHEMA_DATABASE_PROP = "hoodie.deltastreamer.schemaprovider.target.schema.hive.database";
+    private static final String TARGET_SCHEMA_TABLE_PROP = "hoodie.deltastreamer.schemaprovider.target.schema.hive.table";
+  }
+
+  private static final Logger LOG = LogManager.getLogger(HiveSchemaProvider.class);
+
+  private final Schema sourceSchema;
+
+  private Schema targetSchema;
+
+  public HiveSchemaProvider(TypedProperties props, JavaSparkContext jssc) {
+    super(props, jssc);
+    DataSourceUtils.checkRequiredProperties(props, Collections.singletonList(Config.SOURCE_SCHEMA_TABLE_PROP));
+    String sourceSchemaDBName = props.getString(Config.SOURCE_SCHEMA_DATABASE_PROP, "default");
+    String sourceSchemaTableName = props.getString(Config.SOURCE_SCHEMA_TABLE_PROP);
+    SparkSession spark = SparkSession.builder().config(jssc.getConf()).enableHiveSupport().getOrCreate();
+    try {
+      TableIdentifier sourceSchemaTable = new TableIdentifier(sourceSchemaTableName, scala.Option.apply(sourceSchemaDBName));
+      StructType sourceSchema = spark.sessionState().catalog().getTableMetadata(sourceSchemaTable).schema();
+
+      this.sourceSchema = AvroConversionUtils.convertStructTypeToAvroSchema(
+              sourceSchema,
+              sourceSchemaTableName,
+              "hoodie." + sourceSchemaDBName);
+
+      if (props.containsKey(Config.TARGET_SCHEMA_TABLE_PROP)) {
+        String targetSchemaDBName = props.getString(Config.TARGET_SCHEMA_DATABASE_PROP, "default");
+        String targetSchemaTableName = props.getString(Config.TARGET_SCHEMA_TABLE_PROP);
+        TableIdentifier targetSchemaTable = new TableIdentifier(targetSchemaTableName, scala.Option.apply(targetSchemaDBName));
+        StructType targetSchema = spark.sessionState().catalog().getTableMetadata(targetSchemaTable).schema();
+        this.targetSchema = AvroConversionUtils.convertStructTypeToAvroSchema(
+                targetSchema,
+                targetSchemaTableName,
+                "hoodie." + targetSchemaDBName);
+        LOG.warn(this.targetSchema.toString(true));
+
+      }
+    } catch (NoSuchTableException | NoSuchDatabaseException e) {
+      List<String> tables = new ArrayList<String>() {{
+        add(sourceSchemaTableName);
+      }};
+      if (props.containsKey(Config.TARGET_SCHEMA_TABLE_PROP)) {
+        tables.add(props.getString(Config.SOURCE_SCHEMA_TABLE_PROP));
+      }
+      String message = String.format("Can't find Hive table(s): %s", String.join(",", tables));
+      throw new IllegalArgumentException(message, e);
     }
+  }
 
-    private static final Logger LOG = LogManager.getLogger(HiveSchemaProvider.class);
+  @Override
+  public Schema getSourceSchema() {
+    return sourceSchema;
+  }
 
-    private final Schema sourceSchema;
-
-    private Schema targetSchema;
-
-    public HiveSchemaProvider(TypedProperties props, JavaSparkContext jssc) {
-        super(props, jssc);
-        DataSourceUtils.checkRequiredProperties(props, Collections.singletonList(Config.SOURCE_SCHEMA_TABLE_PROP));
-        String sourceSchemaDBName = props.getString(Config.SOURCE_SCHEMA_DATABASE_PROP, "default");
-        String sourceSchemaTableName = props.getString(Config.SOURCE_SCHEMA_TABLE_PROP);
-        SparkSession spark = SparkSession.builder().config(jssc.getConf()).enableHiveSupport().getOrCreate();
-        try {
-            TableIdentifier sourceSchemaTable = new TableIdentifier(sourceSchemaTableName, scala.Option.apply(sourceSchemaDBName));
-            StructType sourceSchema = spark.sessionState().catalog().getTableMetadata(sourceSchemaTable).schema();
-
-            this.sourceSchema = AvroConversionUtils.convertStructTypeToAvroSchema(
-                    sourceSchema,
-                    sourceSchemaTableName,
-                    "hoodie." + sourceSchemaDBName);
-
-            if (props.containsKey(Config.TARGET_SCHEMA_TABLE_PROP)) {
-                String targetSchemaDBName = props.getString(Config.TARGET_SCHEMA_DATABASE_PROP, "default");
-                String targetSchemaTableName = props.getString(Config.TARGET_SCHEMA_TABLE_PROP);
-                TableIdentifier targetSchemaTable = new TableIdentifier(targetSchemaTableName, scala.Option.apply(targetSchemaDBName));
-                StructType targetSchema = spark.sessionState().catalog().getTableMetadata(targetSchemaTable).schema();
-                this.targetSchema = AvroConversionUtils.convertStructTypeToAvroSchema(
-                        targetSchema,
-                        targetSchemaTableName,
-                        "hoodie." + targetSchemaDBName);
-                LOG.warn(this.targetSchema.toString(true));
-
-            }
-        } catch (NoSuchTableException | NoSuchDatabaseException e) {
-            List<String> tables = new ArrayList<String>() {{ add(sourceSchemaTableName); }};
-            if (props.containsKey(Config.TARGET_SCHEMA_TABLE_PROP)) {
-                tables.add(props.getString(Config.SOURCE_SCHEMA_TABLE_PROP));
-            }
-            String message = String.format("Can't find Hive table(s): %s", String.join(",", tables));
-            throw new IllegalArgumentException(message, e);
-        }
+  @Override
+  public Schema getTargetSchema() {
+    if (targetSchema != null) {
+      return targetSchema;
+    } else {
+      return super.getTargetSchema();
     }
-
-    @Override
-    public Schema getSourceSchema() {
-        return sourceSchema;
-    }
-
-    @Override
-    public Schema getTargetSchema() {
-        if (targetSchema != null) {
-            return targetSchema;
-        } else {
-            return super.getTargetSchema();
-        }
-    }
+  }
 
 }
