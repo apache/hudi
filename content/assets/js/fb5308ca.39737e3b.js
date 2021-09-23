fdef06f177de5dae4fd212d6de368b8503aec34a
CI <cibot@hudi.apache.org>
2021-09-23 00:55:01 +0000

    Travis CI build asf-site

---
+++ content/assets/js/fb5308ca.39737e3b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhudi=self.webpackChunkhudi||[]).push([[9949],{71029:function(e){e.exports=JSON.parse('{"pluginId":"learn","version":"current","label":"Next","banner":"none","isLast":true,"docsSidebars":{"defaultSidebar":[{"type":"link","label":"FAQs","href":"/learn/faq"}]},"permalinkToSidebar":{"/learn/faq":"defaultSidebar"}}')}}]);
---
+++ content/cn/assets/js/fb5308ca.26c80496.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhudi=self.webpackChunkhudi||[]).push([[9949],{71029:function(e){e.exports=JSON.parse('{"pluginId":"learn","version":"current","label":"Next","banner":"none","isLast":true,"docsSidebars":{"defaultSidebar":[{"type":"link","label":"FAQs","href":"/cn/learn/faq"}]},"permalinkToSidebar":{"/cn/learn/faq":"defaultSidebar"}}')}}]);
