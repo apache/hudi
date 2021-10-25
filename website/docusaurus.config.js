0dcd62f7a5350970fc2e8ee980c8ba027ea94f54
Laurie Li <laurieliyang@outlook.com>
2021-10-25 17:30:25 +0800

    Fix "Edit this page" url on the page

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -408,8 +408,20 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/apache/hudi/edit/asf-site/website/docs/',
+          editUrl: ({ version, versionDocsDirPath, docPath, locale }) => {
+            if (locale != this.defaultLocale) {
+              return `https://github.com/apache/hudi/tree/asf-site/website/${versionDocsDirPath}/${docPath}`
+            } else {
+              return `https://github.com/apache/hudi/tree/asf-site/website/i18n/${locale}/docusaurus-plugin-content-${versionDocsDirPath}/${version}/${docPath}`
+            }
+          },
+          // type EditUrlFunction = (params: {
+          //   version: string;
+          //   versionDocsDirPath: string;
+          //   docPath: string;
+          //   permalink: string;
+          //   locale: string;
+          // }) => string | undefined;
           includeCurrentVersion: true,
           versions: {
             current: {
