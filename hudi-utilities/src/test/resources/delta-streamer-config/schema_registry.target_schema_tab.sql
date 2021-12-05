734c9f5f2d6c3ea87c23cef962e69b0b68f72581
冯健 <fengjian428@gmail.com>
2021-12-05 16:10:13 +0800

    [HUDI-2418] Support HiveSchemaProvider (#3671)
    Co-authored-by: jian.feng <fengjian428@gmial.com>

---
+++ hudi-utilities/src/test/resources/delta-streamer-config/schema_registry.target_schema_tab.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `schema_registry`.`target_schema_tab`(
+    `id` BIGINT,
+    `name` STRING,
+    `num1` INT,
+    `num2` BIGINT,
+    `num3` DECIMAL(20,0),
+    `num4` TINYINT,
+    `num5` FLOAT,
+    `num6` DOUBLE,
+    `bool` BOOLEAN,
+    `bin` BINARY
+)
