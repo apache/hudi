e3c67a09fb4b559a0c44e9d21b070e7e3e8c289e
RexAn <anh131@126.com>
2021-12-09 18:34:11 +0800

    [HUDI-2779] Cache BaseDir if HudiTableNotFound Exception thrown (#4014)

--- hudi-hadoop-mr/src/test/java/org/apache/hudi/hadoop/TestHoodieROTablePathFilter.java
+++ hudi-hadoop-mr/src/test/java/org/apache/hudi/hadoop/TestHoodieROTablePathFilter.java
@@ -68,9 +68,11 @@ public class TestHoodieROTablePathFilter extends HoodieCommonTestHarness {
     assertFalse(pathFilter.accept(testTable.getInflightCommitFilePath("003")));
     assertFalse(pathFilter.accept(testTable.getRequestedCompactionFilePath("004")));
     assertFalse(pathFilter.accept(new Path("file:///" + basePath + "/" + HoodieTableMetaClient.METAFOLDER_NAME + "/")));
+    assertFalse(pathFilter.accept(new Path("file:///" + basePath + "/" + HoodieTableMetaClient.METAFOLDER_NAME + "/hoodie.properties")));
     assertFalse(pathFilter.accept(new Path("file:///" + basePath + "/" + HoodieTableMetaClient.METAFOLDER_NAME)));
 
     assertEquals(1, pathFilter.metaClientCache.size());
+    assertEquals(0, pathFilter.nonHoodiePathCache.size(), "NonHoodiePathCache size should be 0");
   }
 
   @Test
@@ -82,6 +84,7 @@ public class TestHoodieROTablePathFilter extends HoodieCommonTestHarness {
     java.nio.file.Path path2 = Paths.get(basePath, "nonhoodiefolder/somefile");
     Files.createFile(path2);
     assertTrue(pathFilter.accept(new Path(path2.toUri())));
+    assertEquals(2, pathFilter.nonHoodiePathCache.size(), "NonHoodiePathCache size should be 2");
   }
 
   @Test
@@ -93,5 +96,6 @@ public class TestHoodieROTablePathFilter extends HoodieCommonTestHarness {
     Path partitionPath2 = testTable.getPartitionPath(p2).getParent();
     assertTrue(pathFilter.accept(partitionPath1), "Directories should be accepted");
     assertTrue(pathFilter.accept(partitionPath2), "Directories should be accepted");
+    assertEquals(2, pathFilter.nonHoodiePathCache.size(), "NonHoodiePathCache size should be 2");
   }
 }
