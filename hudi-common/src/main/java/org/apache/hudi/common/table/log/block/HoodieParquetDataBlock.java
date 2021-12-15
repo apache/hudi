f715050ed04211c30c034e108ac3e249bf9cccfe
Alexey Kudinkin <alexey@infinilake.com>
2021-12-15 15:57:18 -0800

    Completed `HoodieParquetDataBlock`:   - Implemented `deserializeRecords`   - Cleaned up `serializeRecords`

--- hudi-common/src/main/java/org/apache/hudi/common/table/log/block/HoodieParquetDataBlock.java
+++ hudi-common/src/main/java/org/apache/hudi/common/table/log/block/HoodieParquetDataBlock.java
@@ -18,31 +18,33 @@
 
 package org.apache.hudi.common.table.log.block;
 
-import org.apache.hudi.avro.HoodieAvroWriteSupport;
-import org.apache.hudi.common.bloom.BloomFilter;
-import org.apache.hudi.common.bloom.BloomFilterFactory;
-import org.apache.hudi.common.bloom.BloomFilterTypeCode;
-import org.apache.hudi.common.fs.inline.InLineFSUtils;
-import org.apache.hudi.common.fs.inline.InLineFileSystem;
-import org.apache.hudi.common.model.HoodieLogFile;
-import org.apache.hudi.common.model.HoodieRecord;
-import org.apache.hudi.common.util.Option;
-import org.apache.hudi.exception.HoodieIOException;
-import org.apache.hudi.io.storage.HoodieAvroParquetConfig;
-import org.apache.hudi.io.storage.HoodieParquetReader;
-import org.apache.hudi.io.storage.HoodieParquetStreamWriter;
-
 import org.apache.avro.Schema;
 import org.apache.avro.generic.IndexedRecord;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.Path;
+import org.apache.hudi.avro.HoodieAvroWriteSupport;
+import org.apache.hudi.common.bloom.BloomFilter;
+import org.apache.hudi.common.bloom.BloomFilterFactory;
+import org.apache.hudi.common.bloom.BloomFilterTypeCode;
+import org.apache.hudi.common.model.HoodieLogFile;
+import org.apache.hudi.common.model.HoodieRecord;
+import org.apache.hudi.common.util.Option;
+import org.apache.hudi.common.util.ParquetReaderIterator;
+import org.apache.hudi.common.util.io.ByteBufferBackedInputStream;
+import org.apache.hudi.exception.HoodieIOException;
+import org.apache.hudi.io.storage.HoodieAvroParquetConfig;
+import org.apache.hudi.io.storage.HoodieParquetStreamWriter;
+import org.apache.parquet.avro.AvroParquetReader;
+import org.apache.parquet.avro.AvroReadSupport;
 import org.apache.parquet.avro.AvroSchemaConverter;
+import org.apache.parquet.hadoop.ParquetReader;
 import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
+import org.apache.parquet.io.DelegatingSeekableInputStream;
+import org.apache.parquet.io.InputFile;
+import org.apache.parquet.io.SeekableInputStream;
 
 import javax.annotation.Nonnull;
-
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -57,23 +59,51 @@ import java.util.Map;
  */
 public class HoodieParquetDataBlock extends HoodieDataBlock {
 
-  public HoodieParquetDataBlock(@Nonnull Map<HeaderMetadataType, String> logBlockHeader,
-                                @Nonnull Map<HeaderMetadataType, String> logBlockFooter,
-                                @Nonnull Option<HoodieLogBlockContentLocation> blockContentLocation, @Nonnull Option<byte[]> content,
-                                FSDataInputStream inputStream, boolean readBlockLazily) {
+  public HoodieParquetDataBlock(
+      @Nonnull Map<HeaderMetadataType, String> logBlockHeader,
+      @Nonnull Map<HeaderMetadataType, String> logBlockFooter,
+      @Nonnull Option<HoodieLogBlockContentLocation> blockContentLocation,
+      @Nonnull Option<byte[]> content,
+      FSDataInputStream inputStream,
+      boolean readBlockLazily
+  ) {
     super(logBlockHeader, logBlockFooter, blockContentLocation, content, inputStream, readBlockLazily);
   }
 
-  public HoodieParquetDataBlock(HoodieLogFile logFile, FSDataInputStream inputStream, Option<byte[]> content,
-                                boolean readBlockLazily, long position, long blockSize, long blockEndpos, Schema readerSchema,
-                                Map<HeaderMetadataType, String> header, Map<HeaderMetadataType, String> footer) {
-    super(content, inputStream, readBlockLazily,
-        Option.of(new HoodieLogBlockContentLocation(logFile, position, blockSize, blockEndpos)), readerSchema, header,
-        footer);
+  public HoodieParquetDataBlock(
+      HoodieLogFile logFile,
+      FSDataInputStream inputStream,
+      Option<byte[]> content,
+      boolean readBlockLazily, long position, long blockSize, long blockEndpos,
+      Schema readerSchema,
+      Map<HeaderMetadataType, String> header,
+      Map<HeaderMetadataType, String> footer,
+      String keyField
+  ) {
+    super(
+        content,
+        inputStream,
+        readBlockLazily,
+        Option.of(new HoodieLogBlockContentLocation(logFile, position, blockSize, blockEndpos)),
+        readerSchema,
+        header,
+        footer,
+        keyField);
   }
 
-  public HoodieParquetDataBlock(@Nonnull List<IndexedRecord> records, @Nonnull Map<HeaderMetadataType, String> header) {
-    super(records, header, new HashMap<>());
+  public HoodieParquetDataBlock(
+      @Nonnull List<IndexedRecord> records,
+      @Nonnull Map<HeaderMetadataType, String> header,
+      @Nonnull String keyField
+  ) {
+    super(records, header, new HashMap<>(), keyField);
+  }
+
+  public HoodieParquetDataBlock(
+      @Nonnull List<IndexedRecord> records,
+      @Nonnull Map<HeaderMetadataType, String> header
+  ) {
+    super(records, header, new HashMap<>(), HoodieRecord.RECORD_KEY_METADATA_FIELD);
   }
 
   @Override
@@ -81,8 +111,23 @@ public class HoodieParquetDataBlock extends HoodieDataBlock {
     return HoodieLogBlockType.PARQUET_DATA_BLOCK;
   }
 
+  public static Iterator<IndexedRecord> getParquetRecordsIterator(
+      Configuration conf,
+      Schema schema,
+      InputFile inputFile
+  ) throws IOException {
+    AvroReadSupport.setAvroReadSchema(conf, schema);
+    ParquetReader<IndexedRecord> reader =
+        AvroParquetReader.<IndexedRecord>builder(inputFile).withConf(conf).build();
+    return new ParquetReaderIterator<>(reader);
+  }
+
   @Override
-  protected byte[] serializeRecords() throws IOException {
+  protected byte[] serializeRecords(List<IndexedRecord> records) throws IOException {
+    if (records.size() == 0) {
+      return new byte[0];
+    }
+
     // TODO: Need to decide from where to fetch all config values required below. We can't re-use index config as the purpose is different.
     // And these are very specific to data blocks. Once we have consensus, we might need to route them to log block constructors (
     // as of now, log block constructors does not take in any configs in general).
@@ -92,38 +137,38 @@ public class HoodieParquetDataBlock extends HoodieDataBlock {
         Integer.parseInt("100000"),//HoodieIndexConfig.HOODIE_BLOOM_INDEX_FILTER_DYNAMIC_MAX_ENTRIES.defaultValue()),
         BloomFilterTypeCode.SIMPLE.name());//HoodieIndexConfig.BLOOM_INDEX_FILTER_TYPE.defaultValue());
 
-    HoodieAvroWriteSupport writeSupport = new HoodieAvroWriteSupport(
-        new AvroSchemaConverter().convert(schema), schema, Option.of(filter));
+    Schema writerSchema = new Schema.Parser().parse(super.getLogBlockHeader().get(HeaderMetadataType.SCHEMA));
 
-    HoodieAvroParquetConfig avroParquetConfig = new HoodieAvroParquetConfig(writeSupport, CompressionCodecName.GZIP,
-        ParquetWriter.DEFAULT_BLOCK_SIZE, ParquetWriter.DEFAULT_PAGE_SIZE, 1024 * 1024 * 1024,
-        new Configuration(), Double.parseDouble(String.valueOf(0.1)));//HoodieStorageConfig.PARQUET_COMPRESSION_RATIO.defaultValue()));
+    HoodieAvroWriteSupport writeSupport = new HoodieAvroWriteSupport(
+        new AvroSchemaConverter().convert(writerSchema), writerSchema, Option.of(filter));
+
+    HoodieAvroParquetConfig avroParquetConfig =
+        new HoodieAvroParquetConfig(
+            writeSupport,
+            // TODO fetch compression codec from the config
+            CompressionCodecName.GZIP,
+            ParquetWriter.DEFAULT_BLOCK_SIZE,
+            ParquetWriter.DEFAULT_PAGE_SIZE,
+            1024 * 1024 * 1024,
+            new Configuration(),
+            Double.parseDouble(String.valueOf(0.1)));//HoodieStorageConfig.PARQUET_COMPRESSION_RATIO.defaultValue()));
 
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    DataOutputStream outputStream = null;
-    HoodieParquetStreamWriter<IndexedRecord> parquetWriter = null;
-    try {
-      outputStream = new DataOutputStream(baos);
-      parquetWriter = new HoodieParquetStreamWriter<>(outputStream, avroParquetConfig);
-      Iterator<IndexedRecord> itr = records.iterator();
-      if (records.size() > 0) {
-        Schema.Field keyField = records.get(0).getSchema().getField(HoodieRecord.RECORD_KEY_METADATA_FIELD);
-        if (keyField == null) {
+
+    try (DataOutputStream outputStream = new DataOutputStream(baos)) {
+      try (HoodieParquetStreamWriter<IndexedRecord> parquetWriter = new HoodieParquetStreamWriter<>(outputStream, avroParquetConfig)){
+        Iterator<IndexedRecord> itr = records.iterator();
+        if (getRecordKey(records.get(0)) == null) {
           throw new HoodieIOException("Record key field missing from schema for records to be written to Parquet data block");
         }
+
         while (itr.hasNext()) {
           IndexedRecord record = itr.next();
-          String recordKey = record.get(keyField.pos()).toString();
+          String recordKey = getRecordKey(record);
           parquetWriter.writeAvro(recordKey, record);
         }
+
         outputStream.flush();
-      }
-    } finally {
-      if (outputStream != null) {
-        outputStream.close();
-      }
-      if (parquetWriter != null) {
-        parquetWriter.close();
       }
     }
 
@@ -131,38 +176,51 @@ public class HoodieParquetDataBlock extends HoodieDataBlock {
   }
 
   @Override
-  public List<IndexedRecord> getRecords() {
-    try {
-      records = new ArrayList<>();
-      // Get schema from the header
-      Schema writerSchema = new Schema.Parser().parse(super.getLogBlockHeader().get(HeaderMetadataType.SCHEMA));
-      // If readerSchema was not present, use writerSchema
-      if (schema == null) {
-        schema = writerSchema;
-      }
-      Configuration conf = new Configuration();
-      Configuration inlineConf = new Configuration();
-      inlineConf.set("fs." + InLineFileSystem.SCHEME + ".impl", InLineFileSystem.class.getName());
+  protected List<IndexedRecord> deserializeRecords(byte[] content) throws IOException {
+    Configuration conf = new Configuration();
 
-      Path inlinePath = InLineFSUtils.getInlineFilePath(
-          getBlockContentLocation().get().getLogFile().getPath(),
-          getBlockContentLocation().get().getLogFile().getPath().getFileSystem(conf).getScheme(),
-          getBlockContentLocation().get().getContentPositionInLogFile(),
-          getBlockContentLocation().get().getBlockSize());
+    ArrayList<IndexedRecord> records = new ArrayList<>();
 
-      HoodieParquetReader<IndexedRecord> parquetReader = new HoodieParquetReader<>(inlineConf, inlinePath);
-      Iterator<IndexedRecord> recordIterator = parquetReader.getRecordIterator(schema);
-      while (recordIterator.hasNext()) {
-        records.add(recordIterator.next());
-      }
-      return records;
-    } catch (IOException e) {
-      throw new HoodieIOException("Reading parquet inlining failed ", e);
+    getParquetRecordsIterator(conf, readerSchema, new ByteBufferBackedInputFile(content))
+        .forEachRemaining(records::add);
+
+    return records;
+  }
+
+  static class ByteBufferBackedInputFile implements InputFile {
+
+    private final byte[] buffer;
+    private final int offset;
+    private final int length;
+
+    ByteBufferBackedInputFile(byte[] buffer, int offset, int length) {
+      this.buffer = buffer;
+      this.offset = offset;
+      this.length = length;
+    }
+
+    ByteBufferBackedInputFile(byte[] buffer) {
+      this(buffer, 0, buffer.length);
+    }
+
+    @Override
+    public long getLength() {
+      return length;
+    }
+
+    @Override
+    public SeekableInputStream newStream() {
+      return new DelegatingSeekableInputStream(new ByteBufferBackedInputStream(buffer, offset, length)) {
+        @Override
+        public long getPos() {
+          return ((ByteBufferBackedInputStream) getStream()).getPosition();
+        }
+
+        @Override
+        public void seek(long newPos) {
+          ((ByteBufferBackedInputStream) getStream()).seek(newPos);
+        }
+      };
     }
   }
-
-  @Override
-  protected void deserializeRecords() throws IOException {
-    throw new IOException("Not implemented");
-  }
-} 
+}
