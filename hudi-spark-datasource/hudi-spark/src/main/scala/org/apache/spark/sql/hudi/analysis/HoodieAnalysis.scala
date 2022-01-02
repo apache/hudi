54b205a313e7530c7e8e215ba9dcf3dde4c8d6e1
leesf <leesf@apache.org>
2022-01-02 23:49:00 +0800

    fix spark 3.1.x test failure

--- hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/hudi/analysis/HoodieAnalysis.scala
+++ hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/hudi/analysis/HoodieAnalysis.scala
@@ -51,7 +51,7 @@ object HoodieAnalysis {
     ) ++ extraPostHocResolutionRules()
 
   def extraResolutionRules(): Seq[SparkSession => Rule[LogicalPlan]] = {
-    if (HoodieSparkUtils.isSpark3) {
+    if (HoodieSparkUtils.isSpark3_2) {
       val spark3AnalysisClass = "org.apache.spark.sql.hudi.analysis.HoodieSpark3Analysis"
       val spark3Analysis: SparkSession => Rule[LogicalPlan] =
         session => ReflectionUtils.loadClass(spark3AnalysisClass, session).asInstanceOf[Rule[LogicalPlan]]
@@ -67,7 +67,7 @@ object HoodieAnalysis {
   }
 
   def extraPostHocResolutionRules(): Seq[SparkSession => Rule[LogicalPlan]] =
-    if (HoodieSparkUtils.isSpark3) {
+    if (HoodieSparkUtils.isSpark3_2) {
       val spark3PostHocResolutionClass = "org.apache.spark.sql.hudi.analysis.HoodieSpark3PostAnalysisRule"
       val spark3PostHocResolution: SparkSession => Rule[LogicalPlan] =
         session => ReflectionUtils.loadClass(spark3PostHocResolutionClass, session).asInstanceOf[Rule[LogicalPlan]]
