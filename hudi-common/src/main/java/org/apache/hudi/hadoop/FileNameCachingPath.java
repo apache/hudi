24a23036f633ef6f6b717d5e4ba4319ac30dbe71
Alexey Kudinkin <alexey@infinilake.com>
2022-01-05 13:45:03 -0800

    `PathCachingFileName` > `FileNameCachingPath`

--- hudi-common/src/main/java/org/apache/hudi/hadoop/FileNameCachingPath.java
+++ hudi-common/src/main/java/org/apache/hudi/hadoop/FileNameCachingPath.java
@@ -25,14 +25,14 @@ import java.net.URI;
 /**
  * NOTE: This class is thread-safe
  */
-public class PathCachingFileName extends Path {
+public class FileNameCachingPath extends Path {
 
   // NOTE: volatile keyword is redundant here and put mostly for reader notice, since all
   //       reads/writes to references are always atomic (including 64-bit JVMs)
   //       https://docs.oracle.com/javase/specs/jls/se8/html/jls-17.html#jls-17.7
   private volatile String fileName;
 
-  public PathCachingFileName(URI aUri) {
+  public FileNameCachingPath(URI aUri) {
     super(aUri);
   }
 
