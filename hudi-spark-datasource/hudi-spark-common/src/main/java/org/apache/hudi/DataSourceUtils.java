063a05e67fc0bb49fd6ccd6d06c51125b3614178
Raymond Xu <2701446+xushiyan@users.noreply.github.com>
2022-01-06 23:26:35 -0800

    [HUDI-3100] Add config for hive conditional sync (#4440)

--- hudi-spark-datasource/hudi-spark-common/src/main/java/org/apache/hudi/DataSourceUtils.java
+++ hudi-spark-datasource/hudi-spark-common/src/main/java/org/apache/hudi/DataSourceUtils.java
@@ -309,6 +309,8 @@ public class DataSourceUtils {
         DataSourceWriteOptions.HIVE_SKIP_RO_SUFFIX_FOR_READ_OPTIMIZED_TABLE().defaultValue()));
     hiveSyncConfig.supportTimestamp = Boolean.valueOf(props.getString(DataSourceWriteOptions.HIVE_SUPPORT_TIMESTAMP_TYPE().key(),
         DataSourceWriteOptions.HIVE_SUPPORT_TIMESTAMP_TYPE().defaultValue()));
+    hiveSyncConfig.isConditionalSync = Boolean.valueOf(props.getString(DataSourceWriteOptions.HIVE_CONDITIONAL_SYNC().key(),
+        DataSourceWriteOptions.HIVE_CONDITIONAL_SYNC().defaultValue()));
     return hiveSyncConfig;
   }
 
