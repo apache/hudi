2b98d909886c34a8a64c520f459cdf9a1a3a9c6c
Thinking Chen <cdmikechen@hotmail.com>
2022-01-10 07:31:57 +0800

    [HUDI-3112] Fix KafkaConnect cannot sync to Hive Problem (#4458)

--- hudi-kafka-connect/src/main/java/org/apache/hudi/connect/writers/KafkaConnectConfigs.java
+++ hudi-kafka-connect/src/main/java/org/apache/hudi/connect/writers/KafkaConnectConfigs.java
@@ -164,6 +164,22 @@ public class KafkaConnectConfigs extends HoodieConfig {
     return getString(HADOOP_HOME);
   }
 
+  public static final String HIVE_USE_PRE_APACHE_INPUT_FORMAT = "hoodie.datasource.hive_sync.use_pre_apache_input_format";
+  public static final String HIVE_DATABASE = "hoodie.datasource.hive_sync.database";
+  public static final String HIVE_TABLE = "hoodie.datasource.hive_sync.table";
+  public static final String HIVE_USER = "hoodie.datasource.hive_sync.username";
+  public static final String HIVE_PASS = "hoodie.datasource.hive_sync.password";
+  public static final String HIVE_URL = "hoodie.datasource.hive_sync.jdbcurl";
+  public static final String HIVE_PARTITION_FIELDS = "hoodie.datasource.hive_sync.partition_fields";
+  public static final String HIVE_PARTITION_EXTRACTOR_CLASS = "hoodie.datasource.hive_sync.partition_extractor_class";
+  public static final String HIVE_USE_JDBC = "hoodie.datasource.hive_sync.use_jdbc";
+  public static final String HIVE_SYNC_MODE = "hoodie.datasource.hive_sync.mode";
+  public static final String HIVE_AUTO_CREATE_DATABASE = "hoodie.datasource.hive_sync.auto_create_database";
+  public static final String HIVE_IGNORE_EXCEPTIONS = "hoodie.datasource.hive_sync.ignore_exceptions";
+  public static final String HIVE_SKIP_RO_SUFFIX_FOR_READ_OPTIMIZED_TABLE = "hoodie.datasource.hive_sync.skip_ro_suffix";
+  public static final String HIVE_SUPPORT_TIMESTAMP_TYPE = "hoodie.datasource.hive_sync.support_timestamp";
+  public static final String HIVE_METASTORE_URIS = "hive.metastore.uris";
+
   public static class Builder {
 
     protected final KafkaConnectConfigs connectConfigs = new KafkaConnectConfigs();
