a4247118e2e35b2fe819ec45489ea67d82a28187
Y Ethan Guo <ethan.guoyihua@gmail.com>
2022-01-11 11:09:20 -0800

    Fix checkstyle

--- hudi-client/hudi-client-common/src/test/java/org/apache/hudi/HoodieTestCommitGenerator.java
+++ hudi-client/hudi-client-common/src/test/java/org/apache/hudi/HoodieTestCommitGenerator.java
@@ -38,8 +38,6 @@ import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
