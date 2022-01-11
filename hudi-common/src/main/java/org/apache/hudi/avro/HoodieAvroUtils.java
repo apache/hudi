f1e3762a947d9bc978beb5bb5ae845b8d5dd6290
Alexey Kudinkin <alexey@infinilake.com>
2022-01-10 18:23:22 -0800

    [HUDI-2950] Addressing performance traps in Bulk Insert/Layout Optimization (#4234)
    * Cleaned up Z-curve/Hilbert ordering seqs:
      - Streamlined flow
      - Removed unnecessary operations (double-mapping, boxing, etc)
    Updated `CollectionUtils::combine` to avoid AL resizing
    
    * Tidying up
    
    * Reducing small objects churn due to Scala/Java conversions by re-using `RowFactory`, passing `Object[]`
    
    * Fixing name resolution (disambiguation overloads)
    
    * `lint`
    
    * Replaced `OverwriteAvroPayloadRecord` w/ `RewriteRecordPayload` to avoid unnecessary Avro ser/de loop
    
    * Added `PathCachingFileName` to avoid fetching substrings every time file-name is fetched;
    Inject `PathCachingFileName` into `HoodieWrapperFileSystem.convertPathWithScheme`
    
    * Drastically reducing size of the `ArrayDeque` allocated by `ObjectSizeCalculator`
    
    * XXX
    
    * Missing license
    
    * Fixed refs (after rebase)
    
    * Fixing compilation failure in Scala 2.11
    
    * `PathCachingFileName` > `FileNameCachingPath`
    
    * Tidying up

--- hudi-common/src/main/java/org/apache/hudi/avro/HoodieAvroUtils.java
+++ hudi-common/src/main/java/org/apache/hudi/avro/HoodieAvroUtils.java
@@ -596,6 +596,7 @@ public class HoodieAvroUtils {
       if (columns.length == 1) {
         return HoodieAvroUtils.getNestedFieldVal(genericRecord, columns[0], true, consistentLogicalTimestampEnabled);
       } else {
+        // TODO this is inefficient, instead we can simply return array of Comparable
         StringBuilder sb = new StringBuilder();
         for (String col : columns) {
           sb.append(HoodieAvroUtils.getNestedFieldValAsString(genericRecord, col, true, consistentLogicalTimestampEnabled));
