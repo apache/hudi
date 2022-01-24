adeb1bfa833939705fa550f6dfda7069810ca793
XuQianJin-Stars <forwardxu@apache.com>
2022-01-24 16:01:29 +0800

    [HUDI-2941] Show _hoodie_operation in spark sql results

--- hudi-common/src/main/java/org/apache/hudi/common/table/TableSchemaResolver.java
+++ hudi-common/src/main/java/org/apache/hudi/common/table/TableSchemaResolver.java
@@ -58,9 +58,11 @@ public class TableSchemaResolver {
 
   private static final Logger LOG = LogManager.getLogger(TableSchemaResolver.class);
   private final HoodieTableMetaClient metaClient;
+  private final boolean withOperationField;
 
   public TableSchemaResolver(HoodieTableMetaClient metaClient) {
     this.metaClient = metaClient;
+    this.withOperationField = hasOperationField();
   }
 
   /**
@@ -142,7 +144,7 @@ public class TableSchemaResolver {
     Option<Schema> schemaFromTableConfig = metaClient.getTableConfig().getTableCreateSchema();
     if (schemaFromTableConfig.isPresent()) {
       if (includeMetadataFields) {
-        return HoodieAvroUtils.addMetadataFields(schemaFromTableConfig.get(), hasOperationField());
+        return HoodieAvroUtils.addMetadataFields(schemaFromTableConfig.get(), withOperationField);
       } else {
         return schemaFromTableConfig.get();
       }
@@ -167,7 +169,7 @@ public class TableSchemaResolver {
     }
     Option<Schema> schemaFromTableConfig = metaClient.getTableConfig().getTableCreateSchema();
     if (schemaFromTableConfig.isPresent()) {
-      Schema schema = HoodieAvroUtils.addMetadataFields(schemaFromTableConfig.get(), hasOperationField());
+      Schema schema = HoodieAvroUtils.addMetadataFields(schemaFromTableConfig.get(), withOperationField);
       return convertAvroSchemaToParquet(schema);
     }
     return getTableParquetSchemaFromDataFile();
@@ -235,7 +237,7 @@ public class TableSchemaResolver {
 
       Schema schema = new Schema.Parser().parse(existingSchemaStr);
       if (includeMetadataFields) {
-        schema = HoodieAvroUtils.addMetadataFields(schema, hasOperationField());
+        schema = HoodieAvroUtils.addMetadataFields(schema, withOperationField);
       }
       return Option.of(schema);
     } catch (Exception e) {
