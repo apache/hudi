51a1ef82111fe4afac47203849558e18fac77998
sivabalan <n.siva.b@gmail.com>
2022-02-14 07:27:22 -0500

    Fixing release link in homepage

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -29,7 +29,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/releases/release-0.10.0">
+            to="/releases/release-0.10.1">
              Latest Releases
           </Link>
           <Link
