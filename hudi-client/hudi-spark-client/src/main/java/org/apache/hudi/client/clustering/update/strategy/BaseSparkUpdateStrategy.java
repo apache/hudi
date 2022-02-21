801fdab55c97d1abd815f90ae64c0ecbe8e84f94
RexAn <anh131@126.com>
2022-02-21 22:53:09 +0800

    [HUDI-3042] Abstract Spark update Strategy to make code more clean and remove duplicates (#4845)
    Co-authored-by: Hui An <hui.an@shopee.com>

--- hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/client/clustering/update/strategy/BaseSparkUpdateStrategy.java
+++ hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/client/clustering/update/strategy/BaseSparkUpdateStrategy.java
@@ -22,31 +22,32 @@ import org.apache.hudi.client.common.HoodieSparkEngineContext;
 import org.apache.hudi.common.model.HoodieFileGroupId;
 import org.apache.hudi.common.model.HoodieRecord;
 import org.apache.hudi.common.model.HoodieRecordPayload;
-import org.apache.hudi.common.util.collection.Pair;
+import org.apache.hudi.table.action.cluster.strategy.UpdateStrategy;
 
 import org.apache.spark.api.java.JavaRDD;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
- * Allow ingestion commits during clustering job.
+ * Spark base update strategy, write records to the file groups which are in clustering
+ * need to check. Spark relate implementations should extend this base class.
  */
-public class SparkAllowUpdateStrategy<T extends HoodieRecordPayload<T>> extends BaseSparkUpdateStrategy<T> {
+public abstract class BaseSparkUpdateStrategy<T extends HoodieRecordPayload<T>> extends UpdateStrategy<T, JavaRDD<HoodieRecord<T>>> {
 
-  public SparkAllowUpdateStrategy(HoodieSparkEngineContext engineContext,
-                                  HashSet<HoodieFileGroupId> fileGroupsInPendingClustering) {
+  public BaseSparkUpdateStrategy(HoodieSparkEngineContext engineContext,
+                                 Set<HoodieFileGroupId> fileGroupsInPendingClustering) {
     super(engineContext, fileGroupsInPendingClustering);
   }
 
-  @Override
-  public Pair<JavaRDD<HoodieRecord<T>>, Set<HoodieFileGroupId>> handleUpdate(JavaRDD<HoodieRecord<T>> taggedRecordsRDD) {
-    List<HoodieFileGroupId> fileGroupIdsWithRecordUpdate = getGroupIdsWithUpdate(taggedRecordsRDD);
-    Set<HoodieFileGroupId> fileGroupIdsWithUpdatesAndPendingClustering = fileGroupIdsWithRecordUpdate.stream()
-        .filter(f -> fileGroupsInPendingClustering.contains(f))
-        .collect(Collectors.toSet());
-    return Pair.of(taggedRecordsRDD, fileGroupIdsWithUpdatesAndPendingClustering);
+  /**
+   * Get records matched file group ids.
+   * @param inputRecords the records to write, tagged with target file id
+   * @return the records matched file group ids
+   */
+  protected List<HoodieFileGroupId> getGroupIdsWithUpdate(JavaRDD<HoodieRecord<T>> inputRecords) {
+    return inputRecords
+            .filter(record -> record.getCurrentLocation() != null)
+            .map(record -> new HoodieFileGroupId(record.getPartitionPath(), record.getCurrentLocation().getFileId())).distinct().collect();
   }
 }
