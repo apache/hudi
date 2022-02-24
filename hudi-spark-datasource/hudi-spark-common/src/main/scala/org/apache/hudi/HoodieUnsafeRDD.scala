45d82651cc3f77369a4ca47fd806b0cf489b8813
Alexey Kudinkin <alexey@infinilake.com>
2022-02-24 14:14:39 -0800

    Replacing self-type restriction w/ sub-classing

--- hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/HoodieUnsafeRDD.scala
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/HoodieUnsafeRDD.scala
@@ -1,27 +1,7 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-package org.apache.hudi
+package org.apache.hudi.rdd
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.{Partition, SparkContext, TaskContext}
 
 /**
  * !!! PLEASE READ CAREFULLY !!!
@@ -32,20 +12,20 @@ import org.apache.spark.{Partition, SparkContext, TaskContext}
  *
  * <pre>
  *   1. Does NOT deserialize from [[InternalRow]] to [[Row]] (therefore only providing access to
- *   Catalyst internal representations (often mutable) of the read row)
+ * Catalyst internal representations (often mutable) of the read row)
  *
- *   2. DOES NOT COPY UNDERLYING ROW OUT OF THE BOX, meaning that
+ * 2. DOES NOT COPY UNDERLYING ROW OUT OF THE BOX, meaning that
  *
- *      a) access to this RDD is NOT thread-safe
+ * a) access to this RDD is NOT thread-safe
  *
- *      b) iterating over it reference to a _mutable_ underlying instance (of [[InternalRow]]) is
- *      returned, entailing that after [[Iterator#next()]] is invoked on the provided iterator,
- *      previous reference becomes **invalid**. Therefore, you will have to copy underlying mutable
- *      instance of [[InternalRow]] if you plan to access it after [[Iterator#next()]] is invoked (filling
- *      it with the next row's payload)
+ * b) iterating over it reference to a _mutable_ underlying instance (of [[InternalRow]]) is
+ * returned, entailing that after [[Iterator#next()]] is invoked on the provided iterator,
+ * previous reference becomes **invalid**. Therefore, you will have to copy underlying mutable
+ * instance of [[InternalRow]] if you plan to access it after [[Iterator#next()]] is invoked (filling
+ * it with the next row's payload)
  *
- *      c) due to item b) above, no operation other than the iteration will produce meaningful
- *      results on it and will likely fail [1]
+ * c) due to item b) above, no operation other than the iteration will produce meaningful
+ * results on it and will likely fail [1]
  * </pre>
  *
  * [1] For example, [[RDD#collect]] method on this implementation would not work correctly, as it's
@@ -54,11 +34,9 @@ import org.apache.spark.{Partition, SparkContext, TaskContext}
  * before concatenating into the final output. Please refer to [[HoodieRDDUtils#collect]] for more details.
  *
  * NOTE: It enforces, for ex, that all of the RDDs implement [[compute]] method returning
- *       [[InternalRow]] to avoid superfluous ser/de
+ * [[InternalRow]] to avoid superfluous ser/de
  */
-trait HoodieUnsafeRDD {
-  self: RDD[InternalRow] =>
-
+trait HoodieUnsafeRDD extends RDD[InternalRow] {
   override def collect(): Array[InternalRow] =
     throw new UnsupportedOperationException(
       "This method will not function correctly, please refer to scala-doc for HoodieUnsafeRDD"
--- hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/spark/HoodieUnsafeRDDUtils.scala
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/spark/HoodieUnsafeRDDUtils.scala
@@ -18,6 +18,7 @@
 package org.apache.spark
 
 import org.apache.hudi.HoodieUnsafeRDD
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.util.MutablePair
 
