b28311f65dcb5561fd4b58b3a9409d96911f3b70
rkkalluri <rkalluri@gmail.com>
2022-02-28 22:28:30 -0600

    Add test coverage to MetricsReportFactory

--- hudi-metrics-common/src/test/java/org/apache/hudi/metrics/TestMetricsReporterFactory.java
+++ hudi-metrics-common/src/test/java/org/apache/hudi/metrics/TestMetricsReporterFactory.java
@@ -20,9 +20,13 @@
 package org.apache.hudi.metrics;
 
 import org.apache.hudi.common.config.TypedProperties;
+import org.apache.hudi.common.testutils.NetworkTestUtils;
 import org.apache.hudi.exception.HoodieException;
 import org.apache.hudi.metrics.config.HoodieMetricsConfig;
 import org.apache.hudi.metrics.custom.CustomizableMetricsReporter;
+import org.apache.hudi.metrics.datadog.DatadogHttpClient;
+import org.apache.hudi.metrics.datadog.DatadogMetricsReporter;
+import org.apache.hudi.metrics.prometheus.PrometheusReporter;
 
 import com.codahale.metrics.MetricRegistry;
 import org.junit.jupiter.api.Test;
@@ -31,6 +35,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.Closeable;
+import java.util.Arrays;
 import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -52,6 +57,51 @@ public class TestMetricsReporterFactory {
     when(config.getMetricsReporterType()).thenReturn(MetricsReporterType.INMEMORY);
     MetricsReporter reporter = MetricsReporterFactory.createReporter(config, registry);
     assertTrue(reporter instanceof InMemoryMetricsReporter);
+  }
+
+  @Test
+  public void metricsReporterFactoryShouldReturnJMXReporter() {
+    when(config.getMetricsReporterType()).thenReturn(MetricsReporterType.JMX);
+    when(config.getJmxHost()).thenReturn("localhost");
+    when(config.getJmxPort()).thenReturn(String.valueOf(NetworkTestUtils.nextFreePort()));
+    MetricsReporter reporter = MetricsReporterFactory.createReporter(config, registry);
+    assertTrue(reporter instanceof JmxMetricsReporter);
+  }
+
+  @Test
+  public void metricsReporterFactoryShouldReturnGraphiteReporter() {
+    when(config.getMetricsReporterType()).thenReturn(MetricsReporterType.GRAPHITE);
+    when(config.getGraphiteServerHost()).thenReturn("localhost");
+    when(config.getGraphiteServerPort()).thenReturn(NetworkTestUtils.nextFreePort());
+    MetricsReporter reporter = MetricsReporterFactory.createReporter(config, registry);
+    assertTrue(reporter instanceof MetricsGraphiteReporter);
+  }
+
+  @Test
+  public void metricsReporterFactoryShouldReturnPrometheusReporter() {
+    when(config.getMetricsReporterType()).thenReturn(MetricsReporterType.PROMETHEUS);
+    MetricsReporter reporter = MetricsReporterFactory.createReporter(config, registry);
+    assertTrue(reporter instanceof PrometheusReporter);
+  }
+
+  @Test
+  public void metricsReporterFactoryShouldReturnConsoleReporter() {
+    when(config.getMetricsReporterType()).thenReturn(MetricsReporterType.CONSOLE);
+    MetricsReporter reporter = MetricsReporterFactory.createReporter(config, registry);
+    assertTrue(reporter instanceof ConsoleMetricsReporter);
+  }
+
+  @Test
+  public void metricsReporterFactoryShouldReturnDatadogReporter() {
+    when(config.getMetricsReporterType()).thenReturn(MetricsReporterType.DATADOG);
+    when(config.getDatadogApiSite()).thenReturn(DatadogHttpClient.ApiSite.EU);
+    when(config.getDatadogApiKey()).thenReturn("foo");
+    when(config.getDatadogApiKeySkipValidation()).thenReturn(true);
+    when(config.getDatadogMetricPrefix()).thenReturn("bar");
+    when(config.getDatadogMetricHost()).thenReturn("foo");
+    when(config.getDatadogMetricTags()).thenReturn(Arrays.asList("baz", "foo"));
+    MetricsReporter reporter = MetricsReporterFactory.createReporter(config, registry);
+    assertTrue(reporter instanceof DatadogMetricsReporter);
   }
 
   @Test
