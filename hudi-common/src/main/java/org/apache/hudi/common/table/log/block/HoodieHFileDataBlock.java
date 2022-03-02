cf882fb8ca3f279fe2841f6ae99d77e834e8c339
Y Ethan Guo <ethan.guoyihua@gmail.com>
2022-03-02 15:56:27 -0800

    Fix rebase issue

--- hudi-common/src/main/java/org/apache/hudi/common/table/log/block/HoodieHFileDataBlock.java
+++ hudi-common/src/main/java/org/apache/hudi/common/table/log/block/HoodieHFileDataBlock.java
@@ -41,7 +41,6 @@ import org.apache.hadoop.hbase.io.hfile.CacheConfig;
 import org.apache.hadoop.hbase.io.hfile.HFile;
 import org.apache.hadoop.hbase.io.hfile.HFileContext;
 import org.apache.hadoop.hbase.io.hfile.HFileContextBuilder;
-import org.apache.hadoop.hbase.util.Pair;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
