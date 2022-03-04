b3136adef2a4ee6bad5d452b4fd762a3b12c9a59
Alexey Kudinkin <alexey@infinilake.com>
2022-03-04 09:34:25 -0800

    Fixing compilation

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/client/BaseHoodieWriteClient.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/client/BaseHoodieWriteClient.java
@@ -90,7 +90,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
