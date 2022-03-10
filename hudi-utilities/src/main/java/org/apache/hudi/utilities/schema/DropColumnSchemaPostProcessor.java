5542a4582e31032113a7b9083f92f913701e6f28
wangxianghu <wxhjsxz@126.com>
2022-03-10 10:44:13 +0400

    Fix case sensitivity

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/schema/DropColumnSchemaPostProcessor.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/schema/DropColumnSchemaPostProcessor.java
@@ -27,10 +27,12 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.spark.api.java.JavaSparkContext;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * A {@link SchemaPostProcessor} that support to delete column(s) from given schema.
@@ -62,16 +64,17 @@ public class DropColumnSchemaPostProcessor extends SchemaPostProcessor {
       LOG.warn(String.format("Param %s is null or empty, return original schema", Config.DELETE_COLUMN_POST_PROCESSOR_COLUMN_PROP));
     }
 
-    List<String> columnsToDelete = new ArrayList<>(Arrays.asList(columnToDeleteStr.split(",")));
+    // convert field to lowerCase for compare purpose
+    Set<String> columnsToDelete = Arrays.stream(columnToDeleteStr.split(","))
+        .map(filed -> filed.toLowerCase(Locale.ROOT))
+        .collect(Collectors.toSet());
 
     List<Schema.Field> sourceFields = schema.getFields();
     List<Schema.Field> targetFields = new LinkedList<>();
 
     for (Schema.Field sourceField : sourceFields) {
-      if (!columnsToDelete.contains(sourceField.name())) {
+      if (!columnsToDelete.contains(sourceField.name().toLowerCase(Locale.ROOT))) {
         targetFields.add(new Schema.Field(sourceField.name(), sourceField.schema(), sourceField.doc(), sourceField.defaultVal()));
-      } else {
-        columnsToDelete.remove(sourceField.name());
       }
     }
 
@@ -79,9 +82,6 @@ public class DropColumnSchemaPostProcessor extends SchemaPostProcessor {
       throw new HoodieSchemaPostProcessException("Target schema is empty, you can not remove all columns!");
     }
 
-    if (!columnsToDelete.isEmpty()) {
-      LOG.warn("Source schema dos not contain these column(s) : " + columnsToDelete);
-    }
     return Schema.createRecord(schema.getName(), schema.getDoc(), schema.getNamespace(), false, targetFields);
   }
 
