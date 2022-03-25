201d5d06a61173b9250183bcc45c3d244475362f
Rahil Chertara <rchertar@amazon.com>
2022-03-25 09:37:09 -0700

    Get Docker Setup Working

---
+++ docker/hoodie/hadoop/rahil.sh
@@ -0,0 +1,19 @@
+docker build base -t apachehudi/hudi-hadoop_3.1.0-base
+docker build namenode -t apachehudi/hudi-hadoop_3.1.0-namenode
+docker build datanode -t apachehudi/hudi-hadoop_3.1.0-datanode
+docker build historyserver -t apachehudi/hudi-hadoop_3.1.0-history
+
+docker build hive_base -t apachehudi/hudi-hadoop_3.1.0-hive_3.1.2
+
+docker build spark_base -t apachehudi/hudi-hadoop_3.1.0-hive_3.1.2-sparkbase_2.4.4
+docker build sparkmaster -t apachehudi/hudi-hadoop_3.1.0-hive_3.1.2-sparkmaster_2.4.4
+docker build sparkadhoc -t apachehudi/hudi-hadoop_3.1.0-hive_3.1.2-sparkadhoc_2.4.4
+docker build sparkworker -t apachehudi/hudi-hadoop_3.1.0-hive_3.1.2-sparkworker_2.4.4
+
+docker build base_java11 -t apachehudi/hudi-hadoop_3.1.0-base-java11
+
+docker build prestobase -t apachehudi/hudi-hadoop_3.1.0-prestobase_0.271
+
+docker build trinobase -t apachehudi/hudi-hadoop_3.1.0-trinobase_368
+docker build trinocoordinator -t apachehudi/hudi-hadoop_3.1.0-trinocoordinator_368
+docker build trinoworker -t apachehudi/hudi-hadoop_3.1.0-trinoworker_368
