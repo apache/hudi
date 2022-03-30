9801fc0ea98c2f45ddafb79db6e2df24de7fd5f5
liujinhui <965147871@qq.com>
2022-03-30 16:50:31 +0800

    testCI

---
+++ hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/hudi/TestCI.scala
@@ -0,0 +1,12 @@
+package org.apache.hudi
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class TestCI {
+
+  @Test
+  def testException(): Unit = {
+    assertEquals(1, 2)
+  }
+}
