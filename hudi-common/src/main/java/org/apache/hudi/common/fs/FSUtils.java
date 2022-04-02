27e37a5f5f2c60465d7e5536ca38c6f944513409
Alexey Kudinkin <alexey@infinilake.com>
2022-04-02 09:10:56 -0700

    Fixed NPEs

--- hudi-common/src/main/java/org/apache/hudi/common/fs/FSUtils.java
+++ hudi-common/src/main/java/org/apache/hudi/common/fs/FSUtils.java
@@ -28,6 +28,7 @@ import org.apache.hudi.common.table.HoodieTableConfig;
 import org.apache.hudi.common.table.HoodieTableMetaClient;
 import org.apache.hudi.common.table.view.FileSystemViewStorageConfig;
 import org.apache.hudi.common.util.Option;
+import org.apache.hudi.common.util.StringUtils;
 import org.apache.hudi.common.util.collection.ImmutablePair;
 import org.apache.hudi.common.util.collection.Pair;
 import org.apache.hudi.exception.HoodieException;
@@ -585,6 +586,10 @@ public class FSUtils {
   }
 
   public static Path getPartitionPath(String basePath, String partitionPath) {
+    if (StringUtils.isNullOrEmpty(partitionPath)) {
+      return new Path(basePath);
+    }
+
     // NOTE: We have to chop leading "/" to make sure Hadoop does not treat it like
     //       absolute path
     String properPartitionPath = partitionPath.startsWith("/")
@@ -595,7 +600,7 @@ public class FSUtils {
 
   public static Path getPartitionPath(Path basePath, String partitionPath) {
     // FOr non-partitioned table, return only base-path
-    return ((partitionPath == null) || (partitionPath.isEmpty())) ? basePath : new Path(basePath, partitionPath);
+    return StringUtils.isNullOrEmpty(partitionPath) ? basePath : new Path(basePath, partitionPath);
   }
 
   /**
