3a1fa7cc2d653584715718ecf09491f651a7b08e
Alexey Kudinkin <alexey@infinilake.com>
2022-05-06 18:06:04 -0700

    Abstracted away nested-field path access, and Spark's `Row` conversion w/in corresponding `SparkRowAccessor`, `SparkRowConverter` w/in `BuiltinKeyGenerator`

--- hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/keygen/BuiltinKeyGenerator.java
+++ hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/keygen/BuiltinKeyGenerator.java
@@ -24,19 +24,19 @@ import org.apache.hudi.HoodieSparkUtils;
 import org.apache.hudi.client.utils.SparkRowSerDe;
 import org.apache.hudi.PublicAPIMethod;
 import org.apache.hudi.common.config.TypedProperties;
+import org.apache.spark.sql.HoodieUnsafeRowUtils;
+import org.apache.spark.sql.HoodieUnsafeRowUtils$;
 import org.apache.hudi.common.util.collection.Pair;
 import org.apache.hudi.exception.HoodieException;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.catalyst.InternalRow;
-import org.apache.spark.sql.types.DataType;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.unsafe.types.UTF8String;
 import scala.Function1;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.annotation.concurrent.ThreadSafe;
+
+import static org.apache.hudi.common.util.TypeUtils.unsafeCast;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -49,17 +49,11 @@ import java.util.concurrent.atomic.AtomicBoolean;
  *
  * TODO rename to AvroFallbackBaseKeyGenerator
  */
+@ThreadSafe
 public abstract class BuiltinKeyGenerator extends BaseKeyGenerator implements SparkKeyGeneratorInterface {
 
-  private static final String STRUCT_NAME = "hoodieRowTopLevelField";
-  private static final String NAMESPACE = "hoodieRow";
-
-  private transient Function1<Row, GenericRecord> converterFn = null;
-  private final AtomicBoolean validatePartitionFields = new AtomicBoolean(false);
-  protected transient StructType structType;
-
-  protected Map<String, Pair<List<Integer>, DataType>> recordKeySchemaInfo = new HashMap<>();
-  protected Map<String, Pair<List<Integer>, DataType>> partitionPathSchemaInfo = new HashMap<>();
+  protected transient volatile SparkRowConverter rowConverter;
+  protected transient volatile SparkRowAccessor rowAccessor;
 
   protected BuiltinKeyGenerator(TypedProperties config) {
     super(config);
@@ -67,75 +61,127 @@ public abstract class BuiltinKeyGenerator extends BaseKeyGenerator implements Sp
 
   @Override
   public String getRecordKey(Row row) {
-    if (null == converterFn) {
-      converterFn = AvroConversionUtils.createConverterToAvro(row.schema(), STRUCT_NAME, NAMESPACE);
-    }
-    return getKey(converterFn.apply(row)).getRecordKey();
+    tryInitRowConverter(row.schema());
+    // NOTE: This implementation has considerable computational overhead and has to be overridden
+    //       to provide for optimal performance on Spark. This implementation provided exclusively
+    //       for compatibility reasons.
+    return getRecordKey(rowConverter.convertToAvro(row));
   }
 
   @Override
   public UTF8String getRecordKey(InternalRow internalRow, StructType schema) {
-    try {
-      // TODO fix
-      buildFieldSchemaInfoIfNeeded(schema);
-      return UTF8String.fromString(RowKeyGeneratorHelper.getRecordKeyFromInternalRow(internalRow, getRecordKeyFields(), recordKeySchemaInfo, false));
-    } catch (Exception e) {
-      throw new HoodieException("Conversion of InternalRow to Row failed with exception", e);
-    }
+    tryInitRowConverter(schema);
+    // NOTE: This implementation has considerable computational overhead and has to be overridden
+    //       to provide for optimal performance on Spark. This implementation provided exclusively
+    //       for compatibility reasons.
+    return UTF8String.fromString(getRecordKey(rowConverter.convertToAvro(internalRow)));
   }
 
   @Override
   public String getPartitionPath(Row row) {
-    if (null == converterFn) {
-      converterFn = AvroConversionUtils.createConverterToAvro(row.schema(), STRUCT_NAME, NAMESPACE);
-    }
-    return getKey(converterFn.apply(row)).getPartitionPath();
+    tryInitRowConverter(row.schema());
+    // NOTE: This implementation has considerable computational overhead and has to be overridden
+    //       to provide for optimal performance on Spark. This implementation provided exclusively
+    //       for compatibility reasons.
+    return getPartitionPath(rowConverter.convertToAvro(row));
   }
 
-  /**
-   * Fetch partition path from {@link InternalRow}.
-   *
-   * @param internalRow {@link InternalRow} instance from which partition path needs to be fetched from.
-   * @param structType  schema of the internalRow.
-   * @return the partition path.
-   */
   @Override
-  public UTF8String getPartitionPath(InternalRow internalRow, StructType structType) {
-    try {
-      buildFieldSchemaInfoIfNeeded(structType);
-      return UTF8String.fromString(RowKeyGeneratorHelper.getPartitionPathFromInternalRow(internalRow, getPartitionPathFields(), hiveStylePartitioning, partitionPathSchemaInfo));
-    } catch (Exception e) {
-      throw new HoodieException("Conversion of InternalRow to Row failed with exception", e);
-    }
+  public UTF8String getPartitionPath(InternalRow internalRow, StructType schema) {
+    tryInitRowConverter(schema);
+    // NOTE: This implementation has considerable computational overhead and has to be overridden
+    //       to provide for optimal performance on Spark. This implementation provided exclusively
+    //       for compatibility reasons.
+    GenericRecord avroRecord = rowConverter.convertToAvro(internalRow);
+    return UTF8String.fromString(getPartitionPath(avroRecord));
   }
 
-  void buildFieldSchemaInfoIfNeeded(StructType structType) {
-    if (this.structType == null) {
-      this.structType = structType;
-      getRecordKeyFields()
-          .stream().filter(f -> !f.isEmpty())
-          .forEach(f -> recordKeySchemaInfo.put(f, RowKeyGeneratorHelper.getFieldSchemaInfo(structType, f, true)));
-      if (getPartitionPathFields() != null) {
-        getPartitionPathFields().stream().filter(f -> !f.isEmpty())
-            .forEach(f -> partitionPathSchemaInfo.put(f, RowKeyGeneratorHelper.getFieldSchemaInfo(structType, f, false)));
+  private void tryInitRowConverter(StructType structType) {
+    if (rowConverter == null) {
+      synchronized (this) {
+        if (rowConverter == null) {
+          rowConverter = new SparkRowConverter(structType);
+        }
       }
     }
   }
 
-  protected String getPartitionPathInternal(InternalRow row, StructType structType) {
-    buildFieldSchemaInfoIfNeeded(structType);
-    validatePartitionFieldsForInternalRow();
-    return RowKeyGeneratorHelper.getPartitionPathFromInternalRow(row, getPartitionPathFields(),
-        hiveStylePartitioning, partitionPathSchemaInfo);
+  void tryInitRowAccessor(StructType schema) {
+    if (this.rowAccessor == null) {
+      synchronized (this) {
+        if (this.rowAccessor == null) {
+          this.rowAccessor = new SparkRowAccessor(schema);
+        }
+      }
+    }
   }
 
-  protected void validatePartitionFieldsForInternalRow() {
-    if (!validatePartitionFields.getAndSet(true)) {
-      partitionPathSchemaInfo.values().forEach(entry -> {
-        if (entry.getKey().size() > 1) {
-          throw new IllegalArgumentException("Nested column for partitioning is not supported with disabling meta columns");
-        }
-      });
+  protected static class SparkRowConverter {
+    private static final String STRUCT_NAME = "hoodieRowTopLevelField";
+    private static final String NAMESPACE = "hoodieRow";
+
+    private final Function1<Row, GenericRecord> avroConverter;
+    private final SparkRowSerDe rowSerDe;
+
+    SparkRowConverter(StructType schema) {
+      this.rowSerDe = HoodieSparkUtils.getDeserializer(schema);
+      this.avroConverter = AvroConversionUtils.createConverterToAvro(schema, STRUCT_NAME, NAMESPACE);
+    }
+
+    GenericRecord convertToAvro(Row row) {
+      return avroConverter.apply(row);
+    }
+
+    GenericRecord convertToAvro(InternalRow row) {
+      return avroConverter.apply(rowSerDe.deserializeRow(row));
+    }
+  }
+
+  protected class SparkRowAccessor {
+    private final HoodieUnsafeRowUtils.NestedFieldPath[] recordKeyFieldsPaths;
+    private final HoodieUnsafeRowUtils.NestedFieldPath[] partitionPathFieldsPaths;
+
+    SparkRowAccessor(StructType schema) {
+      this.recordKeyFieldsPaths = unsafeCast(
+          getRecordKeyFields().stream()
+              .map(recordKeyField -> HoodieUnsafeRowUtils$.MODULE$.composeNestedFieldPath(schema, recordKeyField))
+              .toArray());
+      this.partitionPathFieldsPaths = unsafeCast(
+          getPartitionPathFields().stream()
+              .map(recordKeyField -> HoodieUnsafeRowUtils$.MODULE$.composeNestedFieldPath(schema, recordKeyField))
+              .toArray());
+    }
+
+    public Object[] getRecordKeys(Row row) {
+      return getNestedFieldValues(row, recordKeyFieldsPaths);
+    }
+
+    public Object[] getRecordPartitionPathValues(Row row) {
+      return getNestedFieldValues(row, partitionPathFieldsPaths);
+    }
+
+    public Object[] getRecordKeys(InternalRow row) {
+      return getNestedFieldValues(row, recordKeyFieldsPaths);
+    }
+
+    public Object[] getRecordPartitionPathValues(InternalRow row) {
+      return getNestedFieldValues(row, partitionPathFieldsPaths);
+    }
+
+    private Object[] getNestedFieldValues(Row row, HoodieUnsafeRowUtils.NestedFieldPath[] nestedFieldsPaths) {
+      Object[] nestedFieldValues = new Object[nestedFieldsPaths.length];
+      for (int i = 0; i < nestedFieldsPaths.length; ++i) {
+        nestedFieldValues[i] = HoodieUnsafeRowUtils$.MODULE$.getNestedRowValue(row, nestedFieldsPaths[i]);
+      }
+      return nestedFieldValues;
+    }
+
+    private Object[] getNestedFieldValues(InternalRow row, HoodieUnsafeRowUtils.NestedFieldPath[] nestedFieldsPaths) {
+      Object[] nestedFieldValues = new Object[nestedFieldsPaths.length];
+      for (int i = 0; i < nestedFieldsPaths.length; ++i) {
+        nestedFieldValues[i] = HoodieUnsafeRowUtils$.MODULE$.getNestedInternalRowValue(row, nestedFieldsPaths[i]);
+      }
+      return nestedFieldValues;
     }
   }
 }
