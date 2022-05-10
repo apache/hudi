1790651bdfb8e5345065eb65fb46a10767cd6fbd
Y Ethan Guo <ethan.guoyihua@gmail.com>
2022-05-09 20:14:49 -0700

    Change docker configs for CI

--- docker/hoodie/hadoop/rahil.sh
+++ docker/hoodie/hadoop/rahil.sh
@@ -1,19 +1,19 @@
-docker build base -t apachehudi/hudi-hadoop_3.1.0-base
-docker build namenode -t apachehudi/hudi-hadoop_3.1.0-namenode
-docker build datanode -t apachehudi/hudi-hadoop_3.1.0-datanode
-docker build historyserver -t apachehudi/hudi-hadoop_3.1.0-history
+docker build base -t yihua/hudi-hadoop_3.1.0-base
+docker build namenode -t yihua/hudi-hadoop_3.1.0-namenode
+docker build datanode -t yihua/hudi-hadoop_3.1.0-datanode
+docker build historyserver -t yihua/hudi-hadoop_3.1.0-history
 
-docker build hive_base -t apachehudi/hudi-hadoop_3.1.0-hive_3.1.2
+docker build hive_base -t yihua/hudi-hadoop_3.1.0-hive_3.1.2
 
-docker build spark_base -t apachehudi/hudi-hadoop_3.1.0-hive_3.1.2-sparkbase_3.2.1
-docker build sparkmaster -t apachehudi/hudi-hadoop_3.1.0-hive_3.1.2-sparkmaster_3.2.1
-docker build sparkadhoc -t apachehudi/hudi-hadoop_3.1.0-hive_3.1.2-sparkadhoc_3.2.1
-docker build sparkworker -t apachehudi/hudi-hadoop_3.1.0-hive_3.1.2-sparkworker_3.2.1
+docker build spark_base -t yihua/hudi-hadoop_3.1.0-hive_3.1.2-sparkbase_3.2.1
+docker build sparkmaster -t yihua/hudi-hadoop_3.1.0-hive_3.1.2-sparkmaster_3.2.1
+docker build sparkadhoc -t yihua/hudi-hadoop_3.1.0-hive_3.1.2-sparkadhoc_3.2.1
+docker build sparkworker -t yihua/hudi-hadoop_3.1.0-hive_3.1.2-sparkworker_3.2.1
 
 
-docker build prestobase -t apachehudi/hudi-hadoop_3.1.0-prestobase_0.271
+docker build prestobase -t yihua/hudi-hadoop_3.1.0-prestobase_0.271
 
-docker build base_java11 -t apachehudi/hudi-hadoop_3.1.0-base-java11
-docker build trinobase -t apachehudi/hudi-hadoop_3.1.0-trinobase_368
-docker build trinocoordinator -t apachehudi/hudi-hadoop_3.1.0-trinocoordinator_368
-docker build trinoworker -t apachehudi/hudi-hadoop_3.1.0-trinoworker_368
+docker build base_java11 -t yihua/hudi-hadoop_3.1.0-base-java11
+docker build trinobase -t yihua/hudi-hadoop_3.1.0-trinobase_368
+docker build trinocoordinator -t yihua/hudi-hadoop_3.1.0-trinocoordinator_368
+docker build trinoworker -t yihua/hudi-hadoop_3.1.0-trinoworker_368
