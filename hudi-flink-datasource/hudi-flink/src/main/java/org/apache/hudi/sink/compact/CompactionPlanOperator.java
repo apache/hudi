54ed452c939e7722d0b9b9e1aa7a9c1b1f2e1e09
yuzhao.cyz <yuzhao.cyz@gmail.com>
2022-05-18 15:15:23 +0800

    Revert "[HUDI-3870] Add timeout rollback for flink online compaction (#5314)"
    This reverts commit 6f9b02decb5bb2b83709b1b6ec04a97e4d102c11.

--- hudi-flink-datasource/hudi-flink/src/main/java/org/apache/hudi/sink/compact/CompactionPlanOperator.java
+++ hudi-flink-datasource/hudi-flink/src/main/java/org/apache/hudi/sink/compact/CompactionPlanOperator.java
@@ -89,7 +89,8 @@ public class CompactionPlanOperator extends AbstractStreamOperator<CompactionPla
       // when the earliest inflight instant has timed out, assumes it has failed
       // already and just rolls it back.
 
-      CompactionUtil.rollbackEarliestCompaction(table, conf);
+      // comment out: do we really need the timeout rollback ?
+      // CompactionUtil.rollbackEarliestCompaction(table, conf);
       scheduleCompaction(table, checkpointId);
     } catch (Throwable throwable) {
       // make it fail-safe
@@ -99,19 +100,11 @@ public class CompactionPlanOperator extends AbstractStreamOperator<CompactionPla
 
   private void scheduleCompaction(HoodieFlinkTable<?> table, long checkpointId) throws IOException {
     // the first instant takes the highest priority.
-    HoodieTimeline pendingCompactionTimeline = table.getActiveTimeline().filterPendingCompactionTimeline();
-    Option<HoodieInstant> firstRequested = pendingCompactionTimeline
+    Option<HoodieInstant> firstRequested = table.getActiveTimeline().filterPendingCompactionTimeline()
         .filter(instant -> instant.getState() == HoodieInstant.State.REQUESTED).firstInstant();
     if (!firstRequested.isPresent()) {
       // do nothing.
       LOG.info("No compaction plan for checkpoint " + checkpointId);
-      return;
-    }
-
-    Option<HoodieInstant> firstInflight = pendingCompactionTimeline
-        .filter(instant -> instant.getState() == HoodieInstant.State.INFLIGHT).firstInstant();
-    if (firstInflight.isPresent()) {
-      LOG.warn("Waiting for pending compaction instant : " + firstInflight + " to complete, skip scheduling new compaction plans");
       return;
     }
 
