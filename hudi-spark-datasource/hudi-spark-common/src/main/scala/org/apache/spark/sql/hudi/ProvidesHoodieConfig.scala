f965bb834567d0ee976d15df6511a505bf9eed5c
Rex An <bonean131@gmail.com>
2022-05-25 14:44:39 +0800

    Remove unnecessary changes

--- hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/spark/sql/hudi/ProvidesHoodieConfig.scala
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/spark/sql/hudi/ProvidesHoodieConfig.scala
@@ -40,6 +40,7 @@ import org.apache.spark.sql.types.StructType
 
 import java.util
 import java.util.Locale
+
 import scala.collection.JavaConverters._
 
 trait ProvidesHoodieConfig extends Logging {
