3ffde1c6c3241d61fdaeee46388d4753687b7d51
nicolas paris <nicolas.paris@riseup.net>
2022-06-01 18:28:47 +0200

    Fix partition order aws glue sync

--- hudi-aws/src/main/java/org/apache/hudi/aws/sync/AWSGlueCatalogSyncClient.java
+++ hudi-aws/src/main/java/org/apache/hudi/aws/sync/AWSGlueCatalogSyncClient.java
@@ -271,18 +271,28 @@ public class AWSGlueCatalogSyncClient extends AbstractHiveSyncHoodieClient {
     try {
       Map<String, String> mapSchema = parquetSchemaToMapSchema(storageSchema, syncConfig.supportTimestamp, false);
 
-      List<Column> schemaPartitionKeys = new ArrayList<>();
       List<Column> schemaWithoutPartitionKeys = new ArrayList<>();
       for (String key : mapSchema.keySet()) {
         String keyType = getPartitionKeyType(mapSchema, key);
         Column column = new Column().withName(key).withType(keyType.toLowerCase()).withComment("");
         // In Glue, the full schema should exclude the partition keys
-        if (syncConfig.partitionFields.contains(key)) {
-          schemaPartitionKeys.add(column);
-        } else {
+        if (!syncConfig.partitionFields.contains(key)) {
           schemaWithoutPartitionKeys.add(column);
         }
       }
+
+      // now create the schema partition
+      List<Column> schemaPartitionKeys =
+          syncConfig.partitionFields.stream()
+              .map(
+                  partitionKey -> {
+                    String keyType = getPartitionKeyType(mapSchema, partitionKey);
+		    return new Column()
+                            .withName(partitionKey)
+                            .withType(keyType.toLowerCase())
+                            .withComment("");
+                  })
+              .collect(Collectors.toList());
 
       StorageDescriptor storageDescriptor = new StorageDescriptor();
       serdeProperties.put("serialization.format", "1");
