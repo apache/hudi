de1cd7d3aca3ad9b70dbf03790d270866e6de052
sivabalan <n.siva.b@gmail.com>
2022-06-06 10:25:42 -0400

    addressing comments

--- hudi-hadoop-mr/src/main/java/org/apache/hudi/hadoop/realtime/RealtimeSplit.java
+++ hudi-hadoop-mr/src/main/java/org/apache/hudi/hadoop/realtime/RealtimeSplit.java
@@ -107,13 +107,10 @@ public interface RealtimeSplit extends InputSplitWithLocationInfo {
     } else {
       InputSplitUtils.writeBoolean(true, out);
       InputSplitUtils.writeString(virtualKeyInfoOpt.get().getRecordKeyField(), out);
+      InputSplitUtils.writeString(String.valueOf(virtualKeyInfoOpt.get().getRecordKeyFieldIndex()), out);
       InputSplitUtils.writeBoolean(virtualKeyInfoOpt.get().getPartitionPathField().isPresent(), out);
       if (virtualKeyInfoOpt.get().getPartitionPathField().isPresent()) {
         InputSplitUtils.writeString(virtualKeyInfoOpt.get().getPartitionPathField().get(), out);
-      }
-      InputSplitUtils.writeString(String.valueOf(virtualKeyInfoOpt.get().getRecordKeyFieldIndex()), out);
-      // if partition path field exists, partition path field index should also exists. So, don't need another boolean
-      if (virtualKeyInfoOpt.get().getPartitionPathFieldIndex().isPresent()) {
         InputSplitUtils.writeString(String.valueOf(virtualKeyInfoOpt.get().getPartitionPathFieldIndex()), out);
       }
     }
@@ -136,9 +133,9 @@ public interface RealtimeSplit extends InputSplitWithLocationInfo {
     boolean hoodieVirtualKeyPresent = InputSplitUtils.readBoolean(in);
     if (hoodieVirtualKeyPresent) {
       String recordKeyField = InputSplitUtils.readString(in);
+      int recordFieldIndex = Integer.parseInt(InputSplitUtils.readString(in));
       boolean isPartitionPathFieldPresent = InputSplitUtils.readBoolean(in);
       Option<String> partitionPathField = isPartitionPathFieldPresent ? Option.of(InputSplitUtils.readString(in)) : Option.empty();
-      int recordFieldIndex = Integer.parseInt(InputSplitUtils.readString(in));
       Option<Integer> partitionPathIndex = isPartitionPathFieldPresent ? Option.of(Integer.parseInt(InputSplitUtils.readString(in))) : Option.empty();
       setVirtualKeyInfo(Option.of(new HoodieVirtualKeyInfo(recordKeyField, partitionPathField, recordFieldIndex, partitionPathIndex)));
     }
