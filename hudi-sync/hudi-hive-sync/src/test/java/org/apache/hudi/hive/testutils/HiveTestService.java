655fb1b1db38d2b0c3dee3e21507e6c237f3a1ca
Rahil Chertara <rchertar@amazon.com>
2022-06-07 23:01:34 -0400

    increase wait time in hivetestservice

--- hudi-sync/hudi-hive-sync/src/test/java/org/apache/hudi/hive/testutils/HiveTestService.java
+++ hudi-sync/hudi-hive-sync/src/test/java/org/apache/hudi/hive/testutils/HiveTestService.java
@@ -214,7 +214,7 @@ public class HiveTestService {
     while (true) {
       try {
         new HiveMetaStoreClient(serverConf);
-        Thread.sleep(12000);
+        Thread.sleep(30000);
         return true;
       } catch (MetaException | InterruptedException e) {
         // ignore as this is expected
