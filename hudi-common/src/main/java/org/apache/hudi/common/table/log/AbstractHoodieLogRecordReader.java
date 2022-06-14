apachehudi-ci/hudi-branch-ci

pass read schema

hudi-common/src/main/java/org/apache/hudi/common/table/log/AbstractHoodieLogRecordReader.java
@@ -385,10 +385,21 @@ public abstract class AbstractHoodieLogRecordReader {
     Map<String, Object> mapperConfig = MapperUtils.buildMapperConfig(this.payloadClassFQN, this.preCombineField, this.simpleKeyGenFields, this.withOperationField, this.partitionName);
     try (ClosableIterator<HoodieRecord> recordIterator = getRecordsIterator(dataBlock, keySpecOpt, recordType, mapperConfig)) {
       Option<Schema> schemaOption = getMergedSchema(dataBlock);
+      Schema finalReadSchema;
+      if (recordIterator instanceof MappingIterator) {
+        Schema schema = ((MappingIterator) recordIterator).getSchema();
+        if (schema != null) {
+          finalReadSchema = schema;
+        } else {
+          finalReadSchema = dataBlock.getSchema();
+        }
+      } else {
+        finalReadSchema = dataBlock.getSchema();
+      }
       while (recordIterator.hasNext()) {
         HoodieRecord<?> currentRecord = recordIterator.next();
         HoodieRecord<?> record = schemaOption.isPresent()
-            ? currentRecord.rewriteRecordWithNewSchema(dataBlock.getSchema(), new Properties(), schemaOption.get(), new HashMap<>()) : currentRecord;
+            ? currentRecord.rewriteRecordWithNewSchema(finalReadSchema, new Properties(), schemaOption.get(), new HashMap<>()) : currentRecord;
         processNextRecord(record);
         totalLogRecords.incrementAndGet();
       }

apache/hudi

hudi-common/src/main/java/org/apache/hudi/common/table/log/AbstractHoodieLogRecordReader.java
@@ -385,10 +385,11 @@ public abstract class AbstractHoodieLogRecordReader {
     Map<String, Object> mapperConfig = MapperUtils.buildMapperConfig(this.payloadClassFQN, this.preCombineField, this.simpleKeyGenFields, this.withOperationField, this.partitionName);
     try (ClosableIterator<HoodieRecord> recordIterator = getRecordsIterator(dataBlock, keySpecOpt, recordType, mapperConfig)) {
       Option<Schema> schemaOption = getMergedSchema(dataBlock);
+      Schema finalReadSchema = ((MappingIterator) recordIterator).getSchema();
       while (recordIterator.hasNext()) {
         HoodieRecord<?> currentRecord = recordIterator.next();
         HoodieRecord<?> record = schemaOption.isPresent()
-            ? currentRecord.rewriteRecordWithNewSchema(dataBlock.getSchema(), new Properties(), schemaOption.get(), new HashMap<>()) : currentRecord;
+            ? currentRecord.rewriteRecordWithNewSchema(finalReadSchema, new Properties(), schemaOption.get(), new HashMap<>()) : currentRecord;
         processNextRecord(record);
         totalLogRecords.incrementAndGet();
       }
@@ -473,6 +474,18 @@ public abstract class AbstractHoodieLogRecordReader {
       iter = unsafeCast(dataBlock.getRecordIterator(type));
     }
 
+    Schema finalReadSchema;
+    if (iter instanceof MappingIterator) {
+      Schema schema = ((MappingIterator) iter).getSchema();
+      if (schema != null) {
+        finalReadSchema = schema;
+      } else {
+        finalReadSchema = dataBlock.getSchema();
+      }
+    } else {
+      finalReadSchema = dataBlock.getSchema();
+    }
+
     return new MappingIterator<>(iter, rec -> {
       try {
         return rec.expansion(readerSchema, new Properties(), mapperConfig);
@@ -480,7 +493,7 @@ public abstract class AbstractHoodieLogRecordReader {
         LOG.error("Error expanse " + rec, e);
         throw new HoodieException(e);
       }
-    });
+    }, finalReadSchema);
   }
 
   /**