571eb4ac7d26647a7c009b61bfa1169450c6bb0b
Rahil Chertara <rchertar@amazon.com>
2022-06-14 20:50:53 -0400

    Disabled UtilitiesTestBase due to jetty conflict

--- hudi-utilities/src/test/java/org/apache/hudi/utilities/testutils/UtilitiesTestBase.java
+++ hudi-utilities/src/test/java/org/apache/hudi/utilities/testutils/UtilitiesTestBase.java
@@ -76,6 +76,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import scala.Tuple2;
 
 import java.io.BufferedReader;
@@ -92,6 +93,7 @@ import java.util.List;
  * Abstract test that provides a dfs & spark contexts.
  *
  */
+@Disabled
 public class UtilitiesTestBase {
 
   protected static String dfsBasePath;
