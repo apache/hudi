5a6f50a0dccd2cfe47877874631c17ecc90c151f
Alexey Kudinkin <alexey@infinilake.com>
2022-07-15 18:50:25 -0700

    Fixing invalid cast

--- hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/keygen/BuiltinKeyGenerator.java
+++ hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/keygen/BuiltinKeyGenerator.java
@@ -24,18 +24,17 @@ import org.apache.hudi.HoodieSparkUtils;
 import org.apache.hudi.client.utils.SparkRowSerDe;
 import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.exception.HoodieKeyException;
+import org.apache.hudi.unsafe.UTF8StringBuilder;
 import org.apache.spark.sql.HoodieUnsafeRowUtils;
 import org.apache.spark.sql.HoodieUnsafeRowUtils$;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.types.StructType;
-import org.apache.hudi.unsafe.UTF8StringBuilder;
 import org.apache.spark.unsafe.types.UTF8String;
 import scala.Function1;
 
 import javax.annotation.concurrent.ThreadSafe;
 
-import static org.apache.hudi.common.util.TypeUtils.unsafeCast;
 import static org.apache.hudi.common.util.ValidationUtils.checkState;
 import static org.apache.hudi.keygen.KeyGenUtils.DEFAULT_PARTITION_PATH_SEPARATOR;
 import static org.apache.hudi.keygen.KeyGenUtils.DEFAULT_RECORD_KEY_PARTS_SEPARATOR;
@@ -293,14 +292,14 @@ public abstract class BuiltinKeyGenerator extends BaseKeyGenerator implements Sp
     private final HoodieUnsafeRowUtils.NestedFieldPath[] partitionPathFieldsPaths;
 
     SparkRowAccessor(StructType schema) {
-      this.recordKeyFieldsPaths = unsafeCast(
+      this.recordKeyFieldsPaths =
           getRecordKeyFields().stream()
               .map(recordKeyField -> HoodieUnsafeRowUtils$.MODULE$.composeNestedFieldPath(schema, recordKeyField))
-              .toArray());
-      this.partitionPathFieldsPaths = unsafeCast(
+              .toArray(HoodieUnsafeRowUtils.NestedFieldPath[]::new);
+      this.partitionPathFieldsPaths =
           getPartitionPathFields().stream()
               .map(recordKeyField -> HoodieUnsafeRowUtils$.MODULE$.composeNestedFieldPath(schema, recordKeyField))
-              .toArray());
+              .toArray(HoodieUnsafeRowUtils.NestedFieldPath[]::new);
     }
 
     public Object[] getRecordKeyParts(Row row) {
