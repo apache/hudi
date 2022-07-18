dc1c556ee92455bbada74b0851a8844bfd4b6c9a
Alexey Kudinkin <alexey@infinilake.com>
2022-07-18 14:30:25 -0700

    Fixing raw values deserialization when extracted from `InternalRow`s; Relocated `convertToLogicalType` to `BuiltinKeyGenerator`

--- hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/keygen/BuiltinKeyGenerator.java
+++ hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/keygen/BuiltinKeyGenerator.java
@@ -30,12 +30,20 @@ import org.apache.spark.sql.HoodieUnsafeRowUtils;
 import org.apache.spark.sql.HoodieUnsafeRowUtils$;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.catalyst.InternalRow;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.DateType;
 import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.types.TimestampType;
 import org.apache.spark.unsafe.types.UTF8String;
 import scala.Function1;
 
 import javax.annotation.concurrent.ThreadSafe;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
+
+import static org.apache.hudi.common.util.CollectionUtils.tail;
 import static org.apache.hudi.common.util.ValidationUtils.checkState;
 import static org.apache.hudi.keygen.KeyGenUtils.DEFAULT_PARTITION_PATH_SEPARATOR;
 import static org.apache.hudi.keygen.KeyGenUtils.DEFAULT_RECORD_KEY_PARTS_SEPARATOR;
@@ -343,10 +351,42 @@ public abstract class BuiltinKeyGenerator extends BaseKeyGenerator implements Sp
     private Object[] getNestedFieldValues(InternalRow row, HoodieUnsafeRowUtils.NestedFieldPath[] nestedFieldsPaths) {
       Object[] nestedFieldValues = new Object[nestedFieldsPaths.length];
       for (int i = 0; i < nestedFieldsPaths.length; ++i) {
-        nestedFieldValues[i] = HoodieUnsafeRowUtils$.MODULE$.getNestedInternalRowValue(row, nestedFieldsPaths[i]);
+        Object rawValue = HoodieUnsafeRowUtils$.MODULE$.getNestedInternalRowValue(row, nestedFieldsPaths[i]);
+        DataType dataType = tail(nestedFieldsPaths[i].parts())._2.dataType();
+
+        nestedFieldValues[i] = convertToLogicalDataType(dataType, rawValue);
       }
+
       return nestedFieldValues;
     }
+  }
+
+  /**
+   * Converts provided (raw) value extracted from the {@link InternalRow} object into a deserialized,
+   * JVM native format (for ex, converting {@code Long} into {@link Instant},
+   * {@code Integer} to {@link LocalDate}, etc)
+   *
+   * This method allows to avoid costly full-row deserialization sequence. Note, that this method
+   * should be maintained in sync w/
+   *
+   * <ol>
+   *   <li>{@code RowEncoder#deserializerFor}, as well as</li>
+   *   <li>{@code HoodieAvroUtils#convertValueForAvroLogicalTypes}</li>
+   * </ol>
+   *
+   * @param dataType target data-type of the given value
+   * @param value target value to be converted
+   */
+  private static Object convertToLogicalDataType(DataType dataType, Object value) {
+    if (dataType instanceof TimestampType) {
+      // Provided value have to be [[Long]] in this case, representing micros since epoch
+      return new Timestamp((Long) value / 1000);
+    } else if (dataType instanceof DateType) {
+      // Provided value have to be [[Int]] in this case
+      return LocalDate.ofEpochDay((Integer) value);
+    }
+
+    return value;
   }
 }
 
