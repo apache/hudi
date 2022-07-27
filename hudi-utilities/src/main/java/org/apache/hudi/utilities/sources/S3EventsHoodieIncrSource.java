7de8bd084f95b56329820c9f6df85987a3144455
Vamshi Gudavarthi <vamshi@onehouse.ai>
2022-07-27 13:57:18 -0700

    Fix mistaken edit

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/sources/S3EventsHoodieIncrSource.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/sources/S3EventsHoodieIncrSource.java
@@ -20,7 +20,6 @@ package org.apache.hudi.utilities.sources;
 
 import org.apache.hudi.DataSourceReadOptions;
 import org.apache.hudi.DataSourceUtils;
-import org.apache.hudi.common.config.SerializableConfiguration;
 import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.common.fs.FSUtils;
 import org.apache.hudi.common.model.HoodieRecord;
@@ -33,7 +32,6 @@ import org.apache.hudi.utilities.sources.helpers.IncrSourceHelper;
 
 import com.esotericsoftware.minlog.Log;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.log4j.LogManager;
@@ -45,13 +43,10 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static org.apache.hudi.utilities.sources.HoodieIncrSource.Config.DEFAULT_NUM_INSTANTS_PER_FETCH;
 import static org.apache.hudi.utilities.sources.HoodieIncrSource.Config.DEFAULT_READ_LATEST_INSTANT_ON_MISSING_CKPT;
@@ -156,47 +151,37 @@ public class S3EventsHoodieIncrSource extends HoodieIncrSource {
     String s3FS = props.getString(Config.S3_FS_PREFIX, "s3").toLowerCase();
     String s3Prefix = s3FS + "://";
 
-    // Create S3 paths
-    final boolean checkExists = props.getBoolean(Config.ENABLE_EXISTS_CHECK, Config.DEFAULT_ENABLE_EXISTS_CHECK);
-    SerializableConfiguration serializableConfiguration = new SerializableConfiguration(sparkContext.hadoopConfiguration());
-    List<String> cloudFiles = source
+    // Extract distinct file keys from s3 meta hoodie table
+    final List<Row> cloudMetaDf = source
         .filter(filter)
         .select("s3.bucket.name", "s3.object.key")
         .distinct()
-        .rdd().toJavaRDD().mapPartitions(fileListIterator -> {
-          List<String> cloudFilesPerPartition = new ArrayList<>();
-          fileListIterator.forEachRemaining(row -> {
-            final Configuration configuration = serializableConfiguration.newCopy();
-            String bucket = row.getString(0);
-            String filePath = s3Prefix + bucket + "/" + row.getString(1);
-            try {
-              String decodeUrl = URLDecoder.decode(filePath, StandardCharsets.UTF_8.name());
-              if (checkExists) {
-                FileSystem fs = FSUtils.getFs(s3Prefix + bucket, configuration);
-                try {
-                  if (fs.exists(new Path(decodeUrl))) {
-                    cloudFilesPerPartition.add(decodeUrl);
-                  }
-                } catch (IOException e) {
-                  LOG.error(String.format("Error while checking path exists for %s ", decodeUrl), e);
-                }
-              } else {
-                cloudFilesPerPartition.add(decodeUrl);
-              }
-            } catch (Exception exception) {
-              LOG.warn("Failed to add cloud file ", exception);
-            }
-          });
-          return cloudFilesPerPartition.iterator();
-        }).collect();
-
+        .collectAsList();
+    // Create S3 paths
+    final boolean checkExists = props.getBoolean(Config.ENABLE_EXISTS_CHECK, Config.DEFAULT_ENABLE_EXISTS_CHECK);
+    List<String> cloudFiles = new ArrayList<>();
+    for (Row row : cloudMetaDf) {
+      // construct file path, row index 0 refers to bucket and 1 refers to key
+      String bucket = row.getString(0);
+      String filePath = s3Prefix + bucket + "/" + row.getString(1);
+      if (checkExists) {
+        FileSystem fs = FSUtils.getFs(s3Prefix + bucket, sparkSession.sparkContext().hadoopConfiguration());
+        try {
+          if (fs.exists(new Path(filePath))) {
+            cloudFiles.add(filePath);
+          }
+        } catch (IOException e) {
+          LOG.error(String.format("Error while checking path exists for %s ", filePath), e);
+        }
+      } else {
+        cloudFiles.add(filePath);
+      }
+    }
     Option<Dataset<Row>> dataset = Option.empty();
     if (!cloudFiles.isEmpty()) {
       DataFrameReader dataFrameReader = getDataFrameReader(fileFormat);
       dataset = Option.of(dataFrameReader.load(cloudFiles.toArray(new String[0])));
     }
-    LOG.debug("Extracted distinct files " + cloudFiles.size()
-        + " and some samples " + cloudFiles.stream().limit(10).collect(Collectors.toList()));
     return Pair.of(dataset, queryTypeAndInstantEndpts.getRight().getRight());
   }
 
