d75dc750ed43af3d4b80d124d4e46dc1b9cbbc91
Pramod Biligiri <pramodbiligiri@gmail.com>
2022-08-11 15:06:53 +0530

    a working version which is closer to the Google example and does not use google-api-services-pubsub - https://cloud.google.com/pubsub/docs/pull#synchronous_pull

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/sources/GcsEventsSource.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/sources/GcsEventsSource.java
@@ -1,16 +1,27 @@
 package org.apache.hudi.utilities.sources;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
-import com.google.api.client.googleapis.util.Utils;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
+//import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+//import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+//import com.google.api.client.googleapis.util.Utils;
+//import com.google.api.client.http.HttpTransport;
+//import com.google.api.client.json.JsonFactory;
+
+/** TODO: See if api-services-pubsub is needed at all
 import com.google.api.services.pubsub.Pubsub;
 import com.google.api.services.pubsub.model.PullRequest;
 import com.google.api.services.pubsub.model.PullResponse;
 import com.google.api.services.pubsub.model.ReceivedMessage;
 import com.google.api.services.pubsub.model.Subscription;
+*/
+import com.google.cloud.pubsub.v1.stub.GrpcSubscriberStub;
+import com.google.cloud.pubsub.v1.stub.SubscriberStub;
+import com.google.cloud.pubsub.v1.stub.SubscriberStubSettings;
+import com.google.pubsub.v1.AcknowledgeRequest;
+import com.google.pubsub.v1.ProjectSubscriptionName;
+import com.google.pubsub.v1.PullRequest;
+import com.google.pubsub.v1.PullResponse;
+import com.google.pubsub.v1.ReceivedMessage;
 import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.common.util.Option;
 import org.apache.hudi.common.util.collection.Pair;
@@ -39,7 +50,7 @@ public class GcsEventsSource extends RowSource {
 
     private static final Logger LOG = LogManager.getLogger(GcsEventsSource.class);
 
-    private final int BATCH_SIZE = 2;
+    private final int BATCH_SIZE = 10;
 
     public GcsEventsSource(
             TypedProperties props,
@@ -54,8 +65,10 @@ public class GcsEventsSource extends RowSource {
     protected Pair<Option<Dataset<Row>>, String> fetchNextBatch(Option<String> lastCkptStr, long sourceLimit) {
         LOG.info("Fetching next batch");
 
-        Pair<List<String>, String> messagesAndMaxTime = pull("redacted", "redacted",
-                "redacted");
+//        Pair<List<String>, String> messagesAndMaxTime = pull("redacted", "redacted",
+//                "redacted");
+
+        Pair<List<String>, String> messagesAndMaxTime = pull2("redacted", "redacted");
 
         Dataset<String> eventRecords = sparkSession.createDataset(messagesAndMaxTime.getLeft(), Encoders.STRING());
 
@@ -65,49 +78,109 @@ public class GcsEventsSource extends RowSource {
         );
     }
 
-    public Pair<List<String>, String> pull(String projectName, String topicName, String subscriptionName) {
-        String projectFullName = "projects/" + projectName;
-        String topicFullName = projectFullName + "/topics/" + topicName;
-        String subscriptionFullName = projectFullName + "/subscriptions/" + subscriptionName;
-
-        Pubsub client = createAuthorizedClient();
+    public Pair<List<String>, String> pull2(String projectId, String subscriptionId) {
+        SubscriberStubSettings subscriberStubSettings = null;
 
         try {
-            Subscription subscription = new Subscription().setTopic(topicFullName);
-            try {
-                subscription =
-                        client.projects().subscriptions().create(subscriptionFullName, subscription).execute();
-            } catch (GoogleJsonResponseException e) {
-                if (e.getDetails().getCode() == HttpURLConnection.HTTP_CONFLICT) {
-                    // Subscription already exists, but that's the expected behavior with multiple receivers.
-                } else {
-                   throw e;
+            subscriberStubSettings = SubscriberStubSettings.newBuilder()
+                    .setTransportChannelProvider(
+                            SubscriberStubSettings.defaultGrpcTransportProviderBuilder()
+                                    .setMaxInboundMessageSize(20 * 1024 * 1024) // 20MB (maximum message size).
+                                    .build())
+                    .build();
+
+            try (SubscriberStub subscriber = GrpcSubscriberStub.create(subscriberStubSettings)) {
+                String subscriptionName = ProjectSubscriptionName.format(projectId, subscriptionId);
+                PullRequest pullRequest =
+                        PullRequest.newBuilder()
+                                .setMaxMessages(BATCH_SIZE)
+                                .setSubscription(subscriptionName)
+                                .build();
+
+                // Use pullCallable().futureCall to asynchronously perform this operation.
+                PullResponse pullResponse = subscriber.pullCallable().call(pullRequest);
+
+                // Stop the program if the pull response is empty to avoid acknowledging
+                // an empty list of ack IDs.
+                List<ReceivedMessage> receivedMessages = pullResponse.getReceivedMessagesList();
+
+                if (receivedMessages.isEmpty()) {
+                    System.out.println("No message was pulled. Exiting.");
+                    return null;
                 }
+
+//                ackMessages(subscriber, subscriptionName, receivedMessages);
+
+                return processMessages(receivedMessages);
             }
-
-            // TODO: Timeouts etc
-            PullRequest pullRequest =
-                new PullRequest().setReturnImmediately(false).setMaxMessages(BATCH_SIZE);
-
-            PullResponse pullResponse =
-                client.projects().subscriptions().pull(subscriptionFullName, pullRequest).execute();
-
-            List<ReceivedMessage> receivedMessages = pullResponse.getReceivedMessages();
-            if (receivedMessages == null) {
-                return Pair.of(Collections.emptyList(), "0");
-            }
-
-            return processMessages(receivedMessages);
-
-        } catch (GoogleJsonResponseException e) {
-            // TODO: Check if this catch block is needed
-            LOG.error("Error", e);
         } catch (IOException e) {
             LOG.error("Error", e);
         }
 
         return Pair.of(Collections.emptyList(), "0");
     }
+
+    private void ackMessages(SubscriberStub subscriber, String subscriptionName,
+                             List<ReceivedMessage> receivedMessages) {
+            List<String> ackIds = new ArrayList<>();
+            for (ReceivedMessage message : receivedMessages) {
+                ackIds.add(message.getAckId());
+            }
+
+            AcknowledgeRequest acknowledgeRequest =
+                    AcknowledgeRequest.newBuilder()
+                            .setSubscription(subscriptionName)
+                            .addAllAckIds(ackIds)
+                            .build();
+
+            // Use acknowledgeCallable().futureCall to asynchronously perform this operation.
+            subscriber.acknowledgeCallable().call(acknowledgeRequest);
+    }
+
+
+//    public Pair<List<String>, String> pull(String projectName, String topicName, String subscriptionName) {
+//        String projectFullName = "projects/" + projectName;
+//        String topicFullName = projectFullName + "/topics/" + topicName;
+//        String subscriptionFullName = projectFullName + "/subscriptions/" + subscriptionName;
+//
+//        Pubsub client = createAuthorizedClient();
+//
+//        try {
+//            Subscription subscription = new Subscription().setTopic(topicFullName);
+//            try {
+//                subscription =
+//                        client.projects().subscriptions().create(subscriptionFullName, subscription).execute();
+//            } catch (GoogleJsonResponseException e) {
+//                if (e.getDetails().getCode() == HttpURLConnection.HTTP_CONFLICT) {
+//                    // Subscription already exists, but that's the expected behavior with multiple receivers.
+//                } else {
+//                   throw e;
+//                }
+//            }
+//
+//            // TODO: Timeouts etc
+//            PullRequest pullRequest =
+//                new PullRequest().setReturnImmediately(false).setMaxMessages(BATCH_SIZE);
+//
+//            PullResponse pullResponse =
+//                client.projects().subscriptions().pull(subscriptionFullName, pullRequest).execute();
+//
+//            List<ReceivedMessage> receivedMessages = pullResponse.getReceivedMessages();
+//            if (receivedMessages == null) {
+//                return Pair.of(Collections.emptyList(), "0");
+//            }
+//
+//            return processMessages(receivedMessages);
+//
+//        } catch (GoogleJsonResponseException e) {
+//            // TODO: Check if this catch block is needed
+//            LOG.error("Error", e);
+//        } catch (IOException e) {
+//            LOG.error("Error", e);
+//        }
+//
+//        return Pair.of(Collections.emptyList(), "0");
+//    }
 
     private Pair<List<String>, String> processMessages(List<ReceivedMessage> receivedMessages) throws IOException {
         List<String> messages = mapToString(receivedMessages);
@@ -129,16 +202,15 @@ public class GcsEventsSource extends RowSource {
         List<String> msgTxt = messages
                 .stream()
                 .filter(m -> m.getMessage() != null)
-                .filter(m -> m.getMessage().decodeData() != null)
+                .filter(m -> m.getMessage().getData() != null)
                 .map(
                     m -> {
-                        try {
-                            String s = new String(m.getMessage().decodeData(), "UTF-8");
-                            return s;
-                        } catch (UnsupportedEncodingException e) {
-                            // Wrong encode
-                            return null;
-                        }
+//                        try {
+                            return m.getMessage().getData().toStringUtf8();
+//                        } catch (UnsupportedEncodingException e) {
+//                            // Wrong encode
+//                            return null;
+//                        }
                     })
                 .filter(Objects::nonNull)
                 .collect(Collectors.toList());
@@ -147,22 +219,22 @@ public class GcsEventsSource extends RowSource {
     }
 
     // TODO: Fix hardcoded auths
-    private Pubsub createAuthorizedClient() {
-        try {
-            HttpTransport httpTransport = Utils.getDefaultTransport();
-            JsonFactory jsonFactory = Utils.getDefaultJsonFactory();
-            GoogleCredential creds =
-                    GoogleCredential.getApplicationDefault(httpTransport, jsonFactory);
-
-            return new Pubsub.Builder(httpTransport, jsonFactory, req -> req.setInterceptor(creds)).
-                    setApplicationName("spark-pubsub-receiver").
-                    setHttpRequestInitializer(req -> req.setInterceptor(creds))
-                    .build();
-
-        } catch (IOException e) {
-            LOG.error("Error", e);
-        }
-
-        return null;
-    }
+//    private Pubsub createAuthorizedClient() {
+//        try {
+//            HttpTransport httpTransport = Utils.getDefaultTransport();
+//            JsonFactory jsonFactory = Utils.getDefaultJsonFactory();
+//            GoogleCredential creds =
+//                    GoogleCredential.getApplicationDefault(httpTransport, jsonFactory);
+//
+//            return new Pubsub.Builder(httpTransport, jsonFactory, req -> req.setInterceptor(creds)).
+//                    setApplicationName("spark-pubsub-receiver").
+//                    setHttpRequestInitializer(req -> req.setInterceptor(creds))
+//                    .build();
+//
+//        } catch (IOException e) {
+//            LOG.error("Error", e);
+//        }
+//
+//        return null;
+//    }
 }
--- hudi-utilities/src/main/java/org/apache/hudi/utilities/sources/helpers/GPubsubMessageReceiver.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/sources/helpers/GPubsubMessageReceiver.java
@@ -1,133 +1,133 @@
-package org.apache.hudi.utilities.sources.helpers;
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
-import com.google.api.client.googleapis.util.Utils;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.services.pubsub.Pubsub;
-import com.google.api.services.pubsub.model.PullRequest;
-import com.google.api.services.pubsub.model.PullResponse;
-import com.google.api.services.pubsub.model.ReceivedMessage;
-import com.google.api.services.pubsub.model.Subscription;
-import org.apache.spark.storage.StorageLevel;
-import org.apache.spark.streaming.receiver.Receiver;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.util.List;
-
-/**
- * Implements Spark Streaming's Receiver interface. Can be used within a Spark Streaming
- * job to receive messages from a Cloud Pubsub topic
- *
- * TODO: Can be removed if GcsEventsSource can be made to work without using
- * Spark's StreamingContext
- */
-public class GPubsubMessageReceiver extends Receiver<String> {
-
-    private final int BATCH_SIZE = 2;
-
-    private final String projectFullName;
-    private final String topicFullName;
-    private final String subscriptionFullName;
-
-    public GPubsubMessageReceiver(String projectName, String topicName, String subscription) {
-        super(StorageLevel.MEMORY_AND_DISK_2());
-        this.projectFullName = "projects/" + projectName;
-        this.topicFullName = projectFullName + "/topics/" + topicName;
-        this.subscriptionFullName = projectFullName + "/subscriptions/" + subscription;
-    }
-
-    public void onStart() {
-        Pubsub client = createAuthorizedClient();
-        Subscription subscription = new Subscription().setTopic(topicFullName);
-        try {
-            subscription =
-                    client.projects().subscriptions().create(subscriptionFullName, subscription).execute();
-        } catch (GoogleJsonResponseException e) {
-            if (e.getDetails().getCode() == HttpURLConnection.HTTP_CONFLICT) {
-                // Subscription already exists, but that's the expected behavior with multiple receivers.
-            } else {
-                e.printStackTrace();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        new Thread() {
-            @Override
-            public void run() {
-                receive();
-            }
-        }.start();
-    }
-
-    public void onStop() {
-        System.out.println("Stopping GPubsubMessageReceiver");
-    }
-
-    // Pull messages from Pubsub
-    private void receive() {
-        Pubsub client = createAuthorizedClient();
-        PullRequest pullRequest =
-                new PullRequest().setReturnImmediately(false).setMaxMessages(BATCH_SIZE);
-        do {
-            try {
-                PullResponse pullResponse =
-                        client.projects().subscriptions().pull(subscriptionFullName, pullRequest).execute();
-
-                List<ReceivedMessage> receivedMessages = pullResponse.getReceivedMessages();
-                if (receivedMessages != null) {
-                    storeMessages(receivedMessages);
-                }
-            } catch (GoogleJsonResponseException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } while (!isStopped());
-    }
-
-    private void storeMessages(List<ReceivedMessage> messages) {
-        store(
-            messages
-            .stream()
-            .filter(m -> m.getMessage() != null)
-            .filter(m -> m.getMessage().decodeData() != null)
-            .map(
-                m -> {
-                    try {
-                        String s = new String(m.getMessage().decodeData(), "UTF-8");
-                        return s;
-                    } catch (UnsupportedEncodingException e) {
-                        // Wrong encode
-                        return null;
-                    }
-                })
-            .filter(m -> m != null)
-            .iterator()
-        );
-    }
-
-    private Pubsub createAuthorizedClient() {
-        try {
-            HttpTransport httpTransport = Utils.getDefaultTransport();
-            JsonFactory jsonFactory = Utils.getDefaultJsonFactory();
-            GoogleCredential creds =
-                    GoogleCredential.getApplicationDefault(httpTransport, jsonFactory);
-
-            return new Pubsub.Builder(httpTransport, jsonFactory, req -> req.setInterceptor(creds)).
-                    setApplicationName("spark-pubsub-receiver").
-                    setHttpRequestInitializer(req -> req.setInterceptor(creds))
-            .build();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-}
+//package org.apache.hudi.utilities.sources.helpers;
+//
+//import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+//import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+//import com.google.api.client.googleapis.util.Utils;
+//import com.google.api.client.http.HttpTransport;
+//import com.google.api.client.json.JsonFactory;
+//import com.google.api.services.pubsub.Pubsub;
+//import com.google.api.services.pubsub.model.PullRequest;
+//import com.google.api.services.pubsub.model.PullResponse;
+//import com.google.api.services.pubsub.model.ReceivedMessage;
+//import com.google.api.services.pubsub.model.Subscription;
+//import org.apache.spark.storage.StorageLevel;
+//import org.apache.spark.streaming.receiver.Receiver;
+//
+//import java.io.IOException;
+//import java.io.UnsupportedEncodingException;
+//import java.net.HttpURLConnection;
+//import java.util.List;
+//
+///**
+// * Implements Spark Streaming's Receiver interface. Can be used within a Spark Streaming
+// * job to receive messages from a Cloud Pubsub topic
+// *
+// * TODO: Can be removed if GcsEventsSource can be made to work without using
+// * Spark's StreamingContext
+// */
+//public class GPubsubMessageReceiver extends Receiver<String> {
+//
+//    private final int BATCH_SIZE = 2;
+//
+//    private final String projectFullName;
+//    private final String topicFullName;
+//    private final String subscriptionFullName;
+//
+//    public GPubsubMessageReceiver(String projectName, String topicName, String subscription) {
+//        super(StorageLevel.MEMORY_AND_DISK_2());
+//        this.projectFullName = "projects/" + projectName;
+//        this.topicFullName = projectFullName + "/topics/" + topicName;
+//        this.subscriptionFullName = projectFullName + "/subscriptions/" + subscription;
+//    }
+//
+//    public void onStart() {
+//        Pubsub client = createAuthorizedClient();
+//        Subscription subscription = new Subscription().setTopic(topicFullName);
+//        try {
+//            subscription =
+//                    client.projects().subscriptions().create(subscriptionFullName, subscription).execute();
+//        } catch (GoogleJsonResponseException e) {
+//            if (e.getDetails().getCode() == HttpURLConnection.HTTP_CONFLICT) {
+//                // Subscription already exists, but that's the expected behavior with multiple receivers.
+//            } else {
+//                e.printStackTrace();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                receive();
+//            }
+//        }.start();
+//    }
+//
+//    public void onStop() {
+//        System.out.println("Stopping GPubsubMessageReceiver");
+//    }
+//
+//    // Pull messages from Pubsub
+//    private void receive() {
+//        Pubsub client = createAuthorizedClient();
+//        PullRequest pullRequest =
+//                new PullRequest().setReturnImmediately(false).setMaxMessages(BATCH_SIZE);
+//        do {
+//            try {
+//                PullResponse pullResponse =
+//                        client.projects().subscriptions().pull(subscriptionFullName, pullRequest).execute();
+//
+//                List<ReceivedMessage> receivedMessages = pullResponse.getReceivedMessages();
+//                if (receivedMessages != null) {
+//                    storeMessages(receivedMessages);
+//                }
+//            } catch (GoogleJsonResponseException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        } while (!isStopped());
+//    }
+//
+//    private void storeMessages(List<ReceivedMessage> messages) {
+//        store(
+//            messages
+//            .stream()
+//            .filter(m -> m.getMessage() != null)
+//            .filter(m -> m.getMessage().decodeData() != null)
+//            .map(
+//                m -> {
+//                    try {
+//                        String s = new String(m.getMessage().decodeData(), "UTF-8");
+//                        return s;
+//                    } catch (UnsupportedEncodingException e) {
+//                        // Wrong encode
+//                        return null;
+//                    }
+//                })
+//            .filter(m -> m != null)
+//            .iterator()
+//        );
+//    }
+//
+//    private Pubsub createAuthorizedClient() {
+//        try {
+//            HttpTransport httpTransport = Utils.getDefaultTransport();
+//            JsonFactory jsonFactory = Utils.getDefaultJsonFactory();
+//            GoogleCredential creds =
+//                    GoogleCredential.getApplicationDefault(httpTransport, jsonFactory);
+//
+//            return new Pubsub.Builder(httpTransport, jsonFactory, req -> req.setInterceptor(creds)).
+//                    setApplicationName("spark-pubsub-receiver").
+//                    setHttpRequestInitializer(req -> req.setInterceptor(creds))
+//            .build();
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return null;
+//    }
+//
+//}
