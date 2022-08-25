cd3d263bc18ea422b3ab124e109cdebcdfda26a3
nicolas paris <nicolas.paris@riseup.net>
2022-08-25 23:04:36 +0200

    Turn already exist error into warning

--- hudi-aws/src/main/java/org/apache/hudi/aws/sync/AWSGlueCatalogSyncClient.java
+++ hudi-aws/src/main/java/org/apache/hudi/aws/sync/AWSGlueCatalogSyncClient.java
@@ -138,8 +138,12 @@ public class AWSGlueCatalogSyncClient extends HoodieSyncClient {
 
         BatchCreatePartitionResult result = awsGlue.batchCreatePartition(request);
         if (CollectionUtils.nonEmpty(result.getErrors())) {
-          throw new HoodieGlueSyncException("Fail to add partitions to " + tableId(databaseName, tableName)
+          if(result.getErrors().stream().allMatch((error) -> "AlreadyExistsException".equals(error.getErrorDetail().getErrorCode()))){
+            LOG.warn("Partitions already exist in glue: " + result.getErrors());
+          } else{
+            throw new HoodieGlueSyncException("Fail to add partitions to " + tableId(databaseName, tableName)
               + " with error(s): " + result.getErrors());
+          }
         }
         Thread.sleep(BATCH_REQUEST_SLEEP_MILLIS);
       }
