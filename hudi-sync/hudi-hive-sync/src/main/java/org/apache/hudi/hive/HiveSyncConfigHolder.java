0a68191e7e6a7b6a08154810ebbf7d6a048c837f
linfey <736320652@qq.com>
2022-08-26 18:48:55 +0800

    sinceVersion added

--- hudi-sync/hudi-hive-sync/src/main/java/org/apache/hudi/hive/HiveSyncConfigHolder.java
+++ hudi-sync/hudi-hive-sync/src/main/java/org/apache/hudi/hive/HiveSyncConfigHolder.java
@@ -80,6 +80,7 @@ public class HiveSyncConfigHolder {
   public static final ConfigProperty<String> HIVE_SKIP_RT_SUFFIX_FOR_READ_SNAPSHOT_TABLE = ConfigProperty
           .key("hoodie.datasource.hive_sync.skip_rt_suffix")
           .defaultValue("false")
+          .sinceVersion("0.12.1") 
           .withDocumentation("Skip the _rt suffix for Read snapshot table, when registering");
   public static final ConfigProperty<String> HIVE_SUPPORT_TIMESTAMP_TYPE = ConfigProperty
       .key("hoodie.datasource.hive_sync.support_timestamp")
