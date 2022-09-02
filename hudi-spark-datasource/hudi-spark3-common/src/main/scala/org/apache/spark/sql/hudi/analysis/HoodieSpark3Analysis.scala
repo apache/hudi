6e6c6f88e0cb287241263fead43983a2b57c96b6
Alexey Kudinkin <alexey@infinilake.com>
2022-09-02 12:11:55 -0700

    Exempt transforms run w/in analysis phase

--- hudi-spark-datasource/hudi-spark3-common/src/main/scala/org/apache/spark/sql/hudi/analysis/HoodieSpark3Analysis.scala
+++ hudi-spark-datasource/hudi-spark3-common/src/main/scala/org/apache/spark/sql/hudi/analysis/HoodieSpark3Analysis.scala
@@ -18,7 +18,7 @@
 package org.apache.spark.sql.hudi.analysis
 
 import org.apache.hudi.SparkAdapterSupport
-import org.apache.spark.sql.catalyst.plans.logical.{HoodieLogicalRelation, LogicalPlan, Project}
+import org.apache.spark.sql.catalyst.plans.logical.{AnalysisHelper, HoodieLogicalRelation, LogicalPlan, Project}
 import org.apache.spark.sql.catalyst.rules.Rule
 import org.apache.spark.sql.catalyst.trees.TreeNodeTag
 import org.apache.spark.sql.hudi.analysis.HoodieSpark3Analysis.sparkAdapter
@@ -30,24 +30,29 @@ case class HoodieSpark3ResolveLogicalRelations() extends Rule[LogicalPlan] {
   private val hudiLogicalRelationTag: TreeNodeTag[Boolean] = TreeNodeTag("__hudi_logical_relation")
 
   override def apply(plan: LogicalPlan): LogicalPlan =
-    plan.transformDown {
-      case lr @ LogicalRelation(_, _, Some(table), _)
-        if sparkAdapter.isHoodieTable(table) && lr.getTagValue(hudiLogicalRelationTag).isEmpty =>
-        // NOTE: Have to make a copy here, since by default Spark is caching resolved [[LogicalRelation]]s
-        val logicalRelation = lr.newInstance()
-        logicalRelation.setTagValue(hudiLogicalRelationTag, true)
+    AnalysisHelper.allowInvokingTransformsInAnalyzer {
+      plan.transformDown {
+        case lr@LogicalRelation(_, _, Some(table), _)
+          if sparkAdapter.isHoodieTable(table) && lr.getTagValue(hudiLogicalRelationTag).isEmpty =>
+          // NOTE: Have to make a copy here, since by default Spark is caching resolved [[LogicalRelation]]s
+          val logicalRelation = lr.newInstance()
+          logicalRelation.setTagValue(hudiLogicalRelationTag, true)
 
-        HoodieLogicalRelation(logicalRelation)
+          HoodieLogicalRelation(logicalRelation)
+      }
     }
 }
 
 // TODO elaborate
 case class HoodieSpark3FoldLogicalRelations() extends Rule[LogicalPlan] {
-  override def apply(plan: LogicalPlan): LogicalPlan =
-    plan.transformDown {
-      // TODO elaborate
-      case hlr @ HoodieLogicalRelation(lr: LogicalRelation) => Project(hlr.output, lr)
+  override def apply(plan: LogicalPlan): LogicalPlan = {
+    AnalysisHelper.allowInvokingTransformsInAnalyzer {
+      plan.transformDown {
+        // TODO elaborate
+        case hlr@HoodieLogicalRelation(lr: LogicalRelation) => Project(hlr.output, lr)
+      }
     }
+  }
 }
 
 object HoodieSpark3Analysis extends SparkAdapterSupport {}
