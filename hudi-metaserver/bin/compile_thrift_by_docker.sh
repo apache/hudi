4cd662d567b82ea1edcac2aac026382756c99f0d
gengxiaoyu <gengxiaoyu@bytedance.com>
2022-09-06 17:03:56 +0800

    add docker

--- hudi-metaserver/bin/compile_thrift_by_docker.sh
+++ hudi-metaserver/bin/compile_thrift_by_docker.sh
@@ -16,8 +16,9 @@
 #  See the License for the specific language governing permissions and
 # limitations under the License.
 
-THRIFT_FILE_PATH=hudi-metaserver/src/main/thrift
+THRIFT_FILE_PATH=$PWD/hudi-metaserver/src/main/thrift
 docker pull thrift:0.12.0
 pwd
+thrift -v
 docker run -v "$THRIFT_FILE_PATH:/thrift" thrift thrift -o /thrift/ --gen java /thrift/hudi-metaserver.thrift
 ls hudi-metaserver/src/main/thrift
