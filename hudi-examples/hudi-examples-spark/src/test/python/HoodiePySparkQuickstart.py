609e1ebef52854a9a6021fbf45397dfef7d7d33b
Jonathan Vexler <jon@onehouse.ai>
2022-09-09 18:16:06 -0400

    made the functions work like the ones in the quick start guide

--- hudi-examples/hudi-examples-spark/src/test/python/HoodiePySparkQuickstart.py
+++ hudi-examples/hudi-examples-spark/src/test/python/HoodiePySparkQuickstart.py
@@ -1,8 +1,26 @@
+#   Licensed to the Apache Software Foundation (ASF) under one
+#   or more contributor license agreements.  See the NOTICE file
+#   distributed with this work for additional information
+#   regarding copyright ownership.  The ASF licenses this file
+#   to you under the Apache License, Version 2.0 (the
+#   "License"); you may not use this file except in compliance
+#   with the License.  You may obtain a copy of the License at
+ 
+#       http://www.apache.org/licenses/LICENSE-2.0
+ 
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+ 
 import sys
+import os
 from pyspark import sql
 from datetime import datetime
 import random
-from py4j.java_gateway import java_import
+from pyspark.sql.functions import lit
+from functools import reduce
 
 
 DEFAULT_FIRST_PARTITION_PATH = "2020/01/01"
@@ -10,28 +28,173 @@ DEFAULT_SECOND_PARTITION_PATH = "2020/01/02"
 DEFAULT_THIRD_PARTITION_PATH = "2020/01/03"
 DEFAULT_PARTITION_PATHS = [DEFAULT_FIRST_PARTITION_PATH, DEFAULT_SECOND_PARTITION_PATH, DEFAULT_THIRD_PARTITION_PATH]
 
+class ExamplePySpark:
+    def __init__(self, spark: sql.SparkSession, tableName: str, basePath: str):
+        self.spark = spark
+        self.tableName = tableName
+        self.basePath = basePath
+        self.hudi_options = {
+            'hoodie.table.name': tableName,
+            'hoodie.datasource.write.recordkey.field': 'uuid',
+            'hoodie.datasource.write.partitionpath.field': 'partitionpath',
+            'hoodie.datasource.write.table.name': tableName,
+            'hoodie.datasource.write.operation': 'upsert',
+            'hoodie.datasource.write.precombine.field': 'ts',
+            'hoodie.upsert.shuffle.parallelism': 2,
+            'hoodie.insert.shuffle.parallelism': 2
+        }
+        self.dataGen = spark._jvm.org.apache.hudi.QuickstartUtils.DataGenerator()
+        self.snapshotQuery = "SELECT begin_lat, begin_lon, driver, end_lat, end_lon, fare, partitionpath, rider, ts, uuid FROM hudi_ro_table"
+        return
 
-def generateGenericRecord(uuid, riderName, driverName, timestamp):
-    return sql.Row(uuid=uuid, ts=timestamp,rider=riderName,driver=driverName,
-    begin_lat=random.random(),begin_lon=random.random(),end_lat=random.random(),
-    end_lon=random.random(),fare=random.random()*100)
+    def runQuickstart(self):
+        self.insertData()
+        self.queryData()
+        self.updateData()
+        self.queryData()
+        self.timeTravelQuery()
+        self.incrementalQuery()
+        self.pointInTimeQuery()
+        self.softDeletes()
+        self.hardDeletes()
+        self.insertOverwrite()
+        return
 
-def insertData(spark, tablename):
-    dataGen = spark._jvm.org.apache.hudi.QuickstartUtils.DataGenerator()
-    inserts = spark._jvm.org.apache.hudi.QuickstartUtils.convertToStringList(dataGen.generateInserts(10))
-    print(inserts.size())
-    df = spark.read.json(spark.sparkContext.parallelize(inserts, 2))
-    commitTime = datetime.now().strftime("%H:%M:%S")
-    print(df.count())
-    print("hello world")
+    def insertData(self):
+        inserts = self.spark._jvm.org.apache.hudi.QuickstartUtils.convertToStringList(self.dataGen.generateInserts(10))
+        df = self.spark.read.json(self.spark.sparkContext.parallelize(inserts, 2))
+        df.write.format("hudi").options(**self.hudi_options).mode("overwrite").save(self.basePath)
+        return
+    def updateData(self):
+        updates = self.spark._jvm.org.apache.hudi.QuickstartUtils.convertToStringList(self.dataGen.generateUpdates(10))
+        df = self.spark.read.json(spark.sparkContext.parallelize(updates, 2))
+        df.write.format("hudi").options(**self.hudi_options).mode("append").save(self.basePath)
+        return
 
-    return
+    def queryData(self):
+        tripsSnapshotDF = self.spark.read.format("hudi").load(self.basePath)
+        tripsSnapshotDF.createOrReplaceTempView("hudi_trips_snapshot")
+        self.spark.sql("SELECT fare, begin_lon, begin_lat, ts FROM  hudi_trips_snapshot WHERE fare > 20.0").show()
+        self.spark.sql("SELECT _hoodie_commit_time, _hoodie_record_key, _hoodie_partition_path, rider, driver, fare FROM  hudi_trips_snapshot").show()
+        return
 
+    def timeTravelQuery(self):
+        self.spark.read.format("hudi").option("as.of.instant", "20210728141108").load(self.basePath)
+        self.spark.read.format("hudi").option("as.of.instant", "2021-07-28 14:11:08.000").load(self.basePath)
+        self.spark.read.format("hudi").option("as.of.instant", "2021-07-28").load(self.basePath)
+        return
+    
+    def incrementalQuery(self):
+        self.spark.read.format("hudi").load(self.basePath).createOrReplaceTempView("hudi_trips_snapshot")
+        self.commits = list(map(lambda row: row[0], self.spark.sql("SELECT DISTINCT(_hoodie_commit_time) AS commitTime FROM  hudi_trips_snapshot ORDER BY commitTime").limit(50).collect()))
+        beginTime = self.commits[len(self.commits) - 2] 
+        incremental_read_options = {
+            'hoodie.datasource.query.type': 'incremental',
+            'hoodie.datasource.read.begin.instanttime': beginTime,
+        }
+        tripsIncrementalDF = self.spark.read.format("hudi").options(**incremental_read_options).load(self.basePath)
+        tripsIncrementalDF.createOrReplaceTempView("hudi_trips_incremental")
+        self.spark.sql("SELECT `_hoodie_commit_time`, fare, begin_lon, begin_lat, ts FROM hudi_trips_incremental WHERE fare > 20.0").show()
 
-def runQuickstart(spark, tablename):
-    insertData(spark,tableName)
+    def pointInTimeQuery(self):
+        beginTime = "000"
+        endTime = self.commits[len(self.commits) - 2]
+        point_in_time_read_options = {
+            'hoodie.datasource.query.type': 'incremental',
+            'hoodie.datasource.read.end.instanttime': endTime,
+            'hoodie.datasource.read.begin.instanttime': beginTime
+        }
 
-    return
+        tripsPointInTimeDF = self.spark.read.format("hudi").options(**point_in_time_read_options).load(self.basePath)
+        tripsPointInTimeDF.createOrReplaceTempView("hudi_trips_point_in_time")
+        self.spark.sql("SELECT `_hoodie_commit_time`, fare, begin_lon, begin_lat, ts FROM hudi_trips_point_in_time WHERE fare > 20.0").show()
+    
+    def softDeletes(self):
+        spark.read.format("hudi").load(self.basePath).createOrReplaceTempView("hudi_trips_snapshot")
+
+        # fetch total records count
+        spark.sql("SELECT uuid, partitionpath FROM hudi_trips_snapshot").count()
+        spark.sql("SELECT uuid, partitionpath FROM hudi_trips_snapshot WHERE rider IS NOT null").count()
+        # fetch two records for soft deletes
+        soft_delete_ds = spark.sql("SELECT * FROM hudi_trips_snapshot").limit(2)
+        # prepare the soft deletes by ensuring the appropriate fields are nullified
+        meta_columns = ["_hoodie_commit_time", "_hoodie_commit_seqno", "_hoodie_record_key", 
+        "_hoodie_partition_path", "_hoodie_file_name"]
+        excluded_columns = meta_columns + ["ts", "uuid", "partitionpath"]
+        nullify_columns = list(filter(lambda field: field[0] not in excluded_columns, \
+        list(map(lambda field: (field.name, field.dataType), soft_delete_ds.schema.fields))))
+
+        hudi_soft_delete_options = {
+        'hoodie.table.name': self.tableName,
+        'hoodie.datasource.write.recordkey.field': 'uuid',
+        'hoodie.datasource.write.partitionpath.field': 'partitionpath',
+        'hoodie.datasource.write.table.name': self.tableName,
+        'hoodie.datasource.write.operation': 'upsert',
+        'hoodie.datasource.write.precombine.field': 'ts',
+        'hoodie.upsert.shuffle.parallelism': 2, 
+        'hoodie.insert.shuffle.parallelism': 2
+        }
+
+        soft_delete_df = reduce(lambda df,col: df.withColumn(col[0], lit(None).cast(col[1])), \
+        nullify_columns, reduce(lambda df,col: df.drop(col[0]), meta_columns, soft_delete_ds))
+
+        # simply upsert the table after setting these fields to null
+        soft_delete_df.write.format("hudi").options(**hudi_soft_delete_options).mode("append").save(self.basePath)
+
+        # reload data
+        self.spark.read.format("hudi").load(self.basePath).createOrReplaceTempView("hudi_trips_snapshot")
+
+        # This should return the same total count as before
+        self.spark.sql("SELECT uuid, partitionpath FROM hudi_trips_snapshot").count()
+        # This should return (total - 2) count as two records are updated with nulls
+        self.spark.sql("SELECT uuid, partitionpath FROM hudi_trips_snapshot WHERE rider IS NOT null").count()
+
+    def hardDeletes(self):
+        # fetch total records count
+        self.spark.sql("SELECT uuid, partitionpath FROM hudi_trips_snapshot").count()
+        # fetch two records to be deleted
+        ds = self.spark.sql("SELECT uuid, partitionpath FROM hudi_trips_snapshot").limit(2)
+
+        # issue deletes
+        hudi_hard_delete_options = {
+            'hoodie.table.name': self.tableName,
+            'hoodie.datasource.write.recordkey.field': 'uuid',
+            'hoodie.datasource.write.partitionpath.field': 'partitionpath',
+            'hoodie.datasource.write.table.name': self.tableName,
+            'hoodie.datasource.write.operation': 'delete',
+            'hoodie.datasource.write.precombine.field': 'ts',
+            'hoodie.upsert.shuffle.parallelism': 2, 
+            'hoodie.insert.shuffle.parallelism': 2
+        }
+
+        deletes = list(map(lambda row: (row[0], row[1]), ds.collect()))
+        hard_delete_df = self.spark.sparkContext.parallelize(deletes).toDF(['uuid', 'partitionpath']).withColumn('ts', lit(0.0))
+        hard_delete_df.write.format("hudi").options(**hudi_hard_delete_options).mode("append").save(self.basePath)
+
+        # run the same read query as above.
+        roAfterDeleteViewDF = self.spark.read.format("hudi").load(self.basePath) 
+        roAfterDeleteViewDF.createOrReplaceTempView("hudi_trips_snapshot")
+        # fetch should return (total - 2) records
+        self.spark.sql("SELECT uuid, partitionpath FROM hudi_trips_snapshot").count()
+    def insertOverwrite(self):
+        self.spark.read.format("hudi").load(self.basePath).select(["uuid","partitionpath"]).sort(["partitionpath", "uuid"]).show(n=100,truncate=False)
+        inserts = self.spark._jvm.org.apache.hudi.QuickstartUtils.convertToStringList(self.dataGen.generateInserts(10))
+        df = self.spark.read.json(self.spark.sparkContext.parallelize(inserts, 2)).filter("partitionpath = 'americas/united_states/san_francisco'")
+        hudi_insert_overwrite_options = {
+            'hoodie.table.name': self.tableName,
+            'hoodie.datasource.write.recordkey.field': 'uuid',
+            'hoodie.datasource.write.partitionpath.field': 'partitionpath',
+            'hoodie.datasource.write.table.name': self.tableName,
+            'hoodie.datasource.write.operation': 'insert_overwrite',
+            'hoodie.datasource.write.precombine.field': 'ts',
+            'hoodie.upsert.shuffle.parallelism': 2,
+            'hoodie.insert.shuffle.parallelism': 2
+        }
+        df.write.format("hudi").options(**hudi_insert_overwrite_options).mode("append").save(self.basePath)
+        self.spark.read.format("hudi").load(self.basePath).select(["uuid","partitionpath"]).sort(["partitionpath", "uuid"]).show(n=100,truncate=False)
+
+    
+
 
 
 
@@ -42,14 +205,19 @@ if __name__ == "__main__":
         print("Usage: HoodiePySparkQuickstart <tablePath> <tableName>")
         quit(-1)
     tableName = sys.argv[1]
+    basePath = "file:///tmp/hudi_trips_cow"
+    SUBMIT_ARGS = "--packages org.apache.hudi:hudi-spark3.3-bundle_2.12:0.12.0 pyspark-shell"
+    os.environ["PYSPARK_SUBMIT_ARGS"] = SUBMIT_ARGS
     spark = sql.SparkSession \
         .builder \
         .appName("Hudi Spark basic example") \
         .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
         .config("spark.kryoserializer.buffer.max", "512m") \
+        .config("spark.sql.extensions", "org.apache.spark.sql.hudi.HoodieSparkSessionExtension") \
         .getOrCreate()
-    java_import(spark._sc._jvm, "org.apache.spark.sql.api.python.*")
-    runQuickstart(spark,tableName)
+    #java_import(spark._sc._jvm, "org.apache.spark.sql.api.python.*")
+    ps = ExamplePySpark(spark,tableName,basePath)
+    ps.runQuickstart()
 
 
 
