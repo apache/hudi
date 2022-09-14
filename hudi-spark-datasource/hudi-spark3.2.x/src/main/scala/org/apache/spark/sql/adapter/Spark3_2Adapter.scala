8c296e03568dca4e98e97a0d81ada509c8238037
Alexey Kudinkin <alexey@infinilake.com>
2022-09-14 08:21:03 -0700

    [HUDI-4691] Cleaning up duplicated classes in Spark 3.3 module (#6550)
    As part of adding support for Spark 3.3 in Hudi 0.12, a lot of the logic 
    from Spark 3.2 module has been simply copied over.
    
    This PR is rectifying that by:
    1. Creating new module "hudi-spark3.2plus-common" 
        (that is shared across Spark 3.2 and Spark 3.3)
    2. Moving shared components under "hudi-spark3.2plus-common"

--- hudi-spark-datasource/hudi-spark3.2.x/src/main/scala/org/apache/spark/sql/adapter/Spark3_2Adapter.scala
+++ hudi-spark-datasource/hudi-spark3.2.x/src/main/scala/org/apache/spark/sql/adapter/Spark3_2Adapter.scala
@@ -17,23 +17,25 @@
 
 package org.apache.spark.sql.adapter
 
-import org.apache.hudi.Spark32HoodieFileScanRDD
 import org.apache.avro.Schema
+import org.apache.hudi.Spark32HoodieFileScanRDD
+import org.apache.spark.sql._
 import org.apache.spark.sql.avro._
+import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.{AttributeReference, Expression}
 import org.apache.spark.sql.catalyst.parser.ParserInterface
-import org.apache.spark.sql.catalyst.plans.logical.{Command, DeleteFromTable, LogicalPlan}
-import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.plans.logical.{Command, DeleteFromTable}
+import org.apache.spark.sql.execution.datasources.parquet.{ParquetFileFormat, Spark32PlusHoodieParquetFileFormat}
 import org.apache.spark.sql.execution.datasources.{FilePartition, FileScanRDD, PartitionedFile}
-import org.apache.spark.sql.execution.datasources.parquet.{ParquetFileFormat, Spark32HoodieParquetFileFormat}
 import org.apache.spark.sql.parser.HoodieSpark3_2ExtendedSqlParser
 import org.apache.spark.sql.types.{DataType, StructType}
-import org.apache.spark.sql._
 
 /**
  * Implementation of [[SparkAdapter]] for Spark 3.2.x branch
  */
 class Spark3_2Adapter extends BaseSpark3Adapter {
+
+  override def getCatalogUtils: HoodieSpark3CatalogUtils = HoodieSpark32CatalogUtils
 
   override def getCatalystExpressionUtils: HoodieCatalystExpressionUtils = HoodieSpark32CatalystExpressionUtils
 
@@ -52,7 +54,7 @@ class Spark3_2Adapter extends BaseSpark3Adapter {
   }
 
   override def createHoodieParquetFileFormat(appendPartitionValues: Boolean): Option[ParquetFileFormat] = {
-    Some(new Spark32HoodieParquetFileFormat(appendPartitionValues))
+    Some(new Spark32PlusHoodieParquetFileFormat(appendPartitionValues))
   }
 
   override def createHoodieFileScanRDD(sparkSession: SparkSession,
