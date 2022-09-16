5e1903ae452385c2e290468bae015eb8e438d622
Yann <biyan900116@gmail.com>
2022-09-16 18:13:25 +0800

    remove cdc-query-related codes

--- hudi-common/src/main/java/org/apache/hudi/common/model/FileSlice.java
+++ hudi-common/src/main/java/org/apache/hudi/common/model/FileSlice.java
@@ -21,7 +21,6 @@ package org.apache.hudi.common.model;
 import org.apache.hudi.common.util.Option;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Objects;
 import java.util.TreeSet;
 import java.util.stream.Stream;
@@ -70,15 +69,6 @@ public class FileSlice implements Serializable {
     this.baseInstantTime = baseInstantTime;
     this.baseFile = null;
     this.logFiles = new TreeSet<>(HoodieLogFile.getReverseLogFileComparator());
-  }
-
-  public FileSlice(HoodieFileGroupId fileGroupId, String baseInstantTime,
-                   HoodieBaseFile baseFile, List<HoodieLogFile> logFiles) {
-    this.fileGroupId = fileGroupId;
-    this.baseInstantTime = baseInstantTime;
-    this.baseFile = baseFile;
-    this.logFiles = new TreeSet<>(HoodieLogFile.getReverseLogFileComparator());
-    this.logFiles.addAll(logFiles);
   }
 
   public void setBaseFile(HoodieBaseFile baseFile) {
