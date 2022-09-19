7f9e43be5806a974753cecbb5706f7be669a7071
eric9204 <90449228+eric9204@users.noreply.github.com>
2022-09-19 20:26:27 +0800

    [HUDI-4810] Fix log4j imports to use bridge API  (#6710)
    Co-authored-by: dongsj <dongsj@asiainfo.com>

--- hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/client/clustering/run/strategy/SparkConsistentBucketClusteringExecutionStrategy.java
+++ hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/client/clustering/run/strategy/SparkConsistentBucketClusteringExecutionStrategy.java
@@ -33,8 +33,8 @@ import org.apache.hudi.table.HoodieTable;
 import org.apache.hudi.table.action.commit.SparkBulkInsertHelper;
 
 import org.apache.avro.Schema;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 import java.util.Map;
