af2e2498e36e5823d910bc6c2e5d65bbac66855d
Pintu Soliya <pintusholiya@gmail.com>
2022-09-20 13:11:46 +0530

    fix: console warning about anchor link

--- website/src/components/AuthorName.js
+++ website/src/components/AuthorName.js
@@ -1,26 +1,36 @@
 import React from "react";
-import Link from '@docusaurus/Link';
+import Link from "@docusaurus/Link";
 
-const AuthorName = ({authors = [], className}) => {
+const AuthorName = ({ authors = [], className, withLink = true }) => {
+  const renderName = (author) => {
     return (
-        <>
-            {authors.map((author, idx) => (
-                <div key={idx}>
-                    <div>
-                        {author.name && (
-                            <div>
-                                {idx !== 0 ? idx !== authors.length - 1 ? ',' : 'and' : ''}
-                                <Link href={author.url} itemProp="url">
-                                    <span className={className} itemProp="name">{author.name}</span>
-                                </Link>
-                            </div>
-                            )
-                        }
-                    </div>
-                </div>
-            ))}
-        </>
-    )
-}
+      <span className={className} itemProp="name">
+        {author.name}
+      </span>
+    );
+  };
+  return (
+    <>
+      {authors.map((author, idx) => (
+        <div key={idx}>
+          <div>
+            {author.name && (
+              <div>
+                {idx !== 0 ? (idx !== authors.length - 1 ? "," : "and") : ""}
+                {withLink ? (
+                  <Link href={author.url} itemProp="url">
+                    {renderName(author)}
+                  </Link>
+                ) : (
+                  renderName(author)
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default AuthorName;
--- website/src/components/BlogsSlider/BlogCard.js
+++ website/src/components/BlogsSlider/BlogCard.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {useBaseUrlUtils} from "@docusaurus/core/lib/client/exports/useBaseUrl";
-import Link from '@docusaurus/Link';
+import { useBaseUrlUtils } from "@docusaurus/core/lib/client/exports/useBaseUrl";
+import Link from "@docusaurus/Link";
 
 import AuthorName from "@site/src/components/AuthorName";
 import ArrowRight from "./Icons/arrow-right.svg";
@@ -8,18 +8,9 @@ import styles from "./styles.module.css";
 
 const BlogCard = ({ blog }) => {
   const { withBaseUrl } = useBaseUrlUtils();
-  const {
-    frontMatter,
-    assets,
-    metadata,
-  } = blog;
-  const {
-    formattedDate,
-    title,
-    authors,
-    permalink,
-  } = metadata;
-  const image = assets.image ?? frontMatter.image ?? '/assets/images/hudi.png';
+  const { frontMatter, assets, metadata } = blog;
+  const { formattedDate, title, authors, permalink } = metadata;
+  const image = assets.image ?? frontMatter.image ?? "/assets/images/hudi.png";
 
   return (
     <div className={styles.blogsWrapper}>
@@ -27,12 +18,20 @@ const BlogCard = ({ blog }) => {
         <div className={styles.cardBlogs}>
           <div>
             <div className={styles.blogImgWrapper}>
-              <img src={withBaseUrl(image, {
-                absolute: true,
-              })} alt={title} className={styles.blogImg} />
+              <img
+                src={withBaseUrl(image, {
+                  absolute: true,
+                })}
+                alt={title}
+                className={styles.blogImg}
+              />
             </div>
             <div className={styles.blogContent}>
-              <AuthorName authors={authors} className={styles.authorName} />
+              <AuthorName
+                authors={authors}
+                className={styles.authorName}
+                withLink={false}
+              />
               <div className={styles.date}>{formattedDate}</div>
             </div>
             <div className={styles.cardTitleWrapper}>
