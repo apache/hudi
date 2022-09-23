d8fb075293a1cf3fcffde65057b75cde3ae7cd31
sivabalan <n.siva.b@gmail.com>
2022-09-23 07:31:08 -0700

    triggering  CI test

--- hudi-client/hudi-spark-client/src/test/java/org/apache/hudi/functional/SparkClientFunctionalTestSuite.java
+++ hudi-client/hudi-spark-client/src/test/java/org/apache/hudi/functional/SparkClientFunctionalTestSuite.java
@@ -19,16 +19,15 @@
 
 package org.apache.hudi.functional;
 
+import org.apache.hudi.client.functional.TestHoodieBackedMetadata;
+
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.IncludeTags;
-import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-@SelectPackages({
-    "org.apache.hudi.client.functional",
-    "org.apache.hudi.table.functional",
-    "org.apache.hudi.index.hbase"})
+@SelectClasses({TestHoodieBackedMetadata.class})
 @IncludeTags("functional")
 public class SparkClientFunctionalTestSuite {
 
