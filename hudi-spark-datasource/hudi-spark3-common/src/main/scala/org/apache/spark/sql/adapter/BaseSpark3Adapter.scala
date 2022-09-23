0f73927a7068b7de37e0cd4ed1835616ad2aeae2
喻兆靖 <yuzhaojing@bytedance.com>
2022-09-23 11:46:09 +0800

    Fix errors from cherry-picking

--- hudi-spark-datasource/hudi-spark3-common/src/main/scala/org/apache/spark/sql/adapter/BaseSpark3Adapter.scala
+++ hudi-spark-datasource/hudi-spark3-common/src/main/scala/org/apache/spark/sql/adapter/BaseSpark3Adapter.scala
@@ -30,12 +30,9 @@ import org.apache.spark.sql.connector.catalog.Table
 import org.apache.spark.sql.execution.datasources._
 import org.apache.spark.sql.execution.datasources.v2.DataSourceV2Relation
 import org.apache.spark.sql.hudi.SparkAdapter
-import org.apache.spark.sql.internal.SQLConf
-import org.apache.spark.sql.{HoodieCatalystPlansUtils, HoodieSpark3CatalystPlanUtils, Row, SparkSession}
-import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.{HoodieSpark3CatalogUtils, SparkSession}
+import org.apache.spark.sql.{HoodieSpark3CatalogUtils, Row, SparkSession}
 import org.apache.spark.storage.StorageLevel
-import org.apache.spark.storage.StorageLevel.{DISK_ONLY, DISK_ONLY_2, DISK_ONLY_3, MEMORY_AND_DISK, MEMORY_AND_DISK_2, MEMORY_AND_DISK_SER, MEMORY_AND_DISK_SER_2, MEMORY_ONLY, MEMORY_ONLY_2, MEMORY_ONLY_SER, MEMORY_ONLY_SER_2, NONE, OFF_HEAP}
+import org.apache.spark.storage.StorageLevel._
 
 import scala.util.control.NonFatal
 
@@ -46,8 +43,7 @@ abstract class BaseSpark3Adapter extends SparkAdapter with Logging {
 
   def getCatalogUtils: HoodieSpark3CatalogUtils
 
-  override def createSparkRowSerDe(schema: StructType): SparkRowSerDe = {
-    val encoder = RowEncoder(schema).resolveAndBind()
+  override def createSparkRowSerDe(encoder: ExpressionEncoder[Row]): SparkRowSerDe = {
     new Spark3RowSerDe(encoder)
   }
 
