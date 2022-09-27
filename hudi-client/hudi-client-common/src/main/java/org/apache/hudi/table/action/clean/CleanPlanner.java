646bddbcf41dcae50ae1367a0ed00e1fde2b6486
sivabalan <n.siva.b@gmail.com>
2022-09-27 10:27:31 -0700

    fixing rebase issues

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/action/clean/CleanPlanner.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/action/clean/CleanPlanner.java
@@ -361,13 +361,6 @@ public class CleanPlanner<T extends HoodieRecordPayload, I, K, O> implements Ser
                   deletePaths.add(new CleanFileInfo(hoodieDataFile.getBootstrapBaseFile().get().getPath(), true));
                 }
               });
-              if (hoodieTable.getMetaClient().getTableType() == HoodieTableType.MERGE_ON_READ
-                  || hoodieTable.getMetaClient().getTableConfig().isCDCEnabled()) {
-                // 1. If merge on read, then clean the log files for the commits as well;
-                // 2. If change log capture is enabled, clean the log files no matter the table type is mor or cow.
-                deletePaths.addAll(aSlice.getLogFiles().map(lf -> new CleanFileInfo(lf.getPath().toString(), false))
-                    .collect(Collectors.toList()));
-              }
             if (hoodieTable.getMetaClient().getTableType() == HoodieTableType.MERGE_ON_READ) {
               // If merge on read, then clean the log files for the commits as well
               Predicate<HoodieLogFile> notCDCLogFile =
