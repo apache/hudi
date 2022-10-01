f7afb216847edeaa10f9388fbf5dc27f385f36d5
Alexey Kudinkin <alexey@infinilake.com>
2022-09-30 18:43:13 -0700

    Fixing compilation

--- hudi-common/src/main/java/org/apache/hudi/avro/HoodieAvroUtils.java
+++ hudi-common/src/main/java/org/apache/hudi/avro/HoodieAvroUtils.java
@@ -72,7 +72,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
