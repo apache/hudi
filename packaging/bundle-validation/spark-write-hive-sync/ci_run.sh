a6312028bdfff15111fe26d29e9302b0655f6034
Raymond Xu <2701446+xushiyan@users.noreply.github.com>
2022-10-19 13:38:50 +0800

    fix for CI env

--- packaging/bundle-validation/spark-write-hive-sync/ci_run.sh
+++ packaging/bundle-validation/spark-write-hive-sync/ci_run.sh
@@ -21,35 +21,36 @@
 # this script is to run by GitHub Actions CI tasks from the project root directory
 # and contains environment-specific variables
 
+HUDI_VERSION=$1
 # to store bundle jars for validation
 mkdir ${GITHUB_WORKSPACE}/jars
-HUDI_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 cp packaging/hudi-spark-bundle/target/hudi-*-$HUDI_VERSION.jar ${GITHUB_WORKSPACE}/jars
-ls ${GITHUB_WORKSPACE}/jars
+echo 'Validating jars below:'
+ls -l ${GITHUB_WORKSPACE}/jars
 
 # choose versions based on build profiles
-if [[ ${SPARK_PROFILE} == '2.4' ]]; then
+if [[ ${SPARK_PROFILE} == 'spark2.4' ]]; then
   HADOOP_VERSION=2.7.7
   HIVE_VERSION=2.3.9
   DERBY_VERSION=10.10.2.0
   SPARK_VERSION=2.4.8
   SPARK_HADOOP_VERSION=2.7
   IMAGE_TAG=spark248hive239
-elif [[ ${SPARK_PROFILE} == '3.1' ]]; then
+elif [[ ${SPARK_PROFILE} == 'spark3.1' ]]; then
   HADOOP_VERSION=2.7.7
   HIVE_VERSION=3.1.3
   DERBY_VERSION=10.14.1.0
   SPARK_VERSION=3.1.3
   SPARK_HADOOP_VERSION=2.7
   IMAGE_TAG=spark313hive313
-elif [[ ${SPARK_PROFILE} == '3.2' ]]; then
+elif [[ ${SPARK_PROFILE} == 'spark3.2' ]]; then
   HADOOP_VERSION=2.7.7
   HIVE_VERSION=3.1.3
   DERBY_VERSION=10.14.1.0
   SPARK_VERSION=3.2.2
   SPARK_HADOOP_VERSION=2.7
   IMAGE_TAG=spark322hive313
-elif [[ ${SPARK_PROFILE} == '3.3' ]]; then
+elif [[ ${SPARK_PROFILE} == 'spark3.3' ]]; then
   HADOOP_VERSION=2.7.7
   HIVE_VERSION=3.1.3
   DERBY_VERSION=10.14.1.0
