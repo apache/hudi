fe64bb4263dee4e7ce1bed5ac62bf0a12347fd64
SteNicholas <programgeek@163.com>
2022-10-24 17:45:49 +0800

    [HUDI-5049] HoodieHiveCatalog supports the implementation of dropPartition

--- hudi-flink-datasource/hudi-flink/src/main/java/org/apache/hudi/table/catalog/HoodieHiveCatalog.java
+++ hudi-flink-datasource/hudi-flink/src/main/java/org/apache/hudi/table/catalog/HoodieHiveCatalog.java
@@ -92,7 +92,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
