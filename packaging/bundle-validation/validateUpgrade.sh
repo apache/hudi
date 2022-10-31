def2bad30344df7e3ed168afc27705acd401f816
Jonathan Vexler <=>
2022-10-31 18:21:33 -0400

    tmp changes

--- packaging/bundle-validation/validateUpgrade.sh
+++ packaging/bundle-validation/validateUpgrade.sh
@@ -73,6 +73,6 @@ apk add curl
 VERSIONS=$(curl https://repo1.maven.org/maven2/org/apache/hudi/hudi-utilities-bundle_2.11/ | grep "<a href=\"0." | awk '{print $2}' | cut -c 7- | rev | cut -c 3- | rev |  sort -t. -k 1,1nr -k 2,2nr -k 3,3nr -k 4,4nr | grep -m 5 -v '-') 
 for VERSION in "$VERSIONS[@]"
 do
-    upgrade_from_version VERSION
+    upgrade_from_version $VERSION
 done
 
