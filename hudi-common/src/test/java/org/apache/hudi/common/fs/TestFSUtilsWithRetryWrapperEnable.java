ce35a792aa22147c7578fd66238c57b287277109
yuzhao.cyz <yuzhao.cyz@gmail.com>
2022-11-28 16:22:39 +0800

    [HUDI-5286] UnsupportedOperationException throws when enabling filesystem retry

--- hudi-common/src/test/java/org/apache/hudi/common/fs/TestFSUtilsWithRetryWrapperEnable.java
+++ hudi-common/src/test/java/org/apache/hudi/common/fs/TestFSUtilsWithRetryWrapperEnable.java
@@ -37,6 +37,7 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
@@ -77,6 +78,14 @@ public class TestFSUtilsWithRetryWrapperEnable extends TestFSUtils {
     List<String> folders =
             Arrays.asList("2016/04/15", ".hoodie/.temp/2/2016/04/15");
     folders.forEach(f -> assertThrows(RuntimeException.class, () -> metaClient.getFs().mkdirs(new Path(new Path(basePath), f))));
+  }
+
+  @Test
+  public void testGetSchema() {
+    FakeRemoteFileSystem fakeFs = new FakeRemoteFileSystem(FSUtils.getFs(metaClient.getMetaPath(), metaClient.getHadoopConf()), 100);
+    FileSystem fileSystem = new HoodieRetryWrapperFileSystem(fakeFs, maxRetryIntervalMs, maxRetryNumbers, initialRetryIntervalMs, "");
+    HoodieWrapperFileSystem fs = new HoodieWrapperFileSystem(fileSystem, new NoOpConsistencyGuard());
+    assertDoesNotThrow(fs::getScheme, "Method #getSchema does not implement correctly");
   }
 
   /**
@@ -204,6 +213,11 @@ public class TestFSUtilsWithRetryWrapperEnable extends TestFSUtils {
     @Override
     public Configuration getConf() {
       return fs.getConf();
+    }
+
+    @Override
+    public String getScheme() {
+      return fs.getScheme();
     }
   }
 }
