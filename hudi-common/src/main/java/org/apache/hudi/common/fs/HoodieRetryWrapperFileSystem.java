e88b4748127c45f287e45a11dda9183e29ad87d5
Danny Chan <yuzhao.cyz@gmail.com>
2022-11-29 10:20:41 +0800

    [HUDI-5286] UnsupportedOperationException throws when enabling filesystem retry (#7313)

--- hudi-common/src/main/java/org/apache/hudi/common/fs/HoodieRetryWrapperFileSystem.java
+++ hudi-common/src/main/java/org/apache/hudi/common/fs/HoodieRetryWrapperFileSystem.java
@@ -258,4 +258,9 @@ public class HoodieRetryWrapperFileSystem extends FileSystem {
   public Configuration getConf() {
     return fileSystem.getConf();
   }
+
+  @Override
+  public String getScheme() {
+    return fileSystem.getScheme();
+  }
 }
