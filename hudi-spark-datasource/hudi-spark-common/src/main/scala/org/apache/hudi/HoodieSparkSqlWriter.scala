12a0897eac9ca7059da44c9cb6e625030493087c
Jon Vexler <jbvexler@gmail.com>
2022-12-07 22:23:34 -0500

    [HUDI-5295] One meta sync failure should not prevent other meta sync from occurring (#7367)
    Co-authored-by: Jonathan Vexler <=>

--- hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/HoodieSparkSqlWriter.scala
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/HoodieSparkSqlWriter.scala
@@ -61,6 +61,7 @@ import org.apache.spark.{SPARK_VERSION, SparkContext}
 
 import scala.collection.JavaConversions._
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 import scala.util.matching.Regex
 
 object HoodieSparkSqlWriter {
@@ -693,9 +694,20 @@ object HoodieSparkSqlWriter {
       properties.put(HoodieSyncConfig.META_SYNC_SPARK_VERSION.key, SPARK_VERSION)
       properties.put(HoodieSyncConfig.META_SYNC_USE_FILE_LISTING_FROM_METADATA.key, hoodieConfig.getBoolean(HoodieMetadataConfig.ENABLE))
 
+      //Collect exceptions in list because we want all sync to run. Then we can throw
+      val metaSyncExceptions = new ListBuffer[HoodieException]()
       syncClientToolClassSet.foreach(impl => {
-        SyncUtilHelpers.runHoodieMetaSync(impl.trim, properties, fs.getConf, fs, basePath.toString, baseFileFormat)
+        try {
+          SyncUtilHelpers.runHoodieMetaSync(impl.trim, properties, fs.getConf, fs, basePath.toString, baseFileFormat)
+        } catch {
+          case e: HoodieException =>
+            log.info("SyncTool class " + impl.trim + " failed with exception", e)
+            metaSyncExceptions.add(e)
+        }
       })
+      if (metaSyncExceptions.nonEmpty) {
+        throw SyncUtilHelpers.getExceptionFromList(metaSyncExceptions)
+      }
     }
 
     // Since Hive tables are now synced as Spark data source tables which are cached after Spark SQL queries
