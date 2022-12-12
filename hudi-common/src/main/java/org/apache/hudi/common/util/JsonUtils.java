5c5a904791a88321430559402e1acd8908bfe24f
Raymond Xu <2701446+xushiyan@users.noreply.github.com>
2022-12-12 17:45:51 +0800

    revert jsr310 related changes

--- hudi-common/src/main/java/org/apache/hudi/common/util/JsonUtils.java
+++ hudi-common/src/main/java/org/apache/hudi/common/util/JsonUtils.java
@@ -26,18 +26,15 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 /**
  * Utils for JSON serialization and deserialization.
  */
 public class JsonUtils {
 
-  private static final ObjectMapper MAPPER = new ObjectMapper().registerModule(new JavaTimeModule());
+  private static final ObjectMapper MAPPER = new ObjectMapper();
 
   static {
-    MAPPER.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     MAPPER.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     // We need to exclude custom getters, setters and creators which can use member fields
     // to derive new fields, so that they are not included in the serialization
