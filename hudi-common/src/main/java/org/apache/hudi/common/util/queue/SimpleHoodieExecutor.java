783a5cd9986d5269a7aeeeeafba3a20c91113ca4
yuezhang <yuezhang@freewheel.tv>
2022-12-13 17:43:27 +0800

    address comments

--- hudi-common/src/main/java/org/apache/hudi/common/util/queue/SimpleHoodieExecutor.java
+++ hudi-common/src/main/java/org/apache/hudi/common/util/queue/SimpleHoodieExecutor.java
@@ -26,7 +26,6 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import java.util.Iterator;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 
 /**
