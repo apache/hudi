9709b1b4a6811c431cf20684012ba1b7a68694c0
Alexey Kudinkin <alexey@infinilake.com>
2023-01-23 23:45:50 -0800

    Added `destroy` method to `Transient` allowing to free up any references held, as well as destroy held objects

--- hudi-common/src/main/java/org/apache/hudi/util/Transient.java
+++ hudi-common/src/main/java/org/apache/hudi/util/Transient.java
@@ -19,9 +19,12 @@
 package org.apache.hudi.util;
 
 import org.apache.hudi.common.function.SerializableSupplier;
+import org.apache.hudi.common.function.ThrowingConsumer;
 
 import javax.annotation.concurrent.ThreadSafe;
 import java.io.Serializable;
+
+import static org.apache.hudi.common.util.ValidationUtils.checkArgument;
 
 /**
  * {@link Serializable} counterpart of {@link Lazy}
@@ -31,18 +34,23 @@ import java.io.Serializable;
 @ThreadSafe
 public class Transient<T> implements Serializable {
 
-  private transient boolean initialized;
+  private SerializableSupplier<T> initializer;
 
-  private final SerializableSupplier<T> initializer;
+  private transient boolean initialized;
   private transient T ref;
 
   private Transient(SerializableSupplier<T> initializer) {
+    checkArgument(initializer != null);
+
     this.initializer = initializer;
     this.ref = null;
     this.initialized = false;
   }
 
   private Transient(T value, SerializableSupplier<T> initializer) {
+    checkArgument(value != null);
+    checkArgument(initializer != null);
+
     this.initializer = initializer;
     this.ref = value;
     this.initialized = true;
@@ -65,6 +73,18 @@ public class Transient<T> implements Serializable {
     synchronized (this) {
       this.ref = null;
       this.initialized = false;
+    }
+  }
+
+  public void destroy(ThrowingConsumer<T> cleaner) throws Exception {
+    synchronized (this) {
+      if (initialized) {
+        cleaner.accept(ref);
+      }
+
+      this.ref = null;
+      this.initialized = false;
+      this.initializer = null;
     }
   }
 
