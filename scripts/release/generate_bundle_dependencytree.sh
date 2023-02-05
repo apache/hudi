c04161b542bbadf5a591f8fe5232672a2a4ce913
Lokesh Jain <ljain@apache.org>
2023-02-05 17:11:52 +0530

    Fix the script

--- scripts/release/generate_bundle_dependencytree.sh
+++ scripts/release/generate_bundle_dependencytree.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 #
 # Licensed to the Apache Software Foundation (ASF) under one
@@ -25,6 +25,7 @@ declare -a ARTIFACT_IDS=(
 "hudi-flink1.13-bundle"
 "hudi-flink1.14-bundle"
 "hudi-flink1.15-bundle"
+"hudi-flink1.16-bundle"
 "hudi-gcp-bundle"
 "hudi-hadoop-mr-bundle"
 "hudi-hive-sync-bundle"
@@ -45,24 +46,57 @@ declare -a ARTIFACT_IDS=(
 "hudi-utilities-slim-bundle_2.12"
 )
 
-VERSION=0.12.2
-SCALA_OPT=""
+VERSION=0.13.0-rc1
 OUTPUT_DIRECTORY="packaging/dep-tree/$VERSION"
 
 mkdir -p $OUTPUT_DIRECTORY
 
 for aid in "${ARTIFACT_IDS[@]}"
 do
+  SCALA_OPT=""
+  EXTRA_OPT=""
+
   if [[ $aid == *_2.11 ]]; then
-    SCALA_OPT="-Pscala-2.11";
+    SCALA_OPT="-Dscala-2.11";
   elif [[ $aid == *_2.12 ]]; then
-    SCALA_OPT="-Pscala-2.12";
+    SCALA_OPT="-Dscala-2.12";
+  fi
+
+  if [[ $aid == hudi-flink1.13* ]]; then
+    EXTRA_OPT="-Dflink1.13";
+    SCALA_OPT="-Dscala-2.12";
+  elif [[ $aid == hudi-flink1.14* ]]; then
+    EXTRA_OPT="-Dflink1.14";
+    SCALA_OPT="-Dscala-2.12";
+  elif [[ $aid == hudi-flink1.15* ]]; then
+    EXTRA_OPT="-Dflink1.15";
+    SCALA_OPT="-Dscala-2.12";
+  elif [[ $aid == hudi-flink1.16* ]]; then
+    EXTRA_OPT="-Dflink1.16";
+    SCALA_OPT="-Dscala-2.12";
+  elif [[ $aid == hudi-flink1.16* ]]; then
+    EXTRA_OPT="-Dflink1.16";
+    SCALA_OPT="-Dscala-2.12";
+  elif [[ $aid == hudi-integ-test-bundle ]]; then
+    EXTRA_OPT="-Pintegration-tests";
+  fi
+
+  if [[ $aid == hudi-spark2.4* ]]; then
+    EXTRA_OPT="-Dspark2.4";
+  elif [[ $aid == hudi-spark3-* ]]; then
+    EXTRA_OPT="-Dspark3";
+  elif [[ $aid == hudi-spark3.1* ]]; then
+    EXTRA_OPT="-Dspark3.1";
+  elif [[ $aid == hudi-spark3.2* ]]; then
+    EXTRA_OPT="-Dspark3.2";
+  elif [[ $aid == hudi-spark3.3* ]]; then
+    EXTRA_OPT="-Dspark3.3";
   fi
 
   cmd="mvn \
   com.github.ferstl:depgraph-maven-plugin:4.0.2:for-artifact \
   -DgraphFormat=text -DshowGroupIds=true -DshowVersions=true -DrepeatTransitiveDependenciesInTextGraph \
-  -DoutputDirectory=$OUTPUT_DIRECTORY -DoutputFileName=$aid.deptree.$VERSION.txt \
-  -DgroupId=org.apache.hudi -DartifactId=$aid -Dversion=$VERSION $SCALA_OPT"
+  -DoutputDirectory=$OUTPUT_DIRECTORY -DoutputFileName=$aid.deptree.txt \
+  -DgroupId=org.apache.hudi -DartifactId=$aid -Dversion=$VERSION $SCALA_OPT $EXTRA_OPT"
   $cmd
 done
