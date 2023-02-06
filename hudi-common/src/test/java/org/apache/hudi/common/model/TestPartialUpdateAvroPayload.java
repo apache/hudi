f400a032c996f52b4153ef8cf5ac89bb6677f100
苏承祥 <sucx@tuya.com>
2023-02-06 19:23:35 +0800

    add test

--- hudi-common/src/test/java/org/apache/hudi/common/model/TestPartialUpdateAvroPayload.java
+++ hudi-common/src/test/java/org/apache/hudi/common/model/TestPartialUpdateAvroPayload.java
@@ -39,8 +39,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
  * Unit tests {@link TestPartialUpdateAvroPayload}.
  */
 public class TestPartialUpdateAvroPayload {
-  private Schema schema;
-
   String jsonSchema = "{\n"
       + "  \"type\": \"record\",\n"
       + "  \"name\": \"partialRecord\", \"namespace\":\"org.apache.hudi\",\n"
@@ -58,6 +56,25 @@ public class TestPartialUpdateAvroPayload {
       + "    {\"name\": \"child\", \"type\": [\"null\", {\"type\": \"array\", \"items\": \"string\"}]}\n"
       + "  ]\n"
       + "}";
+  String jsonSchema2 = "{\n"
+      + "  \"type\": \"record\",\n"
+      + "  \"name\": \"partialRecord\", \"namespace\":\"org.apache.hudi\",\n"
+      + "  \"fields\": [\n"
+      + "    {\"name\": \"_hoodie_commit_time\", \"type\": [\"null\", \"string\"]},\n"
+      + "    {\"name\": \"_hoodie_commit_seqno\", \"type\": [\"null\", \"string\"]},\n"
+      + "    {\"name\": \"_hoodie_record_key\", \"type\": [\"null\", \"string\"]},\n"
+      + "    {\"name\": \"_hoodie_partition_path\", \"type\": [\"null\", \"string\"]},\n"
+      + "    {\"name\": \"_hoodie_file_name\", \"type\": [\"null\", \"string\"]},\n"
+      + "    {\"name\": \"id\", \"type\": [\"null\", \"string\"]},\n"
+      + "    {\"name\": \"partition\", \"type\": [\"null\", \"string\"]},\n"
+      + "    {\"name\": \"ts\", \"type\": [\"null\", \"long\"]},\n"
+      + "    {\"name\": \"ts1\", \"type\": [\"null\", \"long\"]},\n"
+      + "    {\"name\": \"_hoodie_is_deleted\", \"type\": \"boolean\", \"default\": false},\n"
+      + "    {\"name\": \"city\", \"type\": [\"null\", \"string\"]},\n"
+      + "    {\"name\": \"child\", \"type\": [\"null\", {\"type\": \"array\", \"items\": \"string\"}]}\n"
+      + "  ]\n"
+      + "}";
+  private Schema schema;
 
   @BeforeEach
   public void setUp() throws Exception {
@@ -205,4 +222,151 @@ public class TestPartialUpdateAvroPayload {
     assertEquals(mergedRecord2.get("_hoodie_commit_time").toString(), record2.get("_hoodie_commit_time").toString());
     assertEquals(mergedRecord2.get("_hoodie_commit_seqno").toString(), record2.get("_hoodie_commit_seqno").toString());
   }
+
+  @Test
+  public void testWithOrderFieldPropKey() throws IOException {
+    // test with same field config
+    Properties properties = new Properties();
+    properties.put(HoodiePayloadProps.PAYLOAD_ORDERING_FIELD_PROP_KEY, "ts");
+    properties.put("hoodie.datasource.write.precombine.field", "ts");
+
+    GenericRecord record1 = new GenericData.Record(schema);
+    record1.put("id", "1");
+    record1.put("partition", "partition1");
+    record1.put("ts", 0L);
+    record1.put("_hoodie_is_deleted", false);
+    record1.put("city", "NY0");
+    record1.put("child", Arrays.asList("A"));
+
+    GenericRecord record2 = new GenericData.Record(schema);
+    record2.put("id", "1");
+    record2.put("partition", "partition1");
+    record2.put("ts", 1L);
+    record2.put("_hoodie_is_deleted", false);
+    record2.put("city", null);
+    record2.put("child", Arrays.asList("B"));
+
+    GenericRecord record3 = new GenericData.Record(schema);
+    record3.put("id", "1");
+    record3.put("partition", "partition1");
+    record3.put("ts", 2L);
+    record3.put("_hoodie_is_deleted", false);
+    record3.put("city", "NY0");
+    record3.put("child", Arrays.asList("A"));
+
+    GenericRecord record4 = new GenericData.Record(schema);
+    record4.put("id", "1");
+    record4.put("partition", "partition1");
+    record4.put("ts", 1L);
+    record4.put("_hoodie_is_deleted", false);
+    record4.put("city", "NY0");
+    record4.put("child", Arrays.asList("B"));
+
+    // Test preCombine: since payload2's preCombine val is larger, so payload2 will overwrite payload1 with its non-default field's value
+    PartialUpdateAvroPayload payload1 = new PartialUpdateAvroPayload(record1, 0L);
+    PartialUpdateAvroPayload payload2 = new PartialUpdateAvroPayload(record2, 1L);
+    assertArrayEquals(payload1.preCombine(payload2, schema, properties).recordBytes, new PartialUpdateAvroPayload(record4, 1L).recordBytes);
+    assertArrayEquals(payload2.preCombine(payload1, schema, properties).recordBytes, new PartialUpdateAvroPayload(record4, 1L).recordBytes);
+
+
+    assertEquals(record1, payload1.getInsertValue(schema).get());
+    assertEquals(record2, payload2.getInsertValue(schema).get());
+
+    // Test preCombine again: let payload1's preCombine val larger than payload2
+    record1.put("ts", 2L);
+    payload1 = new PartialUpdateAvroPayload(record1, 2L);
+    payload2 = new PartialUpdateAvroPayload(record2, 1L);
+    assertArrayEquals(payload1.preCombine(payload2, schema, properties).recordBytes, new PartialUpdateAvroPayload(record3, 2L).recordBytes);
+    assertArrayEquals(payload2.preCombine(payload1, schema, properties).recordBytes, new PartialUpdateAvroPayload(record3, 2L).recordBytes);
+
+    // Test combineAndGetUpdateValue: let payload1's ordering val larger than payload2, then payload1 will overwrite payload2 with its non-default field's value
+    record1.put("ts", 2L);
+    payload1 = new PartialUpdateAvroPayload(record1, 2L);
+    assertEquals(payload1.combineAndGetUpdateValue(record2, schema, properties).get(), record3);
+    // Test combineAndGetUpdateValue: let payload1's ordering val equal to  payload2, then payload2 will be considered to newer record
+    record1.put("ts", 1L);
+    assertEquals(payload2.combineAndGetUpdateValue(record1, schema, properties).get(), record4);
+
+
+    schema = new Schema.Parser().parse(jsonSchema2);
+
+    // test with diff field config
+    properties = new Properties();
+    // change hoodie.payload.ordering.field config
+    properties.put(HoodiePayloadProps.PAYLOAD_ORDERING_FIELD_PROP_KEY, "ts1");
+    properties.put("hoodie.datasource.write.precombine.field", "ts");
+
+    record1 = new GenericData.Record(schema);
+    record1.put("id", "1");
+    record1.put("partition", "partition1");
+    record1.put("ts", 0L);
+    record1.put("ts1", 0L);
+    record1.put("_hoodie_is_deleted", false);
+    record1.put("city", "NY0");
+    record1.put("child", Arrays.asList("A"));
+
+    record2 = new GenericData.Record(schema);
+    record2.put("id", "1");
+    record2.put("partition", "partition1");
+    record2.put("ts", 1L);
+    record2.put("ts1", 1L);
+    record2.put("_hoodie_is_deleted", false);
+    record2.put("city", null);
+    record2.put("child", Arrays.asList("B"));
+
+    record3 = new GenericData.Record(schema);
+    record3.put("id", "1");
+    record3.put("partition", "partition1");
+    record3.put("ts", 2L);
+    record3.put("ts1", 0L);
+    record3.put("_hoodie_is_deleted", false);
+    record3.put("city", "NY0");
+    record3.put("child", Arrays.asList("A"));
+
+    record4 = new GenericData.Record(schema);
+    record4.put("id", "1");
+    record4.put("partition", "partition1");
+    record4.put("ts", 1L);
+    record4.put("ts1", 1L);
+    record4.put("_hoodie_is_deleted", false);
+    record4.put("city", "NY0");
+    record4.put("child", Arrays.asList("B"));
+
+    GenericRecord record5 = new GenericData.Record(schema);
+    record5.put("id", "1");
+    record5.put("partition", "partition1");
+    record5.put("ts", 2L);
+    record5.put("ts1", 2L);
+    record5.put("_hoodie_is_deleted", false);
+    record5.put("city", "NY0");
+    record5.put("child", Arrays.asList("A"));
+
+    // Test preCombine: since payload2's ordering val is larger, so payload2 will overwrite payload1 with its non-default field's value
+    payload1 = new PartialUpdateAvroPayload(record1, 0L);
+    payload2 = new PartialUpdateAvroPayload(record2, 1L);
+    assertArrayEquals(payload1.preCombine(payload2, schema, properties).recordBytes, new PartialUpdateAvroPayload(record4, 1L).recordBytes);
+    assertArrayEquals(payload2.preCombine(payload1, schema, properties).recordBytes, new PartialUpdateAvroPayload(record4, 1L).recordBytes);
+
+    assertEquals(record1, payload1.getInsertValue(schema).get());
+    assertEquals(record2, payload2.getInsertValue(schema).get());
+
+
+    // Test preCombine again: let payload1's preCombine val larger than payload2
+    record1.put("ts", 2L);
+    payload1 = new PartialUpdateAvroPayload(record1, 2L);
+    payload2 = new PartialUpdateAvroPayload(record2, 1L);
+    assertArrayEquals(payload1.preCombine(payload2, schema, properties).recordBytes, new PartialUpdateAvroPayload(record3, 2L).recordBytes);
+    assertArrayEquals(payload2.preCombine(payload1, schema, properties).recordBytes, new PartialUpdateAvroPayload(record3, 2L).recordBytes);
+
+
+    // Test combineAndGetUpdateValue: let payload1's order val larger than payload2, then payload1 will overwrite payload2 with its non-default field's value
+    record1.put("ts1", 2L);
+    payload1 = new PartialUpdateAvroPayload(record1, 2L);
+    assertEquals(payload1.combineAndGetUpdateValue(record2, schema, properties).get(), record5);
+    // Test combineAndGetUpdateValue: let payload1's order val equal to  payload2, then payload2 will be considered to newer record
+    record1.put("ts1", 1L);
+    assertEquals(payload2.combineAndGetUpdateValue(record1, schema, properties).get(), record4);
+
+  }
+
 }
