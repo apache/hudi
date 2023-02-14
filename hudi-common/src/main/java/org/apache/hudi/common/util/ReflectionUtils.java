d3a34bac1d7b0ef512b3e8cbcb42b5163cb6bca8
Alexey Kudinkin <alexey@infinilake.com>
2023-02-14 12:56:49 -0800

    Revisited `ReflectionUtils` adding new no-throw utils

--- hudi-common/src/main/java/org/apache/hudi/common/util/ReflectionUtils.java
+++ hudi-common/src/main/java/org/apache/hudi/common/util/ReflectionUtils.java
@@ -18,6 +18,8 @@
 
 package org.apache.hudi.common.util;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.apache.hudi.exception.HoodieException;
 
 import org.apache.log4j.LogManager;
@@ -29,12 +31,12 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -45,39 +47,132 @@ public class ReflectionUtils {
 
   private static final Logger LOG = LogManager.getLogger(ReflectionUtils.class);
 
-  private static final Map<String, Class<?>> CLAZZ_CACHE = new HashMap<>();
+  private static final Cache<String, Class<?>> LOADED_CLASSES_CACHE =
+      Caffeine.newBuilder()
+          .maximumSize(2000)
+          .weakValues()
+          .expireAfterAccess(Duration.of(5, ChronoUnit.MINUTES))
+          .build();
 
-  public static Class<?> getClass(String clazzName) {
-    synchronized (CLAZZ_CACHE) {
-      if (!CLAZZ_CACHE.containsKey(clazzName)) {
-        try {
-          Class<?> clazz = Class.forName(clazzName);
-          CLAZZ_CACHE.put(clazzName, clazz);
-        } catch (ClassNotFoundException e) {
-          throw new HoodieException("Unable to load class", e);
-        }
-      }
+  /**
+   * Tries to load class identified by provide {@code qualifiedName}.
+   * Throws {@link HoodieException} in case class couldn't be loaded
+   *
+   * @param qualifiedName target class's qualified name
+   * @return instance of {@link Class} corresponding to the class identified by the {@code qualifiedName}
+   */
+  public static Class<?> getClass(String qualifiedName) {
+    Class<?> klass = getClassNoThrow(qualifiedName);
+    if (klass != null) {
+      return klass;
     }
-    return CLAZZ_CACHE.get(clazzName);
+
+    throw new HoodieException(String.format("Unable to load class '%s'", qualifiedName));
   }
 
-  public static <T> T loadClass(String className) {
+  /**
+   * Tries to load class identified by provide {@code qualifiedName}.
+   * Returns null in case class couldn't be loaded
+   *
+   * @param qualifiedName target class's qualified name
+   * @return instance of {@link Class} corresponding to the class identified by the {@code qualifiedName}
+   */
+  public static Class<?> getClassNoThrow(String qualifiedName) {
+    return LOADED_CLASSES_CACHE.get(qualifiedName, name -> {
+      try {
+        return Class.forName(qualifiedName);
+      } catch (ClassNotFoundException e) {
+        LOG.info(String.format("Failed to load class '%s'", qualifiedName));
+        return null;
+      }
+    });
+  }
+
+  /**
+   * Creates new instance of the class identified by {@code classQualifiedName}.
+   * Will throw in case class couldn't be loaded or instantiated
+   */
+  public static <T> T newInstance(String classQualifiedName) {
     try {
-      return (T) getClass(className).newInstance();
+      return (T) getClass(classQualifiedName).newInstance();
     } catch (InstantiationException | IllegalAccessException e) {
-      throw new HoodieException("Could not load class " + className, e);
+      throw new HoodieException(String.format("Failed to create new instance for class '%s'", classQualifiedName), e);
     }
   }
 
   /**
-   * Creates an instance of the given class. Use this version when dealing with interface types as constructor args.
+   * Creates new instance of the class identified by {@code classQualifiedName}.
+   * Returns null in case class couldn't be loaded or instantiated
    */
+  public static <T> T newInstanceNoThrow(String classQualifiedName) {
+    try {
+      Class<?> klass = getClassNoThrow(classQualifiedName);
+      return klass != null ? (T) klass.newInstance() : null;
+    } catch (InstantiationException | IllegalAccessException e) {
+      LOG.info(String.format("Failed to create new instance for class '%s'", classQualifiedName));
+      return null;
+    }
+  }
+
+  /**
+   * Creates new instance of the class identified by {@code classQualifiedName}.
+   * Will throw in case class couldn't be loaded or instantiated
+   *
+   * @param classQualifiedName qualified name of the target class
+   * @param constructorArgTypes corresponding types of the arguments of class' ctor that should be invoked
+   * @param constructorArgs argument values to be passed into the target ctor
+   * @return new instance of the target class
+   */
+  public static Object newInstance(String classQualifiedName, Class<?>[] constructorArgTypes, Object... constructorArgs) {
+    try {
+      return getClass(classQualifiedName).getConstructor(constructorArgTypes).newInstance(constructorArgs);
+    } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+      throw new HoodieException("Unable to instantiate class " + classQualifiedName, e);
+    }
+  }
+
+  /**
+   * Creates new instance of the class identified by {@code classQualifiedName}.
+   * Will throw in case class couldn't be loaded or instantiated
+   *
+   * @param classQualifiedName qualified name of the target class
+   * @param constructorArgs argument values to be passed into the target ctor
+   * @return new instance of the target class
+   */
+  public static Object newInstance(String classQualifiedName, Object... constructorArgs) {
+    Class<?>[] constructorArgTypes = Arrays.stream(constructorArgs).map(Object::getClass).toArray(Class<?>[]::new);
+    return newInstance(classQualifiedName, constructorArgTypes, constructorArgs);
+  }
+
+  /**
+   * @deprecated use {@link #newInstance(String)} instead
+   */
+  @Deprecated
+  public static <T> T loadClass(String className) {
+    return newInstance(className);
+  }
+
+  /**
+   * Creates an instance of the given class. Use this version when dealing with interface types as constructor args.
+   * @deprecated
+   */
+  @Deprecated
   public static Object loadClass(String clazz, Class<?>[] constructorArgTypes, Object... constructorArgs) {
     try {
       return getClass(clazz).getConstructor(constructorArgTypes).newInstance(constructorArgs);
     } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
       throw new HoodieException("Unable to instantiate class " + clazz, e);
     }
+  }
+
+  /**
+   * Creates an instance of the given class. Constructor arg types are inferred.
+   * @deprecated
+   */
+  @Deprecated
+  public static Object loadClass(String clazz, Object... constructorArgs) {
+    Class<?>[] constructorArgTypes = Arrays.stream(constructorArgs).map(Object::getClass).toArray(Class<?>[]::new);
+    return loadClass(clazz, constructorArgTypes, constructorArgs);
   }
 
   /**
@@ -96,14 +191,6 @@ public class ReflectionUtils {
       LOG.warn("Unable to instantiate class " + clazz, e);
       return false;
     }
-  }
-
-  /**
-   * Creates an instance of the given class. Constructor arg types are inferred.
-   */
-  public static Object loadClass(String clazz, Object... constructorArgs) {
-    Class<?>[] constructorArgTypes = Arrays.stream(constructorArgs).map(Object::getClass).toArray(Class<?>[]::new);
-    return loadClass(clazz, constructorArgTypes, constructorArgs);
   }
 
   /**
@@ -171,18 +258,12 @@ public class ReflectionUtils {
 
   /**
    * Invoke a static method of a class.
-   * @param clazz
-   * @param methodName
-   * @param args
-   * @param parametersType
    * @return the return value of the method
    */
   public static Object invokeStaticMethod(String clazz, String methodName, Object[] args, Class<?>... parametersType) {
     try {
-      Method method = Class.forName(clazz).getMethod(methodName, parametersType);
+      Method method = getClass(clazz).getMethod(methodName, parametersType);
       return method.invoke(null, args);
-    } catch (ClassNotFoundException e) {
-      throw new HoodieException("Unable to find the class " + clazz, e);
     } catch (NoSuchMethodException e) {
       throw new HoodieException(String.format("Unable to find the method %s of the class %s ",  methodName, clazz), e);
     } catch (InvocationTargetException | IllegalAccessException e) {
