d6870f1c1e8e51156d099a370106e7f86e57d51e
lvhu-goodluck <81349721+lvhu-goodluck@users.noreply.github.com>
2023-02-16 11:11:22 +0800

    Update HoodieFileIndex.scala

--- hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/HoodieFileIndex.scala
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/HoodieFileIndex.scala
@@ -38,7 +38,7 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql.{Column, SparkSession}
 import org.apache.spark.unsafe.types.UTF8String
 import java.text.SimpleDateFormat
-
+import javax.annotation.concurrent.NotThreadSafe
 import scala.collection.JavaConverters._
 import scala.util.control.NonFatal
 import scala.util.{Failure, Success, Try}
