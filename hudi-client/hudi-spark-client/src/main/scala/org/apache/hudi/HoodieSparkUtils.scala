97b20e746d1c2b345458093f43c30fe9cb3065ee
Lokesh Jain <ljain@apache.org>
2023-02-22 13:18:21 +0530

    adding row json to quarantine

--- hudi-client/hudi-spark-client/src/main/scala/org/apache/hudi/HoodieSparkUtils.scala
+++ hudi-client/hudi-spark-client/src/main/scala/org/apache/hudi/HoodieSparkUtils.scala
@@ -26,7 +26,7 @@ import org.apache.hudi.client.utils.SparkRowSerDe
 import org.apache.hudi.common.model.HoodieRecord
 import org.apache.spark.SPARK_VERSION
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame}
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.execution.SQLConfInjectingRDD
 import org.apache.spark.sql.internal.SQLConf
@@ -157,10 +157,13 @@ object HoodieSparkUtils extends SparkAdapterSupport with SparkVersionsSupport {
         } else {
           val writerAvroSchema = new Schema.Parser().parse(writerAvroSchemaStr)
           val convert = AvroConversionUtils.createInternalRowToAvroConverter(writerSchema, writerAvroSchema, nullable = nullable)
+          val rowDeserializer = getCatalystRowSerDe(writerSchema)
           val transform: InternalRow => Either[GenericRecord, String] = internalRow => try {
             Left(HoodieAvroUtils.rewriteRecordDeep(convert(internalRow), readerAvroSchema, true))
           } catch {
-            case _: Throwable => Right(internalRow.toSeq(writerSchema).mkString(","))
+            case _: Throwable =>
+              val rdd = df.sparkSession.sparkContext.parallelize(Seq(rowDeserializer.deserializeRow(internalRow)))
+              Right(df.sqlContext.createDataFrame(rdd, writerSchema).toJSON.first())
           }
           rows.map(transform)
         }
