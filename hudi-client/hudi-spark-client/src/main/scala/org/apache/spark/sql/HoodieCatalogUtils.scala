0241dc20d80d9f8d5fee38637ba1c3b5277ed55f
Alexey Kudinkin <alexey@infinilake.com>
2023-02-23 13:23:52 -0800

    Tidying up

--- hudi-client/hudi-spark-client/src/main/scala/org/apache/spark/sql/HoodieCatalogUtils.scala
+++ hudi-client/hudi-spark-client/src/main/scala/org/apache/spark/sql/HoodieCatalogUtils.scala
@@ -17,9 +17,8 @@
 
 package org.apache.spark.sql
 
-import org.apache.spark.sql.catalyst.analysis.UnresolvedRelation
-import org.apache.spark.sql.catalyst.{QualifiedTableName, TableIdentifier}
 import org.apache.spark.sql.catalyst.catalog.CatalogTableType
+import org.apache.spark.sql.catalyst.{QualifiedTableName, TableIdentifier}
 
 /**
  * NOTE: Since support for [[TableCatalog]] was only added in Spark 3, this trait
@@ -29,15 +28,32 @@ trait HoodieCatalogUtils {}
 
 object HoodieCatalogUtils {
 
+  /**
+   * Please check scala-doc for other overloaded [[refreshTable()]] operation
+   */
   def refreshTable(spark: SparkSession, qualifiedTableName: String): Unit = {
     val tableId = spark.sessionState.sqlParser.parseTableIdentifier(qualifiedTableName)
     refreshTable(spark, tableId)
   }
 
   /**
-   * NOTE: This is borrowed from Spark 3.1.3
+   * Refreshes metadata and flushes cached data (resolved [[LogicalPlan]] representation,
+   * already loaded [[InMemoryRelation]]) for the table identified by [[tableId]].
    *
-   * TODO elaborate
+   * This method is usually invoked at the ond of the write operation to make sure cached
+   * data/metadata are synchronized with the state on storage.
+   *
+   * NOTE: PLEASE READ CAREFULLY BEFORE CHANGING
+   *       This is borrowed from Spark 3.1.3 and modified to satisfy Hudi needs:
+   *          - Unlike Spark canonical implementation, in case of Hudi this method is invoked
+   *            after writes carried out via Spark DataSource integration as well and as such
+   *            in these cases data might actually be missing from the caches, therefore
+   *            actually re-triggering resolution phase (involving file-listing, etc) for the
+   *            first time
+   *          - Additionally, this method is modified to avoid refreshing [[LogicalRelation]]
+   *            completely to make sure that we're not re-triggering the file-listing of the
+   *            table, immediately after it's been written, instead deferring it to subsequent
+   *            read operation
    */
   def refreshTable(spark: SparkSession, tableId: TableIdentifier): Unit = {
     val sessionCatalog = spark.sessionState.catalog
@@ -50,8 +66,8 @@ object HoodieCatalogUtils {
       QualifiedTableName(tableId.database.getOrElse(tableMetadata.database), tableId.identifier))
 
     if (cachedPlan != null) {
-      // NOTE: Provided that this table is still cached, following operation wouldn't trigger
-      //       subsequent resolving and ultimately listing of the table
+      // NOTE: Provided that this table is still cached, following operation would not be
+      //       triggering subsequent resolution and listing of the table
       val table = spark.table(tableId)
 
       if (tableMetadata.tableType == CatalogTableType.VIEW) {
@@ -63,28 +79,14 @@ object HoodieCatalogUtils {
         sessionCatalog.refreshTable(tableId)
       }
 
-      // If this table is cached as an InMemoryRelation, drop the original
-      // cached version and make the new version cached lazily
-      val cache = spark.sharedState.cacheManager.lookupCachedData(table)
-
-      // Uncache the logical plan
+      // Here we simply clear up the cached data for this particular logical plan as well as
+      // all other entries dependent on it. On top of that, Spark original implementation however,
+      // also caches the new refreshed logical plan. By avoiding refreshing here (w/in the scope
+      // of the write) we instead defer it to occur during subsequent read operation.
+      //
       // NOTE: This is a no-op for the table itself if it's not cached, but will invalidate all
-      // caches referencing this table.
+      //       caches referencing this table.
       spark.sharedState.cacheManager.uncacheQuery(table, cascade = true)
-
-      // TODO can we refresh the CacheManager if we're not refreshing the relation?
-      //if (cache.nonEmpty) {
-      //  // save the cache name and cache level for recreation
-      //  val cacheName = cache.get.cachedRepresentation.cacheBuilder.tableName
-      //  val cacheLevel = cache.get.cachedRepresentation.cacheBuilder.storageLevel
-      //
-      //  // creates a new logical plan since the old table refers to old relation which
-      //  // should be refreshed
-      //  val newTable = spark.table(tableIdent)
-      //
-      //  // recache with the same name and cache level.
-      //  spark.sharedState.cacheManager.cacheQuery(newTable, cacheName, cacheLevel)
-      //}
     }
   }
 }
