7f0176b3c59118a06ff10387494be7701b78afe5
watermelon12138 <49849410+watermelon12138@users.noreply.github.com>
2023-02-27 19:46:54 +0800

    [MINOR] fix  the compilation failure for master branch. (#8059)
    Co-authored-by: y00617041 <yangxuan42@huawei.com>

--- hudi-flink-datasource/hudi-flink/src/main/java/org/apache/hudi/util/RowDataToAvroConverters.java
+++ hudi-flink-datasource/hudi-flink/src/main/java/org/apache/hudi/util/RowDataToAvroConverters.java
@@ -37,7 +37,6 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.nio.ByteBuffer;
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
