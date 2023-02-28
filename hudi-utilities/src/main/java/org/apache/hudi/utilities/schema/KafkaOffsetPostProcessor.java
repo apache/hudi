0f38caa3fa846795806ff5eaca64b9fa26212719
sivabalan <n.siva.b@gmail.com>
2023-02-27 21:07:02 -0800

    Addressing minor comments

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/schema/KafkaOffsetPostProcessor.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/schema/KafkaOffsetPostProcessor.java
@@ -39,7 +39,9 @@ public class KafkaOffsetPostProcessor extends SchemaPostProcessor {
     public static final ConfigProperty<String> KAFKA_APPEND_OFFSETS = ConfigProperty
         .key("hoodie.deltastreamer.source.kafka.append.offsets")
         .defaultValue("false")
-        .withDocumentation("append kafka offsets");
+        .withDocumentation("When enabled, appends kafka offset info like source offset(_hoodie_kafka_source_offset), "
+            + "partition (_hoodie_kafka_source_partition) and timestamp (_hoodie_kafka_source_timestamp) to the records. "
+            + "By default its disabled and no kafka offsets are added");
 
     public static boolean shouldAddOffsets(TypedProperties props) {
       return  props.getBoolean(KafkaOffsetPostProcessor.Config.KAFKA_APPEND_OFFSETS.key(), Boolean.parseBoolean(KafkaOffsetPostProcessor.Config.KAFKA_APPEND_OFFSETS.defaultValue()));
@@ -58,6 +60,7 @@ public class KafkaOffsetPostProcessor extends SchemaPostProcessor {
 
   @Override
   public Schema processSchema(Schema schema) {
+    // this method adds kafka offset fields namely source offset, partition and timestamp to the schema of the batch.
     List<Schema.Field> fieldList = schema.getFields();
     List<Schema.Field> newFieldList = fieldList.stream()
         .map(f -> new Schema.Field(f.name(), f.schema(), f.doc(), f.defaultVal())).collect(Collectors.toList());
