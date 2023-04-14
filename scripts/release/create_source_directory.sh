9b3e77b8a3f70da310c72fba7177932bef4bb548
Nadine Farah <nfarah86@gmail.com>
2023-04-13 17:44:24 -0700

    change for demo

--- scripts/release/create_source_directory.sh
+++ scripts/release/create_source_directory.sh
@@ -17,7 +17,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-#
+# demo
 
 rsync -a \
   --exclude ".git" --exclude ".gitignore" --exclude ".gitattributes" --exclude ".travis.yml" \
