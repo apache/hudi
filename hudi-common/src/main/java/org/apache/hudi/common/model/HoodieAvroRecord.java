121ba3959bf503891b66b43031b4c5e69a9f145b
Aditya goenka <ad1happy2go@gmail.com>
2023-04-25 15:56:48 +0530

    Handled empty option for Hoodie Avro Record

--- hudi-common/src/main/java/org/apache/hudi/common/model/HoodieAvroRecord.java
+++ hudi-common/src/main/java/org/apache/hudi/common/model/HoodieAvroRecord.java
@@ -157,10 +157,10 @@ public class HoodieAvroRecord<T extends HoodieRecordPayload> extends HoodieRecor
 
   @Override
   public boolean isDelete(Schema recordSchema, Properties props) throws IOException {
-    if (this.data instanceof BaseAvroPayload) {
-      return ((BaseAvroPayload) this.data).isDeleted(recordSchema, props);
-    } else {
+    if (this.data instanceof AWSDmsAvroPayload || !(this.data instanceof BaseAvroPayload)) {
       return !this.data.getInsertValue(recordSchema, props).isPresent();
+    } else {
+      return ((BaseAvroPayload) this.data).isDeleted(recordSchema, props);
     }
   }
 
