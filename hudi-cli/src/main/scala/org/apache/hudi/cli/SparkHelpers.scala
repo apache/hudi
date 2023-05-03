4104055983b36b83ba36246629d1c8760cf38b6f
Pawel Sendyk <pawelsendyk@berkeley.edu>
2023-05-03 14:59:32 -0600

    keep a single random record instance

--- hudi-cli/src/main/scala/org/apache/hudi/cli/SparkHelpers.scala
+++ hudi-cli/src/main/scala/org/apache/hudi/cli/SparkHelpers.scala
@@ -35,65 +35,93 @@ import org.apache.spark.sql.{DataFrame, SQLContext}
 import scala.collection.JavaConversions._
 import scala.collection.mutable._
 
-
 object SparkHelpers {
   @throws[Exception]
-  def skipKeysAndWriteNewFile(instantTime: String, fs: FileSystem, sourceFile: Path, destinationFile: Path, keysToSkip: Set[String]) {
-    val sourceRecords = BaseFileUtils.getInstance(HoodieFileFormat.PARQUET).readAvroRecords(fs.getConf, sourceFile)
+  def skipKeysAndWriteNewFile(
+    instantTime: String,
+    fs: FileSystem,
+    sourceFile: Path,
+    destinationFile: Path,
+    keysToSkip: HashMap[String, Int]
+  ) {
+    val sourceRecords =
+      BaseFileUtils.getInstance(HoodieFileFormat.PARQUET).readAvroRecords(fs.getConf, sourceFile)
     val schema: Schema = sourceRecords.get(0).getSchema
-    val filter: BloomFilter = BloomFilterFactory.createBloomFilter(HoodieIndexConfig.BLOOM_FILTER_NUM_ENTRIES_VALUE.defaultValue.toInt, HoodieIndexConfig.BLOOM_FILTER_FPP_VALUE.defaultValue.toDouble,
-      HoodieIndexConfig.BLOOM_INDEX_FILTER_DYNAMIC_MAX_ENTRIES.defaultValue.toInt, HoodieIndexConfig.BLOOM_FILTER_TYPE.defaultValue);
-    val writeSupport: HoodieAvroWriteSupport = new HoodieAvroWriteSupport(new AvroSchemaConverter(fs.getConf).convert(schema), schema, org.apache.hudi.common.util.Option.of(filter))
-    val parquetConfig: HoodieParquetConfig[HoodieAvroWriteSupport] = new HoodieParquetConfig(writeSupport, CompressionCodecName.GZIP, HoodieStorageConfig.PARQUET_BLOCK_SIZE.defaultValue.toInt, HoodieStorageConfig.PARQUET_PAGE_SIZE.defaultValue.toInt, HoodieStorageConfig.PARQUET_MAX_FILE_SIZE.defaultValue.toInt, fs.getConf, HoodieStorageConfig.PARQUET_COMPRESSION_RATIO_FRACTION.defaultValue.toDouble)
+    val filter: BloomFilter = BloomFilterFactory.createBloomFilter(
+      HoodieIndexConfig.BLOOM_FILTER_NUM_ENTRIES_VALUE.defaultValue.toInt,
+      HoodieIndexConfig.BLOOM_FILTER_FPP_VALUE.defaultValue.toDouble,
+      HoodieIndexConfig.BLOOM_INDEX_FILTER_DYNAMIC_MAX_ENTRIES.defaultValue.toInt,
+      HoodieIndexConfig.BLOOM_FILTER_TYPE.defaultValue
+    );
+    val writeSupport: HoodieAvroWriteSupport = new HoodieAvroWriteSupport(
+      new AvroSchemaConverter(fs.getConf).convert(schema),
+      schema,
+      org.apache.hudi.common.util.Option.of(filter)
+    )
+    val parquetConfig: HoodieParquetConfig[HoodieAvroWriteSupport] =
+      new HoodieParquetConfig(
+        writeSupport,
+        CompressionCodecName.GZIP,
+        HoodieStorageConfig.PARQUET_BLOCK_SIZE.defaultValue.toInt,
+        HoodieStorageConfig.PARQUET_PAGE_SIZE.defaultValue.toInt,
+        HoodieStorageConfig.PARQUET_MAX_FILE_SIZE.defaultValue.toInt,
+        fs.getConf,
+        HoodieStorageConfig.PARQUET_COMPRESSION_RATIO_FRACTION.defaultValue.toDouble,
+        HoodieStorageConfig.PARQUET_DICTIONARY_ENABLED.defaultValue
+      )
 
     // Add current classLoad for config, if not will throw classNotFound of 'HoodieWrapperFileSystem'.
     parquetConfig.getHadoopConf().setClassLoader(Thread.currentThread.getContextClassLoader)
 
-    val writer = new HoodieAvroParquetWriter[IndexedRecord](destinationFile, parquetConfig, instantTime, new SparkTaskContextSupplier(), true)
+    val writer = new HoodieAvroParquetWriter(
+      destinationFile,
+      parquetConfig,
+      instantTime,
+      new SparkTaskContextSupplier(),
+      true
+    )
     for (rec <- sourceRecords) {
       val key: String = rec.get(HoodieRecord.RECORD_KEY_METADATA_FIELD).toString
-      if (!keysToSkip.contains(key)) {
-
+      if (!keysToSkip.contains(key) || keysToSkip(key) == 0) {
         writer.writeAvro(key, rec)
+      } else {
+        keysToSkip(key) -= 1
       }
     }
     writer.close
   }
 }
 
-
 /**
-  * Bunch of Spark Shell/Scala stuff useful for debugging
-  */
+ * Bunch of Spark Shell/Scala stuff useful for debugging
+ */
 class SparkHelper(sqlContext: SQLContext, fs: FileSystem) {
 
-
   /**
-    * Print keys from a file
-    *
-    * @param file
-    */
+   * Print keys from a file
+   *
+   * @param file
+   */
   def printKeysFromFile(file: String) = {
     getRowKeyDF(file).collect().foreach(println(_))
   }
 
   /**
-    *
-    * @param file
-    * @return
-    */
+   *
+   * @param file
+   * @return
+   */
   def getRowKeyDF(file: String): DataFrame = {
     sqlContext.read.parquet(file).select(s"`${HoodieRecord.RECORD_KEY_METADATA_FIELD}`")
   }
 
-
   /**
-    * Does the rowKey actually exist in the file.
-    *
-    * @param rowKey
-    * @param file
-    * @return
-    */
+   * Does the rowKey actually exist in the file.
+   *
+   * @param rowKey
+   * @param file
+   * @return
+   */
   def isFileContainsKey(rowKey: String, file: String): Boolean = {
     println(s"Checking $file for key $rowKey")
     val ff = getRowKeyDF(file).filter(s"`${HoodieRecord.RECORD_KEY_METADATA_FIELD}` = '$rowKey'")
@@ -101,39 +129,46 @@ class SparkHelper(sqlContext: SQLContext, fs: FileSystem) {
   }
 
   /**
-    * Number of keys in a given file
-    *
-    * @param file
-    * @param sqlContext
-    */
+   * Number of keys in a given file
+   *
+   * @param file
+   * @param sqlContext
+   */
   def getKeyCount(file: String, sqlContext: org.apache.spark.sql.SQLContext) = {
     val keyCount = getRowKeyDF(file).collect().length
     println(keyCount)
     keyCount
   }
 
-
   /**
-    *
-    * Checks that all the keys in the file, have been added to the bloom filter
-    * in the footer
-    *
-    * @param conf
-    * @param sqlContext
-    * @param file
-    * @return
-    */
+   *
+   * Checks that all the keys in the file, have been added to the bloom filter
+   * in the footer
+   *
+   * @param conf
+   * @param sqlContext
+   * @param file
+   * @return
+   */
   def fileKeysAgainstBF(conf: Configuration, sqlContext: SQLContext, file: String): Boolean = {
-    val bf = BaseFileUtils.getInstance(HoodieFileFormat.PARQUET).readBloomFilterFromMetadata(conf, new Path(file))
-    val foundCount = sqlContext.parquetFile(file)
+    val bf = BaseFileUtils
+      .getInstance(HoodieFileFormat.PARQUET)
+      .readBloomFilterFromMetadata(conf, new Path(file))
+    val foundCount = sqlContext
+      .parquetFile(file)
       .select(s"`${HoodieRecord.RECORD_KEY_METADATA_FIELD}`")
-      .collect().count(r => !bf.mightContain(r.getString(0)))
+      .collect()
+      .count(r => !bf.mightContain(r.getString(0)))
     val totalCount = getKeyCount(file, sqlContext)
     println(s"totalCount: $totalCount, foundCount: $foundCount")
     totalCount == foundCount
   }
 
   def getDistinctKeyDF(paths: List[String]): DataFrame = {
-    sqlContext.read.parquet(paths: _*).select(s"`${HoodieRecord.RECORD_KEY_METADATA_FIELD}`").distinct()
+    sqlContext
+      .read
+      .parquet(paths: _*)
+      .select(s"`${HoodieRecord.RECORD_KEY_METADATA_FIELD}`")
+      .distinct()
   }
 }
