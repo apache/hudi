apache/hudi

commit a764c2aee387acf4bb5ccaa49ace01ffd2ec10e8

[HUDI-4963] Extend InProcessLockProvider to support multiple table ingestion (#6847)
Co-authored-by: rmahindra123 <rmahindra@Rajeshs-MacBook-Pro.local>

hudi-client/hudi-client-common/src/main/java/org/apache/hudi/client/transaction/lock/InProcessLockProvider.java
@@ -23,7 +23,8 @@ import org.apache.hudi.common.config.LockConfiguration;
 import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.common.lock.LockProvider;
 import org.apache.hudi.common.lock.LockState;
-import org.apache.hudi.common.util.StringUtils;
+import org.apache.hudi.common.util.ValidationUtils;
+import org.apache.hudi.config.HoodieWriteConfig;
 import org.apache.hudi.exception.HoodieLockException;
 
 import org.apache.hadoop.conf.Configuration;
@@ -32,12 +33,15 @@ import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * InProcess level lock. This {@link LockProvider} implementation is to
  * guard table from concurrent operations happening in the local JVM process.
+ * A separate lock is maintained per "table basepath".
  * <p>
  * Note: This Lock provider implementation doesn't allow lock reentrancy.
  * Attempting to reacquire the lock from the same thread will throw
@@ -47,11 +51,16 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class InProcessLockProvider implements LockProvider<ReentrantReadWriteLock>, Serializable {
 
   private static final Logger LOG = LogManager.getLogger(InProcessLockProvider.class);
-  private static final ReentrantReadWriteLock LOCK = new ReentrantReadWriteLock();
+  private static final Map<String, ReentrantReadWriteLock> LOCK_INSTANCE_PER_BASEPATH = new ConcurrentHashMap<>();
+  private final ReentrantReadWriteLock lock;
+  private final String basePath;
   private final long maxWaitTimeMillis;
 
   public InProcessLockProvider(final LockConfiguration lockConfiguration, final Configuration conf) {
     TypedProperties typedProperties = lockConfiguration.getConfig();
+    basePath = lockConfiguration.getConfig().getProperty(HoodieWriteConfig.BASE_PATH.key());
+    ValidationUtils.checkArgument(basePath != null);
+    lock = LOCK_INSTANCE_PER_BASEPATH.computeIfAbsent(basePath, (ignore) -> new ReentrantReadWriteLock());
     maxWaitTimeMillis = typedProperties.getLong(LockConfiguration.LOCK_ACQUIRE_WAIT_TIMEOUT_MS_PROP_KEY,
         LockConfiguration.DEFAULT_ACQUIRE_LOCK_WAIT_TIMEOUT_MS);
   }
@@ -59,10 +68,10 @@ public class InProcessLockProvider implements LockProvider<ReentrantReadWriteLoc
   @Override
   public void lock() {
     LOG.info(getLogMessage(LockState.ACQUIRING));
-    if (LOCK.isWriteLockedByCurrentThread()) {
+    if (lock.isWriteLockedByCurrentThread()) {
       throw new HoodieLockException(getLogMessage(LockState.ALREADY_ACQUIRED));
     }
-    LOCK.writeLock().lock();
+    lock.writeLock().lock();
     LOG.info(getLogMessage(LockState.ACQUIRED));
   }
 
@@ -74,13 +83,13 @@ public class InProcessLockProvider implements LockProvider<ReentrantReadWriteLoc
   @Override
   public boolean tryLock(long time, @NotNull TimeUnit unit) {
     LOG.info(getLogMessage(LockState.ACQUIRING));
-    if (LOCK.isWriteLockedByCurrentThread()) {
+    if (lock.isWriteLockedByCurrentThread()) {
       throw new HoodieLockException(getLogMessage(LockState.ALREADY_ACQUIRED));
     }
 
     boolean isLockAcquired;
     try {
-      isLockAcquired = LOCK.writeLock().tryLock(time, unit);
+      isLockAcquired = lock.writeLock().tryLock(time, unit);
     } catch (InterruptedException e) {
       throw new HoodieLockException(getLogMessage(LockState.FAILED_TO_ACQUIRE));
     }
@@ -93,8 +102,8 @@ public class InProcessLockProvider implements LockProvider<ReentrantReadWriteLoc
   public void unlock() {
     LOG.info(getLogMessage(LockState.RELEASING));
     try {
-      if (LOCK.isWriteLockedByCurrentThread()) {
-        LOCK.writeLock().unlock();
+      if (lock.isWriteLockedByCurrentThread()) {
+        lock.writeLock().unlock();
         LOG.info(getLogMessage(LockState.RELEASED));
       } else {
         LOG.warn("Cannot unlock because the current thread does not hold the lock.");
@@ -106,18 +115,19 @@ public class InProcessLockProvider implements LockProvider<ReentrantReadWriteLoc
 
   @Override
   public ReentrantReadWriteLock getLock() {
-    return LOCK;
+    return lock;
   }
 
   @Override
   public void close() {
-    if (LOCK.isWriteLockedByCurrentThread()) {
-      LOCK.writeLock().unlock();
+    if (lock.isWriteLockedByCurrentThread()) {
+      lock.writeLock().unlock();
     }
+    LOG.info(getLogMessage(LockState.ALREADY_RELEASED));
+    LOCK_INSTANCE_PER_BASEPATH.remove(basePath);
   }
 
   private String getLogMessage(LockState state) {
-    return StringUtils.join("Thread ", String.valueOf(Thread.currentThread().getName()), " ",
-        state.name(), " in-process lock.");
+    return String.format("Base Path %s, Lock Instance %s, Thread %s, In-process lock state %s", basePath, getLock().toString(), Thread.currentThread().getName(), state.name());
   }
 }

hudi-client/hudi-client-common/src/test/java/org/apache/hudi/client/transaction/TestInProcessLockProvider.java
@@ -22,6 +22,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hudi.client.transaction.lock.InProcessLockProvider;
 import org.apache.hudi.common.config.LockConfiguration;
 import org.apache.hudi.common.config.TypedProperties;
+import org.apache.hudi.config.HoodieWriteConfig;
 import org.apache.hudi.exception.HoodieLockException;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -39,11 +40,20 @@ public class TestInProcessLockProvider {
 
   private static final Logger LOG = LogManager.getLogger(TestInProcessLockProvider.class);
   private final Configuration hadoopConfiguration = new Configuration();
-  private final LockConfiguration lockConfiguration = new LockConfiguration(new TypedProperties());
+  private final LockConfiguration lockConfiguration1;
+  private final LockConfiguration lockConfiguration2;
+
+  public TestInProcessLockProvider() {
+    TypedProperties properties = new TypedProperties();
+    properties.put(HoodieWriteConfig.BASE_PATH.key(), "table1");
+    lockConfiguration1 = new LockConfiguration(properties);
+    properties.put(HoodieWriteConfig.BASE_PATH.key(), "table2");
+    lockConfiguration2 = new LockConfiguration(properties);
+  }
 
   @Test
   public void testLockAcquisition() {
-    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
     assertDoesNotThrow(() -> {
       inProcessLockProvider.lock();
     });
@@ -54,7 +64,7 @@ public class TestInProcessLockProvider {
 
   @Test
   public void testLockReAcquisitionBySameThread() {
-    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
     assertDoesNotThrow(() -> {
       inProcessLockProvider.lock();
     });
@@ -67,8 +77,33 @@ public class TestInProcessLockProvider {
   }
 
   @Test
+  public void testLockReAcquisitionBySameThreadWithTwoTables() {
+    InProcessLockProvider inProcessLockProvider1 = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider2 = new InProcessLockProvider(lockConfiguration2, hadoopConfiguration);
+
+    assertDoesNotThrow(() -> {
+      inProcessLockProvider1.lock();
+    });
+    assertDoesNotThrow(() -> {
+      inProcessLockProvider2.lock();
+    });
+    assertThrows(HoodieLockException.class, () -> {
+      inProcessLockProvider2.lock();
+    });
+    assertThrows(HoodieLockException.class, () -> {
+      inProcessLockProvider1.lock();
+    });
+    assertDoesNotThrow(() -> {
+      inProcessLockProvider1.unlock();
+    });
+    assertDoesNotThrow(() -> {
+      inProcessLockProvider2.unlock();
+    });
+  }
+
+  @Test
   public void testLockReAcquisitionByDifferentThread() {
-    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
     final AtomicBoolean writer2Completed = new AtomicBoolean(false);
 
     // Main test thread
@@ -105,8 +140,71 @@ public class TestInProcessLockProvider {
   }
 
   @Test
+  public void testLockReAcquisitionByDifferentThreadWithTwoTables() {
+    InProcessLockProvider inProcessLockProvider1 = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider2 = new InProcessLockProvider(lockConfiguration2, hadoopConfiguration);
+
+    final AtomicBoolean writer2Stream1Completed = new AtomicBoolean(false);
+    final AtomicBoolean writer2Stream2Completed = new AtomicBoolean(false);
+
+    // Main test thread
+    assertDoesNotThrow(() -> {
+      inProcessLockProvider1.lock();
+    });
+    assertDoesNotThrow(() -> {
+      inProcessLockProvider2.lock();
+    });
+
+    // Another writer thread in parallel, should block
+    // and later acquire the lock once it is released
+    Thread writer2Stream1 = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        assertDoesNotThrow(() -> {
+          inProcessLockProvider1.lock();
+        });
+        assertDoesNotThrow(() -> {
+          inProcessLockProvider1.unlock();
+        });
+        writer2Stream1Completed.set(true);
+      }
+    });
+    Thread writer2Stream2 = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        assertDoesNotThrow(() -> {
+          inProcessLockProvider2.lock();
+        });
+        assertDoesNotThrow(() -> {
+          inProcessLockProvider2.unlock();
+        });
+        writer2Stream2Completed.set(true);
+      }
+    });
+
+    writer2Stream1.start();
+    writer2Stream2.start();
+
+    assertDoesNotThrow(() -> {
+      inProcessLockProvider1.unlock();
+    });
+    assertDoesNotThrow(() -> {
+      inProcessLockProvider2.unlock();
+    });
+
+    try {
+      writer2Stream1.join();
+      writer2Stream2.join();
+    } catch (InterruptedException e) {
+      //
+    }
+    Assertions.assertTrue(writer2Stream1Completed.get());
+    Assertions.assertTrue(writer2Stream2Completed.get());
+  }
+
+  @Test
   public void testTryLockAcquisition() {
-    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
     Assertions.assertTrue(inProcessLockProvider.tryLock());
     assertDoesNotThrow(() -> {
       inProcessLockProvider.unlock();
@@ -115,7 +213,7 @@ public class TestInProcessLockProvider {
 
   @Test
   public void testTryLockAcquisitionWithTimeout() {
-    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
     Assertions.assertTrue(inProcessLockProvider.tryLock(1, TimeUnit.MILLISECONDS));
     assertDoesNotThrow(() -> {
       inProcessLockProvider.unlock();
@@ -124,7 +222,7 @@ public class TestInProcessLockProvider {
 
   @Test
   public void testTryLockReAcquisitionBySameThread() {
-    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
     Assertions.assertTrue(inProcessLockProvider.tryLock());
     assertThrows(HoodieLockException.class, () -> {
       inProcessLockProvider.tryLock(1, TimeUnit.MILLISECONDS);
@@ -136,7 +234,7 @@ public class TestInProcessLockProvider {
 
   @Test
   public void testTryLockReAcquisitionByDifferentThread() {
-    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
     final AtomicBoolean writer2Completed = new AtomicBoolean(false);
 
     // Main test thread
@@ -162,7 +260,7 @@ public class TestInProcessLockProvider {
 
   @Test
   public void testTryUnLockByDifferentThread() {
-    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
     final AtomicBoolean writer3Completed = new AtomicBoolean(false);
 
     // Main test thread
@@ -203,7 +301,7 @@ public class TestInProcessLockProvider {
 
   @Test
   public void testTryLockAcquisitionBeforeTimeOutFromTwoThreads() {
-    final InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration, hadoopConfiguration);
+    final InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
     final int threadCount = 3;
     final long awaitMaxTimeoutMs = 2000L;
     final CountDownLatch latch = new CountDownLatch(threadCount);
@@ -261,7 +359,7 @@ public class TestInProcessLockProvider {
 
   @Test
   public void testLockReleaseByClose() {
-    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
     assertDoesNotThrow(() -> {
       inProcessLockProvider.lock();
     });
@@ -272,7 +370,7 @@ public class TestInProcessLockProvider {
 
   @Test
   public void testRedundantUnlock() {
-    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
     assertDoesNotThrow(() -> {
       inProcessLockProvider.lock();
     });
@@ -286,7 +384,7 @@ public class TestInProcessLockProvider {
 
   @Test
   public void testUnlockWithoutLock() {
-    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration, hadoopConfiguration);
+    InProcessLockProvider inProcessLockProvider = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
     assertDoesNotThrow(() -> {
       inProcessLockProvider.unlock();
     });

commit 79412ed2a634fb188da402370694c2baef7d1dfc

[HUDI-6186] Fix lock identity in InProcessLockProvider (#8658)
This commit fixes a bug introduced by #6847. #6847 extends the InProcessLockProvider to support multiple tables in the same process, by having an in-memory static final map storing the mapping of the table base path to the read-write reentrant lock, so that the writer uses the corresponding lock based on the base path. When closing the lock provider, close() removes the lock entry. Since close() is called when closing the write client, the lock is removed and subsequent concurrent writers will get a different lock instance on the same table, causing the locking mechanism on the same table to be useless.  The fix gets rid of the lock removal operation in the `close()` call since it has to be kept for concurrent writers.  A new test `TestInProcessLockProvider#testLockIdentity` based on the above scenario is added to guard the behavior.

hudi-client/hudi-client-common/src/main/java/org/apache/hudi/client/transaction/lock/InProcessLockProvider.java
@@ -124,7 +124,6 @@ public class InProcessLockProvider implements LockProvider<ReentrantReadWriteLoc
       lock.writeLock().unlock();
     }
     LOG.info(getLogMessage(LockState.ALREADY_RELEASED));
-    LOCK_INSTANCE_PER_BASEPATH.remove(basePath);
   }
 
   private String getLogMessage(LockState state) {

hudi-client/hudi-client-common/src/test/java/org/apache/hudi/client/transaction/TestInProcessLockProvider.java
@@ -24,12 +24,15 @@ import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.config.HoodieWriteConfig;
 import org.apache.hudi.exception.HoodieLockException;
 
+import junit.framework.AssertionFailedError;
 import org.apache.hadoop.conf.Configuration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -50,6 +53,120 @@ public class TestInProcessLockProvider {
     lockConfiguration1 = new LockConfiguration(properties);
     properties.put(HoodieWriteConfig.BASE_PATH.key(), "table2");
     lockConfiguration2 = new LockConfiguration(properties);
+  }
+
+  @Test
+  public void testLockIdentity() throws InterruptedException {
+    // The lifecycle of an InProcessLockProvider should not affect the singleton lock
+    // for a single table, i.e., all three writers should hold the same underlying lock instance
+    // on the same table.
+    // Writer 1:   lock |----------------| unlock and close
+    // Writer 2:   try lock   |      ...    lock |------| unlock and close
+    // Writer 3:                          try lock  | ...  lock |------| unlock and close
+    List<InProcessLockProvider> lockProviderList = new ArrayList<>();
+    InProcessLockProvider lockProvider1 = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
+    lockProviderList.add(lockProvider1);
+    AtomicBoolean writer1Completed = new AtomicBoolean(false);
+    AtomicBoolean writer2TryLock = new AtomicBoolean(false);
+    AtomicBoolean writer2Locked = new AtomicBoolean(false);
+    AtomicBoolean writer2Completed = new AtomicBoolean(false);
+    AtomicBoolean writer3TryLock = new AtomicBoolean(false);
+    AtomicBoolean writer3Completed = new AtomicBoolean(false);
+
+    // Writer 1
+    assertDoesNotThrow(() -> {
+      LOG.info("Writer 1 tries to acquire the lock.");
+      lockProvider1.lock();
+      LOG.info("Writer 1 acquires the lock.");
+    });
+    // Writer 2 thread in parallel, should block
+    // and later acquire the lock once it is released
+    Thread writer2 = new Thread(() -> {
+      InProcessLockProvider lockProvider2 = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
+      lockProviderList.add(lockProvider2);
+      assertDoesNotThrow(() -> {
+        LOG.info("Writer 2 tries to acquire the lock.");
+        writer2TryLock.set(true);
+        lockProvider2.lock();
+        LOG.info("Writer 2 acquires the lock.");
+      });
+      writer2Locked.set(true);
+
+      while (!writer3TryLock.get()) {
+        try {
+          Thread.sleep(100);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+
+      assertDoesNotThrow(() -> {
+        lockProvider2.unlock();
+        LOG.info("Writer 2 releases the lock.");
+      });
+      lockProvider2.close();
+      LOG.info("Writer 2 closes the lock provider.");
+      writer2Completed.set(true);
+    });
+
+    Thread writer3 = new Thread(() -> {
+      while (!writer2Locked.get() || !writer1Completed.get()) {
+        try {
+          Thread.sleep(10);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+      // Lock instance of Writer 3 should be held by Writer 2
+      InProcessLockProvider lockProvider3 = new InProcessLockProvider(lockConfiguration1, hadoopConfiguration);
+      lockProviderList.add(lockProvider3);
+      boolean isLocked = lockProvider3.getLock().isWriteLocked();
+      if (!isLocked) {
+        writer3TryLock.set(true);
+        throw new AssertionFailedError("The lock instance in Writer 3 should be held by Writer 2: "
+            + lockProvider3.getLock());
+      }
+      assertDoesNotThrow(() -> {
+        LOG.info("Writer 3 tries to acquire the lock.");
+        writer3TryLock.set(true);
+        lockProvider3.lock();
+        LOG.info("Writer 3 acquires the lock.");
+      });
+
+      assertDoesNotThrow(() -> {
+        lockProvider3.unlock();
+        LOG.info("Writer 3 releases the lock.");
+      });
+      lockProvider3.close();
+      LOG.info("Writer 3 closes the lock provider.");
+      writer3Completed.set(true);
+    });
+
+    writer2.start();
+    writer3.start();
+
+    while (!writer2TryLock.get()) {
+      Thread.sleep(100);
+    }
+
+    assertDoesNotThrow(() -> {
+      lockProvider1.unlock();
+      LOG.info("Writer 1 releases the lock.");
+      lockProvider1.close();
+      LOG.info("Writer 1 closes the lock provider.");
+      writer1Completed.set(true);
+    });
+
+    try {
+      writer2.join();
+      writer3.join();
+    } catch (InterruptedException e) {
+      // Ignore any exception
+    }
+    Assertions.assertTrue(writer2Completed.get());
+    Assertions.assertTrue(writer3Completed.get());
+    Assertions.assertEquals(lockProviderList.get(0).getLock(), lockProviderList.get(1).getLock());
+    Assertions.assertEquals(lockProviderList.get(1).getLock(), lockProviderList.get(2).getLock());
   }
 
   @Test