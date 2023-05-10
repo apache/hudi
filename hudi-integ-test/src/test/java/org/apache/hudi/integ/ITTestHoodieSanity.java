f94f624e726d3182c2f774205ad22dc83a51ab18
xicm <xicm000@gmail.com>
2023-05-10 09:44:55 +0800

    remove unused import

--- hudi-integ-test/src/test/java/org/apache/hudi/integ/ITTestHoodieSanity.java
+++ hudi-integ-test/src/test/java/org/apache/hudi/integ/ITTestHoodieSanity.java
@@ -28,7 +28,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
