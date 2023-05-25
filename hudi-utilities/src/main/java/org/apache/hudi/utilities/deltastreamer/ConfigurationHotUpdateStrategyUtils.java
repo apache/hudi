f887f9ccc721c937f212fa4df30021201c82cf37
wei.kong <wei.kong@shopee.com>
2023-05-25 17:30:13 +0800

    support configuration hot update for deltastreamer continuous mode

---
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/deltastreamer/ConfigurationHotUpdateStrategyUtils.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hudi.utilities.deltastreamer;
+
+import org.apache.hudi.common.config.TypedProperties;
+import org.apache.hudi.common.util.Option;
+import org.apache.hudi.common.util.ReflectionUtils;
+import org.apache.hudi.common.util.StringUtils;
+import org.apache.hudi.exception.HoodieException;
+
+import org.apache.spark.sql.SparkSession;
+
+public class ConfigurationHotUpdateStrategyUtils {
+
+  /**
+   * Create a ConfigurationHotUpdateStrategy class via reflection,
+   * <br>
+   * if the class name of ConfigurationHotUpdateStrategy is configured through the {@link HoodieDeltaStreamer.Config#configHotUpdateStrategyClass}.
+   */
+  public static Option<ConfigurationHotUpdateStrategy> createConfigurationHotUpdateStrategy(String configHotUpdateStrategyClass,
+                                                                                            SparkSession sparkSession,
+                                                                                            HoodieDeltaStreamer.Config cfg,
+                                                                                            TypedProperties properties)
+      throws HoodieException {
+    try {
+      return StringUtils.isNullOrEmpty(configHotUpdateStrategyClass)
+          ? Option.empty() :
+          Option.of((ConfigurationHotUpdateStrategy) ReflectionUtils.loadClass(configHotUpdateStrategyClass, sparkSession, cfg, properties));
+    } catch (Throwable e) {
+      throw new HoodieException("Could not create ConfigurationHotUpdateStrategy class " + configHotUpdateStrategyClass, e);
+    }
+  }
+}
---
+++ hudi-utilities/src/test/java/org/apache/hudi/utilities/deltastreamer/DummyConfigurationHotUpdateStrategy.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.hudi.utilities.deltastreamer;
+
+import org.apache.hudi.common.config.TypedProperties;
+
+import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+
+import static org.apache.hudi.config.HoodieWriteConfig.UPSERT_PARALLELISM_VALUE;
+
+public class DummyConfigurationHotUpdateStrategy implements ConfigurationHotUpdateStrategy, Serializable {
+  private static final Logger LOG = LoggerFactory.getLogger(DummyConfigurationHotUpdateStrategy.class);
+
+  private HoodieDeltaStreamer.Config cfg;
+  private TypedProperties properties;
+  private SparkSession sparkSession;
+
+  public DummyConfigurationHotUpdateStrategy(SparkSession sparkSession, HoodieDeltaStreamer.Config cfg, TypedProperties properties) {
+    this.sparkSession = sparkSession;
+    this.cfg = cfg;
+    this.properties = properties;
+  }
+
+  @Override
+  public boolean updateProps(final TypedProperties properties) {
+    boolean updated = false;
+    if (properties.containsKey(UPSERT_PARALLELISM_VALUE.key())) {
+      long upsertShuffleParallelism = properties.getLong(UPSERT_PARALLELISM_VALUE.key());
+      properties.setProperty(UPSERT_PARALLELISM_VALUE.key(), String.valueOf(upsertShuffleParallelism + 5));
+      LOG.info("update {} from [{}] to [{}]", UPSERT_PARALLELISM_VALUE.key(), upsertShuffleParallelism, upsertShuffleParallelism + 5);
+      updated = true;
+    }
+    return updated;
+  }
+}
