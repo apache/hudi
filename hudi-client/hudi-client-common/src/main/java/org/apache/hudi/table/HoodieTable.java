c41db68570ef6e8812e89de1180bcf3c8400fa7d
Danny Chan <yuzhao.cyz@gmail.com>
2023-05-25 11:21:13 +0800

    Revert "[MINOR] Check the return value from delete during rollback and finalize to ensure the files actually got deleted (#8606)"
    This reverts commit b28eeafea68fd1c35dddbd39fe7bdb995a1ad965.

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/HoodieTable.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/HoodieTable.java
@@ -675,10 +675,7 @@ public abstract class HoodieTable<T, I, K, O> implements Serializable {
           LOG.info("Deleting invalid data file=" + partitionFilePair);
           // Delete
           try {
-            Path pathToBeDeleted = new Path(partitionFilePair.getValue());
-            if (!fileSystem.delete(pathToBeDeleted, false) && fileSystem.exists(pathToBeDeleted)) {
-              throw new HoodieException("Failed to delete the invalid data file " + pathToBeDeleted);
-            }
+            fileSystem.delete(new Path(partitionFilePair.getValue()), false);
           } catch (IOException e) {
             throw new HoodieIOException(e.getMessage(), e);
           }
