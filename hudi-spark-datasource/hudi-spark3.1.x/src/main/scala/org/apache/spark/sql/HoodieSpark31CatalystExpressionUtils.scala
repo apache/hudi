ded9c398ad4cd768290f4418ef5772906fe23dfe
Y Ethan Guo <ethan.guoyihua@gmail.com>
2023-06-05 19:03:14 -0700

    Add new adapter implementation for Spark 3.1

--- hudi-spark-datasource/hudi-spark3.1.x/src/main/scala/org/apache/spark/sql/HoodieSpark31CatalystExpressionUtils.scala
+++ hudi-spark-datasource/hudi-spark3.1.x/src/main/scala/org/apache/spark/sql/HoodieSpark31CatalystExpressionUtils.scala
@@ -22,6 +22,7 @@ import org.apache.spark.sql.HoodieSparkTypeUtils.isCastPreservingOrdering
 import org.apache.spark.sql.catalyst.expressions.{Add, AnsiCast, Attribute, AttributeReference, AttributeSet, BitwiseOr, Cast, DateAdd, DateDiff, DateFormatClass, DateSub, Divide, Exp, Expm1, Expression, FromUTCTimestamp, FromUnixTime, Log, Log10, Log1p, Log2, Lower, Multiply, ParseToDate, ParseToTimestamp, PredicateHelper, ShiftLeft, ShiftRight, ToUTCTimestamp, ToUnixTimestamp, Upper}
 import org.apache.spark.sql.execution.datasources.DataSourceStrategy
 import org.apache.spark.sql.types.DataType
+import org.apache.spark.sql.util.SchemaUtils
 
 object HoodieSpark31CatalystExpressionUtils extends HoodieSpark3CatalystExpressionUtils with PredicateHelper {
 
@@ -97,7 +98,7 @@ object HoodieSpark31CatalystExpressionUtils extends HoodieSpark3CatalystExpressi
         case ShiftRight(OrderPreservingTransformation(attrRef), _) => Some(attrRef)
 
         // Other
-        case cast @ Cast(OrderPreservingTransformation(attrRef), _, _)
+        case cast@Cast(OrderPreservingTransformation(attrRef), _, _)
           if isCastPreservingOrdering(cast.child.dataType, cast.dataType) => Some(attrRef)
 
         // Identity transformation
@@ -106,5 +107,11 @@ object HoodieSpark31CatalystExpressionUtils extends HoodieSpark3CatalystExpressi
         case _ => None
       }
     }
+  }
+
+  override def checkColumnNameDuplication(columnNames: Seq[String],
+                                          colType: String,
+                                          caseSensitiveAnalysis: Boolean): Unit = {
+    SchemaUtils.checkColumnNameDuplication(columnNames, colType, caseSensitiveAnalysis)
   }
 }
