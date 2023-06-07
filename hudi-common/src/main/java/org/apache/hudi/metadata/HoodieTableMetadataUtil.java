cae57c2b03a3131cfa7774fa4335f4d153f85d61
sivabalan <n.siva.b@gmail.com>
2023-06-06 20:02:30 -0700

    Fixing fileId for files partition in MDT

--- hudi-common/src/main/java/org/apache/hudi/metadata/HoodieTableMetadataUtil.java
+++ hudi-common/src/main/java/org/apache/hudi/metadata/HoodieTableMetadataUtil.java
@@ -1453,7 +1453,11 @@ public class HoodieTableMetadataUtil {
    * @return The fileID
    */
   public static String getFileIDForFileGroup(MetadataPartitionType partitionType, int index) {
-    return String.format("%s%04d", partitionType.getFileIdPrefix(), index);
+    if (partitionType == MetadataPartitionType.FILES) {
+      return String.format("%s%04d-%d", partitionType.getFileIdPrefix(), index, 0);
+    } else {
+      return String.format("%s%04d", partitionType.getFileIdPrefix(), index);
+    }
   }
 
   /**
