f275d85204507349b1b18fc87406fd7d2ffc0d04
Lokesh Jain <ljain@apache.org>
2023-06-15 13:03:39 +0530

    [HUDI-6380] Add API to fetch transformed schema in Transformer

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/transform/ErrorTableAwareChainedTransformer.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/transform/ErrorTableAwareChainedTransformer.java
@@ -20,11 +20,15 @@
 package org.apache.hudi.utilities.transform;
 
 import org.apache.hudi.common.config.TypedProperties;
+import org.apache.hudi.common.util.Option;
 import org.apache.hudi.utilities.deltastreamer.ErrorTableUtils;
+
+import org.apache.avro.Schema;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.StructType;
 
 import java.util.List;
 
@@ -34,8 +38,8 @@ import java.util.List;
  * if that column is not dropped in any of the transformations.
  */
 public class ErrorTableAwareChainedTransformer extends ChainedTransformer {
-  public ErrorTableAwareChainedTransformer(List<String> configuredTransformers, int... ignore) {
-    super(configuredTransformers);
+  public ErrorTableAwareChainedTransformer(List<String> configuredTransformers, Option<Schema> sourceSchemaOpt) {
+    super(configuredTransformers, sourceSchemaOpt);
   }
 
   public ErrorTableAwareChainedTransformer(List<Transformer> transformers) {
@@ -54,5 +58,10 @@ public class ErrorTableAwareChainedTransformer extends ChainedTransformer {
       ErrorTableUtils.validate(dataset);
     }
     return dataset;
+  }
+
+  @Override
+  public StructType transformedSchema(JavaSparkContext jsc, SparkSession sparkSession, StructType incomingStruct, TypedProperties properties) {
+    return super.transformedSchema(jsc, sparkSession, incomingStruct, properties);
   }
 }
