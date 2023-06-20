243098c39afffd79d881c056282016a2732eaabc
kazdy <d.kazmirski@gmail.com>
2023-06-20 04:05:51 +0200

    [HUDI-6223] Support Incremental read via Spark SQL table valued function (#8729)
    * add hudi_table_changes TVF
    * add "earliest" start instant option to hudi_table_changes TVF
    * add HoodieTableChanges to catalyst plan analysis
    * the first param of hudi_table_changes is polymophic, it can be the table id or table path
    
    ---------
    
    Co-authored-by: Danny Chan <yuzhao.cyz@gmail.com>

--- hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/spark/sql/hudi/TestHoodieTableValuedFunction.scala
+++ hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/spark/sql/hudi/TestHoodieTableValuedFunction.scala
@@ -18,6 +18,7 @@
 package org.apache.spark.sql.hudi
 
 import org.apache.hudi.HoodieSparkUtils
+import org.apache.spark.sql.functions.{col, from_json}
 
 class TestHoodieTableValuedFunction extends HoodieSparkSqlTestBase {
 
@@ -76,6 +77,270 @@ class TestHoodieTableValuedFunction extends HoodieSparkSqlTestBase {
               Seq(3, "a3", 30.0, 1000)
             )
           }
+        }
+      }
+    }
+  }
+
+  test(s"Test hudi_table_changes latest_state") {
+    if (HoodieSparkUtils.gteqSpark3_2) {
+      withTempDir { tmp =>
+        Seq(
+          ("cow", true),
+          ("mor", true),
+          ("cow", false),
+          ("mor", false)
+        ).foreach { parameters =>
+          val tableType = parameters._1
+          val isTableId = parameters._2
+          val tableName = generateTableName
+          val tablePath = s"${tmp.getCanonicalPath}/$tableName"
+          val identifier = if (isTableId) tableName else tablePath
+          spark.sql("set hoodie.sql.insert.mode = non-strict")
+          spark.sql(
+            s"""
+               |create table $tableName (
+               |  id int,
+               |  name string,
+               |  price double,
+               |  ts long
+               |) using hudi
+               |tblproperties (
+               |  type = '$tableType',
+               |  primaryKey = 'id',
+               |  preCombineField = 'ts'
+               |)
+               |location '$tablePath'
+               |""".stripMargin
+          )
+
+          spark.sql(
+            s"""
+               | insert into $tableName
+               | values (1, 'a1', 10, 1000), (2, 'a2', 20, 1000), (3, 'a3', 30, 1000)
+               | """.stripMargin
+          )
+
+          val firstInstant = spark.sql(s"select min(_hoodie_commit_time) as commitTime from  $tableName order by commitTime").first().getString(0)
+
+          checkAnswer(
+            s"""select id,
+               |name,
+               |price,
+               |ts
+               |from hudi_table_changes('$identifier', 'latest_state', 'earliest')
+               |""".stripMargin
+          )(
+            Seq(1, "a1", 10.0, 1000),
+            Seq(2, "a2", 20.0, 1000),
+            Seq(3, "a3", 30.0, 1000)
+          )
+
+          spark.sql(
+            s"""
+               | insert into $tableName
+               | values (1, 'a1_1', 10, 1100), (2, 'a2_2', 20, 1100), (3, 'a3_3', 30, 1100)
+               | """.stripMargin
+          )
+          val secondInstant = spark.sql(s"select max(_hoodie_commit_time) as commitTime from  $tableName order by commitTime").first().getString(0)
+
+          checkAnswer(
+            s"""select id,
+               |name,
+               |price,
+               |ts
+               |from hudi_table_changes(
+               |'$identifier',
+               |'latest_state',
+               |'$firstInstant')
+               |""".stripMargin
+          )(
+            Seq(1, "a1_1", 10.0, 1100),
+            Seq(2, "a2_2", 20.0, 1100),
+            Seq(3, "a3_3", 30.0, 1100)
+          )
+
+          spark.sql(
+            s"""
+               | insert into $tableName
+               | values (1, 'a1_1', 10, 1200), (2, 'a2_2', 20, 1200), (3, 'a3_3', 30, 1200)
+               | """.stripMargin
+          )
+
+          // should not include the first and latest instant
+          checkAnswer(
+            s"""select id,
+               | name,
+               | price,
+               | ts
+               | from hudi_table_changes(
+               | '$identifier',
+               | 'latest_state',
+               | '$firstInstant',
+               | '$secondInstant')
+               | """.stripMargin
+          )(
+            Seq(1, "a1_1", 10.0, 1100),
+            Seq(2, "a2_2", 20.0, 1100),
+            Seq(3, "a3_3", 30.0, 1100)
+          )
+        }
+      }
+    }
+  }
+
+  test(s"Test hudi_table_changes cdc") {
+    if (HoodieSparkUtils.gteqSpark3_2) {
+      withTempDir { tmp =>
+        Seq(
+          ("cow", true),
+          ("mor", true),
+          ("cow", false),
+          ("mor", false)
+        ).foreach { parameters =>
+          val tableType = parameters._1
+          val isTableId = parameters._2
+          val tableName = generateTableName
+          val tablePath = s"${tmp.getCanonicalPath}/$tableName"
+          val identifier = if (isTableId) tableName else tablePath
+          spark.sql("set hoodie.sql.insert.mode = upsert")
+          spark.sql(
+            s"""
+               |create table $tableName (
+               |  id int,
+               |  name string,
+               |  price double,
+               |  ts long
+               |) using hudi
+               |tblproperties (
+               |  type = '$tableType',
+               |  primaryKey = 'id',
+               |  preCombineField = 'ts',
+               |  'hoodie.table.cdc.enabled' = 'true',
+               |  'hoodie.table.cdc.supplemental.logging.mode' = 'data_before_after'
+               |)
+               |location '$tablePath'
+               |""".stripMargin
+          )
+
+          spark.sql(
+            s"""
+               | insert into $tableName
+               | values (1, 'a1', 10, 1000), (2, 'a2', 20, 1000), (3, 'a3', 30, 1000)
+               | """.stripMargin
+          )
+          val originSchema = spark.read.format("hudi").load(tablePath).schema
+          val firstInstant = spark.sql(s"select min(_hoodie_commit_time) as commitTime from  $tableName order by commitTime").first().getString(0)
+
+          val cdcDataOnly1 = spark.sql(
+            s"""select
+               | op,
+               | before,
+               | after
+               |from hudi_table_changes('$identifier', 'cdc', 'earliest')
+               |""".stripMargin
+          )
+
+          val change1 = cdcDataOnly1.select(
+            col("op"),
+            col("before"),
+            from_json(col("after"), originSchema).as("after")
+          ).select(
+            col("op"),
+            col("before"),
+            col("after.id"),
+            col("after.name"),
+            col("after.price"),
+            col("after.ts")
+          ).orderBy("after.id").collect()
+          checkAnswer(change1)(
+            Seq("i", null, 1, "a1", 10.0, 1000),
+            Seq("i", null, 2, "a2", 20.0, 1000),
+            Seq("i", null, 3, "a3", 30.0, 1000)
+          )
+
+          spark.sql(
+            s"""
+               | insert into $tableName
+               | values (1, 'a1_1', 10, 1100), (2, 'a2_2', 20, 1100), (3, 'a3_3', 30, 1100)
+               | """.stripMargin
+          )
+          val secondInstant = spark.sql(s"select max(_hoodie_commit_time) as commitTime from  $tableName order by commitTime").first().getString(0)
+
+          val cdcDataOnly2 = spark.sql(
+            s"""select
+               | op,
+               | before,
+               | after
+               |from hudi_table_changes(
+               |'$identifier',
+               |'cdc',
+               |'$firstInstant')
+               |""".stripMargin
+          )
+
+          val change2 = cdcDataOnly2.select(
+            col("op"),
+            from_json(col("before"), originSchema).as("before"),
+            from_json(col("after"), originSchema).as("after")
+          ).select(
+            col("op"),
+            col("before.id"),
+            col("before.name"),
+            col("before.price"),
+            col("before.ts"),
+            col("after.id"),
+            col("after.name"),
+            col("after.price"),
+            col("after.ts")
+          ).orderBy("after.id").collect()
+          checkAnswer(change2)(
+            Seq("u", 1, "a1", 10.0, 1000, 1, "a1_1", 10.0, 1100),
+            Seq("u", 2, "a2", 20.0, 1000, 2, "a2_2", 20.0, 1100),
+            Seq("u", 3, "a3", 30.0, 1000, 3, "a3_3", 30.0, 1100)
+          )
+
+          spark.sql(
+            s"""
+               | insert into $tableName
+               | values (1, 'a1_1', 11, 1200), (2, 'a2_2', 21, 1200), (3, 'a3_3', 31, 1200)
+               | """.stripMargin
+          )
+
+          // should not include the first and latest instant
+          val cdcDataOnly3 = spark.sql(
+            s"""select
+               | op,
+               | before,
+               | after
+               | from hudi_table_changes(
+               | '$identifier',
+               | 'cdc',
+               | '$firstInstant',
+               | '$secondInstant')
+               | """.stripMargin
+          )
+
+          val change3 = cdcDataOnly3.select(
+            col("op"),
+            from_json(col("before"), originSchema).as("before"),
+            from_json(col("after"), originSchema).as("after")
+          ).select(
+            col("op"),
+            col("before.id"),
+            col("before.name"),
+            col("before.price"),
+            col("before.ts"),
+            col("after.id"),
+            col("after.name"),
+            col("after.price"),
+            col("after.ts")
+          ).orderBy("after.id").collect()
+          checkAnswer(change3)(
+            Seq("u", 1, "a1", 10.0, 1000, 1, "a1_1", 10.0, 1100),
+            Seq("u", 2, "a2", 20.0, 1000, 2, "a2_2", 20.0, 1100),
+            Seq("u", 3, "a3", 30.0, 1000, 3, "a3_3", 30.0, 1100)
+          )
         }
       }
     }
