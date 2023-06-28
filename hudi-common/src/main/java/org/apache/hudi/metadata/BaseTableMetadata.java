9aec6c2832c210fa6d38708a33fdfb2d36c32509
Lokesh Jain <ljain@apache.org>
2023-06-28 13:08:45 +0530

    Fix checkstyle

--- hudi-common/src/main/java/org/apache/hudi/metadata/BaseTableMetadata.java
+++ hudi-common/src/main/java/org/apache/hudi/metadata/BaseTableMetadata.java
@@ -62,7 +62,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
