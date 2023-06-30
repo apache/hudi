3fad034154ded081d2422b70b19fb0ec81bf55fe
Surya Prasanna Kumar Yalla <syalla@uber.com>
2023-06-30 00:08:29 -0700

    Move isLastIncrementalSyncSuccessful valur from HoodietableMetaClient to IncrementalTimelineSyncFileSystemView

--- hudi-common/src/main/java/org/apache/hudi/common/table/HoodieTableMetaClient.java
+++ hudi-common/src/main/java/org/apache/hudi/common/table/HoodieTableMetaClient.java
@@ -118,8 +118,6 @@ public class HoodieTableMetaClient implements Serializable {
   private ConsistencyGuardConfig consistencyGuardConfig = ConsistencyGuardConfig.newBuilder().build();
   private FileSystemRetryConfig fileSystemRetryConfig = FileSystemRetryConfig.newBuilder().build();
   protected HoodieMetaserverConfig metaserverConfig;
-  // Tracks the status of the last incremental file sync
-  private boolean isLastIncrementalSyncSuccessful;
 
   /**
    *
@@ -365,14 +363,6 @@ public class HoodieTableMetaClient implements Serializable {
   public synchronized HoodieActiveTimeline reloadActiveTimeline() {
     activeTimeline = new HoodieActiveTimeline(this);
     return activeTimeline;
-  }
-
-  public boolean isLastIncrementalSyncSuccessful() {
-    return isLastIncrementalSyncSuccessful;
-  }
-
-  public void setLastIncrementalSyncSuccessful(boolean lastIncrementalSyncSuccessful) {
-    isLastIncrementalSyncSuccessful = lastIncrementalSyncSuccessful;
   }
 
   public ConsistencyGuardConfig getConsistencyGuardConfig() {
