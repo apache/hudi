a8b972a6d624afb07e197ffef99473f644e3305f
Nicolas Paris <nicolas.paris@adevinta.com>
2023-07-10 16:49:32 +0200

    Add docs for as_datasource

--- hudi-sync/hudi-hive-sync/src/main/java/org/apache/hudi/hive/HiveSyncConfigHolder.java
+++ hudi-sync/hudi-hive-sync/src/main/java/org/apache/hudi/hive/HiveSyncConfigHolder.java
@@ -105,7 +105,7 @@ public class HiveSyncConfigHolder {
       .key("hoodie.datasource.hive_sync.sync_as_datasource")
       .defaultValue("true")
       .markAdvanced()
-      .withDocumentation("");
+      .withDocumentation("Adds some spark properties in the table, including json translation of the schema. Since 0.11, there is no usage of them anymore since hudi rely on hudi datasource instead. Moreover showing comments in spark-sql needs to turn this off.");
   public static final ConfigProperty<Integer> HIVE_SYNC_SCHEMA_STRING_LENGTH_THRESHOLD = ConfigProperty
       .key("hoodie.datasource.hive_sync.schema_string_length_thresh")
       .defaultValue(4000)
