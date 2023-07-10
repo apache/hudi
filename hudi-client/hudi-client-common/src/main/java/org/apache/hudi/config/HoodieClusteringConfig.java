b3c2f9d51de2d19a0cad08bdbe23fa6682b3b628
Y Ethan Guo <ethan.guoyihua@gmail.com>
2023-07-10 14:50:06 -0700

    Fix nits in HoodieClusteringConfig

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/config/HoodieClusteringConfig.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/config/HoodieClusteringConfig.java
@@ -159,8 +159,8 @@ public class HoodieClusteringConfig extends HoodieConfig {
   public static final ConfigProperty<Integer> CLUSTERING_MAX_PARALLELISM = ConfigProperty
       .key("hoodie.clustering.max.parallelism")
       .defaultValue(15)
-      .sinceVersion("0.14.0")
       .markAdvanced()
+      .sinceVersion("0.14.0")
       .withDocumentation("Maximum number of parallelism jobs submitted in clustering operation. "
           + "If the resource is sufficient(Like Spark engine has enough idle executors), increasing this "
           + "value will let the clustering job run faster, while it will give additional pressure to the "
