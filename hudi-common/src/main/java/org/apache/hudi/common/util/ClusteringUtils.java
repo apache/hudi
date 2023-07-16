4f599d27b98b90ffc8e48d655eb6d86148bc8780
Surya Prasanna Kumar Yalla <syalla@uber.com>
2023-07-15 17:05:31 -0700

    Fix checkstyle

--- hudi-common/src/main/java/org/apache/hudi/common/util/ClusteringUtils.java
+++ hudi-common/src/main/java/org/apache/hudi/common/util/ClusteringUtils.java
@@ -45,7 +45,6 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.AbstractMap;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
