10f4cb274b2312a2d8a3a59dbf88e17f7a5f67e0
Shawn Chang <yxchang@amazon.com>
2023-07-20 15:52:03 -0700

    10 datanodes

--- hudi-common/src/test/java/org/apache/hudi/common/testutils/HoodieTestUtils.java
+++ hudi-common/src/test/java/org/apache/hudi/common/testutils/HoodieTestUtils.java
@@ -244,7 +244,7 @@ public class HoodieTestUtils {
     Assumptions.assumeTrue(Boolean.valueOf(System.getProperty("use.external.hdfs", "false")));
     Configuration conf = new Configuration();
     conf.set("fs.defaultFS", "hdfs://localhost:9000");
-    conf.set("dfs.replication", "6");
+    conf.set("dfs.replication", "10");
     return (DistributedFileSystem) DistributedFileSystem.get(conf);
   }
 }
--- packaging/bundle-validation/docker_java17/docker_java17_test.sh
+++ packaging/bundle-validation/docker_java17/docker_java17_test.sh
@@ -79,7 +79,7 @@ setup_hdfs () {
   $HADOOP_HOME/sbin/start-dfs.sh
 
   # start datanodes
-  for i in $(seq 1 6)
+  for i in $(seq 1 10)
   do
     start_datanode $i
   done
