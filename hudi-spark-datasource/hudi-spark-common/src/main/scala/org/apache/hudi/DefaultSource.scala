72c0bb1928ea26a98a5513faad931adea3580325
Jonathan Vexler <=>
2023-07-30 11:59:45 -0400

    disable with timestamp keygenerator

--- hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/DefaultSource.scala
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/DefaultSource.scala
@@ -246,7 +246,8 @@ object DefaultSource {
     }
 
     val useMORBootstrapFF = parameters.getOrElse(MOR_BOOTSTRAP_FILE_READER.key,
-      MOR_BOOTSTRAP_FILE_READER.defaultValue).toBoolean && globPaths.isEmpty
+      MOR_BOOTSTRAP_FILE_READER.defaultValue).toBoolean && globPaths.isEmpty &&
+      !metaClient.getTableConfig.getKeyGeneratorClassName.equals("org.apache.hudi.keygen.TimestampBasedKeyGenerator")
 
     if (metaClient.getCommitsTimeline.filterCompletedInstants.countInstants() == 0) {
       new EmptyRelation(sqlContext, resolveSchema(metaClient, parameters, Some(schema)))
