c8652010dd5bbc28bae7a57c896d5babd05203f3
Lokesh Jain <ljain@apache.org>
2023-08-02 16:31:32 +0530

    Add javadocs

--- hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/RecordLevelIndexSupport.scala
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/RecordLevelIndexSupport.scala
@@ -39,6 +39,12 @@ class RecordLevelIndexSupport(spark: SparkSession,
   @transient private lazy val metadataTable: HoodieTableMetadata =
     HoodieTableMetadata.create(engineCtx, metadataConfig, metaClient.getBasePathV2.toString)
 
+  /**
+   * Returns the list of candidate files which should be queried after pruning based on query filters.
+   * @param allFiles - List of all files which needs to be considered for the query
+   * @param queryFilters - Input query filters. List of candidate files are pruned based on these query filters.
+   * @return Sequence of file names which need to be queried
+   */
   def getCandidateFiles(allFiles: Seq[FileStatus], queryFilters: Seq[Expression]): Set[String] = {
     val (_, recordKeys) = filterQueryFiltersWithRecordKey(queryFilters)
     val recordKeyLocationsMap = metadataTable.readRecordIndex(JavaConverters.seqAsJavaListConverter(recordKeys).asJava)
@@ -57,7 +63,10 @@ class RecordLevelIndexSupport(spark: SparkSession,
     candidateFiles.toSet
   }
 
-  def getRecordKey: Option[String] = {
+  /**
+   * Returns the configured record key for the table if it is a simple record key else returns empty option.
+   */
+  private def getRecordKeyConfig: Option[String] = {
     val recordKeysOpt: org.apache.hudi.common.util.Option[Array[String]] = metaClient.getTableConfig.getRecordKeyFields
     val recordKeyOpt = recordKeysOpt.map[String](JFunction.toJavaFunction[Array[String], String](arr =>
       if (arr.length == 1) {
@@ -68,8 +77,13 @@ class RecordLevelIndexSupport(spark: SparkSession,
     Option.apply(recordKeyOpt.orElse(null))
   }
 
+  /**
+   * Matches the configured simple record key with the input attribute name.
+   * @param attributeName The attribute name provided in the query
+   * @return true if input attribute name matches the configured simple record key
+   */
   private def attributeMatchesRecordKey(attributeName: String): Boolean = {
-    val recordKeyOpt = getRecordKey
+    val recordKeyOpt = getRecordKeyConfig
     if (recordKeyOpt.isDefined && recordKeyOpt.get == attributeName) {
       true
     } else {
@@ -77,6 +91,13 @@ class RecordLevelIndexSupport(spark: SparkSession,
     }
   }
 
+  /**
+   * Returns the attribute and literal pair given the operands of a binary operator. The pair is returned only if one of
+   * the operand is an attribute and other is literal. In other cases it returns an empty Option.
+   * @param expression1 - Left operand of the binary operator
+   * @param expression2 - Right operand of the binary operator
+   * @return Attribute and literal pair
+   */
   private def getAttributeLiteralTuple(expression1: Expression, expression2: Expression): Option[(AttributeReference, Literal)] = {
     expression1 match {
       case attr: AttributeReference => expression2 match {
@@ -96,6 +117,12 @@ class RecordLevelIndexSupport(spark: SparkSession,
 
   }
 
+  /**
+   * Given query filters, it filters the EqualTo queries on simple record key columns and returns a tuple of list of such
+   * queries and list of record key literals present in the query.
+   * @param queryFilters The queries that need to be filtered.
+   * @return Tuple of List of filtered queries and list of record key literals that need to be matched
+   */
   private def filterQueryFiltersWithRecordKey(queryFilters: Seq[Expression]): (List[Expression], List[String]) = {
     var recordKeyQueries: List[Expression] = List.empty
     var recordKeys: List[String] = List.empty
@@ -120,6 +147,9 @@ class RecordLevelIndexSupport(spark: SparkSession,
     isIndexAvailable && filterQueryFiltersWithRecordKey(queryFilters)._1.nonEmpty
   }
 
+  /**
+   * Return true is metadata table is enabled and record index metadata partition is enabled.
+   */
   private def isIndexAvailable: Boolean = {
     metadataConfig.enabled && metaClient.getTableConfig.getMetadataPartitions.contains(HoodieTableMetadataUtil.PARTITION_NAME_RECORD_INDEX)
   }
