dfbdb63782a97b6b6d8e10bdddebf3b2cbba48a4
Bhavani Sudha Saktheeswaran <2179254+bhasudha@users.noreply.github.com>
2023-08-02 11:14:14 -0700

    [DOCS] Update Indexing and File Layout pages
    Summary
    - Update page to reflect all index types
    - Updated page to add configs and links

--- website/src/theme/DocPage/index.js
+++ website/src/theme/DocPage/index.js
@@ -128,7 +128,7 @@ function DocPageContent({
   );
 }
 
-const arrayOfPages = (matchPath) => [`${matchPath}/configurations`, `${matchPath}/basic_configurations`, `${matchPath}/timeline`, `${matchPath}/table_types`, `${matchPath}/migration_guide`, `${matchPath}/compaction`, `${matchPath}/clustering`];
+const arrayOfPages = (matchPath) => [`${matchPath}/configurations`, `${matchPath}/basic_configurations`, `${matchPath}/timeline`, `${matchPath}/table_types`, `${matchPath}/migration_guide`, `${matchPath}/compaction`, `${matchPath}/clustering`, `${matchPath}/indexing`];
 const showCustomStylesForDocs = (matchPath, pathname) => arrayOfPages(matchPath).includes(pathname);
 function DocPage(props) {
   const {
