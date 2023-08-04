4c3527927e4c4f6e411b56521517ca6f1344813a
dongsj <dongsj@asiainfo.com>
2023-08-04 11:46:42 +0800

    Remove irrelevant comments

--- hudi-platform-service/hudi-metaserver/src/main/thrift/bin/thrift_in_mac_m1.sh
+++ hudi-platform-service/hudi-metaserver/src/main/thrift/bin/thrift_in_mac_m1.sh
@@ -16,14 +16,6 @@
 #  See the License for the specific language governing permissions and
 # limitations under the License.
 
-#
-# Usage: ./scripts/checkout_pr.sh
-#
-# Checkout a PR given the PR number into a local branch. PR branches are named
-# using the convention "pull/<PR_NUMBER>", to enable pr_push_command.sh to work
-# in tandem.
-#
-
 printf "====== INSTALL THRIFT START ======\n"
 brew install thrift@0.13.0
 printf "====== INSTALL THRIFT END ======\n"
