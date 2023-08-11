a0dc7e93b5eb7c0cab5bbd7b74dfd06230fc4dec
Bhavani Sudha Saktheeswaran <2179254+bhasudha@users.noreply.github.com>
2023-08-11 10:42:44 -0700

    [DOCS] Update clustering page with inline configs (#9429)

--- website/src/theme/DocPage/index.js
+++ website/src/theme/DocPage/index.js
@@ -128,7 +128,7 @@ function DocPageContent({
   );
 }
 
-const arrayOfPages = (matchPath) => [`${matchPath}/configurations`, `${matchPath}/basic_configurations`, `${matchPath}/timeline`, `${matchPath}/table_types`, `${matchPath}/migration_guide`, `${matchPath}/compaction`];
+const arrayOfPages = (matchPath) => [`${matchPath}/configurations`, `${matchPath}/basic_configurations`, `${matchPath}/timeline`, `${matchPath}/table_types`, `${matchPath}/migration_guide`, `${matchPath}/compaction`, `${matchPath}/clustering`];
 const showCustomStylesForDocs = (matchPath, pathname) => arrayOfPages(matchPath).includes(pathname);
 function DocPage(props) {
   const {
