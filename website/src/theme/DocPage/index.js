e597bd0dd33fb1cd1f28ff0b28298d0d52e0030d
Bhavani Sudha Saktheeswaran <2179254+bhasudha@users.noreply.github.com>
2023-08-28 03:21:36 -0700

    [DOCS] Update Metadata table and metadata indexing related pages (#9406)
    Summary:
    - Add inline configs
    - Add specific index descriptions
    - Add high level context
    - Restructure content for readability

--- website/src/theme/DocPage/index.js
+++ website/src/theme/DocPage/index.js
@@ -128,7 +128,7 @@ function DocPageContent({
   );
 }
 
-const arrayOfPages = (matchPath) => [`${matchPath}/configurations`, `${matchPath}/basic_configurations`, `${matchPath}/timeline`, `${matchPath}/table_types`, `${matchPath}/migration_guide`, `${matchPath}/compaction`, `${matchPath}/clustering`, `${matchPath}/indexing`];
+const arrayOfPages = (matchPath) => [`${matchPath}/configurations`, `${matchPath}/basic_configurations`, `${matchPath}/timeline`, `${matchPath}/table_types`, `${matchPath}/migration_guide`, `${matchPath}/compaction`, `${matchPath}/clustering`, `${matchPath}/indexing`, `${matchPath}/metadata`, `${matchPath}/metadata_indexing`];
 const showCustomStylesForDocs = (matchPath, pathname) => arrayOfPages(matchPath).includes(pathname);
 function DocPage(props) {
   const {
