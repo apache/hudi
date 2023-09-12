c5264bff9aee4131abe538169a0cb933d492e6ef
Lokesh Jain <ljain@apache.org>
2023-09-12 19:42:11 +0530

    Disable TestSchemaEvolutionClient

--- hudi-client/hudi-java-client/src/test/java/org/apache/hudi/table/action/commit/TestSchemaEvolutionClient.java
+++ hudi-client/hudi-java-client/src/test/java/org/apache/hudi/table/action/commit/TestSchemaEvolutionClient.java
@@ -31,6 +31,7 @@ import org.apache.hudi.testutils.HoodieJavaClientTestHarness;
 import org.apache.avro.Schema;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -42,6 +43,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Tests for schema evolution client api.
  */
+@Disabled
 public class TestSchemaEvolutionClient extends HoodieJavaClientTestHarness {
 
   private static final Schema SCHEMA = getSchemaFromResource(TestSchemaEvolutionClient.class, "/exampleSchema.avsc");
