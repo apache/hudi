58f02e17db22b5aa0a11a3e0ca1357fa069f98b2
Danny Chan <yuzhao.cyz@gmail.com>
2023-10-13 09:58:00 +0800

    Follow up HUDI-6937, fix the RealtimeCompactedRecordReader props instantiation (#9853)

--- hudi-hadoop-mr/src/main/java/org/apache/hudi/hadoop/realtime/RealtimeCompactedRecordReader.java
+++ hudi-hadoop-mr/src/main/java/org/apache/hudi/hadoop/realtime/RealtimeCompactedRecordReader.java
@@ -22,7 +22,6 @@ import org.apache.hudi.avro.HoodieAvroUtils;
 import org.apache.hudi.common.config.HoodieCommonConfig;
 import org.apache.hudi.common.config.HoodieMemoryConfig;
 import org.apache.hudi.common.config.HoodieReaderConfig;
-import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.common.fs.FSUtils;
 import org.apache.hudi.common.model.HoodieAvroIndexedRecord;
 import org.apache.hudi.common.model.HoodieAvroRecordMerger;
@@ -198,7 +197,7 @@ public class RealtimeCompactedRecordReader extends AbstractRealtimeRecordReader
     GenericRecord genericRecord = HiveAvroSerializer.rewriteRecordIgnoreResultCheck(oldRecord, getLogScannerReaderSchema());
     HoodieRecord record = new HoodieAvroIndexedRecord(genericRecord);
     Option<Pair<HoodieRecord, Schema>> mergeResult = HoodieAvroRecordMerger.INSTANCE.merge(record,
-        genericRecord.getSchema(), newRecord, getLogScannerReaderSchema(), new TypedProperties(payloadProps));
+        genericRecord.getSchema(), newRecord, getLogScannerReaderSchema(), payloadProps);
     return mergeResult.map(p -> (HoodieAvroIndexedRecord) p.getLeft());
   }
 
