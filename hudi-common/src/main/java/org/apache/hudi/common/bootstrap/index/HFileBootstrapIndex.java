7121c9826b03ad895ac0476e7b2076670887a576
Tim Brown <tim@onehouse.ai>
2023-10-18 01:21:51 -0500

    [MINOR] HFileBootstrapIndex: use try-with-resources in two places (#9813)

--- hudi-common/src/main/java/org/apache/hudi/common/bootstrap/index/HFileBootstrapIndex.java
+++ hudi-common/src/main/java/org/apache/hudi/common/bootstrap/index/HFileBootstrapIndex.java
@@ -320,8 +320,7 @@ public class HFileBootstrapIndex extends BootstrapIndex {
 
     @Override
     public List<BootstrapFileMapping> getSourceFileMappingForPartition(String partition) {
-      try {
-        HFileScanner scanner = partitionIndexReader().getScanner(true, false);
+      try (HFileScanner scanner = partitionIndexReader().getScanner(true, false)) {
         KeyValue keyValue = new KeyValue(Bytes.toBytes(getPartitionKey(partition)), new byte[0], new byte[0],
             HConstants.LATEST_TIMESTAMP, KeyValue.Type.Put, new byte[0]);
         if (scanner.seekTo(keyValue) == 0) {
@@ -353,8 +352,7 @@ public class HFileBootstrapIndex extends BootstrapIndex {
       // Arrange input Keys in sorted order for 1 pass scan
       List<HoodieFileGroupId> fileGroupIds = new ArrayList<>(ids);
       Collections.sort(fileGroupIds);
-      try {
-        HFileScanner scanner = fileIdIndexReader().getScanner(true, false);
+      try (HFileScanner scanner = fileIdIndexReader().getScanner(true, false)) {
         for (HoodieFileGroupId fileGroupId : fileGroupIds) {
           KeyValue keyValue = new KeyValue(Bytes.toBytes(getFileGroupKey(fileGroupId)), new byte[0], new byte[0],
               HConstants.LATEST_TIMESTAMP, KeyValue.Type.Put, new byte[0]);
