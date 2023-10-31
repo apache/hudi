542f6f2bfa70aaa6322768cf355d5bae672cd1a5
ksmou <13410244574@163.com>
2023-10-31 11:50:54 +0800

    [HUDI-7010] reduces redundant traversals

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/action/cluster/strategy/PartitionAwareClusteringPlanStrategy.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/action/cluster/strategy/PartitionAwareClusteringPlanStrategy.java
@@ -79,6 +79,11 @@ public abstract class PartitionAwareClusteringPlanStrategy<T,I,K,O> extends Clus
         fileSliceGroups.add(Pair.of(currentGroup, numOutputGroups));
         currentGroup = new ArrayList<>();
         totalSizeSoFar = 0;
+
+        // if fileSliceGroups's size reach the max group, stop loop
+        if (fileSliceGroups.size() >= writeConfig.getClusteringMaxNumGroups()) {
+          break;
+        }
       }
 
       // Add to the current file-group
