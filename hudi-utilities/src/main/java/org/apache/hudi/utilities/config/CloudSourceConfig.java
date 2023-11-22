a6476f06265d7600755e5597af173fea6db2954f
sivabalan <n.siva.b@gmail.com>
2023-11-22 00:52:30 -0800

    adding inference to CloudSourceconfig... cloud.data.datafile.format

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/config/CloudSourceConfig.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/config/CloudSourceConfig.java
@@ -23,6 +23,7 @@ import org.apache.hudi.common.config.ConfigClassProperty;
 import org.apache.hudi.common.config.ConfigGroups;
 import org.apache.hudi.common.config.ConfigProperty;
 import org.apache.hudi.common.config.HoodieConfig;
+import org.apache.hudi.common.util.Option;
 
 import javax.annotation.concurrent.Immutable;
 
@@ -109,6 +110,13 @@ public class CloudSourceConfig extends HoodieConfig {
   public static final ConfigProperty<String> DATAFILE_FORMAT = ConfigProperty
       .key(STREAMER_CONFIG_PREFIX + "source.cloud.data.datafile.format")
       .defaultValue("parquet")
+      .withInferFunction(hoodieConfig -> {
+        if (hoodieConfig.contains(HoodieIncrSourceConfig.SOURCE_FILE_FORMAT)) {
+          return Option.of(hoodieConfig.getString(HoodieIncrSourceConfig.SOURCE_FILE_FORMAT));
+        } else {
+          return Option.empty();
+        }
+      })
       .withAlternatives(DELTA_STREAMER_CONFIG_PREFIX + "source.cloud.data.datafile.format")
       .markAdvanced()
       .withDocumentation("Format of the data file. By default, this will be the same as hoodie.streamer.source.hoodieincr.file.format");
