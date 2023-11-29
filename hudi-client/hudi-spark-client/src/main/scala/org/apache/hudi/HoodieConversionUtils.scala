94c65825e5d6b2619b2c74e6fa45ad8e4cd2871e
rmahindra123 <rmahindra@Rajeshs-MacBook-Pro.local>
2023-11-28 23:26:41 -0800

    Fix tomap

--- hudi-client/hudi-spark-client/src/main/scala/org/apache/hudi/HoodieConversionUtils.scala
+++ hudi-client/hudi-spark-client/src/main/scala/org/apache/hudi/HoodieConversionUtils.scala
@@ -22,7 +22,6 @@ import org.apache.hudi.common.config.TypedProperties
 
 import java.{util => ju}
 import scala.collection.JavaConverters._
-import scala.jdk.CollectionConverters.dictionaryAsScalaMapConverter
 
 object HoodieConversionUtils {
 
@@ -49,9 +48,7 @@ object HoodieConversionUtils {
   }
 
   def fromProperties(props: TypedProperties): Map[String, String] = {
-    props.asScala.map {
-      case (k, v) => (k.toString, v.toString)
-    }.toMap
+    props.asScala.toMap
   }
 
 }
