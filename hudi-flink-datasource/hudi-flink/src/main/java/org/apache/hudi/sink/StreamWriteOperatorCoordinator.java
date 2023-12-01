bd86803c5584dcf3c6c87af08608e4e43df74cf0
ksmou <135721692+ksmou@users.noreply.github.com>
2023-12-01 08:45:52 +0800

    [HUDI-7165] Flink multi writer not close the failed instant heartbeat (#10221)

--- hudi-flink-datasource/hudi-flink/src/main/java/org/apache/hudi/sink/StreamWriteOperatorCoordinator.java
+++ hudi-flink-datasource/hudi-flink/src/main/java/org/apache/hudi/sink/StreamWriteOperatorCoordinator.java
@@ -418,6 +418,10 @@ public class StreamWriteOperatorCoordinator
       }
       commitInstant(instant);
     }
+    // stop the heartbeat for old instant
+    if (writeClient.getConfig().getFailedWritesCleanPolicy().isLazy() && !WriteMetadataEvent.BOOTSTRAP_INSTANT.equals(this.instant)) {
+      writeClient.getHeartbeatClient().stop(this.instant);
+    }
     // starts a new instant
     startInstant();
     // upgrade downgrade
