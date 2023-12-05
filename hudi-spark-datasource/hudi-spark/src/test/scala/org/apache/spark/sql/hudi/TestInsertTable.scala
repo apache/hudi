424bb88d840a9073aaba0ce0adbf63ea6d7bc9b3
Lin Liu <linliu.code@gmail.com>
2023-12-05 12:33:21 -0800

    [HUDI-7080] Enable two tests

--- hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/spark/sql/hudi/TestInsertTable.scala
+++ hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/spark/sql/hudi/TestInsertTable.scala
@@ -1170,8 +1170,6 @@ class TestInsertTable extends HoodieSparkSqlTestBase {
   }
 
   test("Test nested field as primaryKey and preCombineField") {
-    // TODO(HUDI-7080)
-    /*
     withRecordType()(withTempDir { tmp =>
       Seq("cow", "mor").foreach { tableType =>
         val tableName = generateTableName
@@ -1201,7 +1199,7 @@ class TestInsertTable extends HoodieSparkSqlTestBase {
           Seq("name_1", 10.0, 1000, "a", 999)
         )
       }
-    })*/
+    })
   }
 
   test("Test Insert Into With Catalog Identifier for spark >= 3.2.0") {
