8048ed72bc9a21e9d178445fdc39e1a379f183e0
Jonathan Vexler <=>
2023-12-06 17:22:37 -0500

    disable cdc

--- hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/DefaultSource.scala
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/DefaultSource.scala
@@ -247,6 +247,8 @@ object DefaultSource {
     if (metaClient.getCommitsTimeline.filterCompletedInstants.countInstants() == 0) {
       new EmptyRelation(sqlContext, resolveSchema(metaClient, parameters, Some(schema)))
     } else if (isCdcQuery) {
+      CDCRelation.getCDCRelation(sqlContext, metaClient, parameters)
+      /*
       if (useNewParquetFileFormat) {
         if (tableType == COPY_ON_WRITE) {
           new HoodieCopyOnWriteCDCHadoopFsRelationFactory(
@@ -258,6 +260,7 @@ object DefaultSource {
       } else {
         CDCRelation.getCDCRelation(sqlContext, metaClient, parameters)
       }
+       */
     } else {
       lazy val fileFormatUtils = if ((isMultipleBaseFileFormatsEnabled && !isBootstrappedTable)
         || (useNewParquetFileFormat)) {
