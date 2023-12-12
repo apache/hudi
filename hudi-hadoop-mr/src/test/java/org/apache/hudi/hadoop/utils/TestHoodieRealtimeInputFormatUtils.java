5273d8cc9ed428d2ac6896f52664618ed02c98a1
prathit06 <malik.prathit@gmail.com>
2023-12-12 12:14:40 +0530

    fixed checkstyle

--- hudi-hadoop-mr/src/test/java/org/apache/hudi/hadoop/utils/TestHoodieRealtimeInputFormatUtils.java
+++ hudi-hadoop-mr/src/test/java/org/apache/hudi/hadoop/utils/TestHoodieRealtimeInputFormatUtils.java
@@ -20,27 +20,30 @@ package org.apache.hudi.hadoop.utils;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.metastore.api.hive_metastoreConstants;
+
 import org.apache.hudi.common.testutils.HoodieTestUtils;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 public class TestHoodieRealtimeInputFormatUtils {
-    private Configuration hadoopConf;
 
-    @TempDir
-    public java.nio.file.Path basePath;
+  private Configuration hadoopConf;
 
-    @BeforeEach
-    public void setUp() {
-        hadoopConf = HoodieTestUtils.getDefaultHadoopConf();
-        hadoopConf.set("fs.defaultFS", "file:///");
-        hadoopConf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
-    }
+  @TempDir
+  public java.nio.file.Path basePath;
 
-    @Test
-    public void testAddProjectionField() {
-        hadoopConf.set(hive_metastoreConstants.META_TABLE_PARTITION_COLUMNS, "");
-        HoodieRealtimeInputFormatUtils.addProjectionField(hadoopConf, hadoopConf.get(hive_metastoreConstants.META_TABLE_PARTITION_COLUMNS, "").split("/"));
-    }
+  @BeforeEach
+  public void setUp() {
+    hadoopConf = HoodieTestUtils.getDefaultHadoopConf();
+    hadoopConf.set("fs.defaultFS", "file:///");
+    hadoopConf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
+  }
+
+  @Test
+  public void testAddProjectionField() {
+    hadoopConf.set(hive_metastoreConstants.META_TABLE_PARTITION_COLUMNS, "");
+    HoodieRealtimeInputFormatUtils.addProjectionField(hadoopConf, hadoopConf.get(hive_metastoreConstants.META_TABLE_PARTITION_COLUMNS, "").split("/"));
+  }
 }
