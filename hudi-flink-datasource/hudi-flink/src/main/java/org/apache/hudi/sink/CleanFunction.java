080d2f9f08ff95646ec13864b3eb416cf94d817b
Danny Chan <yuzhao.cyz@gmail.com>
2023-12-12 09:50:33 +0800

    [HUDI-7210] In CleanFunction#open, triggers the cleaning under option 'clean.async.enabled' (#10298)

--- hudi-flink-datasource/hudi-flink/src/main/java/org/apache/hudi/sink/CleanFunction.java
+++ hudi-flink-datasource/hudi-flink/src/main/java/org/apache/hudi/sink/CleanFunction.java
@@ -64,14 +64,16 @@ public class CleanFunction<T> extends AbstractRichFunction
     this.executor = NonThrownExecutor.builder(LOG).waitForTasksFinish(true).build();
     String instantTime = HoodieActiveTimeline.createNewInstantTime();
     LOG.info(String.format("exec clean with instant time %s...", instantTime));
-    executor.execute(() -> {
-      this.isCleaning = true;
-      try {
-        this.writeClient.clean(instantTime);
-      } finally {
-        this.isCleaning = false;
-      }
-    }, "wait for cleaning finish");
+    if (conf.getBoolean(FlinkOptions.CLEAN_ASYNC_ENABLED)) {
+      executor.execute(() -> {
+        this.isCleaning = true;
+        try {
+          this.writeClient.clean(instantTime);
+        } finally {
+          this.isCleaning = false;
+        }
+      }, "wait for cleaning finish");
+    }
   }
 
   @Override
