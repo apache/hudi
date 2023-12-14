d454d9afd2fb66e27aa320bc39f2aba2079fa72f
Timothy Brown <tim@onehouse.ai>
2023-12-14 16:21:07 -0600

    add cases for other policies

--- hudi-client/hudi-client-common/src/test/java/org/apache/hudi/table/action/TestCleanPlanner.java
+++ hudi-client/hudi-client-common/src/test/java/org/apache/hudi/table/action/TestCleanPlanner.java
@@ -50,6 +50,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -85,14 +86,15 @@ public class TestCleanPlanner {
     HoodieTableConfig tableConfig = new HoodieTableConfig();
     when(metaClient.getTableConfig()).thenReturn(tableConfig);
     HoodieTimeline mockCompletedCommitsTimeline = mock(HoodieTimeline.class);
-    when(mockCompletedCommitsTimeline.countInstants()).thenReturn(100);
+    when(mockCompletedCommitsTimeline.countInstants()).thenReturn(10);
     when(mockHoodieTable.getCompletedCommitsTimeline()).thenReturn(mockCompletedCommitsTimeline);
   }
 
   @ParameterizedTest
-  @MethodSource("keepLatestByHoursArgs")
-  void keepLatestByHours(String earliestInstant, List<HoodieFileGroup> allFileGroups, List<Pair<String, Option<byte[]>>> savepoints,
+  @MethodSource("testCases")
+  void testGetDeletePaths(HoodieWriteConfig config, String earliestInstant, List<HoodieFileGroup> allFileGroups, List<Pair<String, Option<byte[]>>> savepoints,
                          List<HoodieFileGroup> replacedFileGroups, Pair<Boolean, List<CleanFileInfo>> expected) {
+
     // setup savepoint mocks
     Set<String> savepointTimestamps = savepoints.stream().map(Pair::getLeft).collect(Collectors.toSet());
     when(mockHoodieTable.getSavepointTimestamps()).thenReturn(savepointTimestamps);
@@ -106,94 +108,203 @@ public class TestCleanPlanner {
     }
     String partitionPath = "partition1";
     // setup replaced file groups mocks
-    when(mockFsView.getReplacedFileGroupsBefore(earliestInstant, partitionPath)).thenReturn(replacedFileGroups.stream());
+    if (config.getCleanerPolicy() == HoodieCleaningPolicy.KEEP_LATEST_FILE_VERSIONS) {
+      when(mockFsView.getAllReplacedFileGroups(partitionPath)).thenReturn(replacedFileGroups.stream());
+    } else {
+      when(mockFsView.getReplacedFileGroupsBefore(earliestInstant, partitionPath)).thenReturn(replacedFileGroups.stream());
+    }
     // setup current file groups mocks
     when(mockFsView.getAllFileGroupsStateless(partitionPath)).thenReturn(allFileGroups.stream());
-    HoodieWriteConfig config = HoodieWriteConfig.newBuilder().withPath("/tmp")
-        .withCleanConfig(HoodieCleanConfig.newBuilder()
-            .cleanerNumHoursRetained(24)
-            .withCleanerPolicy(HoodieCleaningPolicy.KEEP_LATEST_BY_HOURS)
-            .build())
-        .build();
+
     CleanPlanner<?, ?, ?, ?> cleanPlanner = new CleanPlanner<>(context, mockHoodieTable, config);
     HoodieInstant earliestCommitToRetain = new HoodieInstant(HoodieInstant.State.COMPLETED, "COMMIT", earliestInstant);
     Pair<Boolean, List<CleanFileInfo>> actual = cleanPlanner.getDeletePaths(partitionPath, Option.of(earliestCommitToRetain));
     assertEquals(expected, actual);
   }
 
-  static Stream<Arguments> keepLatestByHoursArgs() {
+  static Stream<Arguments> testCases() {
+    return Stream.concat(keepLatestByHoursOrCommitsArgs(), keepLatestVersionsArgs());
+  }
+
+  static Stream<Arguments> keepLatestVersionsArgs() {
+    HoodieWriteConfig keepLatestVersionsConfig = HoodieWriteConfig.newBuilder().withPath("/tmp")
+        .withCleanConfig(HoodieCleanConfig.newBuilder()
+            .retainFileVersions(2)
+            .withCleanerPolicy(HoodieCleaningPolicy.KEEP_LATEST_FILE_VERSIONS)
+            .build())
+        .build();
+    String instant1 = "20231205194919610";
+    String instant2 = "20231204194919610";
+    String instant3 = "20231201194919610";
+    String instant4 = "20231127194919610";
+    List<Arguments> arguments = new ArrayList<>();
+    // Two file slices in the group: both should be retained
+    arguments.add(Arguments.of(
+        keepLatestVersionsConfig,
+        instant1,
+        Collections.singletonList(buildFileGroup(Arrays.asList(instant2, instant1))),
+        Collections.emptyList(),
+        Collections.emptyList(),
+        Pair.of(false, Collections.emptyList())));
+    // Four file slices in the group: only the latest two should be retained
+    HoodieFileGroup fileGroup = buildFileGroup(Arrays.asList(instant4, instant3, instant2, instant1));
+    String instant3Path = fileGroup.getAllBaseFiles()
+        .filter(baseFile -> baseFile.getCommitTime().equals(instant3)).findFirst().get().getPath();
+    CleanFileInfo expectedCleanFileInfoForInstant3 = new CleanFileInfo(instant3Path, false);
+    String instant4Path = fileGroup.getAllBaseFiles()
+        .filter(baseFile -> baseFile.getCommitTime().equals(instant4)).findFirst().get().getPath();
+    CleanFileInfo expectedCleanFileInfoForInstant4 = new CleanFileInfo(instant4Path, false);
+    arguments.add(Arguments.of(
+        keepLatestVersionsConfig,
+        instant1,
+        Collections.singletonList(fileGroup),
+        Collections.emptyList(),
+        Collections.emptyList(),
+        Pair.of(false, Arrays.asList(expectedCleanFileInfoForInstant3, expectedCleanFileInfoForInstant4))));
+    // Four file slices in group but instant4 is part of savepiont: only instant 3's files should be cleaned
+    List<Pair<String, Option<byte[]>>> savepoints = Collections.singletonList(Pair.of(instant4, getSavepointBytes("partition1", Collections.singletonList(instant4Path))));
+    arguments.add(Arguments.of(
+        keepLatestVersionsConfig,
+        instant1,
+        Collections.singletonList(fileGroup),
+        savepoints,
+        Collections.emptyList(),
+        Pair.of(false, Arrays.asList(expectedCleanFileInfoForInstant3))));
+    // Two file slices with a replaced file group: only replaced files cleaned up
+    HoodieFileGroup replacedFileGroup = buildFileGroup(Collections.singletonList(instant4));
+    String replacedFilePath = replacedFileGroup.getAllBaseFiles().findFirst().get().getPath();
+    CleanFileInfo expectedReplaceCleanFileInfo = new CleanFileInfo(replacedFilePath, false);
+    arguments.add(Arguments.of(
+        keepLatestVersionsConfig,
+        instant1,
+        Collections.singletonList(buildFileGroup(Arrays.asList(instant2, instant1))),
+        Collections.emptyList(),
+        Collections.singletonList(replacedFileGroup),
+        Pair.of(false, Collections.singletonList(expectedReplaceCleanFileInfo))));
+    // replaced file groups referenced by savepoint should not be cleaned up
+    List<Pair<String, Option<byte[]>>> replacedFileGroupSavepoint = Collections.singletonList(Pair.of(instant4, getSavepointBytes("partition1", Collections.singletonList(replacedFilePath))));
+    arguments.add(Arguments.of(
+        keepLatestVersionsConfig,
+        instant1,
+        Collections.singletonList(buildFileGroup(Arrays.asList(instant2, instant1))),
+        replacedFileGroupSavepoint,
+        Collections.singletonList(replacedFileGroup),
+        Pair.of(false, Collections.emptyList())));
+    return arguments.stream();
+  }
+
+  static Stream<Arguments> keepLatestByHoursOrCommitsArgs() {
     String earliestInstant = "20231204194919610";
     String earliestInstantPlusTwoDays = "20231205194919610";
     String earliestInstantMinusThreeDays = "20231201194919610";
     String earliestInstantMinusOneWeek = "20231127194919610";
     String earliestInstantMinusOneMonth = "20231104194919610";
+    List<Arguments> arguments = new ArrayList<>();
     // Only one file slice in the group: should still be kept even with commit earlier than "earliestInstant"
-    Arguments singleFileSliceCase = Arguments.of(
+    arguments.addAll(buildArgumentsForCleanByHoursAndCommitsCases(
         earliestInstant,
         Collections.singletonList(buildFileGroup(Collections.singletonList(earliestInstantMinusOneMonth))),
         Collections.emptyList(),
         Collections.emptyList(),
-        Pair.of(false, Collections.emptyList()));
+        Pair.of(false, Collections.emptyList())));
     // File group with two slices, both are before the earliestInstant. Only the latest slice should be kept.
     HoodieFileGroup fileGroupsBeforeInstant = buildFileGroup(Arrays.asList(earliestInstantMinusOneMonth, earliestInstantMinusOneWeek));
     CleanFileInfo expectedCleanFileInfoForFirstFile = new CleanFileInfo(fileGroupsBeforeInstant.getAllBaseFiles()
         .filter(baseFile -> baseFile.getCommitTime().equals(earliestInstantMinusOneMonth)).findFirst().get().getPath(), false);
-    Arguments twoFileSlicesBeforeEarliestInstantCase = Arguments.of(
+    arguments.addAll(buildArgumentsForCleanByHoursAndCommitsCases(
         earliestInstant,
         Collections.singletonList(fileGroupsBeforeInstant),
         Collections.emptyList(),
         Collections.emptyList(),
-        Pair.of(false, Collections.singletonList(expectedCleanFileInfoForFirstFile)));
+        Pair.of(false, Collections.singletonList(expectedCleanFileInfoForFirstFile))));
     // File group with two slices, one is after the earliestInstant and the other is before the earliestInstant.
     // We should keep both since base files are required for queries evaluating the table at time NOW - 24hrs (24hrs is configured for test)
-    Arguments twoFileSliceCase = Arguments.of(
+    arguments.addAll(buildArgumentsForCleanByHoursAndCommitsCases(
         earliestInstant,
         Collections.singletonList(buildFileGroup(Arrays.asList(earliestInstantMinusOneMonth, earliestInstantPlusTwoDays))),
         Collections.emptyList(),
         Collections.emptyList(),
-        Pair.of(false, Collections.emptyList()));
+        Pair.of(false, Collections.emptyList())));
     // File group with three slices, one is after the earliestInstant and the other two are before the earliestInstant.
     // Oldest slice will be removed since it is not required for queries evaluating the table at time NOW - 24hrs
     String oldestFileInstant = earliestInstantMinusOneMonth;
     HoodieFileGroup fileGroup = buildFileGroup(Arrays.asList(oldestFileInstant, earliestInstantMinusThreeDays, earliestInstantPlusTwoDays));
     String oldestFilePath = fileGroup.getAllBaseFiles().filter(baseFile -> baseFile.getCommitTime().equals(oldestFileInstant)).findFirst().get().getPath();
     CleanFileInfo expectedCleanFileInfo = new CleanFileInfo(oldestFilePath, false);
-    Arguments threeFileSliceCase = Arguments.of(
+    arguments.addAll(buildArgumentsForCleanByHoursAndCommitsCases(
         earliestInstant,
         Collections.singletonList(fileGroup),
         Collections.emptyList(),
         Collections.emptyList(),
-        Pair.of(false, Collections.singletonList(expectedCleanFileInfo)));
+        Pair.of(false, Collections.singletonList(expectedCleanFileInfo))));
     // File group with three slices, one is after the earliestInstant and the other two are before the earliestInstant. Oldest slice is also in savepoint so should not be removed.
     List<Pair<String, Option<byte[]>>> savepoints = Collections.singletonList(Pair.of(oldestFileInstant, getSavepointBytes("partition1", Collections.singletonList(oldestFilePath))));
-    Arguments threeFileSliceCaseWithSavepointOnOldest = Arguments.of(
+    arguments.addAll(buildArgumentsForCleanByHoursAndCommitsCases(
         earliestInstant,
         Collections.singletonList(fileGroup),
         savepoints,
         Collections.emptyList(),
-        Pair.of(false, Collections.emptyList()));
+        Pair.of(false, Collections.emptyList())));
     // File group is replaced before the earliestInstant. Should be removed.
     HoodieFileGroup replacedFileGroup = buildFileGroup(Collections.singletonList(earliestInstantMinusOneMonth));
     String replacedFilePath = replacedFileGroup.getAllBaseFiles().findFirst().get().getPath();
     CleanFileInfo expectedReplaceCleanFileInfo = new CleanFileInfo(replacedFilePath, false);
-    Arguments replaceFileGroupCase = Arguments.of(
+    arguments.addAll(buildArgumentsForCleanByHoursAndCommitsCases(
         earliestInstant,
         Collections.singletonList(buildFileGroup(Collections.singletonList(earliestInstantMinusOneMonth))),
         Collections.emptyList(),
         Collections.singletonList(replacedFileGroup),
-        Pair.of(false, Collections.singletonList(expectedReplaceCleanFileInfo)));
+        Pair.of(false, Collections.singletonList(expectedReplaceCleanFileInfo))));
     // File group is replaced before the earliestInstant but referenced in a savepoint. Should be retained.
     List<Pair<String, Option<byte[]>>> savepointsForReplacedGroup = Collections.singletonList(Pair.of(oldestFileInstant,
         getSavepointBytes("partition1", Collections.singletonList(replacedFilePath))));
-    Arguments replaceFileGroupInSavepointCase = Arguments.of(
+    arguments.addAll(buildArgumentsForCleanByHoursAndCommitsCases(
         earliestInstant,
         Collections.singletonList(buildFileGroup(Collections.singletonList(earliestInstantMinusOneMonth))),
         savepointsForReplacedGroup,
         Collections.singletonList(replacedFileGroup),
-        Pair.of(false, Collections.emptyList()));
+        Pair.of(false, Collections.emptyList())));
+    // Clean by commits but there are not enough commits in timeline to trigger cleaner
+    HoodieWriteConfig writeConfigWithLargerRetention = HoodieWriteConfig.newBuilder().withPath("/tmp")
+        .withCleanConfig(HoodieCleanConfig.newBuilder()
+            .retainCommits(50)
+            .withCleanerPolicy(HoodieCleaningPolicy.KEEP_LATEST_COMMITS)
+            .build())
+        .build();
+    arguments.add(Arguments.of(
+        writeConfigWithLargerRetention,
+        earliestInstant,
+        Collections.singletonList(buildFileGroup(Collections.singletonList(earliestInstantMinusOneMonth))),
+        Collections.emptyList(),
+        Collections.singletonList(replacedFileGroup),
+        Pair.of(false, Collections.emptyList())));
 
-    return Stream.of(singleFileSliceCase, twoFileSlicesBeforeEarliestInstantCase, twoFileSliceCase, threeFileSliceCase,
-        threeFileSliceCaseWithSavepointOnOldest, replaceFileGroupCase, replaceFileGroupInSavepointCase);
+    return arguments.stream();
+  }
+
+  private static HoodieWriteConfig getCleanByHoursConfig() {
+    return HoodieWriteConfig.newBuilder().withPath("/tmp")
+        .withCleanConfig(HoodieCleanConfig.newBuilder()
+            .cleanerNumHoursRetained(24)
+            .withCleanerPolicy(HoodieCleaningPolicy.KEEP_LATEST_BY_HOURS)
+            .build())
+        .build();
+  }
+
+  private static HoodieWriteConfig getCleanByCommitsConfig() {
+    return HoodieWriteConfig.newBuilder().withPath("/tmp")
+        .withCleanConfig(HoodieCleanConfig.newBuilder()
+            .retainCommits(5)
+            .withCleanerPolicy(HoodieCleaningPolicy.KEEP_LATEST_COMMITS)
+            .build())
+        .build();
+  }
+
+  // helper to build common cases for the two policies
+  private static List<Arguments> buildArgumentsForCleanByHoursAndCommitsCases(String earliestInstant, List<HoodieFileGroup> allFileGroups, List<Pair<String, Option<byte[]>>> savepoints,
+                                                                              List<HoodieFileGroup> replacedFileGroups, Pair<Boolean, List<CleanFileInfo>> expected) {
+    return Arrays.asList(Arguments.of(getCleanByHoursConfig(), earliestInstant, allFileGroups, savepoints, replacedFileGroups, expected),
+        Arguments.of(getCleanByCommitsConfig(), earliestInstant, allFileGroups, savepoints, replacedFileGroups, expected));
   }
 
   private static HoodieFileGroup buildFileGroup(List<String> baseFileCommitTimes) {
