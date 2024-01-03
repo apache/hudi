321e96e848e0eac5c537ed5977c871f5f1696a7b
Jonathan Vexler <=>
2024-01-03 18:35:31 -0500

    add cow back to test

--- hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/spark/sql/hudi/TestSpark3DDL.scala
+++ hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/spark/sql/hudi/TestSpark3DDL.scala
@@ -71,7 +71,7 @@ class TestSpark3DDL extends HoodieSparkSqlTestBase {
 
   test("Test alter column types") {
     withRecordType()(withTempDir { tmp =>
-      Seq("mor").foreach { tableType =>
+      Seq("cow", "mor").foreach { tableType =>
         val tableName = generateTableName
         val tablePath = s"${new Path(tmp.getCanonicalPath, tableName).toUri.toString}"
         if (HoodieSparkUtils.gteqSpark3_1) {
