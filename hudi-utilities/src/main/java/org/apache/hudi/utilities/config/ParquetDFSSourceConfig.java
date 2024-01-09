378a6a619dc288301c70275483bbb0ecfa73a7f1
Rohit Mittapalli <rohit@applied.co>
2024-01-08 17:26:47 -0800

    flag with new parquet dfs source config

---
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/config/ParquetDFSSourceConfig.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.hudi.utilities.config;
+
+import org.apache.hudi.common.config.ConfigClassProperty;
+import org.apache.hudi.common.config.ConfigGroups;
+import org.apache.hudi.common.config.ConfigProperty;
+import org.apache.hudi.common.config.HoodieConfig;
+
+import javax.annotation.concurrent.Immutable;
+
+import static org.apache.hudi.common.util.ConfigUtils.OLD_SCHEMAPROVIDER_CONFIG_PREFIX;
+import static org.apache.hudi.common.util.ConfigUtils.SCHEMAPROVIDER_CONFIG_PREFIX;
+
+/**
+ * Parquet DFS Source Configs
+ */
+@Immutable
+@ConfigClassProperty(name = "Parquet DFS Source Configs",
+        groupName = ConfigGroups.Names.HUDI_STREAMER,
+        subGroupName = ConfigGroups.SubGroupNames.SCHEMA_PROVIDER,
+        description = "Configurations controlling the behavior of Parquet DFS source in Hudi Streamer.")
+public class ParquetDFSSourceConfig extends HoodieConfig {
+
+    public static final ConfigProperty<Boolean> PARQUET_MERGE_SCHEMA = ConfigProperty
+            .key(SCHEMAPROVIDER_CONFIG_PREFIX + "source.schema.parquet.mergeschema")
+            .defaultValue(true)
+            .withAlternatives(OLD_SCHEMAPROVIDER_CONFIG_PREFIX + "source.schema.parquet.mergeschema")
+            .markAdvanced()
+            .withDocumentation("Merge schema across parquet files within a single write");
+}
