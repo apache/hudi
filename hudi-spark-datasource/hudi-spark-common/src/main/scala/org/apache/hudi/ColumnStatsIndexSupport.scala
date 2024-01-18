b2aef89a8737cf3c8eef379883535ba45f194714
majian <47964462+majian1998@users.noreply.github.com>
2024-01-18 20:16:32 +0800

    [HUDI-7246] Fix Data Skipping Issue: No Results When Query Conditions Involve Both Columns with and without Column Stats (#10389)

--- hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/ColumnStatsIndexSupport.scala
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/ColumnStatsIndexSupport.scala
@@ -280,13 +280,17 @@ class ColumnStatsIndexSupport(spark: SparkSession,
                   acc ++= Seq(colStatRecord.getMinValue, colStatRecord.getMaxValue, colStatRecord.getNullCount)
                 case None =>
                   // NOTE: This could occur in either of the following cases:
-                  //    1. Particular file does not have this particular column (which is indexed by Column Stats Index):
-                  //       in this case we're assuming missing column to essentially contain exclusively
-                  //       null values, we set min/max values as null and null-count to be equal to value-count (this
-                  //       behavior is consistent with reading non-existent columns from Parquet)
+                  //    1. When certain columns exist in the schema but are absent in some data files due to
+                  //       schema evolution or other reasons, these columns will not be present in the column stats.
+                  //       In this case, we fill in default values by setting the min, max and null-count to null
+                  //       (this behavior is consistent with reading non-existent columns from Parquet).
+                  //    2. When certain columns are present both in the schema and the data files,
+                  //       but the column stats are absent for these columns due to their types not supporting indexing,
+                  //       we also set these columns to default values.
                   //
-                  // This is a way to determine current column's index without explicit iteration (we're adding 3 stats / column)
-                  acc ++= Seq(null, null, valueCount)
+                  // This approach prevents errors during data skipping and, because the filter includes an isNull check,
+                  // these conditions will not affect the accurate return of files from data skipping.
+                  acc ++= Seq(null, null, null)
               }
           }
 
