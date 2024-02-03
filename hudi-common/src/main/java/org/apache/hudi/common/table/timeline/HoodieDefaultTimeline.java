4a0429297fc891be13f80d646677b3e561e0b6cd
Jon Vexler <jbvexler@gmail.com>
2024-02-03 14:40:18 -0500

    [HUDI-7284] Fix bad method name getLastPendingClusterCommit to getLastPendingClusterInstant (#10613)
    Co-authored-by: Jonathan Vexler <=>

--- hudi-common/src/main/java/org/apache/hudi/common/table/timeline/HoodieDefaultTimeline.java
+++ hudi-common/src/main/java/org/apache/hudi/common/table/timeline/HoodieDefaultTimeline.java
@@ -514,7 +514,7 @@ public class HoodieDefaultTimeline implements HoodieTimeline {
   }
 
   @Override
-  public Option<HoodieInstant> getLastPendingClusterCommit() {
+  public Option<HoodieInstant> getLastPendingClusterInstant() {
     return  Option.fromJavaOptional(filterPendingReplaceTimeline()
         .getReverseOrderedInstants()
         .filter(i -> ClusteringUtils.isPendingClusteringInstant(this, i)).findFirst());
