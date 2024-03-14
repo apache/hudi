d8637622beb14008d88c858dbaf05afda86c2562
xuzifu666 <1206332514@qq.com>
2024-03-14 14:06:28 +0800

    fix payload

--- hudi-common/src/main/java/org/apache/hudi/common/model/OverwriteWithGreaterRecordPayload.java
+++ hudi-common/src/main/java/org/apache/hudi/common/model/OverwriteWithGreaterRecordPayload.java
@@ -38,6 +38,20 @@ public class OverwriteWithGreaterRecordPayload extends DefaultHoodieRecordPayloa
   }
 
   @Override
+  public OverwriteWithLatestAvroPayload preCombine(OverwriteWithLatestAvroPayload oldValue) {
+    if (oldValue.recordBytes.length == 0) {
+      // use natural order for delete record
+      return this;
+    }
+    if (oldValue.orderingVal.compareTo(orderingVal) >= 0) {
+      // pick the payload with greatest ordering value
+      return oldValue;
+    } else {
+      return this;
+    }
+  }
+
+  @Override
   protected boolean needUpdatingPersistedRecord(IndexedRecord currentValue,
                                                 IndexedRecord incomingRecord, Properties properties) {
     /*
