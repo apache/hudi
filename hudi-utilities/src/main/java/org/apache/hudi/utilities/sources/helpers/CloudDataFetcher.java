663de2e588b28a4c9ee6700c652be2b4c98b314d
Vinish Reddy <vinishreddygunner17@gmail.com>
2024-03-18 19:46:27 +0530

    Fix tests

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/sources/helpers/CloudDataFetcher.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/sources/helpers/CloudDataFetcher.java
@@ -101,8 +101,8 @@ public class CloudDataFetcher implements Serializable {
         IncrSourceHelper.filterAndGenerateCheckpointBasedOnSourceLimit(
             filteredSourceData, sourceLimit, queryInfo, cloudObjectIncrCheckpoint);
     if (!checkPointAndDataset.getRight().isPresent()) {
-      LOG.info("Empty source, returning endpoint:" + checkPointAndDataset.getLeft().toString());
-      return Pair.of(Option.empty(), checkPointAndDataset.getLeft().toString());
+      LOG.info("Empty source, returning endpoint:" + queryInfo.getEndInstant());
+      return Pair.of(Option.empty(), queryInfo.getEndInstant());
     }
     LOG.info("Adjusted end checkpoint :" + checkPointAndDataset.getLeft());
 
