geserdugarov/hudi-open-source

[HUDI-7493] Consistent naming of Cleaner configuration parameters

hudi-client/hudi-client-common/src/main/java/org/apache/hudi/config/HoodieWriteConfig.java
@@ -3350,7 +3350,7 @@ public class HoodieWriteConfig extends HoodieConfig {
             HoodieLockConfig.LOCK_PROVIDER_CLASS_NAME.key(), InProcessLockProvider.class.getName()));
       }
 
-      // We check if "hoodie.cleaner.policy.failed.writes"
+      // We check if "hoodie.cleaner.failed.writes.policy"
       // is properly set to LAZY for multi-writers
       WriteConcurrencyMode writeConcurrencyMode = writeConfig.getWriteConcurrencyMode();
       if (writeConcurrencyMode.supportsMultiWriter()) {
@@ -3378,7 +3378,7 @@ public class HoodieWriteConfig extends HoodieConfig {
         checkArgument(!writeConfig.getString(HoodieCleanConfig.FAILED_WRITES_CLEANER_POLICY)
             .equals(HoodieFailedWritesCleaningPolicy.EAGER.name()),
             String.format(
-                "To enable %s, set hoodie.cleaner.policy.failed.writes=LAZY",
+                "To enable %s, set hoodie.cleaner.failed.writes.policy=LAZY",
                 writeConcurrencyMode.name()));
       }
       if (writeConcurrencyMode == WriteConcurrencyMode.NON_BLOCKING_CONCURRENCY_CONTROL) {

hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/action/clean/CleaningTriggerStrategy.java
@@ -24,6 +24,6 @@ import org.apache.hudi.common.config.EnumFieldDescription;
 @EnumDescription("Controls when cleaning is scheduled.")
 public enum CleaningTriggerStrategy {
     // trigger cleaning when reach n commits
-    @EnumFieldDescription("Trigger the cleaning service every N commits, determined by `hoodie.clean.max.commits`.")
+    @EnumFieldDescription("Trigger the cleaning service every N commits, determined by `hoodie.cleaner.trigger.max.commits`.")
     NUM_COMMITS
 }

apache/hudi

hudi-client/hudi-client-common/src/main/java/org/apache/hudi/config/HoodieWriteConfig.java
@@ -3350,7 +3350,7 @@ public class HoodieWriteConfig extends HoodieConfig {
             HoodieLockConfig.LOCK_PROVIDER_CLASS_NAME.key(), InProcessLockProvider.class.getName()));
       }
 
-      // We check if "hoodie.cleaner.policy.failed.writes"
+      // We check if "hoodie.clean.failed.writes.policy"
       // is properly set to LAZY for multi-writers
       WriteConcurrencyMode writeConcurrencyMode = writeConfig.getWriteConcurrencyMode();
       if (writeConcurrencyMode.supportsMultiWriter()) {
@@ -3378,7 +3378,7 @@ public class HoodieWriteConfig extends HoodieConfig {
         checkArgument(!writeConfig.getString(HoodieCleanConfig.FAILED_WRITES_CLEANER_POLICY)
             .equals(HoodieFailedWritesCleaningPolicy.EAGER.name()),
             String.format(
-                "To enable %s, set hoodie.cleaner.policy.failed.writes=LAZY",
+                "To enable %s, set hoodie.clean.failed.writes.policy=LAZY",
                 writeConcurrencyMode.name()));
       }
       if (writeConcurrencyMode == WriteConcurrencyMode.NON_BLOCKING_CONCURRENCY_CONTROL) {

hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/action/clean/CleaningTriggerStrategy.java
@@ -24,6 +24,6 @@ import org.apache.hudi.common.config.EnumFieldDescription;
 @EnumDescription("Controls when cleaning is scheduled.")
 public enum CleaningTriggerStrategy {
     // trigger cleaning when reach n commits
-    @EnumFieldDescription("Trigger the cleaning service every N commits, determined by `hoodie.clean.max.commits`.")
+    @EnumFieldDescription("Trigger the cleaning service every N commits, determined by `hoodie.clean.trigger.max.commits`.")
     NUM_COMMITS
 }