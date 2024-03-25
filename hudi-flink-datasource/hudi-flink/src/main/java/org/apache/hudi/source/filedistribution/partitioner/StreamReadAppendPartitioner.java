b8aa7d883400756f435fa5f3a0a4fc96e5cc7869
zhuanshenbsj1 <34104400+zhuanshenbsj1@users.noreply.github.com>
2024-03-25 09:06:21 +0800

    checkstyle (#10919)

--- hudi-flink-datasource/hudi-flink/src/main/java/org/apache/hudi/source/filedistribution/partitioner/StreamReadAppendPartitioner.java
+++ hudi-flink-datasource/hudi-flink/src/main/java/org/apache/hudi/source/filedistribution/partitioner/StreamReadAppendPartitioner.java
@@ -1,4 +1,3 @@
-package org.apache.hudi.source.filedistribution.partitioner;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,6 +15,8 @@ package org.apache.hudi.source.filedistribution.partitioner;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.hudi.source.filedistribution.partitioner;
 
 import org.apache.flink.api.common.functions.Partitioner;
 
