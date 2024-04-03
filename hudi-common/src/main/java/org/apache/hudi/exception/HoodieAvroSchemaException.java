ea630b524af999cfa7b71a35cc214ebeca12366f
Jon Vexler <jbvexler@gmail.com>
2024-04-02 21:31:20 -0400

    Update hudi-common/src/main/java/org/apache/hudi/exception/HoodieAvroSchemaException.java
    Co-authored-by: Y Ethan Guo <ethan.guoyihua@gmail.com>

--- hudi-common/src/main/java/org/apache/hudi/exception/HoodieAvroSchemaException.java
+++ hudi-common/src/main/java/org/apache/hudi/exception/HoodieAvroSchemaException.java
@@ -21,8 +21,8 @@ package org.apache.hudi.exception;
 
 /**
  * Thrown when we detect in Hudi code that a record schema
- * will violate avro rules. This can happen even when using spark
- * because we use avro schema internally
+ * violates Avro rules. This can happen even when using Spark
+ * because we use Avro schema internally
  */
 public class HoodieAvroSchemaException extends SchemaCompatibilityException {
   public HoodieAvroSchemaException(String message) {
