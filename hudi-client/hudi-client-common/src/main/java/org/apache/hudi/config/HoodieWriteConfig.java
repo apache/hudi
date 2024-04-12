d3438b5f44bdbced3fe3f28577f6e9ce07c938ca
Prashant Wason <pwason@uber.com>
2024-04-12 16:36:23 -0700

    [UBER] Added a config to enable filtering partitions to be cleaned during full clean.
    Summary:
    This can be used to restrict the number of partitions cleaned at a time and deal with OOM failures.
    
    Config is hoodie.cleaner.partition.filter.regex and expects a regex which is matched against partition names.
    
    Example using HUDI Manager tool:
    
    Running clean on only 2024 year partitions
    
    QUEUE=hadoop-platform-komondor-schemaless-speed NUM_EXECUTORS=1024 DRIVER_MEMORY=28g COMMAND=clean ARGS="-basepath=hdfs://ns-router-prod-phx/uber-data/tables/rawdata/schemaless/row/compliancestore_udr_screenings_4,num-executors=1024,hoodie-config=hoodie.cleaner.parallelism=1024,hoodie-config=hoodie.cleaner.partition.filter.regex=2024.*,-hoodie-config=hoodie.cleaner.incremental.mode=false" drogon launch --app hudi_manager -c phx2 -t
    
    Reviewers: bkrishen, O955 Project Hoodie Project Reviewer: Add blocking reviewers, #hoodie_blocking_reviewers
    
    Reviewed By: bkrishen, O955 Project Hoodie Project Reviewer: Add blocking reviewers, #hoodie_blocking_reviewers
    
    JIRA Issues: HUDI-4831
    
    Differential Revision: https://code.uberinternal.com/D13600119

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/config/HoodieWriteConfig.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/config/HoodieWriteConfig.java
@@ -1819,6 +1819,10 @@ public class HoodieWriteConfig extends HoodieConfig {
     return getBoolean(HoodieCleanConfig.CLEANER_INCREMENTAL_MODE_ENABLE);
   }
 
+  public String getCleanerPartitionRegex() {
+    return getString(HoodieCleanConfig.CLEANER_PARTITION_FILTER_REGEX);
+  }
+
   public boolean inlineCompactionEnabled() {
     return getBoolean(HoodieCompactionConfig.INLINE_COMPACT);
   }
