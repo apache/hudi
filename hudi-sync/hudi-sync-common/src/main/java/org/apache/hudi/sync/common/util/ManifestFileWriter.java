c4cecf39045e224578737019f2baeb0b5acbcf16
Sivabalan Narayanan <n.siva.b@gmail.com>
2024-04-22 11:23:08 -0700

    [ENG-8689][HUDI-7654] Optimizing BQ sync for MDT (#653)

--- hudi-sync/hudi-sync-common/src/main/java/org/apache/hudi/sync/common/util/ManifestFileWriter.java
+++ hudi-sync/hudi-sync-common/src/main/java/org/apache/hudi/sync/common/util/ManifestFileWriter.java
@@ -19,13 +19,16 @@
 package org.apache.hudi.sync.common.util;
 
 import org.apache.hudi.common.config.HoodieMetadataConfig;
+import org.apache.hudi.common.engine.HoodieEngineContext;
 import org.apache.hudi.common.engine.HoodieLocalEngineContext;
 import org.apache.hudi.common.fs.FSUtils;
 import org.apache.hudi.common.model.HoodieBaseFile;
 import org.apache.hudi.common.table.HoodieTableMetaClient;
+import org.apache.hudi.common.table.view.FileSystemViewManager;
+import org.apache.hudi.common.table.view.HoodieTableFileSystemView;
 import org.apache.hudi.common.util.ValidationUtils;
+import org.apache.hudi.common.util.VisibleForTesting;
 import org.apache.hudi.exception.HoodieException;
-import org.apache.hudi.metadata.HoodieMetadataFileSystemView;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
@@ -83,20 +86,40 @@ public class ManifestFileWriter {
     }
   }
 
+  @VisibleForTesting
   public static Stream<String> fetchLatestBaseFilesForAllPartitions(HoodieTableMetaClient metaClient,
       boolean useFileListingFromMetadata, boolean assumeDatePartitioning, boolean useAbsolutePath) {
     try {
-      List<String> partitions = FSUtils.getAllPartitionPaths(new HoodieLocalEngineContext(metaClient.getHadoopConf()),
-          metaClient.getBasePath(), useFileListingFromMetadata, assumeDatePartitioning);
-      LOG.info("Retrieve all partitions: " + partitions.size());
       Configuration hadoopConf = metaClient.getHadoopConf();
       HoodieLocalEngineContext engContext = new HoodieLocalEngineContext(hadoopConf);
-      HoodieMetadataFileSystemView fsView = new HoodieMetadataFileSystemView(engContext, metaClient,
-          metaClient.getActiveTimeline().getCommitsTimeline().filterCompletedInstants(),
-          HoodieMetadataConfig.newBuilder().enable(useFileListingFromMetadata).withAssumeDatePartitioning(assumeDatePartitioning).build());
-      return partitions.parallelStream().flatMap(partition -> fsView.getLatestBaseFiles(partition).map(useAbsolutePath ? HoodieBaseFile::getPath : HoodieBaseFile::getFileName));
+      boolean canUseMetadataTable = useFileListingFromMetadata && metaClient.getTableConfig().isMetadataTableAvailable();
+      return getLatestBaseFiles(canUseMetadataTable, engContext, metaClient, useAbsolutePath, assumeDatePartitioning);
     } catch (Exception e) {
       throw new HoodieException("Error in fetching latest base files.", e);
+    }
+  }
+
+  @VisibleForTesting
+  static Stream<String> getLatestBaseFiles(boolean canUseMetadataTable, HoodieEngineContext engContext, HoodieTableMetaClient metaClient,
+                                           boolean useAbsolutePath, boolean assumeDatePartitioning) {
+    List<String> partitions = FSUtils.getAllPartitionPaths(engContext, metaClient.getBasePath(), canUseMetadataTable, assumeDatePartitioning);
+    LOG.info("Retrieve all partitions: " + partitions.size());
+    HoodieTableFileSystemView fsView = null;
+    try {
+      fsView = FileSystemViewManager.createInMemoryFileSystemViewWithTimeline(engContext, metaClient,
+          HoodieMetadataConfig.newBuilder().enable(canUseMetadataTable).withAssumeDatePartitioning(assumeDatePartitioning).build(),
+          metaClient.getActiveTimeline().getCommitsTimeline().filterCompletedInstants());
+      if (canUseMetadataTable) {
+        // incase of MDT, we can load all partitions at once. If not for MDT, we can rely on fsView.getLatestBaseFiles(partition) for each partition to load from FS.
+        fsView.loadAllPartitions();
+      }
+      HoodieTableFileSystemView finalFsView = fsView;
+      // if we do not collect and return stream directly, lazy evaluation happens and we end up closing the fsview in finally block which later
+      // fails the getLatestBaseFiles call. Hence we collect and return a stream.
+      return partitions.parallelStream().flatMap(partition -> finalFsView.getLatestBaseFiles(partition)
+          .map(useAbsolutePath ? HoodieBaseFile::getPath : HoodieBaseFile::getFileName)).collect(Collectors.toList()).stream();
+    } finally {
+      fsView.close();
     }
   }
 
