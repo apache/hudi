9867fda969256c53d295c364ab9557157f82e48a
Ian Streeter <ian.streeter@gmx.com>
2024-05-09 00:17:29 +0100

    [HUDI-7699] Support STS external ids and configurable session names in the AWS StsAssumeRoleCredentialsProvider (#11134)
    [HUDI-6695](https://issues.apache.org/jira/browse/HUDI-6695) added a AWS credentials provider to support assuming a role when syncing to Glue.
    
    We use Hudi in a multi-tenant environment, and our customers give us delegated access to their Glue catalog.  In this multi-tenant setup it is important to use [an external ID](https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_create_for-user_externalid.html) to improve security when assuming IAM roles.
    
    Furthermore, the STS session name is currently hard-coded to "hoodie".  It is helpful for us to have configurable session names so we have better tracability of what entities are creating STS sessions in the cloud.
    
    Currently, the assumed role is configured with the `hoodie.aws.role.arn` config property.  I would like to add the following extra optional config properties, which will be used by the `HoodieConfigAWSAssumedRoleCredentialsProvider`:
    
    - `hoodie.aws.role.external.id`
    - `hoodie.aws.role.session.name`

--- hudi-aws/src/main/java/org/apache/hudi/config/HoodieAWSConfig.java
+++ hudi-aws/src/main/java/org/apache/hudi/config/HoodieAWSConfig.java
@@ -69,6 +69,27 @@ public class HoodieAWSConfig extends HoodieConfig {
       .sinceVersion("0.10.0")
       .withDocumentation("AWS session token");
 
+  public static final ConfigProperty<String> AWS_ASSUME_ROLE_ARN = ConfigProperty
+          .key("hoodie.aws.role.arn")
+          .noDefaultValue()
+          .markAdvanced()
+          .sinceVersion("0.15.0")
+          .withDocumentation("AWS Role ARN to assume");
+
+  public static final ConfigProperty<String> AWS_ASSUME_ROLE_SESSION_NAME = ConfigProperty
+          .key("hoodie.aws.role.session.name")
+          .defaultValue("hoodie")
+          .markAdvanced()
+          .sinceVersion("0.15.0")
+          .withDocumentation("Session name to use when assuming the AWS Role");
+
+  public static final ConfigProperty<String> AWS_ASSUME_ROLE_EXTERNAL_ID = ConfigProperty
+          .key("hoodie.aws.role.external.id")
+          .noDefaultValue()
+          .markAdvanced()
+          .sinceVersion("0.15.0")
+          .withDocumentation("External ID use when assuming the AWS Role");
+
   public static final ConfigProperty<String> AWS_GLUE_ENDPOINT = ConfigProperty
           .key("hoodie.aws.glue.endpoint")
           .noDefaultValue()
@@ -103,6 +124,18 @@ public class HoodieAWSConfig extends HoodieConfig {
     return getString(AWS_SESSION_TOKEN);
   }
 
+  public String getAWSAssumeRoleARN() {
+    return getString(AWS_ASSUME_ROLE_ARN);
+  }
+
+  public String getAWSAssumeRoleExternalID() {
+    return getString(AWS_ASSUME_ROLE_EXTERNAL_ID);
+  }
+
+  public String getAWSAssumeRoleSessionName() {
+    return getString(AWS_ASSUME_ROLE_SESSION_NAME);
+  }
+
   public static class Builder {
 
     private final HoodieAWSConfig awsConfig = new HoodieAWSConfig();
@@ -131,6 +164,21 @@ public class HoodieAWSConfig extends HoodieConfig {
 
     public HoodieAWSConfig.Builder withSessionToken(String sessionToken) {
       awsConfig.setValue(AWS_SESSION_TOKEN, sessionToken);
+      return this;
+    }
+
+    public HoodieAWSConfig.Builder withAssumeRoleARN(String assumeRoleARN) {
+      awsConfig.setValue(AWS_ASSUME_ROLE_ARN, assumeRoleARN);
+      return this;
+    }
+
+    public HoodieAWSConfig.Builder withAssumeRoleExternalID(String assumeRoleExternalID) {
+      awsConfig.setValue(AWS_ASSUME_ROLE_EXTERNAL_ID, assumeRoleExternalID);
+      return this;
+    }
+
+    public HoodieAWSConfig.Builder withAssumeRoleSessionName(String assumeRoleSessionName) {
+      awsConfig.setValue(AWS_ASSUME_ROLE_SESSION_NAME, assumeRoleSessionName);
       return this;
     }
 
