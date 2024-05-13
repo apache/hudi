0b75508a22ea20c00b25a6558e5044a86e468d38
Vinish Reddy <vinishreddygunner17@gmail.com>
2024-05-13 07:23:31 +0530

    [HUDI-7501] Use source profile for S3 and GCS sources (#10861)
    Co-authored-by: Y Ethan Guo <ethan.guoyihua@gmail.com>

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/sources/helpers/CloudDataFetcher.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/sources/helpers/CloudDataFetcher.java
@@ -21,8 +21,11 @@ package org.apache.hudi.utilities.sources.helpers;
 import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.common.util.Option;
 import org.apache.hudi.common.util.StringUtils;
+import org.apache.hudi.common.util.collection.Pair;
 import org.apache.hudi.utilities.schema.SchemaProvider;
+import org.apache.hudi.utilities.streamer.SourceProfileSupplier;
 
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -32,10 +35,13 @@ import org.slf4j.LoggerFactory;
 import java.io.Serializable;
 import java.util.List;
 
+import static org.apache.hudi.common.config.HoodieStorageConfig.PARQUET_MAX_FILE_SIZE;
+import static org.apache.hudi.common.util.ConfigUtils.getBooleanWithAltKeys;
 import static org.apache.hudi.common.util.ConfigUtils.getStringWithAltKeys;
 import static org.apache.hudi.utilities.config.CloudSourceConfig.DATAFILE_FORMAT;
+import static org.apache.hudi.utilities.config.CloudSourceConfig.ENABLE_EXISTS_CHECK;
+import static org.apache.hudi.utilities.config.CloudSourceConfig.SOURCE_MAX_BYTES_PER_PARTITION;
 import static org.apache.hudi.utilities.config.HoodieIncrSourceConfig.SOURCE_FILE_FORMAT;
-import static org.apache.hudi.utilities.sources.helpers.CloudObjectsSelectorCommon.loadAsDataset;
 
 /**
  * Connects to S3/GCS from Spark and downloads data from a given list of files.
@@ -45,14 +51,24 @@ public class CloudDataFetcher implements Serializable {
 
   private static final String EMPTY_STRING = "";
 
-  private final TypedProperties props;
+  private transient TypedProperties props;
+  private transient JavaSparkContext sparkContext;
+  private transient SparkSession sparkSession;
+  private transient CloudObjectsSelectorCommon cloudObjectsSelectorCommon;
 
   private static final Logger LOG = LoggerFactory.getLogger(CloudDataFetcher.class);
 
   private static final long serialVersionUID = 1L;
 
-  public CloudDataFetcher(TypedProperties props) {
+  public CloudDataFetcher(TypedProperties props, JavaSparkContext jsc, SparkSession sparkSession) {
+    this(props, jsc, sparkSession, new CloudObjectsSelectorCommon(props));
+  }
+
+  public CloudDataFetcher(TypedProperties props, JavaSparkContext jsc, SparkSession sparkSession, CloudObjectsSelectorCommon cloudObjectsSelectorCommon) {
     this.props = props;
+    this.sparkContext = jsc;
+    this.sparkSession = sparkSession;
+    this.cloudObjectsSelectorCommon = cloudObjectsSelectorCommon;
   }
 
   public static String getFileFormat(TypedProperties props) {
@@ -63,8 +79,59 @@ public class CloudDataFetcher implements Serializable {
         : getStringWithAltKeys(props, DATAFILE_FORMAT, EMPTY_STRING);
   }
 
-  public Option<Dataset<Row>> getCloudObjectDataDF(SparkSession spark, List<CloudObjectMetadata> cloudObjectMetadata,
-                                                   TypedProperties props, Option<SchemaProvider> schemaProviderOption) {
-    return loadAsDataset(spark, cloudObjectMetadata, props, getFileFormat(props), schemaProviderOption);
+  public Pair<Option<Dataset<Row>>, String> fetchPartitionedSource(
+      CloudObjectsSelectorCommon.Type cloudType,
+      CloudObjectIncrCheckpoint cloudObjectIncrCheckpoint,
+      Option<SourceProfileSupplier> sourceProfileSupplier,
+      Pair<QueryInfo, Dataset<Row>> queryInfoDatasetPair,
+      Option<SchemaProvider> schemaProvider,
+      long sourceLimit) {
+    boolean isSourceProfileSupplierAvailable = sourceProfileSupplier.isPresent() && sourceProfileSupplier.get().getSourceProfile() != null;
+    if (isSourceProfileSupplierAvailable) {
+      LOG.debug("Using source limit from source profile sourceLimitFromConfig {} sourceLimitFromProfile {}", sourceLimit, sourceProfileSupplier.get().getSourceProfile().getMaxSourceBytes());
+      sourceLimit = sourceProfileSupplier.get().getSourceProfile().getMaxSourceBytes();
+    }
+
+    QueryInfo queryInfo = queryInfoDatasetPair.getLeft();
+    String filter = CloudObjectsSelectorCommon.generateFilter(cloudType, props);
+    LOG.info("Adding filter string to Dataset: " + filter);
+    Dataset<Row> filteredSourceData = queryInfoDatasetPair.getRight().filter(filter);
+
+    LOG.info("Adjusting end checkpoint:" + queryInfo.getEndInstant() + " based on sourceLimit :" + sourceLimit);
+    Pair<CloudObjectIncrCheckpoint, Option<Dataset<Row>>> checkPointAndDataset =
+        IncrSourceHelper.filterAndGenerateCheckpointBasedOnSourceLimit(
+            filteredSourceData, sourceLimit, queryInfo, cloudObjectIncrCheckpoint);
+    if (!checkPointAndDataset.getRight().isPresent()) {
+      LOG.info("Empty source, returning endpoint:" + checkPointAndDataset.getLeft());
+      return Pair.of(Option.empty(), checkPointAndDataset.getLeft().toString());
+    }
+    LOG.info("Adjusted end checkpoint :" + checkPointAndDataset.getLeft());
+
+    boolean checkIfFileExists = getBooleanWithAltKeys(props, ENABLE_EXISTS_CHECK);
+    List<CloudObjectMetadata> cloudObjectMetadata = CloudObjectsSelectorCommon.getObjectMetadata(cloudType, sparkContext, checkPointAndDataset.getRight().get(), checkIfFileExists, props);
+    LOG.info("Total number of files to process :" + cloudObjectMetadata.size());
+
+    long bytesPerPartition = props.containsKey(SOURCE_MAX_BYTES_PER_PARTITION.key()) ? props.getLong(SOURCE_MAX_BYTES_PER_PARTITION.key()) :
+        props.getLong(PARQUET_MAX_FILE_SIZE.key(), Long.parseLong(PARQUET_MAX_FILE_SIZE.defaultValue()));
+    if (isSourceProfileSupplierAvailable) {
+      long bytesPerPartitionFromProfile = (long) sourceProfileSupplier.get().getSourceProfile().getSourceSpecificContext();
+      if (bytesPerPartitionFromProfile > 0) {
+        LOG.debug("Using bytesPerPartition from source profile bytesPerPartitionFromConfig {} bytesPerPartitionFromProfile {}", bytesPerPartition, bytesPerPartitionFromProfile);
+        bytesPerPartition = bytesPerPartitionFromProfile;
+      }
+    }
+    Option<Dataset<Row>> datasetOption = getCloudObjectDataDF(cloudObjectMetadata, schemaProvider, bytesPerPartition);
+    return Pair.of(datasetOption, checkPointAndDataset.getLeft().toString());
+  }
+
+  private Option<Dataset<Row>> getCloudObjectDataDF(List<CloudObjectMetadata> cloudObjectMetadata, Option<SchemaProvider> schemaProviderOption, long bytesPerPartition) {
+    long totalSize = 0;
+    for (CloudObjectMetadata o : cloudObjectMetadata) {
+      totalSize += o.getSize();
+    }
+    // inflate 10% for potential hoodie meta fields
+    double totalSizeWithHoodieMetaFields = totalSize * 1.1;
+    int numPartitions = (int) Math.max(Math.ceil(totalSizeWithHoodieMetaFields / bytesPerPartition), 1);
+    return cloudObjectsSelectorCommon.loadAsDataset(sparkSession, cloudObjectMetadata, getFileFormat(props), schemaProviderOption, numPartitions);
   }
 }
