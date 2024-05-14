d9eb7b469bf112a0185729ec1ca98f22632755d0
Vinish Reddy <vinishreddygunner17@gmail.com>
2024-05-14 13:51:09 +0530

    [HUDI-7535] Add metrics for sourceParallelism and Refresh profile in S3/GCS (#10918)
    Co-authored-by: Y Ethan Guo <ethan.guoyihua@gmail.com>

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/ingestion/HoodieIngestionMetrics.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/ingestion/HoodieIngestionMetrics.java
@@ -62,5 +62,9 @@ public abstract class HoodieIngestionMetrics implements Serializable {
 
   public abstract void updateStreamerSourceNewMessageCount(String sourceMetricName, long sourceNewMessageCount);
 
+  public abstract void updateStreamerSourceParallelism(int sourceParallelism);
+
+  public abstract void updateStreamerSourceBytesToBeIngestedInSyncRound(long sourceBytesToBeIngested);
+
   public abstract void shutdown();
 }
