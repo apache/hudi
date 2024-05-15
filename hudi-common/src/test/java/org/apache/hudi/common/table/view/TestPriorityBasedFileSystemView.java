4db72fd2f5c6ecee61df77074fd5c80886a02e24
Y Ethan Guo <ethan.guoyihua@gmail.com>
2024-05-14 17:25:45 -0700

    [HUDI-7759] Remove Hadoop dependencies in hudi-common module (#11220)
    Co-authored-by: Jonathan Vexler <=>

--- hudi-common/src/test/java/org/apache/hudi/common/table/view/TestPriorityBasedFileSystemView.java
+++ hudi-common/src/test/java/org/apache/hudi/common/table/view/TestPriorityBasedFileSystemView.java
@@ -741,7 +741,7 @@ public class TestPriorityBasedFileSystemView {
 
     @Override
     public void append(LogEvent event) {
-      log.add(event);
+      log.add(event.toImmutable());
     }
 
     public List<LogEvent> getLog() {
