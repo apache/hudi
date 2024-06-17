b10291a05a36452b2a759cec3099248a7c5c7a48
Geser Dugarov <geserdugarov@gmail.com>
2024-06-17 10:51:47 +0700

    [HUDI-7847] Fixed check of MOR type

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/upgrade/SevenToEightUpgradeHandler.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/upgrade/SevenToEightUpgradeHandler.java
@@ -22,12 +22,13 @@ import org.apache.hudi.common.config.ConfigProperty;
 import org.apache.hudi.common.config.RecordMergeMode;
 import org.apache.hudi.common.engine.HoodieEngineContext;
 import org.apache.hudi.common.model.DefaultHoodieRecordPayload;
+import org.apache.hudi.common.model.HoodieTableType;
 import org.apache.hudi.common.model.OverwriteWithLatestAvroPayload;
 import org.apache.hudi.common.table.HoodieTableConfig;
 import org.apache.hudi.common.util.ValidationUtils;
 import org.apache.hudi.config.HoodieWriteConfig;
-import org.apache.hudi.table.HoodieTable;
 
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -40,30 +41,34 @@ public class SevenToEightUpgradeHandler implements UpgradeHandler {
   @Override
   public Map<ConfigProperty, String> upgrade(HoodieWriteConfig config, HoodieEngineContext context,
                                              String instantTime, SupportsUpgradeDowngrade upgradeDowngradeHelper) {
-    final HoodieTable table = upgradeDowngradeHelper.getTable(config, context);
+    final HoodieTableConfig tableConfig = upgradeDowngradeHelper.getTable(config, context).getMetaClient().getTableConfig();
 
-    // Record merge mode is required to dictate the merging behavior in version 8,
-    // playing the same role as the payload class config in version 7.
-    // Inferring of record merge mode from payload class here.
-    String payloadClassName = table.getMetaClient().getTableConfig().getPayloadClass();
-    String propToAdd;
-    if (null != payloadClassName) {
-      if (payloadClassName.equals(OverwriteWithLatestAvroPayload.class.getName())) {
-        propToAdd = RecordMergeMode.OVERWRITE_WITH_LATEST.toString();
-      } else if (payloadClassName.equals(DefaultHoodieRecordPayload.class.getName())) {
-        propToAdd = RecordMergeMode.EVENT_TIME_ORDERING.toString();
+    if (tableConfig.getTableType().equals(HoodieTableType.MERGE_ON_READ)) {
+      // Record merge mode is required to dictate the merging behavior in version 8,
+      // playing the same role as the payload class config in version 7.
+      // Inferring of record merge mode from payload class here.
+      String payloadClassName = tableConfig.getPayloadClass();
+      String propToAdd;
+      if (null != payloadClassName) {
+        if (payloadClassName.equals(OverwriteWithLatestAvroPayload.class.getName())) {
+          propToAdd = RecordMergeMode.OVERWRITE_WITH_LATEST.toString();
+        } else if (payloadClassName.equals(DefaultHoodieRecordPayload.class.getName())) {
+          propToAdd = RecordMergeMode.EVENT_TIME_ORDERING.toString();
+        } else {
+          propToAdd = RecordMergeMode.CUSTOM.toString();
+        }
       } else {
-        propToAdd = RecordMergeMode.CUSTOM.toString();
+        propToAdd =  RecordMergeMode.CUSTOM.toString();
       }
+      ValidationUtils.checkState(null != propToAdd, String.format("Couldn't infer (%s) from (%s) class name",
+          HoodieTableConfig.RECORD_MERGE_MODE.key(), payloadClassName));
+
+      Map<ConfigProperty, String> tablePropsToAdd = new Hashtable<>();
+      tablePropsToAdd.put(HoodieTableConfig.RECORD_MERGE_MODE, propToAdd);
+
+      return tablePropsToAdd;
     } else {
-      propToAdd =  RecordMergeMode.CUSTOM.toString();
+      return Collections.emptyMap();
     }
-    ValidationUtils.checkState(null != propToAdd, String.format("Couldn't infer (%s) from (%s) class name",
-        HoodieTableConfig.RECORD_MERGE_MODE.key(), payloadClassName));
-
-    Map<ConfigProperty, String> tablePropsToAdd = new Hashtable<>();
-    tablePropsToAdd.put(HoodieTableConfig.RECORD_MERGE_MODE, propToAdd);
-
-    return tablePropsToAdd;
   }
 }
