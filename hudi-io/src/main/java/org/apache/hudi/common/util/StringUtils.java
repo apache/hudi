2cc45cc228a01b6decc39b167844449f8d8a856a
Praveen Gajulapalli <13733716+pkgajulapalli@users.noreply.github.com>
2024-06-22 11:05:03 +0530

    [HUDI-7881] Verify table base path as well for syncing table in bigquery metastore (#11460)

--- hudi-io/src/main/java/org/apache/hudi/common/util/StringUtils.java
+++ hudi-io/src/main/java/org/apache/hudi/common/util/StringUtils.java
@@ -302,4 +302,29 @@ public class StringUtils {
     buf.append(text.substring(start));
     return buf.toString();
   }
+
+  /**
+   * Strips given characters from end of the string and returns the result
+   * @param str - string to be stripped
+   * @param stripChars - characters to strip
+   */
+  public static String stripEnd(final String str, final String stripChars) {
+    int end;
+    if (str == null || (end = str.length()) == 0) {
+      return str;
+    }
+
+    if (stripChars == null) {
+      while (end != 0 && Character.isWhitespace(str.charAt(end - 1))) {
+        end--;
+      }
+    } else if (stripChars.isEmpty()) {
+      return str;
+    } else {
+      while (end != 0 && stripChars.indexOf(str.charAt(end - 1)) != INDEX_NOT_FOUND) {
+        end--;
+      }
+    }
+    return str.substring(0, end);
+  }
 }
