c9c8ae4e879934e7f7f00a586575841625f43432
Sagar Sumit <sagarsumit09@gmail.com>
2024-07-24 19:43:46 +0530

    [HUDI-7964] Error out if partition schema and table schema do not match in create table command (#11650)
    * [HUDI-7964] Error out if partition schema and table schema do not match in create table command
    
    * Handle df.saveAsTable
    
    * add fields in the log message

--- hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/spark/sql/hudi/ddl/TestCreateTable.scala
+++ hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/spark/sql/hudi/ddl/TestCreateTable.scala
@@ -1463,4 +1463,54 @@ class TestCreateTable extends HoodieSparkSqlTestBase {
       }
     }
   }
+
+  test("Test Create Hoodie Table With Multiple Partitions") {
+    withTempDir { tmp =>
+      val tableName = generateTableName
+      val tablePath = s"${tmp.getCanonicalPath}"
+      // throws error if order in partition by different from that in create table
+      assertThrows[IllegalArgumentException] {
+        spark.sql(
+          s"""
+             | create table $tableName (
+             |    ts BIGINT,
+             |    id STRING,
+             |    rider STRING,
+             |    driver STRING,
+             |    fare DOUBLE,
+             |    city STRING,
+             |    state STRING
+             |) using hudi
+             | options(
+             |    primaryKey ='id'
+             |)
+             |PARTITIONED BY (state, city)
+             |location '$tablePath';
+       """.stripMargin)
+      }
+      // otherwise successful
+      spark.sql(
+        s"""
+           | create table $tableName (
+           |    ts BIGINT,
+           |    id STRING,
+           |    rider STRING,
+           |    driver STRING,
+           |    fare DOUBLE,
+           |    city STRING,
+           |    state STRING
+           |) using hudi
+           | options(
+           |    primaryKey ='id'
+           |)
+           |PARTITIONED BY (city, state)
+           |location '$tablePath';
+       """.stripMargin)
+      // insert and validate
+      spark.sql(s"insert into $tableName values(1695332066,'trip3','rider-E','driver-O',93.50,'austin','texas')")
+      checkAnswer(s"select ts, id, rider, driver, fare, city, state from $tableName")(
+        Seq(1695332066, "trip3", "rider-E", "driver-O", 93.50, "austin", "texas")
+      )
+    }
+  }
 }
