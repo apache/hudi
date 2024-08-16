fe09772ad57945ec00ade8b579789662d10adec6
Davis Zhang <davis@onehouse.ai>
2024-08-16 13:44:09 -0700

    [ENG-12764] Add new zookeeper based lock provider

--- hudi-io/src/main/java/org/apache/hudi/common/util/StringUtils.java
+++ hudi-io/src/main/java/org/apache/hudi/common/util/StringUtils.java
@@ -329,34 +329,34 @@ public class StringUtils {
   }
 
   /**
-   * Concatenates two strings such that the total byte length does not exceed the threshold.
-   * If the total byte length exceeds the threshold, the function will find the maximum length of the first string
-   * that fits within the threshold and concatenate that with the second string.
+   * Concatenates two strings such that the total byte length does not exceed the byteLengthThreshold.
+   * If the total byte length exceeds the byteLengthThreshold, the function will find the maximum length of the first string
+   * that fits within the byteLengthThreshold and concatenate that with the second string.
    *
    * @param a         The first string
    * @param b         The second string
-   * @param threshold The maximum byte length
+   * @param byteLengthThreshold The maximum byte length
    */
-  public static String concatenateWithThreshold(String a, String b, int threshold) {
+  public static String concatenateWithThreshold(String a, String b, int byteLengthThreshold) {
     // Convert both strings to byte arrays in UTF-8 encoding
     byte[] bytesA = getUTF8Bytes(a);
     byte[] bytesB = getUTF8Bytes(b);
-    if (bytesB.length > threshold) {
+    if (bytesB.length > byteLengthThreshold) {
       throw new IllegalArgumentException(String.format(
-          "Length of the Second string to concatenate exceeds the threshold (%d > %d)",
-          bytesB.length, threshold));
+          "Length of the Second string to concatenate exceeds the byteLengthThreshold (%d > %d)",
+          bytesB.length, byteLengthThreshold));
     }
 
     // Calculate total bytes
     int totalBytes = bytesA.length + bytesB.length;
 
-    // If total bytes is within the threshold, return concatenated string
-    if (totalBytes <= threshold) {
+    // If total bytes is within the byteLengthThreshold, return concatenated string
+    if (totalBytes <= byteLengthThreshold) {
       return a + b;
     }
 
     // Calculate the maximum bytes 'a' can take
-    int bestLength = getBestLength(a, threshold - bytesB.length);
+    int bestLength = getBestLength(a, byteLengthThreshold - bytesB.length);
 
     // Concatenate the valid substring of 'a' with 'b'
     return a.substring(0, bestLength) + b;
