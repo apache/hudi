52fd36d0e11c6014d4b97e015bd9a4e0fcde89f8
majian <47964462+majian1998@users.noreply.github.com>
2024-09-04 13:13:51 +0800

    [HUDI-8109] Fix the error related to locking during clustering/compaction when calling the file system which is not atomic (#11809)

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/HoodieCompactor.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/HoodieCompactor.java
@@ -73,7 +73,7 @@ public class HoodieCompactor {
     this.props.put(HoodieCleanConfig.ASYNC_CLEAN.key(), false);
     if (this.metaClient.getTableConfig().isMetadataTableAvailable()) {
       // add default lock config options if MDT is enabled.
-      UtilHelpers.addLockOptions(cfg.basePath, this.props);
+      UtilHelpers.addLockOptions(cfg.basePath, this.metaClient.getBasePath().toUri().getScheme(),  this.props);
     }
   }
 
