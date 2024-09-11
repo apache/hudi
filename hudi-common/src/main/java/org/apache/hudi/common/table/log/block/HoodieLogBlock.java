ef3b0fa1067210bf87c26376f89f578bfe1a2df9
Bradley <bradley.b.pitt@gmail.com>
2024-09-11 15:23:45 +0800

    [HUDI-8190] Implement efficient streaming reads for HoodieDataBlocks

--- hudi-common/src/main/java/org/apache/hudi/common/table/log/block/HoodieLogBlock.java
+++ hudi-common/src/main/java/org/apache/hudi/common/table/log/block/HoodieLogBlock.java
@@ -301,6 +301,10 @@ public abstract class HoodieLogBlock {
     return Option.of(content);
   }
 
+  protected Supplier<SeekableDataInputStream> getInputStreamSupplier() {
+    return inputStreamSupplier;
+  }
+
   /**
    * When lazyReading of blocks is turned on, inflate the content of a log block from disk.
    */
