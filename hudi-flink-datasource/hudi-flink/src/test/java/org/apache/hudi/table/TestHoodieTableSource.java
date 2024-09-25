b53a85428c17a226ba18c7baf1ca2a55a9f09cf8
David N Perkins <david.n.perkins@gmail.com>
2024-09-25 09:35:03 -0400

    GH-730 remove extra line

--- hudi-flink-datasource/hudi-flink/src/test/java/org/apache/hudi/table/TestHoodieTableSource.java
+++ hudi-flink-datasource/hudi-flink/src/test/java/org/apache/hudi/table/TestHoodieTableSource.java
@@ -73,7 +73,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-
 /**
  * Test cases for HoodieTableSource.
  */
