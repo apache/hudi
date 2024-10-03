a291ccca8860d3010ba24838be2666a2a6ccd804
Vamsi <vamsi@onehouse.ai>
2024-10-03 14:55:27 +0530

    fix config documentation

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/config/CloudSourceConfig.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/config/CloudSourceConfig.java
@@ -95,7 +95,7 @@ public class CloudSourceConfig extends HoodieConfig {
       .withDocumentation("Only selects objects in the bucket whose relative path matches this regex"
           + "For example: When hoodie.streamer.source.cloud.data.select.relpath.prefix is set to /path/prefix, and the hoodie.streamer.source.cloud.data.select.relative.path.regex"
           + " is regex/files[0-9]+,only files located in the /path/prefix/regex directory that match the pattern (e.g., file1, file2, etc.) will be ingested.\n"
-          + "If hoodie.streamer.source.cloud.data.select.relpath.prefix is not set, the ingestion process will look for files matching /regex/files[0-9]+ in the root directory.");
+          + "If hoodie.streamer.source.cloud.data.select.relpath.prefix is not set, the ingestion process will look for files matching /regex/files[0-9]+ in the source bucket.");
 
   public static final ConfigProperty<String> IGNORE_RELATIVE_PATH_PREFIX = ConfigProperty
       .key(STREAMER_CONFIG_PREFIX + "source.cloud.data.ignore.relpath.prefix")
