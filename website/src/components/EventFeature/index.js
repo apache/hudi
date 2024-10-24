c3cfc450ce63ff6257dd6099bf430bf3c4552902
Bhavani Sudha Saktheeswaran <2179254+bhasudha@users.noreply.github.com>
2024-10-24 14:34:38 -0700

    [SITE][MINOR] Add event banner for upcoming community sync (#12162)

--- website/src/components/EventFeature/index.js
+++ website/src/components/EventFeature/index.js
@@ -11,13 +11,13 @@ const Events = () => {
                  <div className="container">
                      <div className={styles.flexContainer}>
                         <div className={styles.bannercontent}>
-                            <h3> Modernizing Data Infrastructure at Peloton using Apache Hudi</h3>
+                            <h3> Innovative Solution for Real-time Analytics at Shopee using Apache Hudi</h3>
                             <p className={styles.flexParagraph}><span className={styles.sideMicrophone}><Microphone/></span>
                              Linkedin Live Event | <span className={styles.sideCalendar}><Calendar/></span>
-                           Sep 12th, 9am Pacific Time</p>
+                           Oct 30th, 9am Pacific Time</p>
                         </div>
                          <div className={styles.joinButton}>
-                             <LinkButton class={styles.registerbutton} type="secondary" to="https://www.linkedin.com/events/modernizingdatainfrastructureat7235419044852424705/theater/">
+                             <LinkButton class={styles.registerbutton} type="secondary" to="https://www.linkedin.com/events/innovativesolutionforreal-timea7254659236473749504/theater/">
                                              Join Now
                              </LinkButton>
                          </div>
