192953bba9226dfacaad585f66a65ae266bb82b0
dipankarmazumdar <dp976894@dal.ca>
2024-11-14 09:17:46 -0500

    amazon banner-remove shopee

--- website/src/components/EventFeature/index.js
+++ website/src/components/EventFeature/index.js
@@ -11,13 +11,13 @@ const Events = () => {
                  <div className="container">
                      <div className={styles.flexContainer}>
                         <div className={styles.bannercontent}>
-                            <h3> Innovative Solution for Real-time Analytics at Shopee using Apache Hudi</h3>
+                            <h3> Powering Amazon Unit Economics with Configurations and Apache Hudi</h3>
                             <p className={styles.flexParagraph}><span className={styles.sideMicrophone}><Microphone/></span>
                              Linkedin Live Event | <span className={styles.sideCalendar}><Calendar/></span>
-                           Oct 30th, 9am Pacific Time</p>
+                           Nov 19th, 9am Pacific Time</p>
                         </div>
                          <div className={styles.joinButton}>
-                             <LinkButton class={styles.registerbutton} type="secondary" to="https://www.linkedin.com/events/innovativesolutionforreal-timea7254659236473749504/theater/">
+                             <LinkButton class={styles.registerbutton} type="secondary" to="https://www.linkedin.com/events/poweringamazonuniteconomicswith7259635618979229696/theater/">
                                              Join Now
                              </LinkButton>
                          </div>
