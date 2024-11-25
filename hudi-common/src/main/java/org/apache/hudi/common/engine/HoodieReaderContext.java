7643abc1500f7e19ffa682a7856d8d63c5f7cd02
Jonathan Vexler <=>
2024-11-25 17:50:52 -0500

    make some changes to schema handler and add tests. Still need mor bootstrap tests

--- hudi-common/src/main/java/org/apache/hudi/common/engine/HoodieReaderContext.java
+++ hudi-common/src/main/java/org/apache/hudi/common/engine/HoodieReaderContext.java
@@ -61,6 +61,8 @@ public abstract class HoodieReaderContext<T> {
   private Boolean hasLogFiles = null;
   private Boolean hasBootstrapBaseFile = null;
   private Boolean needsBootstrapMerge = null;
+
+  // should we do position based merging for mor
   private Boolean shouldMergeUseRecordPosition = null;
 
   // Getter and Setter for schemaHandler
