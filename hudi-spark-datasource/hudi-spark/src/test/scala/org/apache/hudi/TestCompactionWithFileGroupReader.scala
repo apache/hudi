721ab8437e0c8c331058d857b9feafdf62cdf1ee
Lin Liu <linliu.code@gmail.com>
2024-11-30 17:56:52 -0800

    Update tests

--- hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/hudi/TestCompactionWithFileGroupReader.scala
+++ hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/hudi/TestCompactionWithFileGroupReader.scala
@@ -20,7 +20,7 @@
 package org.apache.hudi
 
 import org.apache.hadoop.conf.Configuration
-import org.apache.hudi.DataSourceWriteOptions.{ENABLE_ROW_WRITER, RECORD_MERGE_IMPL_CLASSES}
+import org.apache.hudi.DataSourceWriteOptions.{ENABLE_MERGE_INTO_PARTIAL_UPDATES, ENABLE_ROW_WRITER, RECORD_MERGE_IMPL_CLASSES}
 import org.apache.hudi.common.config.HoodieReaderConfig.MERGE_USE_RECORD_POSITIONS
 import org.apache.hudi.common.config.HoodieStorageConfig.LOGFILE_DATA_BLOCK_FORMAT
 import org.apache.hudi.common.table.HoodieTableConfig.LOG_FILE_FORMAT
@@ -28,13 +28,15 @@ import org.apache.hudi.common.table.HoodieTableMetaClient
 import org.apache.hudi.config.HoodieCleanConfig.AUTO_CLEAN
 import org.apache.hudi.config.HoodieClusteringConfig.{INLINE_CLUSTERING, INLINE_CLUSTERING_MAX_COMMITS}
 import org.apache.hudi.config.HoodieCompactionConfig.{INLINE_COMPACT, INLINE_COMPACT_NUM_DELTA_COMMITS, PARQUET_SMALL_FILE_LIMIT}
-import org.apache.hudi.config.HoodieWriteConfig.{TABLE_SERVICES_ENABLED}
+import org.apache.hudi.config.HoodieWriteConfig.TABLE_SERVICES_ENABLED
 import org.apache.hudi.storage.hadoop.HadoopStorageConfiguration
 import org.apache.hudi.storage.{HoodieStorage, HoodieStorageUtils, StorageConfiguration, StoragePath}
 import org.apache.hudi.testutils.SparkClientFunctionalTestHarness
 import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
 import org.apache.spark.sql.{Row, SaveMode}
 import org.junit.jupiter.api.Assertions.{assertEquals, assertTrue}
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.{After, Before, Test}
 import org.slf4j.{Logger, LoggerFactory}
 
@@ -70,7 +72,7 @@ class TestCompactionWithFileGroupReader extends SparkClientFunctionalTestHarness
   }
 
   @Test
-  def testCompactionUsingFileGroupReader(): Unit = {
+  def testCompactionWithFileGroupReader(): Unit = {
     val tableOptions = Map(
       "hoodie.datasource.write.table.name" -> "test_table",
       "hoodie.datasource.write.table.type" -> "MERGE_ON_READ",
@@ -83,7 +85,6 @@ class TestCompactionWithFileGroupReader extends SparkClientFunctionalTestHarness
       LOGFILE_DATA_BLOCK_FORMAT.key -> "parquet",
       RECORD_MERGE_IMPL_CLASSES.key -> classOf[DefaultSparkRecordMerger].getName,
       MERGE_USE_RECORD_POSITIONS.key -> "false")
-
     val serviceOptions = Map(
       TABLE_SERVICES_ENABLED.key -> "true",
       INLINE_COMPACT.key -> "true",
@@ -92,57 +93,50 @@ class TestCompactionWithFileGroupReader extends SparkClientFunctionalTestHarness
       AUTO_CLEAN.key -> "false",
       INLINE_CLUSTERING.key -> "false",
       INLINE_CLUSTERING_MAX_COMMITS.key -> "1")
-
-    val options = tableOptions ++ serviceOptions
-
     val schema = new StructType(Array(
       StructField("id", StringType, nullable = true),
       StructField("name", StringType, nullable = true),
       StructField("ts", LongType, nullable = true)
     ))
 
-    def writeTestData(data: Seq[Row], mode: SaveMode): Unit = {
-      spark.createDataFrame(spark.sparkContext.parallelize(data), schema)
-        .write
-        .format("hudi")
-        .options(options)
-        .mode(mode)
-        .save(tablePath)
-    }
+    var options = tableOptions ++ serviceOptions
+    for (partialUpdateEnabled <- List("false", "true")) {
+      options += (ENABLE_MERGE_INTO_PARTIAL_UPDATES.key -> partialUpdateEnabled)
 
-    // Write initial dataset
-    val df1 = spark.createDataFrame(spark.sparkContext.parallelize(Seq(
+      // Write initial dataset
+      val df1 = spark.createDataFrame(spark.sparkContext.parallelize(Seq(
         Row("id1", "name1", 1L),
         Row("id2", "name1", 2L),
         Row("id3", "name1", 3L)
       )), schema)
-    df1.write.format("hudi").options(options).mode(SaveMode.Overwrite).save(tablePath)
+      df1.write.format("hudi").options(options).mode(SaveMode.Overwrite).save(tablePath)
 
-    // Write additional dataset
-    val df2 = spark.createDataFrame(spark.sparkContext.parallelize(Seq(
+      // Write additional dataset
+      val df2 = spark.createDataFrame(spark.sparkContext.parallelize(Seq(
         Row("id1", "name1", 4L),
         Row("id2", "name1", 5L),
         Row("id3", "name1", 6L)
       )), schema)
-    df2.write.format("hudi").options(options).mode(SaveMode.Append).save(tablePath)
+      df2.write.format("hudi").options(options).mode(SaveMode.Append).save(tablePath)
 
-    // Validate the results
-    metaClient = HoodieTableMetaClient.builder()
-      .setBasePath(tablePath)
-      .setStorage(storage)
-      .build()
+      // Validate the results
+      metaClient = HoodieTableMetaClient.builder()
+        .setBasePath(tablePath)
+        .setStorage(storage)
+        .build()
 
-    val instants = metaClient.getActiveTimeline.getInstants.asScala.toList
-    assertTrue(instants.exists(i => i.getAction == "commit" && i.isCompleted))
+      val instants = metaClient.getActiveTimeline.getInstants.asScala.toList
+      assertTrue(instants.exists(i => i.getAction == "commit" && i.isCompleted))
 
-    val finalData = spark.read.format("hudi").load(tablePath)
-    val finalDf = finalData.select("id", "name", "ts").sort("id")
-    finalDf.show(false)
-    val expectedDf = df2.sort("id")
-    expectedDf.show(false)
+      val finalData = spark.read.format("hudi").load(tablePath)
+      val finalDf = finalData.select("id", "name", "ts").sort("id")
+      finalDf.show(false)
+      val expectedDf = df2.sort("id")
+      expectedDf.show(false)
 
-    assertEquals(3, finalData.count())
-    assertTrue(finalDf.except(expectedDf).isEmpty)
-    assertTrue(expectedDf.except(finalDf).isEmpty)
+      assertEquals(3, finalData.count())
+      assertTrue(finalDf.except(expectedDf).isEmpty)
+      assertTrue(expectedDf.except(finalDf).isEmpty)
+    }
   }
 }
