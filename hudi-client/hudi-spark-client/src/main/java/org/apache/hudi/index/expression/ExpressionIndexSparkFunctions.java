a3e40949e2550bb18b3615661f564ae8bb2e4f2a
Lokesh Jain <ljain@apache.org>
2024-12-13 19:33:42 +0530

    Fix checkstyle

--- hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/index/expression/ExpressionIndexSparkFunctions.java
+++ hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/index/expression/ExpressionIndexSparkFunctions.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hudi.index.expression;
 
 import org.apache.hudi.common.util.ValidationUtils;
@@ -79,8 +96,11 @@ public class ExpressionIndexSparkFunctions {
    * Custom interface to support Spark functions
    */
   interface SparkFunction extends Serializable {
+
     String getFunctionName();
+
     Set<String> getValidOptions();
+
     Column apply(List<Column> columns, Map<String, String> options);
 
     default void validateOptions(Map<String, String> options) {
