912c52471dd33c616e9aa029968ac1d77a0d458c
Y Ethan Guo <ethan.guoyihua@gmail.com>
2025-01-06 17:09:18 -0800

    [HUDI-8634] Support schema on read in file group reader-based compaction and clustering in Spark

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/action/compact/HoodieCompactor.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/action/compact/HoodieCompactor.java
@@ -149,7 +149,6 @@ public abstract class HoodieCompactor<T, I, K, O> implements Serializable {
         && config.getBooleanOrDefault(HoodieReaderConfig.FILE_GROUP_READER_ENABLED)
         && operationType == WriteOperationType.COMPACT
         && !hasBootstrapFile(operations)                                            // bootstrap file read for fg reader is not ready
-        && StringUtils.isNullOrEmpty(config.getInternalSchema())                    // schema evolution support for fg reader is not ready
         && config.populateMetaFields();                                             // Virtual key support by fg reader is not ready
 
     if (useFileGroupReaderBasedCompaction) {
