5ca19a8f0fb4d36d43630e476bd16d6421adf96a
vamshi <vamshipasunuru@gmail.com>
2025-01-17 01:04:34 -0800

    fix cs

--- hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/client/utils/SparkMetadataWriterUtils.java
+++ hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/client/utils/SparkMetadataWriterUtils.java
@@ -193,7 +193,6 @@ public class SparkMetadataWriterUtils {
     scanner.scan(false);
     return scanner.iterator();
   }
-
   
   private static Iterator<HoodieRecord> getBaseFileRecords(HoodieBaseFile baseFile, HoodieTableMetaClient metaClient, Schema readerSchema) {
     HoodieRecordMerger recordMerger =
