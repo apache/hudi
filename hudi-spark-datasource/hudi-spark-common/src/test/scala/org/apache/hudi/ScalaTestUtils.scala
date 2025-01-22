4d00f613044c1e6da4e02abc75b9343bbd043f7d
Lokesh Jain <ljain@apache.org>
2025-01-22 11:39:09 +0530

    [HUDI-8897] Add a retry API for running test multiple times

---
+++ hudi-spark-datasource/hudi-spark-common/src/test/scala/org/apache/hudi/ScalaTestUtils.scala
@@ -0,0 +1,19 @@
+package org.apache.hudi
+
+object ScalaTestUtils {
+  /**
+   * Utility method to run a test multiple times. For using the test call the method on the test block.
+   * Example:
+   * repeatTest(5) {
+   *   ... // test code
+   * }
+   *
+   * @param numRuns Number of test runs
+   * @param test    Test to run
+   */
+  def repeatTest(numRuns: Int)(test: => Unit): Unit = {
+    for (_ <- 1 to numRuns) {
+      test
+    }
+  }
+}
