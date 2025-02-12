0216728a2b355e32fc3437f68dc84999a391ee72
Shubham21k <85898765+Shubham21k@users.noreply.github.com>
2025-02-12 16:04:24 +0530

    [HUDI-8941] Stop spark context with proper exit code in HoodieStreamer (#12742)
    (cherry picked from commit b658989e86a43f87bbf108dc81771092ea3077b5)

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/HoodieTTLJob.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/HoodieTTLJob.java
@@ -18,6 +18,7 @@
 
 package org.apache.hudi.utilities;
 
+import org.apache.hudi.SparkAdapterSupport$;
 import org.apache.hudi.client.SparkRDDWriteClient;
 import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.common.model.HoodieRecordPayload;
@@ -117,12 +118,14 @@ public class HoodieTTLJob {
     String dirName = new Path(cfg.basePath).getName();
     JavaSparkContext jssc = UtilHelpers.buildSparkContext("hoodie-ttl-job-" + dirName, cfg.sparkMaster);
 
+    int exitCode = 0;
     try {
       new HoodieTTLJob(jssc, cfg).run();
     } catch (Throwable throwable) {
+      exitCode = 1;
       throw new HoodieException("Failed to run ttl for " + cfg.basePath, throwable);
     } finally {
-      jssc.stop();
+      SparkAdapterSupport$.MODULE$.sparkAdapter().stopSparkContext(jssc, exitCode);
     }
 
     LOG.info("Hoodie TTL job ran successfully");
