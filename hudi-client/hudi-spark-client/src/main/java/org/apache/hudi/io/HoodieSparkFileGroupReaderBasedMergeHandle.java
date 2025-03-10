b72659f7dc5ed4d683aaacaf1b6ce3e11d0a463b
TheR1sing3un <chaoyang@apache.org>
2025-03-11 04:34:19 +0800

    [HUDI-9125] Pass compaction/merge related props to HoodieBaseFileGroupRecordBuffer (#12925)
    * feat: pass compaction/merge related props to HoodieBaseFileGroupRecordBuffer
    
    1. pass compaction/merge related props to HoodieBaseFileGroupRecordBuffer
    
    Signed-off-by: TheR1sing3un <chaoyang@apache.org>
    
    * fix: resolve multiple precombine-related configuration conflicts
    
    1. resolve multiple precombine-related configuration conflicts
    2. assume that precombine is based on table config
    
    Signed-off-by: TheR1sing3un <chaoyang@apache.org>
    
    * style: simplify lambda expression
    
    1. simplify lambda expression
    
    Signed-off-by: TheR1sing3un <chaoyang@apache.org>
    
    * fix: fix the `record_key` and `_hoodie_record_key` are not mapped when the record is created in SparkDatasetTestUtils
    
    1. fix the `record_key` and `_hoodie_record_key` are not mapped when the record is created in SparkDatasetTestUtils
    
    Signed-off-by: TheR1sing3un <chaoyang@apache.org>
    
    * rerun
    
    * feat: Remove the default value for PAYLOAD_ORDERING_FIELD_PROP_KEY to avoid taking the default value from props as a valid configuration
    
    1. Remove the default value for PAYLOAD_ORDERING_FIELD_PROP_KEY to avoid taking the default value from props as a valid configuration优化排序字段获取逻辑，移除默认值配置。
    
    Signed-off-by: TheR1sing3un <chaoyang@apache.org>
    
    * Update hudi-common/src/main/java/org/apache/hudi/common/util/collection/ExternalSpillableMap.java
    
    ---------
    
    Signed-off-by: TheR1sing3un <chaoyang@apache.org>
    Co-authored-by: Y Ethan Guo <ethan.guoyihua@gmail.com>
    (cherry picked from commit 63362ad7b69d0bea50caceb1e744c53454a1cc23)

--- hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/io/HoodieSparkFileGroupReaderBasedMergeHandle.java
+++ hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/io/HoodieSparkFileGroupReaderBasedMergeHandle.java
@@ -21,6 +21,7 @@ package org.apache.hudi.io;
 
 import org.apache.hudi.AvroConversionUtils;
 import org.apache.hudi.client.WriteStatus;
+import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.common.engine.HoodieReaderContext;
 import org.apache.hudi.common.engine.TaskContextSupplier;
 import org.apache.hudi.common.fs.FSUtils;
@@ -180,6 +181,9 @@ public class HoodieSparkFileGroupReaderBasedMergeHandle<T, I, K, O> extends Hood
     if (!StringUtils.isNullOrEmpty(config.getInternalSchema())) {
       internalSchemaOption = SerDeHelper.fromJson(config.getInternalSchema());
     }
+    TypedProperties props = new TypedProperties();
+    hoodieTable.getMetaClient().getTableConfig().getProps().forEach(props::putIfAbsent);
+    config.getProps().forEach(props::putIfAbsent);
     // Initializes file group reader
     try (HoodieFileGroupReader<T> fileGroupReader = new HoodieFileGroupReader<>(
         readerContext,
@@ -191,7 +195,7 @@ public class HoodieSparkFileGroupReaderBasedMergeHandle<T, I, K, O> extends Hood
         writeSchemaWithMetaFields,
         internalSchemaOption,
         hoodieTable.getMetaClient(),
-        hoodieTable.getMetaClient().getTableConfig().getProps(),
+        props,
         0,
         Long.MAX_VALUE,
         usePosition)) {
