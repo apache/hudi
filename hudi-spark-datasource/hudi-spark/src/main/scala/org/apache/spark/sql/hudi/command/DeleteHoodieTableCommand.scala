66b520af14d34fd5f292ce5f77d51ccbdabbc0d2
TheR1sing3un <chaoyang@apache.org>
2025-03-12 18:33:01 +0800

    feat: introduce schema pruning for delete record
    1. introduce schema pruning for delete record
    
    NOTE: For the record we need to delete, we only need to read the `hoodie_meta_fields`, `record_keys` and the columns involved in the delete condition from the table, which can greatly reduce the amount of read data when deleting.
    Signed-off-by: TheR1sing3un <chaoyang@apache.org>

--- hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/hudi/command/DeleteHoodieTableCommand.scala
+++ hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/hudi/command/DeleteHoodieTableCommand.scala
@@ -17,12 +17,14 @@
 
 package org.apache.spark.sql.hudi.command
 
-import org.apache.hudi.DataSourceWriteOptions.{SPARK_SQL_OPTIMIZED_WRITES, SPARK_SQL_WRITES_PREPPED_KEY}
+import org.apache.hudi.DataSourceWriteOptions.{DELETE_RECORD_SCHEMA_PRUNING, SPARK_SQL_OPTIMIZED_WRITES, SPARK_SQL_WRITES_PREPPED_KEY}
 import org.apache.hudi.SparkAdapterSupport
+import org.apache.hudi.common.table.HoodieTableConfig
 
 import org.apache.spark.sql._
 import org.apache.spark.sql.catalyst.catalog.HoodieCatalogTable
-import org.apache.spark.sql.catalyst.plans.logical.{DeleteFromTable, Filter}
+import org.apache.spark.sql.catalyst.plans.logical.{DeleteFromTable, Filter, LogicalPlan, Project}
+import org.apache.spark.sql.hudi.HoodieSqlCommonUtils.isMetaField
 import org.apache.spark.sql.hudi.ProvidesHoodieConfig
 import org.apache.spark.sql.hudi.command.HoodieLeafRunnableCommand.stripMetaFieldAttributes
 
@@ -41,9 +43,34 @@ case class DeleteHoodieTableCommand(dft: DeleteFromTable) extends HoodieLeafRunn
 
     val condition = sparkAdapter.extractDeleteCondition(dft)
 
+    val config = if (sparkSession.sqlContext.conf.getConfString(SPARK_SQL_OPTIMIZED_WRITES.key()
+      , SPARK_SQL_OPTIMIZED_WRITES.defaultValue()) == "true") {
+      buildHoodieDeleteTableConfig(catalogTable, sparkSession) + (SPARK_SQL_WRITES_PREPPED_KEY -> "true")
+    } else {
+      buildHoodieDeleteTableConfig(catalogTable, sparkSession)
+    }
+
+    val recordKeysStr = config.getOrElse(HoodieTableConfig.RECORDKEY_FIELDS.key(), "")
+    val recordKeys = recordKeysStr.split(",").filter(_.nonEmpty)
+
+    // get all columns which are used in condition
+    val conditionColumns = if (condition == null) {
+      Seq.empty[String]
+    } else {
+      condition.references.map(_.name).toSeq
+    }
+
+    val requiredCols = recordKeys ++ conditionColumns
+
     val targetLogicalPlan = if (sparkSession.sqlContext.conf.getConfString(SPARK_SQL_OPTIMIZED_WRITES.key()
       , SPARK_SQL_OPTIMIZED_WRITES.defaultValue()) == "true") {
-      dft.table
+      if (sparkSession.sqlContext.conf.getConfString(DELETE_RECORD_SCHEMA_PRUNING.key(),
+        DELETE_RECORD_SCHEMA_PRUNING.defaultValue()) == "true") {
+        logInfo(s"Pruning delete record schema for $tableId, required columns: ${requiredCols.mkString(",")}")
+        tryPruningDeleteRecordSchema(dft.table, requiredCols)
+      } else {
+        dft.table
+      }
     } else {
       stripMetaFieldAttributes(dft.table)
     }
@@ -52,13 +79,6 @@ case class DeleteHoodieTableCommand(dft: DeleteFromTable) extends HoodieLeafRunn
       Filter(condition, targetLogicalPlan)
     } else {
       targetLogicalPlan
-    }
-
-    val config = if (sparkSession.sqlContext.conf.getConfString(SPARK_SQL_OPTIMIZED_WRITES.key()
-      , SPARK_SQL_OPTIMIZED_WRITES.defaultValue()) == "true") {
-      buildHoodieDeleteTableConfig(catalogTable, sparkSession) + (SPARK_SQL_WRITES_PREPPED_KEY -> "true")
-    } else {
-      buildHoodieDeleteTableConfig(catalogTable, sparkSession)
     }
 
     val df = Dataset.ofRows(sparkSession, filteredPlan)
@@ -73,5 +93,14 @@ case class DeleteHoodieTableCommand(dft: DeleteFromTable) extends HoodieLeafRunn
     logInfo(s"Finished executing 'DELETE FROM' command for $tableId")
 
     Seq.empty[Row]
+  }
+
+  def tryPruningDeleteRecordSchema(query: LogicalPlan, requiredColNames: Seq[String]): LogicalPlan = {
+    val filteredOutput = query.output.filter(attr => isMetaField(attr.name) || requiredColNames.contains(attr.name))
+    if (filteredOutput == query.output) {
+      query
+    } else {
+      Project(filteredOutput, query)
+    }
   }
 }
