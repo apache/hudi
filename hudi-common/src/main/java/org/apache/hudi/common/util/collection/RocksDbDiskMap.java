4959b0ab8ce7bfba586c97059b39dbddb2f071a0
Tim Brown <tim@onehouse.ai>
2025-03-14 20:22:52 -0500

    [ENG-23823][HUDI-9187] Use thread safe data structures in ExternalSpillableMap (#1277)

--- hudi-common/src/main/java/org/apache/hudi/common/util/collection/RocksDbDiskMap.java
+++ hudi-common/src/main/java/org/apache/hudi/common/util/collection/RocksDbDiskMap.java
@@ -32,6 +32,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.Spliterators;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -55,7 +56,7 @@ public final class RocksDbDiskMap<T extends Serializable, R> extends DiskMap<T, 
   public RocksDbDiskMap(String rocksDbStoragePath, CustomSerializer<R> valueSerializer) throws IOException {
     super(rocksDbStoragePath, ExternalSpillableMap.DiskMapType.ROCKS_DB.name());
     this.valueSerializer = valueSerializer;
-    this.keySet = new HashSet<>();
+    this.keySet = ConcurrentHashMap.newKeySet();
   }
 
   @Override
