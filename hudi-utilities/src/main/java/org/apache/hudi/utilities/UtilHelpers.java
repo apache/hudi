070d9645d4c0b9cc53c32b60e7459e5d0e91ed0b
sivabalan <n.siva.b@gmail.com>
2025-03-16 17:40:26 -0700

    Fixing few more tests

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/UtilHelpers.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/UtilHelpers.java
@@ -397,6 +397,7 @@ public class UtilHelpers {
         .orElse(HoodieCompactionConfig.newBuilder().withInlineCompaction(false).build());
     HoodieWriteConfig config =
         HoodieWriteConfig.newBuilder().withPath(basePath)
+            .withAutoCommit(true)
             .withParallelism(parallelism, parallelism)
             .withBulkInsertParallelism(parallelism)
             .withDeleteParallelism(parallelism)
