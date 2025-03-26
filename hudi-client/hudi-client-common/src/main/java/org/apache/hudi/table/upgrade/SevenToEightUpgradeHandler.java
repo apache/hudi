e62646a5d3d6b4e97e5292143f4f5b5931f6049b
Sagar Sumit <sagarsumit09@gmail.com>
2025-03-26 10:28:00 +0530

    remove redundant timezone config setting

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/upgrade/SevenToEightUpgradeHandler.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/upgrade/SevenToEightUpgradeHandler.java
@@ -72,7 +72,6 @@ import static org.apache.hudi.common.table.timeline.HoodieTimeline.REPLACE_COMMI
 import static org.apache.hudi.common.table.timeline.TimelineLayout.TIMELINE_LAYOUT_V1;
 import static org.apache.hudi.common.table.timeline.TimelineLayout.TIMELINE_LAYOUT_V2;
 import static org.apache.hudi.table.upgrade.UpgradeDowngradeUtils.SIX_TO_EIGHT_TIMELINE_ACTION_MAP;
-import static org.apache.hudi.table.upgrade.UpgradeDowngradeUtils.getTimelineTimezone;
 import static org.apache.hudi.table.upgrade.UpgradeDowngradeUtils.rollbackFailedWritesAndCompact;
 
 /**
@@ -149,8 +148,6 @@ public class SevenToEightUpgradeHandler implements UpgradeHandler {
     }
 
     upgradeToLSMTimeline(table, context, config);
-    // add TIMELINE_TIMEZONE to table properties
-    tablePropsToAdd.put(HoodieTableConfig.TIMELINE_TIMEZONE, getTimelineTimezone(upgradeDowngradeHelper.getTable(config, context).getMetaClient().getTableConfig()));
     return tablePropsToAdd;
   }
 
