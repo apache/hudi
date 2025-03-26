aaf34e7fc117c83ab36f522ec1c57a07f4f89abb
Shawn Chang <yxchang@amazon.com>
2025-03-25 21:11:53 -0700

    minor

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/action/commit/BaseCommitActionExecutor.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/action/commit/BaseCommitActionExecutor.java
@@ -240,11 +240,11 @@ public abstract class BaseCommitActionExecutor<T, I, K, O, R>
       result.setCommitMetadata(Option.of(metadata));
       // update cols to Index as applicable
       try {
-          HoodieColumnStatsIndexUtils.updateColsToIndex(table, config, metadata, actionType,
-                  (Functions.Function2<HoodieTableMetaClient, List<String>, Void>) (metaClient, columnsToIndex) -> {
-                      updateColumnsToIndexForColumnStats(metaClient, columnsToIndex);
-                      return null;
-                  });
+        HoodieColumnStatsIndexUtils.updateColsToIndex(table, config, metadata, actionType,
+                (Functions.Function2<HoodieTableMetaClient, List<String>, Void>) (metaClient, columnsToIndex) -> {
+                    updateColumnsToIndexForColumnStats(metaClient, columnsToIndex);
+                    return null;
+                });
       } catch (UnsupportedOperationException uoe) {
         LOG.warn("Skip updating column stats index in the metadata table for bootstrap operation because it is not supported", uoe);
       }
