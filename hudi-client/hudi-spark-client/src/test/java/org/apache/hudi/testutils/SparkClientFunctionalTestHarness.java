6b162b32290f9cdeee3df1a98cd6520a81e80a74
Y Ethan Guo <ethan.guoyihua@gmail.com>
2025-03-28 14:54:11 -0700

    Remove unused method

--- hudi-client/hudi-spark-client/src/test/java/org/apache/hudi/testutils/SparkClientFunctionalTestHarness.java
+++ hudi-client/hudi-spark-client/src/test/java/org/apache/hudi/testutils/SparkClientFunctionalTestHarness.java
@@ -355,10 +355,6 @@ public class SparkClientFunctionalTestHarness implements SparkProvider, HoodieMe
     return HoodieTestTable.of(table.getMetaClient()).listAllBaseFiles(table.getBaseFileExtension());
   }
 
-  protected List<StoragePathInfo> listAllLogFilesInPath(HoodieTable table) throws IOException {
-    return HoodieTestTable.of(table.getMetaClient()).listAllLogFiles();
-  }
-
   protected Properties getPropertiesForKeyGen() {
     return getPropertiesForKeyGen(false);
   }
