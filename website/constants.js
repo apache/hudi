b4189540168a70cc0eeadd11f62e3f7e1aaf3690
Bhavani Sudha Saktheeswaran <2179254+bhasudha@users.noreply.github.com>
2025-04-09 05:29:30 -0700

    [MINOR][SITE] Update slack shared invite link

--- website/constants.js
+++ website/constants.js
@@ -1,3 +1,3 @@
 module.exports = {
-    slackUrl: "https://join.slack.com/t/apache-hudi/shared_invite/zt-2ggm1fub8-_yt4Reu9djwqqVRFC7X49g"
+    slackUrl: "https://join.slack.com/t/apache-hudi/shared_invite/zt-33fabmxb7-Q7QSUtNOHYCwUdYM8LbauA"
 }
