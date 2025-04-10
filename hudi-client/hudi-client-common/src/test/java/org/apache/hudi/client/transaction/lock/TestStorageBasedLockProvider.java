40ebfc4fad4be9ff4e4f6bf212d0ccbf6651e90d
Alex Rhee <arheebikes@gmail.com>
2025-04-10 12:46:24 -0700

    checkstyle fix

--- hudi-client/hudi-client-common/src/test/java/org/apache/hudi/client/transaction/lock/TestStorageBasedLockProvider.java
+++ hudi-client/hudi-client-common/src/test/java/org/apache/hudi/client/transaction/lock/TestStorageBasedLockProvider.java
@@ -35,7 +35,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mock;
 import org.slf4j.Logger;
 
 import java.net.URISyntaxException;
