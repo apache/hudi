015ba56556f6cb53af28fc0219f8aed309a16998
Alex Rhee <arheebikes@gmail.com>
2025-04-09 21:01:37 -0700

    rename class

--- hudi-client/hudi-client-common/src/test/java/org/apache/hudi/client/transaction/lock/TestStorageBasedLockConfig.java
+++ hudi-client/hudi-client-common/src/test/java/org/apache/hudi/client/transaction/lock/TestStorageBasedLockConfig.java
@@ -27,7 +27,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class StorageBasedLockConfigTest {
+class TestStorageBasedLockConfig {
 
   @Test
   void testDefaultValues() {
