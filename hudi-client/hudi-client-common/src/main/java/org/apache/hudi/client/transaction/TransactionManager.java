c5647113ee5858fa44dfb57c1fc3bbfcec07a9b7
Aditya Goenka <ad1happy2go@gmail.com>
2025-04-11 17:49:33 +0530

    Added other configs to the storage config in Transaction manager

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/client/transaction/TransactionManager.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/client/transaction/TransactionManager.java
@@ -42,13 +42,18 @@ public class TransactionManager implements Serializable {
   protected Option<HoodieInstant> currentTxnOwnerInstant = Option.empty();
   private Option<HoodieInstant> lastCompletedTxnOwnerInstant = Option.empty();
 
-  public TransactionManager(HoodieWriteConfig config, HoodieStorage storage) {
-    this(new LockManager(config, (FileSystem) storage.getFileSystem()), config.isLockRequired());
+  public TransactionManager(HoodieWriteConfig config, FileSystem fs) {
+    this(createLockManager(config, fs), config.isLockRequired());
   }
 
   protected TransactionManager(LockManager lockManager, boolean isLockRequired) {
     this.lockManager = lockManager;
     this.isLockRequired = isLockRequired;
+  }
+
+  private static LockManager createLockManager(HoodieWriteConfig config, FileSystem fs) {
+    fs.getConf().addResource(fs.getConf());
+    return new LockManager(config, fs);
   }
 
   public void beginTransaction(Option<HoodieInstant> newTxnOwnerInstant,
