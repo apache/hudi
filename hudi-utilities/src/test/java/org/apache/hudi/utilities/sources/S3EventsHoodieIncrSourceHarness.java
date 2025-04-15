10fb4a7f61c60f83dc33d41004d60778e6b3b18a
sivabalan <n.siva.b@gmail.com>
2025-04-15 00:22:54 -0700

    Fixing tests apr 15

--- hudi-utilities/src/test/java/org/apache/hudi/utilities/sources/S3EventsHoodieIncrSourceHarness.java
+++ hudi-utilities/src/test/java/org/apache/hudi/utilities/sources/S3EventsHoodieIncrSourceHarness.java
@@ -69,12 +69,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.apache.hudi.testutils.Assertions.assertNoWriteErrors;
+import static org.apache.hudi.common.table.timeline.HoodieTimeline.COMMIT_ACTION;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -223,10 +224,7 @@ public class S3EventsHoodieIncrSourceHarness extends SparkClientFunctionalTestHa
           generateS3EventMetadata(commitTime, "bucket-1", "data-file-1.json", 1L)
       );
       JavaRDD<WriteStatus> result = writeClient.upsert(jsc().parallelize(s3MetadataRecords, 1), commitTime);
-
-      List<WriteStatus> statuses = result.collect();
-      assertNoWriteErrors(statuses);
-
+      writeClient.commit(commitTime, result, Option.empty(), COMMIT_ACTION, Collections.emptyMap(), Option.empty());
       return Pair.of(commitTime, s3MetadataRecords);
     }
   }
