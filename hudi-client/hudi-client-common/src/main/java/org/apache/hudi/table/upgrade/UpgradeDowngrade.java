d3c051cb584378f1e2981d5a9d1ff1f9365181a9
Lokesh Jain <ljain@apache.org>
2025-04-15 20:06:35 +0530

    [HUDI-9203] Fix usability w/ upgrading from 0.x to 1.x and wish to stay in table version 6 (#13145)

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/upgrade/UpgradeDowngrade.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/upgrade/UpgradeDowngrade.java
@@ -80,12 +80,6 @@ public class UpgradeDowngrade {
       throw new HoodieUpgradeDowngradeException(
           String.format("Please upgrade table from version %s to %s before upgrading to version %s.", fromTableVersion, HoodieTableVersion.SIX.versionCode(), toWriteVersion));
     }
-    // If autoUpgrade is disabled and metadata is enabled, and table version is SIX or SEVEN, while toWriteVersion is EIGHT or greater, then we should disable metadata first
-    if (!config.autoUpgrade() && metaClient.getTableConfig().isMetadataTableAvailable()
-        && (fromTableVersion == HoodieTableVersion.SIX || fromTableVersion == HoodieTableVersion.SEVEN) && toWriteVersion.versionCode() >= HoodieTableVersion.EIGHT.versionCode()) {
-      throw new HoodieUpgradeDowngradeException(
-          String.format("Please disable metadata table before upgrading from version %s to %s.", fromTableVersion, toWriteVersion));
-    }
 
     // allow upgrades otherwise.
     return toWriteVersion.versionCode() > fromTableVersion.versionCode();
