a4a3f4c64cce166fc4d7129f8f3abdb78b8d3840
sivabalan <n.siva.b@gmail.com>
2025-04-16 11:31:19 -0700

    Fixing tests in hudi-utilities phase1

--- hudi-utilities/src/test/java/org/apache/hudi/utilities/multitable/TestHoodieMultiTableServicesMain.java
+++ hudi-utilities/src/test/java/org/apache/hudi/utilities/multitable/TestHoodieMultiTableServicesMain.java
@@ -59,7 +59,6 @@ import org.apache.spark.sql.SparkSession;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -79,7 +78,6 @@ import static org.apache.hudi.utilities.multitable.MultiTableServiceUtils.Consta
  * Tests for HoodieMultiTableServicesMain
  * @see HoodieMultiTableServicesMain
  */
-@Disabled("to fix")
 class TestHoodieMultiTableServicesMain extends HoodieCommonTestHarness implements SparkProvider {
 
   private static final Logger LOG = LoggerFactory.getLogger(TestHoodieMultiTableServicesMain.class);
