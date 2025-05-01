263283e746dad5d6bb79d074da594630f1e62c29
Y Ethan Guo <ethan.guoyihua@gmail.com>
2025-04-30 20:50:32 -0700

    Fix MetadataPartitionType test

--- hudi-common/src/test/java/org/apache/hudi/metadata/TestMetadataPartitionType.java
+++ hudi-common/src/test/java/org/apache/hudi/metadata/TestMetadataPartitionType.java
@@ -24,18 +24,23 @@ import org.apache.hudi.common.model.HoodieIndexDefinition;
 import org.apache.hudi.common.model.HoodieIndexMetadata;
 import org.apache.hudi.common.table.HoodieTableConfig;
 import org.apache.hudi.common.table.HoodieTableMetaClient;
+import org.apache.hudi.common.table.HoodieTableVersion;
 import org.apache.hudi.common.util.Option;
 import org.apache.hudi.common.util.StringUtils;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -49,38 +54,59 @@ import static org.mockito.Mockito.when;
  */
 public class TestMetadataPartitionType {
 
+  private static Stream<Arguments> testArgumentsForPartitionEnabledByConfigOnly() {
+    return Arrays.stream(MetadataPartitionType.values()).flatMap(
+        partitionType -> Arrays.stream(
+                new HoodieTableVersion[] {HoodieTableVersion.EIGHT, HoodieTableVersion.SIX})
+            .map(tableVersion -> Arguments.of(partitionType, tableVersion)));
+  }
+
   @ParameterizedTest
-  @EnumSource(MetadataPartitionType.class)
-  public void testPartitionEnabledByConfigOnly(MetadataPartitionType partitionType) {
+  @MethodSource("testArgumentsForPartitionEnabledByConfigOnly")
+  public void testPartitionEnabledByConfigOnly(MetadataPartitionType partitionType,
+                                               HoodieTableVersion tableVersion) {
     HoodieTableMetaClient metaClient = Mockito.mock(HoodieTableMetaClient.class);
     HoodieTableConfig tableConfig = Mockito.mock(HoodieTableConfig.class);
 
     // Simulate the configuration enabling given partition type, but the meta client not having it available (yet to initialize the partition)
     Mockito.when(metaClient.getTableConfig()).thenReturn(tableConfig);
     Mockito.when(tableConfig.isMetadataPartitionAvailable(partitionType)).thenReturn(false);
+    Mockito.when(tableConfig.getTableVersion()).thenReturn(tableVersion);
     Mockito.when(metaClient.getIndexMetadata()).thenReturn(Option.empty());
     HoodieMetadataConfig.Builder metadataConfigBuilder = HoodieMetadataConfig.newBuilder();
     int expectedEnabledPartitions;
+    boolean tableVersionEightOrAbove = tableVersion.greaterThanOrEquals(HoodieTableVersion.EIGHT);
+    boolean isEnabled;
     switch (partitionType) {
       case EXPRESSION_INDEX:
         metadataConfigBuilder.enable(true).withExpressionIndexEnabled(true);
-        expectedEnabledPartitions = 4;
+        expectedEnabledPartitions = tableVersionEightOrAbove ? 4 : 2;
+        isEnabled = tableVersionEightOrAbove;
         break;
       case SECONDARY_INDEX:
         metadataConfigBuilder.enable(true).withEnableRecordIndex(true).withSecondaryIndexEnabled(true);
-        expectedEnabledPartitions = 5;
+        expectedEnabledPartitions = tableVersionEightOrAbove ? 5 : 3;
+        isEnabled = tableVersionEightOrAbove;
+        break;
+      case PARTITION_STATS:
+        metadataConfigBuilder.enable(true).withMetadataIndexPartitionStats(true);
+        expectedEnabledPartitions = tableVersionEightOrAbove ? 3 : 2;
+        isEnabled = tableVersionEightOrAbove;
         break;
       case BLOOM_FILTERS:
         metadataConfigBuilder.enable(true).withMetadataIndexBloomFilter(true);
-        expectedEnabledPartitions = 4;
+        expectedEnabledPartitions = tableVersionEightOrAbove ? 4 : 3;
+        isEnabled = true;
         break;
       case RECORD_INDEX:
         metadataConfigBuilder.enable(true).withEnableRecordIndex(true);
-        expectedEnabledPartitions = 5;
+        expectedEnabledPartitions = tableVersionEightOrAbove ? 5 : 3;
+        isEnabled = true;
         break;
       default:
         metadataConfigBuilder.enable(true);
-        expectedEnabledPartitions = 3; // by default, FILES, COLUMN_STATS, PARTITION_STATS are enabled
+        expectedEnabledPartitions = tableVersionEightOrAbove ? 3 : 2; // by default, FILES, COLUMN_STATS, PARTITION_STATS are enabled
+        isEnabled = true;
         break;
     }
 
@@ -89,7 +115,7 @@ public class TestMetadataPartitionType {
 
     // Verify partition type is enabled due to config
     assertEquals(expectedEnabledPartitions, enabledPartitions.size());
-    assertTrue(enabledPartitions.contains(partitionType) || MetadataPartitionType.ALL_PARTITIONS.equals(partitionType));
+    assertEquals(isEnabled, enabledPartitions.contains(partitionType) || MetadataPartitionType.ALL_PARTITIONS.equals(partitionType));
   }
 
   @Test
