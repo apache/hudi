0c92d16098e04b922de1b7c0b786372591056733
Alex Rhee <arheebikes@gmail.com>
2025-05-05 15:46:42 -0700

    fix indentation

--- hudi-common/src/main/java/org/apache/hudi/common/table/timeline/versioning/v1/ArchivedTimelineV1.java
+++ hudi-common/src/main/java/org/apache/hudi/common/table/timeline/versioning/v1/ArchivedTimelineV1.java
@@ -60,8 +60,7 @@ public class ArchivedTimelineV1 extends BaseTimelineV1 implements HoodieArchived
   private static final String STATE_TRANSITION_TIME = "stateTransitionTime";
   private HoodieTableMetaClient metaClient;
   // The first key is the timestamp -> multiple action types -> hoodie instant state and contents
-  private final Map<String, Map<String, Map<HoodieInstant.State, byte[]>>> 
-  readCommits = new HashMap<>();
+  private final Map<String, Map<String, Map<HoodieInstant.State, byte[]>>> readCommits = new HashMap<>();
   private final ArchivedTimelineLoaderV1 timelineLoader = new ArchivedTimelineLoaderV1();
 
   private static final Logger LOG = LoggerFactory.getLogger(org.apache.hudi.common.table.timeline.HoodieArchivedTimeline.class);
