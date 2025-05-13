62a80eacaab13911eb51afcee6a7144335ea1356
Lokesh Jain <ljain@apache.org>
2025-05-13 16:59:14 +0530

    Add more API and tests

---
+++ hudi-client/hudi-client-common/src/test/java/org/apache/hudi/metadata/TestMetadataIndexGenerator.java
@@ -0,0 +1,106 @@
+package org.apache.hudi.metadata;
+
+import org.apache.hudi.avro.model.HoodieRecordIndexInfo;
+import org.apache.hudi.client.WriteStatus;
+import org.apache.hudi.common.model.EmptyHoodieRecordPayload;
+import org.apache.hudi.common.model.HoodieRecord;
+import org.apache.hudi.common.model.HoodieRecordLocation;
+import org.apache.hudi.common.testutils.HoodieCommonTestHarness;
+import org.apache.hudi.common.testutils.HoodieTestDataGenerator;
+import org.apache.hudi.common.testutils.InProcessTimeGenerator;
+import org.apache.hudi.common.util.Option;
+import org.apache.hudi.common.util.StringUtils;
+import org.apache.hudi.common.util.collection.Pair;
+import org.apache.hudi.config.HoodieWriteConfig;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMetadataIndexGenerator extends HoodieCommonTestHarness {
+
+  @Test
+  public void testRLIIndexGeneratorWithInsertsAndUpserts() throws Exception {
+    // Generate write status with inserts and upserts
+    WriteStatus writeStatus = new WriteStatus(true, 0.0d);
+    HoodieTestDataGenerator dataGenerator = new HoodieTestDataGenerator();
+    List<HoodieRecord> records = dataGenerator.generateInserts("001", 10);
+    for (int i = 0; i < records.size(); i++) {
+      HoodieRecord record = records.get(i);
+      String fileId = UUID.randomUUID().toString();
+      if (i < 5) {
+        // 5 updates
+        record.setCurrentLocation(new HoodieRecordLocation(InProcessTimeGenerator.createNewInstantTime(), fileId));
+      }
+      // 5 inserts
+      record.setNewLocation(new HoodieRecordLocation(InProcessTimeGenerator.createNewInstantTime(), fileId));
+      writeStatus.markSuccess(record, Option.empty());
+    }
+
+    // Generate RLI records
+    HoodieWriteConfig writeConfig = HoodieWriteConfig.newBuilder()
+        .withPath("random")
+        .build();
+    MetadataIndexGenerator.PerWriteStatsBasedIndexGenerator perWriteStatsBasedIndexGenerator = new MetadataIndexGenerator.PerWriteStatsBasedIndexGenerator(
+        Collections.singletonList(MetadataPartitionType.RECORD_INDEX), writeConfig, storageConf, "001");
+    Iterator<Pair<String, HoodieRecord>> rliRecordsPair = perWriteStatsBasedIndexGenerator.apply(writeStatus);
+    AtomicInteger totalRLIRecords = new AtomicInteger();
+    rliRecordsPair.forEachRemaining(pair -> {
+      totalRLIRecords.getAndIncrement();
+      assertEquals(MetadataPartitionType.RECORD_INDEX.getPartitionPath(), pair.getLeft());
+
+      // verify RLI metadata
+      HoodieRecord<HoodieMetadataPayload> record = pair.getRight();
+      assertEquals(MetadataPartitionType.RECORD_INDEX.getPartitionPath(), record.getKey().getPartitionPath());
+      HoodieRecordIndexInfo recordIndexInfo = record.getData().recordIndexMetadata;
+      assertNotNull(recordIndexInfo);
+      assertTrue(StringUtils.nonEmpty(recordIndexInfo.getPartitionName()));
+      assertNotNull(recordIndexInfo.getFileIdHighBits());
+      assertNotNull(recordIndexInfo.getFileIdLowBits());
+    });
+    // RLI is only updated for inserts
+    assertEquals(5, totalRLIRecords.get());
+  }
+
+  @Test
+  public void testRLIIndexGeneratorWithDeletes() throws Exception {
+    // Generate write status with deletes
+    WriteStatus writeStatus = new WriteStatus(true, 0.0d);
+    HoodieTestDataGenerator dataGenerator = new HoodieTestDataGenerator();
+    List<HoodieRecord> records = dataGenerator.generateInserts("001", 10);
+    for (int i = 0; i < records.size(); i++) {
+      HoodieRecord record = records.get(i);
+      String fileId = UUID.randomUUID().toString();
+      record.setCurrentLocation(new HoodieRecordLocation(InProcessTimeGenerator.createNewInstantTime(), fileId));
+      writeStatus.markSuccess(record, Option.empty());
+    }
+
+    // Generate RLI records
+    HoodieWriteConfig writeConfig = HoodieWriteConfig.newBuilder()
+        .withPath("random")
+        .build();
+    MetadataIndexGenerator.PerWriteStatsBasedIndexGenerator perWriteStatsBasedIndexGenerator = new MetadataIndexGenerator.PerWriteStatsBasedIndexGenerator(
+        Collections.singletonList(MetadataPartitionType.RECORD_INDEX), writeConfig, storageConf, "001");
+    Iterator<Pair<String, HoodieRecord>> rliRecordsPair = perWriteStatsBasedIndexGenerator.apply(writeStatus);
+    AtomicInteger totalRLIRecords = new AtomicInteger();
+    rliRecordsPair.forEachRemaining(pair -> {
+      totalRLIRecords.getAndIncrement();
+      assertEquals(MetadataPartitionType.RECORD_INDEX.getPartitionPath(), pair.getLeft());
+
+      // verify RLI record payload is EmptyHoodieRecordPayload
+      HoodieRecord<EmptyHoodieRecordPayload> record = pair.getRight();
+      assertEquals(MetadataPartitionType.RECORD_INDEX.getPartitionPath(), record.getKey().getPartitionPath());
+      assertTrue(record.getData() instanceof EmptyHoodieRecordPayload);
+    });
+    // RLI is only updated for inserts
+    assertEquals(10, totalRLIRecords.get());
+  }
+}
