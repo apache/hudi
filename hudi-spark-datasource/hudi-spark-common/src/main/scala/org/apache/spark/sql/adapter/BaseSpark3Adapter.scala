c19342bd0e48402068e8b83e222aeab673289054
Vova Kolmakov <kolmakov.vladimir@huawei.com>
2025-05-15 13:40:17 +0700

    [HUDI-9410] Moved the code from hudi-spark3-common to hudi-spark-common module (#13301)
    Co-authored-by: Vova Kolmakov <kolmakov.vladimir@huawei.com>
    
    (cherry picked from commit b5a25f7de6842965296c09b5fc25342a73b915f6)

---
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/spark/sql/adapter/BaseSpark3Adapter.scala
@@ -0,0 +1,160 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.adapter
+
+import org.apache.hudi.{AvroConversionUtils, DefaultSource, HoodiePartitionCDCFileGroupMapping, HoodiePartitionFileSliceMapping, Spark3HoodiePartitionCDCFileGroupMapping, Spark3HoodiePartitionFileSliceMapping, Spark3RowSerDe}
+import org.apache.hudi.client.model.{HoodieInternalRow, Spark3HoodieInternalRow}
+import org.apache.hudi.client.utils.SparkRowSerDe
+import org.apache.hudi.common.model.FileSlice
+import org.apache.hudi.common.table.HoodieTableMetaClient
+import org.apache.hudi.common.table.cdc.HoodieCDCFileSplit
+import org.apache.hudi.common.util.JsonUtils
+import org.apache.hudi.spark.internal.ReflectUtil
+import org.apache.hudi.storage.StoragePath
+
+import org.apache.avro.Schema
+import org.apache.spark.api.java.JavaSparkContext
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.{AnalysisException, HoodieSpark3CatalogUtils, SparkSession, SQLContext}
+import org.apache.spark.sql.avro.{HoodieAvroSchemaConverters, HoodieSparkAvroSchemaConverters}
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.expressions.{Expression, InterpretedPredicate, Predicate}
+import org.apache.spark.sql.catalyst.parser.ParseException
+import org.apache.spark.sql.catalyst.trees.Origin
+import org.apache.spark.sql.catalyst.util.DateFormatter
+import org.apache.spark.sql.execution.{PartitionedFileUtil, QueryExecution, SQLExecution}
+import org.apache.spark.sql.execution.datasources._
+import org.apache.spark.sql.hudi.SparkAdapter
+import org.apache.spark.sql.sources.{BaseRelation, Filter}
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.vectorized.{ColumnarBatch, ColumnVector}
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.unsafe.types.UTF8String
+
+import java.time.ZoneId
+import java.util.TimeZone
+import java.util.concurrent.ConcurrentHashMap
+
+import scala.collection.JavaConverters._
+
+/**
+ * Base implementation of [[SparkAdapter]] for Spark 3.x branch
+ */
+abstract class BaseSpark3Adapter extends SparkAdapter with Logging {
+  JsonUtils.registerModules()
+
+  private val cache = new ConcurrentHashMap[ZoneId, DateFormatter](1)
+
+  def getCatalogUtils: HoodieSpark3CatalogUtils
+
+  override def createSparkRowSerDe(schema: StructType): SparkRowSerDe = {
+    new Spark3RowSerDe(getCatalystExpressionUtils.getEncoder(schema))
+  }
+
+  override def getAvroSchemaConverters: HoodieAvroSchemaConverters = HoodieSparkAvroSchemaConverters
+
+  override def getSparkParsePartitionUtil: SparkParsePartitionUtil = Spark3ParsePartitionUtil
+
+  override def getDateFormatter(tz: TimeZone): DateFormatter = {
+    cache.computeIfAbsent(tz.toZoneId, zoneId => ReflectUtil.getDateFormatter(zoneId))
+  }
+
+  /**
+   * Combine [[PartitionedFile]] to [[FilePartition]] according to `maxSplitBytes`.
+   */
+  override def getFilePartitions(
+      sparkSession: SparkSession,
+      partitionedFiles: Seq[PartitionedFile],
+      maxSplitBytes: Long): Seq[FilePartition] = {
+    FilePartition.getFilePartitions(sparkSession, partitionedFiles, maxSplitBytes)
+  }
+
+  override def createInterpretedPredicate(e: Expression): InterpretedPredicate = {
+    Predicate.createInterpreted(e)
+  }
+
+  override def createRelation(sqlContext: SQLContext,
+                              metaClient: HoodieTableMetaClient,
+                              schema: Schema,
+                              globPaths: Array[StoragePath],
+                              parameters: java.util.Map[String, String]): BaseRelation = {
+    val dataSchema = Option(schema).map(AvroConversionUtils.convertAvroSchemaToStructType).orNull
+    DefaultSource.createRelation(sqlContext, metaClient, dataSchema, globPaths, parameters.asScala.toMap)
+  }
+
+  override def convertStorageLevelToString(level: StorageLevel): String
+
+  override def translateFilter(predicate: Expression,
+                               supportNestedPredicatePushdown: Boolean = false): Option[Filter] = {
+    DataSourceStrategy.translateFilter(predicate, supportNestedPredicatePushdown)
+  }
+
+  override def makeColumnarBatch(vectors: Array[ColumnVector], numRows: Int): ColumnarBatch = {
+    new ColumnarBatch(vectors, numRows)
+  }
+
+  override def sqlExecutionWithNewExecutionId[T](sparkSession: SparkSession,
+                                                 queryExecution: QueryExecution,
+                                                 name: Option[String])(body: => T): T = {
+      SQLExecution.withNewExecutionId(queryExecution, name)(body)
+  }
+
+  def stopSparkContext(jssc: JavaSparkContext, exitCode: Int): Unit
+
+  override def createInternalRow(commitTime: UTF8String,
+                                commitSeqNumber: UTF8String,
+                                recordKey: UTF8String,
+                                partitionPath: UTF8String,
+                                fileName: UTF8String,
+                                sourceRow: InternalRow,
+                                sourceContainsMetaFields: Boolean): HoodieInternalRow = {
+    new Spark3HoodieInternalRow(commitTime, commitSeqNumber, recordKey, partitionPath, fileName, sourceRow, sourceContainsMetaFields)
+  }
+
+  override def createInternalRow(metaFields: Array[UTF8String],
+                                sourceRow: InternalRow,
+                                sourceContainsMetaFields: Boolean): HoodieInternalRow = {
+    new Spark3HoodieInternalRow(metaFields, sourceRow, sourceContainsMetaFields)
+  }
+
+  override def createHoodiePartitionCDCFileGroupMapping(partitionValues: InternalRow,
+                                                        fileSplits: List[HoodieCDCFileSplit]): HoodiePartitionCDCFileGroupMapping = {
+    new Spark3HoodiePartitionCDCFileGroupMapping(partitionValues, fileSplits)
+  }
+
+  override def createHoodiePartitionFileSliceMapping(values: InternalRow,
+                                                     slices: Map[String, FileSlice]): HoodiePartitionFileSliceMapping = {
+    new Spark3HoodiePartitionFileSliceMapping(values, slices)
+  }
+
+  override def newParseException(command: Option[String],
+                                 exception: AnalysisException,
+                                 start: Origin,
+                                 stop: Origin): ParseException = {
+    new ParseException(command, exception.getMessage, start, stop)
+  }
+
+  override def splitFiles(sparkSession: SparkSession,
+                          partitionDirectory: PartitionDirectory,
+                          isSplitable: Boolean,
+                          maxSplitSize: Long): Seq[PartitionedFile] = {
+    partitionDirectory.files.flatMap(file =>
+      PartitionedFileUtil.splitFiles(sparkSession, file, file.getPath, isSplitable, maxSplitSize, partitionDirectory.values)
+    )
+  }
+}
---
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/HoodieQuery.scala
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.catalyst.plans.logical
+
+import org.apache.hudi.common.util.ValidationUtils.checkState
+
+import org.apache.spark.sql.catalyst.expressions.{Attribute, Expression}
+import org.apache.spark.sql.catalyst.plans.logical.LeafNode
+import org.apache.spark.sql.hudi.command.exception.HoodieAnalysisException
+
+case class HoodieQuery(args: Seq[Expression]) extends LeafNode {
+
+  override def output: Seq[Attribute] = Nil
+
+  override lazy val resolved: Boolean = false
+
+}
+
+object HoodieQuery {
+
+  val FUNC_NAME = "hudi_query";
+
+  def parseOptions(exprs: Seq[Expression]): (String, Map[String, String]) = {
+    val args = exprs.map(_.eval().toString)
+
+    val (Seq(identifier, queryMode), remaining) = args.splitAt(2)
+
+    val opts = queryMode match {
+      case "read_optimized" | "snapshot" =>
+        checkState(remaining.isEmpty, s"No additional args are expected in `$queryMode` mode")
+        Map("hoodie.datasource.query.type" -> queryMode)
+
+      case _ =>
+        throw new HoodieAnalysisException(s"'hudi_query' doesn't currently support `$queryMode`")
+    }
+
+    (identifier, opts)
+  }
+}
