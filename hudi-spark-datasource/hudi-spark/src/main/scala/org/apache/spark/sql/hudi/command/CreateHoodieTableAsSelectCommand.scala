4d92b040432c6c4cd15ef52c81808fe7d536b115
Vova Kolmakov <kolmakov.vladimir@huawei.com>
2025-05-21 10:06:59 +0700

    remove path from CTAScommand

--- hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/hudi/command/CreateHoodieTableAsSelectCommand.scala
+++ hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/hudi/command/CreateHoodieTableAsSelectCommand.scala
@@ -106,8 +106,7 @@ case class CreateHoodieTableAsSelectCommand(
         HiveSyncConfigHolder.HIVE_TABLE_PROPERTIES.key -> ConfigUtils.configToString(updatedTable.properties.asJava),
         HoodieWriteConfig.COMBINE_BEFORE_INSERT.key -> "false",
         DataSourceWriteOptions.SQL_INSERT_MODE.key -> InsertMode.NON_STRICT.value(),
-        DataSourceWriteOptions.SQL_ENABLE_BULK_INSERT.key -> "true",
-        "path" -> tablePath
+        DataSourceWriteOptions.SQL_ENABLE_BULK_INSERT.key -> "true"
       )
       val partitionSpec = updatedTable.partitionColumnNames.map((_, None)).toMap
       val success = InsertIntoHoodieTableCommand.run(sparkSession, updatedTable, plan, partitionSpec,
