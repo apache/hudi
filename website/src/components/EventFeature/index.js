186290a728761d161e3a6fcf1f3ef1844b7c027d
dipankarmazumdar <dp976894@dal.ca>
2025-06-04 11:32:58 -0400

    New event banner

--- website/src/components/EventFeature/index.js
+++ website/src/components/EventFeature/index.js
@@ -11,13 +11,13 @@ const Events = () => {
                  <div className="container">
                      <div className={styles.flexContainer}>
                         <div className={styles.bannercontent}>
-                            <h3> Modernizing Data Infrastructure at Southwest Airlines using Apache Hudi</h3>
+                            <h3> Beyond Adoption: Halodoc Lakehouse Transformation with Apache Hudi</h3>
                             <p className= {styles. flexParagraph}><span className={styles. sideMicrophone}><Microphone/></span>
                             Linkedin: Live Event | <span className={styles.sideCalendar}><Calendar/></span>
-                            Mar 19th, 9am Pacific Time</p>
+                            June 10th, 8AM Pacific Time</p>
                         </div>
                          <div className={styles.joinButton}>
-                             <LinkButton class={styles.registerbutton} type="secondary" to="https://www.linkedin.com/events/modernizingdatainfrastructureat7303469421048573952/theater/">
+                             <LinkButton class={styles.registerbutton} type="secondary" to="https://www.linkedin.com/events/beyondadoption-halodoc-slakehou7333879866762629120/theater/">
                                              Join Now
                              </LinkButton>
                          </div>
