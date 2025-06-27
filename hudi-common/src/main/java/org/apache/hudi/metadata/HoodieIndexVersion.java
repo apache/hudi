04769a01d6893334d19a7d2d4d4843ff8bc423ff
Davis Zhang <davis@onehouse.ai>
2025-06-27 11:31:47 -0700

    index version interface (Jun 27, make it index type indifferent)

--- hudi-common/src/main/java/org/apache/hudi/metadata/HoodieIndexVersion.java
+++ hudi-common/src/main/java/org/apache/hudi/metadata/HoodieIndexVersion.java
@@ -20,82 +20,133 @@ package org.apache.hudi.metadata;
 
 import org.apache.hudi.common.model.HoodieIndexDefinition;
 import org.apache.hudi.common.table.HoodieTableVersion;
-import org.apache.hudi.common.util.ValidationUtils;
 
-import java.util.Arrays;
-import java.util.List;
-
+/**
+ * Enum representing different versions of Hoodie indexes.
+ *
+ * <p>This enum defines the supported versions of metadata indexes in Apache Hudi.
+ * Each version may have different features, performance characteristics, and compatibility
+ * requirements. The versioning system allows for evolution of the metadata index format
+ * while maintaining backward compatibility.</p>
+ * 
+ * <p>Version codes are used for comparison and determining compatibility between
+ * different index versions for the same index type. Different indexes may have different
+ * version codes. Versions adopted by one index is not comparable with another index.</p>
+ */
 public enum HoodieIndexVersion {
-  // Implementation comes in downstream commits
-  DUMMY(MetadataPartitionType.PARTITION_STATS, 1, Arrays.asList(""));
+  V1(1),
+  V2(2);
 
-  private final MetadataPartitionType partitionType;
   private final int versionCode;
-  private final List<String> releaseVersions;
 
-  HoodieIndexVersion(MetadataPartitionType partitionType, int versionCode, List<String> releaseVersions) {
-    this.partitionType = partitionType;
+  /**
+   * Constructor for HoodieIndexVersion.
+   *
+   * @param versionCode the numeric version code for this index version
+   */
+  HoodieIndexVersion(int versionCode) {
     this.versionCode = versionCode;
-    this.releaseVersions = releaseVersions;
   }
 
-  public MetadataPartitionType getPartitionType() {
-    return partitionType;
-  }
-
+  /**
+   * Returns the numeric version code for this index version.
+   *
+   * @return the version code as an integer
+   */
   public int versionCode() {
     return versionCode;
   }
 
-  public List<String> getReleaseVersions() {
-    return releaseVersions;
-  }
-
+  /**
+   * Gets the current index version for a given table version and partition path.
+   *
+   * <p>This method determines the appropriate index version based on the table version
+   * and the specific partition path. The partition path is used to identify the type
+   * of metadata partition being accessed.</p>
+   *
+   * @param tableVersion the version of the Hoodie table
+   * @param partitionPath the partition path to determine the metadata partition type
+   * @return the appropriate HoodieIndexVersion for the given parameters
+   */
   public static HoodieIndexVersion getCurrentVersion(HoodieTableVersion tableVersion, String partitionPath) {
     return getCurrentVersion(tableVersion, MetadataPartitionType.fromPartitionPath(partitionPath));
   }
 
+  /**
+   * Gets the current index version for a given table version and metadata partition type.
+   *
+   * <p>This method determines the appropriate index version based on the table version
+   * and the specific metadata partition type. Different partition types may support
+   * different index versions.</p>
+   *
+   * @param tableVersion the version of the Hoodie table
+   * @param partitionType the type of metadata partition
+   * @return the appropriate HoodieIndexVersion for the given parameters
+   */
   public static HoodieIndexVersion getCurrentVersion(HoodieTableVersion tableVersion, MetadataPartitionType partitionType) {
     return null;
   }
 
+  /**
+   * Validates if an index definition is valid for a given table version.
+   *
+   * <p>This method checks whether the provided index definition is compatible
+   * with the specified table version. Different table versions may have different
+   * requirements for index definitions.</p>
+   *
+   * @param tv the table version to validate against
+   * @param idxDef the index definition to validate
+   * @return true if the index definition is valid for the table version, false otherwise
+   */
   public static boolean isValidIndexDefinition(HoodieTableVersion tv, HoodieIndexDefinition idxDef) {
     return true;
   }
 
+  /**
+   * Checks if this index version is greater than the specified version.
+   *
+   * @param other the index version to compare against
+   * @return true if this version is greater than the other version, false otherwise
+   */
   public boolean greaterThan(HoodieIndexVersion other) {
-    checkSamePartitionType(other);
     return this.versionCode > other.versionCode;
   }
 
+  /**
+   * Checks if this index version is greater than or equal to the specified version.
+   *
+   * @param other the index version to compare against
+   * @return true if this version is greater than or equal to the other version, false otherwise
+   */
   public boolean greaterThanOrEquals(HoodieIndexVersion other) {
-    checkSamePartitionType(other);
     return this.versionCode >= other.versionCode;
   }
 
+  /**
+   * Checks if this index version is lower than the specified version.
+   *
+   * @param other the index version to compare against
+   * @return true if this version is lower than the other version, false otherwise
+   */
   public boolean lowerThan(HoodieIndexVersion other) {
-    checkSamePartitionType(other);
     return this.versionCode < other.versionCode;
   }
 
+  /**
+   * Checks if this index version is lower than or equal to the specified version.
+   *
+   * @param other the index version to compare against
+   * @return true if this version is lower than or equal to the other version, false otherwise
+   */
   public boolean lowerThanOrEquals(HoodieIndexVersion other) {
-    checkSamePartitionType(other);
     return this.versionCode <= other.versionCode;
   }
 
-  private void checkSamePartitionType(HoodieIndexVersion other) {
-    ValidationUtils.checkArgument(this.partitionType.equals(other.partitionType),
-        "Hoodie index version partition type mismatches with the incoming "
-            + "one: Expected" + this.partitionType + ", got " + other.partitionType);
-  }
-
-  public void ensureVersionCanBeAssignedToIndexType(MetadataPartitionType partitionType) {
-    ValidationUtils.checkArgument(this.partitionType.equals(partitionType)
-        || this.partitionType.equals(MetadataPartitionType.EXPRESSION_INDEX),
-        String.format("Hoodie index version %s is not allowed to be assigned to partition type %s",
-          this, partitionType));
-  }
-
+  /**
+   * Returns the string representation of this index version.
+   *
+   * @return the name of this enum constant
+   */
   @Override
   public String toString() {
     return name();
