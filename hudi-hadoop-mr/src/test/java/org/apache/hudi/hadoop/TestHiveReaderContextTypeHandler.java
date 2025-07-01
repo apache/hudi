e93ece67c8de4e06521d67413beec830f634874e
Lin Liu <linliu.code@gmail.com>
2025-07-01 09:47:34 -0700

    Add more tests

---
+++ hudi-hadoop-mr/src/test/java/org/apache/hudi/hadoop/TestHiveReaderContextTypeHandler.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.hudi.hadoop;
+
+import org.apache.hadoop.io.BooleanWritable;
+import org.apache.hadoop.io.Text;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestHiveReaderContextTypeHandler {
+  private final HiveReaderContextTypeHandler handler = new HiveReaderContextTypeHandler();
+
+  @Test
+  void testCastToBoolean_withValidBooleanWritableTrue() {
+    BooleanWritable input = new BooleanWritable(true);
+    assertTrue(handler.castToBoolean(input));
+  }
+
+  @Test
+  void testCastToBoolean_withValidBooleanWritableFalse() {
+    BooleanWritable input = new BooleanWritable(false);
+    assertFalse(handler.castToBoolean(input));
+  }
+
+  @Test
+  void testCastToBoolean_withInvalidType() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      handler.castToBoolean("not a BooleanWritable");
+    });
+    assertTrue(exception.getMessage().contains("Expected BooleanWritable but got"));
+  }
+
+  @Test
+  void testCastToString_withValidText() {
+    Text input = new Text("test string");
+    assertEquals("test string", handler.castToString(input));
+  }
+
+  @Test
+  void testCastToString_withInvalidType() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      handler.castToString(new BooleanWritable(true));
+    });
+    assertTrue(exception.getMessage().contains("Expected BooleanWritable but got"));
+  }
+}
