b141eeea6a105909ea3a7986481ce14e3cd85b03
Jonathan Vexler <=>
2025-07-07 16:38:46 -0400

    choose which incremental relation to use in the hadoop factory

--- hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/HoodieIncrementalFileIndex.scala
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/HoodieIncrementalFileIndex.scala
@@ -40,12 +40,11 @@ class HoodieIncrementalFileIndex(override val spark: SparkSession,
                                  override val options: Map[String, String],
                                  @transient override val fileStatusCache: FileStatusCache = NoopCache,
                                  override val includeLogFiles: Boolean,
-                                 override val shouldEmbedFileSlices: Boolean)
+                                 override val shouldEmbedFileSlices: Boolean,
+                                 mergeOnReadIncrementalRelation: MergeOnReadIncrementalRelation)
   extends HoodieFileIndex(
     spark, metaClient, schemaSpec, options, fileStatusCache, includeLogFiles, shouldEmbedFileSlices
   ) with FileIndex {
-  val mergeOnReadIncrementalRelation: MergeOnReadIncrementalRelationV2 = MergeOnReadIncrementalRelationV2(
-    spark.sqlContext, options, metaClient, schemaSpec, schemaSpec)
 
   override def listFiles(partitionFilters: Seq[Expression], dataFilters: Seq[Expression]): Seq[PartitionDirectory] = {
     hasPushedDownPartitionPredicates = true
