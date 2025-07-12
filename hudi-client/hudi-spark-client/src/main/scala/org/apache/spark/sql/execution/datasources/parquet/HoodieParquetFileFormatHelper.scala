1a706c0d4df596acfbaf92e10622fd6996647c3f
Jonathan Vexler <=>
2025-07-12 11:31:34 -0400

    fix spark float double cast issue

--- hudi-client/hudi-spark-client/src/main/scala/org/apache/spark/sql/execution/datasources/parquet/HoodieParquetFileFormatHelper.scala
+++ hudi-client/hudi-spark-client/src/main/scala/org/apache/spark/sql/execution/datasources/parquet/HoodieParquetFileFormatHelper.scala
@@ -23,7 +23,7 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.parquet.hadoop.metadata.FileMetaData
 import org.apache.spark.sql.HoodieSchemaUtils
 import org.apache.spark.sql.catalyst.expressions.codegen.GenerateUnsafeProjection
-import org.apache.spark.sql.catalyst.expressions.{Attribute, Cast, UnsafeProjection}
+import org.apache.spark.sql.catalyst.expressions.{ArrayTransform, Attribute, Cast, CreateNamedStruct, CreateStruct, Expression, GetStructField, LambdaFunction, Literal, MapEntries, MapFromEntries, NamedLambdaVariable, UnsafeProjection}
 import org.apache.spark.sql.types.{ArrayType, DataType, DoubleType, FloatType, MapType, StringType, StructField, StructType}
 
 object HoodieParquetFileFormatHelper {
@@ -98,13 +98,47 @@ object HoodieParquetFileFormatHelper {
     case _ => fileType
   }
 
+
   def generateUnsafeProjection(fullSchema: Seq[Attribute],
                                timeZoneId: Option[String],
                                typeChangeInfos: java.util.Map[Integer, org.apache.hudi.common.util.collection.Pair[DataType, DataType]],
                                requiredSchema: StructType,
                                partitionSchema: StructType,
                                schemaUtils: HoodieSchemaUtils): UnsafeProjection = {
-
+    def repairFloatDoubleConversion(expr: Expression, srcType: DataType, dstType: DataType): Expression = {
+      lazy val needTimeZone = Cast.needsTimeZone(srcType, dstType)
+      if (srcType == FloatType && dstType == DoubleType) {
+        val toStr = Cast(expr, StringType, if (needTimeZone) timeZoneId else None)
+        Cast(toStr, dstType, if (needTimeZone) timeZoneId else None)
+      } else (srcType, dstType) match {
+        case (s: StructType, d: StructType) =>
+          val structFields = s.fields.zip(d.fields).zipWithIndex.map {
+            case ((srcField, dstField), i) =>
+              val child = GetStructField(expr, i, Some(dstField.name))
+              repairFloatDoubleConversion(child, srcField.dataType, dstField.dataType)
+          }
+          CreateNamedStruct(d.fields.zip(structFields).flatMap {
+            case (f, c) => Seq(Literal(f.name), c)
+          })
+        case (ArrayType(sElementType, containsNull), ArrayType(dElementType, _)) =>
+          val lambdaVar = NamedLambdaVariable("x", sElementType, containsNull)
+          val body = repairFloatDoubleConversion(lambdaVar, sElementType, dElementType)
+          val func = LambdaFunction(body, Seq(lambdaVar))
+          ArrayTransform(expr, func)
+        case (MapType(sKeyType, sValType, vnull), MapType(dKeyType, dValType, _)) =>
+          val kv = NamedLambdaVariable("kv", new StructType()
+            .add("key", sKeyType, nullable = false)
+            .add("value", sValType, nullable = vnull), nullable = false)
+          val newKey = repairFloatDoubleConversion(GetStructField(kv, 0), sKeyType, dKeyType)
+          val newVal = repairFloatDoubleConversion(GetStructField(kv, 1), sValType, dValType)
+          val entry = CreateStruct(Seq(newKey, newVal))
+          val func = LambdaFunction(entry, Seq(kv))
+          val transformed = ArrayTransform(MapEntries(expr), func)
+          MapFromEntries(transformed)
+        case _ =>
+          Cast(expr, dstType, if (needTimeZone) timeZoneId else None)
+      }
+    }
     if (typeChangeInfos.isEmpty) {
       GenerateUnsafeProjection.generate(fullSchema, fullSchema)
     } else {
@@ -119,16 +153,7 @@ object HoodieParquetFileFormatHelper {
         if (typeChangeInfos.containsKey(i)) {
           val srcType = typeChangeInfos.get(i).getRight
           val dstType = typeChangeInfos.get(i).getLeft
-          val needTimeZone = Cast.needsTimeZone(srcType, dstType)
-
-          // work around for the case when cast float to double
-          if (srcType == FloatType && dstType == DoubleType) {
-            // first cast to string and then to double
-            val toStringAttr = Cast(attr, StringType, if (needTimeZone) timeZoneId else None)
-            Cast(toStringAttr, dstType, if (needTimeZone) timeZoneId else None)
-          } else {
-            Cast(attr, dstType, if (needTimeZone) timeZoneId else None)
-          }
+          repairFloatDoubleConversion(attr, srcType, dstType)
         } else attr
       }
       GenerateUnsafeProjection.generate(castSchema, newFullSchema)
