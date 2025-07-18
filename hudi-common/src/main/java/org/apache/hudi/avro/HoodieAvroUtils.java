faf77ecf7a881c164beff0e2cf487e3365faa48d
Jonathan Vexler <=>
2025-07-18 14:53:13 -0400

    avro fix for missing fields

--- hudi-common/src/main/java/org/apache/hudi/avro/HoodieAvroUtils.java
+++ hudi-common/src/main/java/org/apache/hudi/avro/HoodieAvroUtils.java
@@ -1387,15 +1387,12 @@ public class HoodieAvroUtils {
     }
     switch (readerSchema.getType()) {
       case RECORD:
-        Map<String, Schema.Field> writerFields = new HashMap<>();
-        for (Schema.Field field : writerSchema.getFields()) {
-          writerFields.put(field.name(), field);
-        }
         for (Schema.Field field : readerSchema.getFields()) {
-          if (writerFields.containsKey(field.name())) {
-            if (recordNeedsRewriteForExtendedAvroTypePromotion(writerFields.get(field.name()).schema(), field.schema())) {
-              return true;
-            }
+          Schema.Field writerField = writerSchema.getField(field.name());
+          if (writerField == null) {
+            return true;
+          } else if (recordNeedsRewriteForExtendedAvroTypePromotion(writerField.schema(), field.schema())) {
+            return true;
           }
         }
         return false;
