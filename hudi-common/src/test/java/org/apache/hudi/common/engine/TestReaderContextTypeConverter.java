6b3b897c6cf161e484135d3d4eb356ab7c2f1512
Geser Dugarov <geserdugarov@gmail.com>
2025-07-18 07:46:55 +0700

    [MINOR] Fix typo in import (#13569)

--- hudi-common/src/test/java/org/apache/hudi/common/engine/TestReaderContextTypeConverter.java
+++ hudi-common/src/test/java/org/apache/hudi/common/engine/TestReaderContextTypeConverter.java
@@ -23,9 +23,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.AssertionsKt.assertNull;
 
 class TestReaderContextTypeConverter {
   private final ReaderContextTypeConverter typeHandler = new ReaderContextTypeConverter();
