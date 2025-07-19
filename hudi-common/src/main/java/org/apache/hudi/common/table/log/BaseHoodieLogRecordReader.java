712c2ce306671ba625a8f9de7b7f653ffdd44f34
Timothy Brown <tim@onehouse.ai>
2025-07-19 16:51:54 -0400

    fix npe

--- hudi-common/src/main/java/org/apache/hudi/common/table/log/BaseHoodieLogRecordReader.java
+++ hudi-common/src/main/java/org/apache/hudi/common/table/log/BaseHoodieLogRecordReader.java
@@ -698,7 +698,9 @@ public abstract class BaseHoodieLogRecordReader<T> {
       }
       // Done
       progress = 1.0f;
-      totalLogRecords.set(recordBuffer.getTotalLogRecords());
+      if (recordBuffer != null) {
+        totalLogRecords.set(recordBuffer.getTotalLogRecords());
+      }
     } catch (IOException e) {
       LOG.error("Got IOException when reading log file", e);
       throw new HoodieIOException("IOException when reading log file ", e);
