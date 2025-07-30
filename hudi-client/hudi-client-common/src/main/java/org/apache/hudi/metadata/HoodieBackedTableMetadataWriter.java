46060dd14a07436c7b55b469a9f0730d7593409c
vinoth chandar <vinoth@apache.org>
2025-07-30 01:10:43 -0700

    Fixing checkstyle

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/metadata/HoodieBackedTableMetadataWriter.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/metadata/HoodieBackedTableMetadataWriter.java
@@ -1605,7 +1605,8 @@ public abstract class HoodieBackedTableMetadataWriter<I, O> implements HoodieTab
         String syncCommitTime = createRestoreTimestamp(metadataTableCommit);
         processAndCommit(syncCommitTime, () -> HoodieTableMetadataUtil.convertMissingPartitionRecords(engineContext,
             partitionsToDelete, partitionFilesToAdd, partitionFilesToDelete, syncCommitTime));
-        return null;
+        // empty result
+        return Option.empty();
       });
       closeInternal();
     } catch (IOException e) {
