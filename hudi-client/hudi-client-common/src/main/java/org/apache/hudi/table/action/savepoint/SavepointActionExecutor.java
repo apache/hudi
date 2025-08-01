75caf7a962155f29959b7a58240e39f9fec8fe6c
vinoth chandar <vinoth@apache.org>
2025-08-01 16:27:23 -0700

    Remove one-off instantiation of txn manager in SavepointActionExecutor

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/action/savepoint/SavepointActionExecutor.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/table/action/savepoint/SavepointActionExecutor.java
@@ -57,16 +57,19 @@ public class SavepointActionExecutor<T, I, K, O> extends BaseActionExecutor<T, I
 
   private final String user;
   private final String comment;
+  private final String completionTime;
 
   public SavepointActionExecutor(HoodieEngineContext context,
                                  HoodieWriteConfig config,
                                  HoodieTable<T, I, K, O> table,
                                  String instantTime,
+                                 String completionTime,
                                  String user,
                                  String comment) {
     super(context, config, table, instantTime);
     this.user = user;
     this.comment = comment;
+    this.completionTime = completionTime;
   }
 
   @Override
@@ -125,18 +128,17 @@ public class SavepointActionExecutor<T, I, K, O> extends BaseActionExecutor<T, I
         }, null);
       }
 
-      try (TransactionManager transactionManager = new TransactionManager(config, false, table.getStorage())) {
-        HoodieSavepointMetadata metadata = TimelineMetadataUtils.convertSavepointMetadata(user, comment, latestFilesMap);
-        // Nothing to save in the savepoint
-        table.getActiveTimeline().createNewInstant(
-            instantGenerator.createNewInstant(HoodieInstant.State.INFLIGHT, HoodieTimeline.SAVEPOINT_ACTION, instantTime));
-        table.getActiveTimeline()
-            .saveAsComplete(instantGenerator.createNewInstant(HoodieInstant.State.INFLIGHT, HoodieTimeline.SAVEPOINT_ACTION, instantTime), Option.of(metadata),
-                transactionManager.generateInstantTime(),
-                savepointCompletedInstant -> table.getMetaClient().getTableFormat().savepoint(savepointCompletedInstant, table.getContext(), table.getMetaClient(), table.getViewManager()));
-        LOG.info("Savepoint {} created", instantTime);
-        return metadata;
-      }
+      HoodieSavepointMetadata metadata = TimelineMetadataUtils.convertSavepointMetadata(user, comment, latestFilesMap);
+      // Nothing to save in the savepoint
+      table.getActiveTimeline().createNewInstant(
+              instantGenerator.createNewInstant(HoodieInstant.State.INFLIGHT, HoodieTimeline.SAVEPOINT_ACTION, instantTime));
+      table.getActiveTimeline().saveAsComplete(instantGenerator.createNewInstant(HoodieInstant.State.INFLIGHT, HoodieTimeline.SAVEPOINT_ACTION, instantTime),
+              Option.of(metadata),
+              completionTime,
+              savepointCompletedInstant -> table.getMetaClient().getTableFormat().savepoint(savepointCompletedInstant, table.getContext(), table.getMetaClient(), table.getViewManager())
+      );
+      LOG.info("Savepoint {} created", instantTime);
+      return metadata;
     } catch (HoodieIOException e) {
       throw new HoodieSavepointException("Failed to savepoint " + instantTime, e);
     }
