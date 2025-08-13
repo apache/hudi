26b727c6430263df1a62a5e716fabb19863f7008
Tien <tienanhnguyen@Tiens-MBP.attlocal.net>
2025-08-13 12:29:00 -0700

    [HUDI-9757]: support TLS authentication for Datahub

--- hudi-sync/hudi-datahub-sync/src/test/java/org/apache/hudi/sync/datahub/config/TestDataHubSyncConfig.java
+++ hudi-sync/hudi-datahub-sync/src/test/java/org/apache/hudi/sync/datahub/config/TestDataHubSyncConfig.java
@@ -22,9 +22,16 @@ package org.apache.hudi.sync.datahub.config;
 import com.linkedin.common.FabricType;
 import com.linkedin.common.urn.DatasetUrn;
 import datahub.client.rest.RestEmitter;
+import org.apache.hudi.sync.datahub.HoodieDataHubSyncException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 import java.util.Properties;
 
 import static org.apache.hudi.sync.common.HoodieSyncConfig.META_SYNC_DATABASE_NAME;
@@ -33,8 +40,11 @@ import static org.apache.hudi.sync.datahub.config.DataHubSyncConfig.META_SYNC_DA
 import static org.apache.hudi.sync.datahub.config.DataHubSyncConfig.META_SYNC_DATAHUB_DATASET_IDENTIFIER_CLASS;
 import static org.apache.hudi.sync.datahub.config.DataHubSyncConfig.META_SYNC_DATAHUB_EMITTER_SUPPLIER_CLASS;
 import static org.apache.hudi.sync.datahub.config.DataHubSyncConfig.META_SYNC_DATAHUB_TABLE_NAME;
+import static org.apache.hudi.sync.datahub.config.DataHubSyncConfig.META_SYNC_DATAHUB_TLS_KEYSTORE_PATH;
+import static org.apache.hudi.sync.datahub.config.DataHubSyncConfig.META_SYNC_DATAHUB_TLS_KEYSTORE_PASSWORD;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TestDataHubSyncConfig {
 
@@ -89,6 +99,84 @@ class TestDataHubSyncConfig {
     assertEquals("foo", datasetUrn.getPlatformEntity().getPlatformNameEntity());
     assertEquals("project.database.table", datasetUrn.getDatasetNameEntity());
     assertEquals(FabricType.PROD, datasetUrn.getOriginEntity());
+  }
+
+  @Test
+  void testGetEmitterWithTlsEnabledSupplier(@TempDir Path tempDir) throws IOException {
+    String testServerUrl = "https://datahub.example.com";
+    String testToken = "test-token-123";
+    Path caCertPath = tempDir.resolve("ca-cert.pem");
+    
+    try (InputStream certStream = getClass().getClassLoader().getResourceAsStream("test-ca-cert.pem")) {
+      Objects.requireNonNull(certStream, "test-ca-cert.pem not found in resources");
+      Files.copy(certStream, caCertPath);
+    }
+    
+    Properties props = new Properties();
+    props.setProperty(META_SYNC_DATAHUB_EMITTER_SUPPLIER_CLASS.key(), TlsEnabledDataHubEmitterSupplier.class.getName());
+    props.setProperty(DataHubSyncConfig.META_SYNC_DATAHUB_EMITTER_SERVER.key(), testServerUrl);
+    props.setProperty(DataHubSyncConfig.META_SYNC_DATAHUB_EMITTER_TOKEN.key(), testToken);
+    props.setProperty(DataHubSyncConfig.META_SYNC_DATAHUB_TLS_CA_CERT_PATH.key(), caCertPath.toString());
+    
+    DataHubSyncConfig syncConfig = new DataHubSyncConfig(props);
+    RestEmitter emitter = syncConfig.getRestEmitter();
+    assertNotNull(emitter);
+  }
+
+  @Test
+  void testGetEmitterWithTlsEnabledSupplierWithoutCert() {
+    String testServerUrl = "https://datahub.example.com";
+    
+    Properties props = new Properties();
+    props.setProperty(META_SYNC_DATAHUB_EMITTER_SUPPLIER_CLASS.key(), TlsEnabledDataHubEmitterSupplier.class.getName());
+    props.setProperty(DataHubSyncConfig.META_SYNC_DATAHUB_EMITTER_SERVER.key(), testServerUrl);
+    
+    DataHubSyncConfig syncConfig = new DataHubSyncConfig(props);
+    RestEmitter emitter = syncConfig.getRestEmitter();
+    assertNotNull(emitter);
+  }
+
+  @Test
+  void testGetEmitterWithTlsEnabledSupplierMissingServer() {
+    Properties props = new Properties();
+    props.setProperty(META_SYNC_DATAHUB_EMITTER_SUPPLIER_CLASS.key(), TlsEnabledDataHubEmitterSupplier.class.getName());
+    
+    DataHubSyncConfig syncConfig = new DataHubSyncConfig(props);
+    assertThrows(HoodieDataHubSyncException.class, syncConfig::getRestEmitter);
+  }
+
+  @Test
+  void testGetEmitterWithTlsEnabledSupplierInvalidCertPath() {
+    String testServerUrl = "https://datahub.example.com";
+    
+    Properties props = new Properties();
+    props.setProperty(META_SYNC_DATAHUB_EMITTER_SUPPLIER_CLASS.key(), TlsEnabledDataHubEmitterSupplier.class.getName());
+    props.setProperty(DataHubSyncConfig.META_SYNC_DATAHUB_EMITTER_SERVER.key(), testServerUrl);
+    props.setProperty(DataHubSyncConfig.META_SYNC_DATAHUB_TLS_CA_CERT_PATH.key(), "/nonexistent/path/cert.pem");
+    
+    DataHubSyncConfig syncConfig = new DataHubSyncConfig(props);
+    assertThrows(HoodieDataHubSyncException.class, syncConfig::getRestEmitter);
+  }
+
+  @Test
+  void testGetEmitterWithTlsEnabledSupplierKeystore(@TempDir Path tempDir) throws IOException {
+    String testServerUrl = "https://datahub.example.com";
+    Path keystorePath = tempDir.resolve("test-keystore.p12");
+    
+    try (InputStream keystoreStream = getClass().getClassLoader().getResourceAsStream("test-keystore.p12")) {
+      Objects.requireNonNull(keystoreStream, "test-keystore.p12 not found in resources");
+      Files.copy(keystoreStream, keystorePath);
+    }
+    
+    Properties props = new Properties();
+    props.setProperty(META_SYNC_DATAHUB_EMITTER_SUPPLIER_CLASS.key(), TlsEnabledDataHubEmitterSupplier.class.getName());
+    props.setProperty(DataHubSyncConfig.META_SYNC_DATAHUB_EMITTER_SERVER.key(), testServerUrl);
+    props.setProperty(META_SYNC_DATAHUB_TLS_KEYSTORE_PATH.key(), keystorePath.toString());
+    props.setProperty(META_SYNC_DATAHUB_TLS_KEYSTORE_PASSWORD.key(), "testpass");
+    
+    DataHubSyncConfig syncConfig = new DataHubSyncConfig(props);
+    RestEmitter emitter = syncConfig.getRestEmitter();
+    assertNotNull(emitter);
   }
 
   public static class DummySupplier implements DataHubEmitterSupplier {
