6e825f90319f49dd86a0bfa81292f44fa889f148
Lokesh Jain <ljain@192.168.0.234>
2025-08-21 00:35:58 +0530

    [HUDI-9731] Add index definition for Record level Index

---
+++ hudi-common/src/main/java/org/apache/hudi/index/record/HoodieRecordIndex.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.hudi.index.record;
+
+import org.apache.hudi.common.model.HoodieIndexDefinition;
+
+/**
+ * Interface representing a record index in Hudi which provides options and other parameters used for record index.
+ */
+public interface HoodieRecordIndex {
+  String IS_PARTITIONED_OPTION = "isPartitioned";
+
+  static boolean isPartitioned(HoodieIndexDefinition indexDefinition) {
+    return Boolean.parseBoolean(indexDefinition.getIndexOptions().getOrDefault(IS_PARTITIONED_OPTION, Boolean.FALSE.toString()));
+  }
+}
