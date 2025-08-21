109b98db469d3fc039423f846fb2b7d1e3c6feca
Xuehai-Chen <xuehai8chen@gmail.com>
2025-08-21 14:07:55 +0800

    [HUDI-9678][TEST][FOLLOWUP] Add a unit test to cover cases that insert table with hive supported SparkSession

---
+++ hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/spark/sql/hudi/hive/dml/TestInsertTableWithHiveSupport.scala
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.spark.sql.hudi.hive.dml
+
+import org.apache.hudi.sync.common.HoodieSyncTool
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.hudi.dml.insert.TestInsertTable
+import org.apache.spark.util.Utils
+
+import java.io.File
+import java.util.Properties
+
+class TestInsertTableWithHiveSupport extends TestInsertTable {
+
+  val metastoreDerbyLocation = "/tmp/hive_metastore_db"
+
+  override lazy val spark: SparkSession = SparkSession.builder()
+    .config("spark.sql.warehouse.dir", sparkWareHouse.getCanonicalPath)
+    .config("spark.sql.session.timeZone", "UTC")
+    .config("hoodie.insert.shuffle.parallelism", "4")
+    .config("hoodie.upsert.shuffle.parallelism", "4")
+    .config("hoodie.delete.shuffle.parallelism", "4")
+    .config("hoodie.datasource.hive_sync.enable", "false")
+    .config("hoodie.datasource.meta.sync.enable", "false")
+    .config("hoodie.meta.sync.client.tool.class", classOf[DummySyncTool].getName)
+    .config("spark.hadoop.javax.jdo.option.ConnectionURL", s"jdbc:derby:$metastoreDerbyLocation;create=true")
+    .config(sparkConf())
+    .enableHiveSupport()
+    .getOrCreate()
+
+  override def afterAll(): Unit = {
+    // Clean up metastore derby location
+    Utils.deleteRecursively(new File(metastoreDerbyLocation))
+    super.afterAll()
+  }
+
+}
+
+class DummySyncTool(props: Properties, hadoopConf: Configuration) extends HoodieSyncTool(props, hadoopConf) {
+  override def syncHoodieTable(): Unit = {
+    // do nothing here
+  }
+}
