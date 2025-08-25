899e30bfd2f76616db8492a4a7db792136956d8b
Jonathan Vexler <=>
2025-08-25 15:49:41 -0400

    fix archive issue and test

--- hudi-hadoop-common/src/test/java/org/apache/hudi/common/table/timeline/TestArchivedTimelineV1.java
+++ hudi-hadoop-common/src/test/java/org/apache/hudi/common/table/timeline/TestArchivedTimelineV1.java
@@ -32,6 +32,7 @@ import org.apache.hudi.common.model.HoodieArchivedLogFile;
 import org.apache.hudi.common.model.HoodieAvroIndexedRecord;
 import org.apache.hudi.common.model.HoodieCleaningPolicy;
 import org.apache.hudi.common.model.HoodieCommitMetadata;
+import org.apache.hudi.common.model.HoodieLogFile;
 import org.apache.hudi.common.model.HoodieRecord;
 import org.apache.hudi.common.model.HoodieRollingStatMetadata;
 import org.apache.hudi.common.model.WriteOperationType;
@@ -47,6 +48,8 @@ import org.apache.hudi.common.table.timeline.versioning.v1.InstantGeneratorV1;
 import org.apache.hudi.common.testutils.HoodieCommonTestHarness;
 import org.apache.hudi.common.util.CompactionUtils;
 import org.apache.hudi.common.util.Option;
+import org.apache.hudi.common.util.collection.ClosableIterator;
+import org.apache.hudi.storage.HoodieStorage;
 import org.apache.hudi.storage.StoragePath;
 import org.apache.hudi.storage.StoragePathFilter;
 
@@ -56,6 +59,8 @@ import org.apache.avro.generic.IndexedRecord;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -73,6 +78,7 @@ import static org.apache.hudi.common.table.timeline.HoodieInstant.State.COMPLETE
 import static org.apache.hudi.common.table.timeline.HoodieInstant.State.INFLIGHT;
 import static org.apache.hudi.common.table.timeline.HoodieInstant.State.REQUESTED;
 import static org.apache.hudi.common.testutils.HoodieTestUtils.getDefaultStorageConf;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -432,6 +438,32 @@ public class TestArchivedTimelineV1 extends HoodieCommonTestHarness {
     HoodieArchivedTimeline timeline = new ArchivedTimelineV1(metaClient);
     timeline.loadCompletedInstantDetailsInMemory();
     assertEquals(3, timeline.getInstants().size());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"hudi_0_13_0_archive", "hudi_0_8_0_archive"})
+  void shouldReadArchivedFileAndValidateContent(String archivePath) {
+    String path = this.getClass().getClassLoader().getResource(archivePath).getPath() + "/.commits_.archive.1_1-0-1";
+    assertDoesNotThrow(() -> readAndValidateArchivedFile(path, metaClient.getStorage()));
+  }
+
+  private void readAndValidateArchivedFile(String path, HoodieStorage storage) throws IOException {
+    try (HoodieLogFormat.Reader reader = HoodieLogFormat.newReader(
+        storage, new HoodieLogFile(path), HoodieArchivedMetaEntry.getClassSchema())) {
+
+      while (reader.hasNext()) {
+        HoodieLogBlock block = reader.next();
+        if (block instanceof HoodieAvroDataBlock) {
+          HoodieAvroDataBlock avroBlock = (HoodieAvroDataBlock) block;
+          try (ClosableIterator<HoodieRecord<IndexedRecord>> itr =
+                   avroBlock.getRecordIterator(HoodieRecord.HoodieRecordType.AVRO)) {
+            if (itr.hasNext()) {
+              itr.next();
+            }
+          }
+        }
+      }
+    }
   }
 
   /**
