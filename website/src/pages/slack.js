cf4c832cf6fe9a9b84240fafcaa43fbc20986ee7
deepakpanda93 <deepakpanda93@gmail.com>
2025-09-15 14:41:40 +0530

    [SITE][DOCS] Used slackUrl constant for Slack redirect

--- website/src/pages/slack.js
+++ website/src/pages/slack.js
@@ -4,7 +4,7 @@ import { slackUrl } from '../../constants';
 
 export default function SlackRedirect() {
   useEffect(() => {
-    window.location.href =  slackUrl; /*"https://join.slack.com/t/apache-hudi/shared_invite/zt-33fabmxb7-Q7QSUtNOHYCwUdYM8LbauA";*/
+    window.location.href =  slackUrl;
   }, []);
   return <p>Redirecting to Slack...</p>;
 }
