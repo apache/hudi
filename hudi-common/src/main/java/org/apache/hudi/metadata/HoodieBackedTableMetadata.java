39ea986f0d036454fff312a8ca935bd54fa38a0a
voonhous <voonhousu@gmail.com>
2025-09-16 21:47:24 +0800

    [HUDI-7219] Add caching support for HFileBlocks (#13724)
    Co-authored-by: Y Ethan Guo <ethan.guoyihua@gmail.com>

--- hudi-common/src/main/java/org/apache/hudi/metadata/HoodieBackedTableMetadata.java
+++ hudi-common/src/main/java/org/apache/hudi/metadata/HoodieBackedTableMetadata.java
@@ -22,7 +22,9 @@ import org.apache.hudi.avro.HoodieAvroReaderContext;
 import org.apache.hudi.avro.HoodieAvroUtils;
 import org.apache.hudi.avro.model.HoodieMetadataRecord;
 import org.apache.hudi.common.config.HoodieCommonConfig;
+import org.apache.hudi.common.config.HoodieConfig;
 import org.apache.hudi.common.config.HoodieMetadataConfig;
+import org.apache.hudi.common.config.HoodieReaderConfig;
 import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.common.data.HoodieData;
 import org.apache.hudi.common.data.HoodieListData;
@@ -86,6 +88,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
@@ -506,14 +509,18 @@ public class HoodieBackedTableMetadata extends BaseTableMetadata {
     // If reuse is enabled and full scan is allowed for the partition, we can reuse the file readers for base files and the reader context for the log files.
     Map<StoragePath, HoodieAvroFileReader> baseFileReaders = Collections.emptyMap();
     ReusableFileGroupRecordBufferLoader<IndexedRecord> recordBufferLoader = null;
-    if (reuse && isFullScanAllowedForPartition(fileSlice.getPartitionPath())) {
+    boolean shouldReuse = reuse && isFullScanAllowedForPartition(fileSlice.getPartitionPath());
+    if (shouldReuse) {
       Pair<HoodieAvroFileReader, ReusableFileGroupRecordBufferLoader<IndexedRecord>> readers =
           reusableFileReaders.computeIfAbsent(fileSlice.getFileGroupId(), fgId -> {
             try {
               HoodieAvroFileReader baseFileReader = null;
               if (fileSlice.getBaseFile().isPresent()) {
+                TypedProperties props = TypedProperties.copy(metadataConfig.getProps());
+                setHFileBlockCacheProps(props);
+                HoodieConfig newConfig = new HoodieConfig(props);
                 baseFileReader = (HoodieAvroFileReader) HoodieIOFactory.getIOFactory(storage).getReaderFactory(HoodieRecord.HoodieRecordType.AVRO)
-                    .getFileReader(metadataConfig, fileSlice.getBaseFile().get().getStoragePath(), metadataMetaClient.getTableConfig().getBaseFileFormat(), Option.empty());
+                    .getFileReader(newConfig, fileSlice.getBaseFile().get().getStoragePath(), metadataMetaClient.getTableConfig().getBaseFileFormat(), Option.empty());
               }
               return Pair.of(baseFileReader, buildReusableRecordBufferLoader(fileSlice, latestMetadataInstantTime, instantRange));
             } catch (IOException ex) {
@@ -542,7 +549,7 @@ public class HoodieBackedTableMetadata extends BaseTableMetadata {
         .withFileSlice(fileSlice)
         .withDataSchema(SCHEMA)
         .withRequestedSchema(SCHEMA)
-        .withProps(buildFileGroupReaderProperties(metadataConfig))
+        .withProps(buildFileGroupReaderProperties(metadataConfig, shouldReuse))
         .withRecordBufferLoader(recordBufferLoader)
         .withEnableOptimizedLogBlockScan(metadataConfig.isOptimizedLogBlocksScanEnabled())
         .build();
@@ -855,7 +862,7 @@ public class HoodieBackedTableMetadata extends BaseTableMetadata {
   /**
    * Derive necessary properties for FG reader.
    */
-  TypedProperties buildFileGroupReaderProperties(HoodieMetadataConfig metadataConfig) {
+  TypedProperties buildFileGroupReaderProperties(HoodieMetadataConfig metadataConfig, boolean shouldReuse) {
     HoodieCommonConfig commonConfig = HoodieCommonConfig.newBuilder()
         .fromProperties(metadataConfig.getProps()).build();
     TypedProperties props = new TypedProperties();
@@ -871,6 +878,21 @@ public class HoodieBackedTableMetadata extends BaseTableMetadata {
     props.setProperty(
         DISK_MAP_BITCASK_COMPRESSION_ENABLED.key(),
         Boolean.toString(commonConfig.isBitCaskDiskMapCompressionEnabled()));
+    if (shouldReuse) {
+      setHFileBlockCacheProps(props);
+    } else {
+      props.setProperty(HoodieReaderConfig.HFILE_BLOCK_CACHE_ENABLED.key(),
+          metadataConfig.getStringOrDefault(HoodieReaderConfig.HFILE_BLOCK_CACHE_ENABLED));
+    }
+    props.setProperty(HoodieReaderConfig.HFILE_BLOCK_CACHE_SIZE.key(),
+        metadataConfig.getStringOrDefault(HoodieReaderConfig.HFILE_BLOCK_CACHE_SIZE));
+    props.setProperty(HoodieReaderConfig.HFILE_BLOCK_CACHE_TTL_MINUTES.key(),
+        metadataConfig.getStringOrDefault(HoodieReaderConfig.HFILE_BLOCK_CACHE_TTL_MINUTES));
     return props;
+  }
+
+  private void setHFileBlockCacheProps(Properties props) {
+    // Enable HFile block caching for resue and full scan usage
+    props.setProperty(HoodieReaderConfig.HFILE_BLOCK_CACHE_ENABLED.key(), "true");
   }
 }
