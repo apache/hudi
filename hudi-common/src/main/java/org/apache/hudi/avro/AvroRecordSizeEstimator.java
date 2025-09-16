8dac723c778bb51ec90db80251b2b95b5ef20a49
sivabalan <n.siva.b@gmail.com>
2025-09-15 17:03:30 -0700

    Fixing record size estimator for avro record

--- hudi-common/src/main/java/org/apache/hudi/avro/AvroRecordSizeEstimator.java
+++ hudi-common/src/main/java/org/apache/hudi/avro/AvroRecordSizeEstimator.java
@@ -18,6 +18,7 @@
 
 package org.apache.hudi.avro;
 
+import org.apache.hudi.common.model.HoodieRecord;
 import org.apache.hudi.common.table.read.BufferedRecord;
 import org.apache.hudi.common.util.ObjectSizeCalculator;
 import org.apache.hudi.common.util.SizeEstimator;
@@ -26,15 +27,49 @@ import org.apache.avro.Schema;
 import org.apache.avro.generic.IndexedRecord;
 import org.apache.avro.specific.SpecificRecord;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * An implementation of {@link SizeEstimator} for Avro {@link BufferedRecord}, which estimates the size of
  * Avro record excluding the internal {@link Schema}.
  */
 public class AvroRecordSizeEstimator implements SizeEstimator<BufferedRecord<IndexedRecord>> {
   private final long sizeOfSchema;
+  private final long sizeOfSchemaWithoutMeta;
 
   public AvroRecordSizeEstimator(Schema recordSchema) {
     sizeOfSchema = ObjectSizeCalculator.getObjectSize(recordSchema);
+    if (recordSchema.getFields().get(0).name().equals(HoodieRecord.COMMIT_TIME_METADATA_FIELD)) {
+      sizeOfSchemaWithoutMeta = ObjectSizeCalculator.getObjectSize(schemaWithoutMeta(recordSchema));
+    } else {
+      sizeOfSchemaWithoutMeta = sizeOfSchema;
+    }
+  }
+
+  private Schema schemaWithoutMeta(Schema originalSchema) {
+    List<Schema.Field> originalFields = originalSchema.getFields();
+    List<Schema.Field> remainingFields = new ArrayList<>();
+    int startIndex = originalSchema.getFields().get(5).name().equals(HoodieRecord.OPERATION_METADATA_FIELD) ? 6 : 5;
+    for (int i = startIndex; i < originalFields.size(); i++) {
+      Schema.Field originalField = originalFields.get(i);
+      Schema.Field copiedField = new Schema.Field(
+          originalField.name(),
+          originalField.schema(),
+          originalField.doc(),
+          originalField.defaultVal(),
+          originalField.order()
+      );
+      remainingFields.add(copiedField);
+    }
+    Schema newSchema = Schema.createRecord(
+        originalSchema.getName() + "_Subset",
+        originalSchema.getDoc(),
+        originalSchema.getNamespace(),
+        false
+    );
+    newSchema.setFields(remainingFields);
+    return newSchema;
   }
 
   @Override
@@ -45,6 +80,11 @@ public class AvroRecordSizeEstimator implements SizeEstimator<BufferedRecord<Ind
       return sizeOfRecord;
     }
     // do not contain size of Avro schema as the schema is reused among records
-    return sizeOfRecord - sizeOfSchema + 8;
+    long toReturn =  sizeOfRecord - sizeOfSchema + 8;
+    if (toReturn < 0) {
+       return sizeOfRecord - sizeOfSchemaWithoutMeta + 8;
+    } else {
+      return toReturn;
+    }
   }
 }
