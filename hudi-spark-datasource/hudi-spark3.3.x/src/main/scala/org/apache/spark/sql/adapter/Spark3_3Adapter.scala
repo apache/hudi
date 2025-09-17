dad350c1b08167044947c85e414b2e51d4201555
Y Ethan Guo <ethan.guoyihua@gmail.com>
2025-09-16 23:30:25 -0700

    Fix adapter class for Spark 3.3

--- hudi-spark-datasource/hudi-spark3.3.x/src/main/scala/org/apache/spark/sql/adapter/Spark3_3Adapter.scala
+++ hudi-spark-datasource/hudi-spark3.3.x/src/main/scala/org/apache/spark/sql/adapter/Spark3_3Adapter.scala
@@ -168,6 +168,8 @@ class Spark3_3Adapter extends BaseSpark3Adapter {
     value == LegacyBehaviorPolicy.LEGACY
   }
 
+  override def isTimestampNTZType(dataType: DataType): Boolean = false
+
   override def getSchema(conn: Connection,
                          resultSet: ResultSet,
                          dialect: JdbcDialect,
