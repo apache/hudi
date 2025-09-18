681833e32696bc5181a8f1a0b10064d562b230b7
Jonathan Vexler <=>
2025-09-18 16:50:20 -0400

    restore validate script

--- packaging/bundle-validation/validate.sh
+++ packaging/bundle-validation/validate.sh
@@ -102,8 +102,7 @@ test_spark_hadoop_mr_bundles () {
     hiveqlresultsdir=/tmp/hadoop-mr-bundle/hiveql/trips/results
     mkdir -p $hiveqlresultsdir
     $HIVE_HOME/bin/beeline --hiveconf hive.input.format=org.apache.hudi.hadoop.HoodieParquetInputFormat \
-      --hiveconf hive.root.logger=DEBUG,console \
-      -u jdbc:hive2://localhost:10000/default --verbose=true --showHeader=false --outputformat=csv2 \
+      -u jdbc:hive2://localhost:10000/default --showHeader=false --outputformat=csv2 \
       -e 'select * from trips' >> $hiveqlresultsdir/results.csv
     numRecordsHiveQL=$(cat $hiveqlresultsdir/*.csv | wc -l)
     if [ "$numRecordsHiveQL" -ne 10 ]; then
