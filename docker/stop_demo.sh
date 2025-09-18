e99466488e330f9060be5daf758fbb05e25f0e1b
Jonathan Vexler <=>
2025-09-18 12:42:44 -0400

    update docker compose file name

--- docker/stop_demo.sh
+++ docker/stop_demo.sh
@@ -22,7 +22,7 @@ HUDI_DEMO_ENV=$1
 WS_ROOT=`dirname $SCRIPT_PATH`
 COMPOSE_FILE_NAME="docker-compose_hadoop284_hive233_spark353_amd64.yml"
 if [ "$HUDI_DEMO_ENV" = "--mac-aarch64" ]; then
-  COMPOSE_FILE_NAME="docker-compose_hadoop284_hive233_spark244_mac_aarch64.yml"
+  COMPOSE_FILE_NAME="docker-compose_hadoop284_hive233_spark353_arm64.yml"
 fi
 # shut down cluster
 HUDI_WS=${WS_ROOT} docker compose -f ${SCRIPT_PATH}/compose/${COMPOSE_FILE_NAME} down
