6b1fe2e6b7474ba4c82158604f2bcf481cdfa573
Jonathan Vexler <=>
2025-09-18 11:40:00 -0400

    another try to get more debugging

--- packaging/bundle-validation/validate.sh
+++ packaging/bundle-validation/validate.sh
@@ -102,12 +102,12 @@ test_spark_hadoop_mr_bundles () {
     hiveqlresultsdir=/tmp/hadoop-mr-bundle/hiveql/trips/results
     mkdir -p $hiveqlresultsdir
     $HIVE_HOME/bin/beeline --hiveconf hive.input.format=org.apache.hudi.hadoop.HoodieParquetInputFormat \
+      --hiveconf hive.root.logger=DEBUG,console \
       -u jdbc:hive2://localhost:10000/default --verbose=true --showHeader=false --outputformat=csv2 \
       -e 'select * from trips' >> $hiveqlresultsdir/results.csv
     numRecordsHiveQL=$(cat $hiveqlresultsdir/*.csv | wc -l)
     if [ "$numRecordsHiveQL" -ne 10 ]; then
         echo "::error::validate.sh HiveQL validation failed."
-        cat $HIVE_HOME/logs/hiveserver2.log
         exit 1
     fi
     echo "::warning::validate.sh spark & hadoop-mr bundles validation was successful."
