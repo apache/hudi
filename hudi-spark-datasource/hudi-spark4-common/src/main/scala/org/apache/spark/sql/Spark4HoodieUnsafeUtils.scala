d58556f80c15f5f9d5bf5db33597b40d5c851bd3
Y Ethan Guo <ethan.guoyihua@gmail.com>
2025-09-19 00:24:59 -0700

    Fix build

--- hudi-spark-datasource/hudi-spark4-common/src/main/scala/org/apache/spark/sql/Spark4HoodieUnsafeUtils.scala
+++ hudi-spark-datasource/hudi-spark4-common/src/main/scala/org/apache/spark/sql/Spark4HoodieUnsafeUtils.scala
@@ -28,8 +28,6 @@ import org.apache.spark.sql.execution.LogicalRDD
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.util.MutablePair
 
-import scala.collection.JavaConverters
-
 /**
  * Suite of utilities helping in handling instances of [[HoodieUnsafeRDD]]
  */
