79d87488dc8114a790b694e2f88e4254abaad224
sivabalan <n.siva.b@gmail.com>
2025-09-22 14:30:52 -0700

    Fixing ordering value wiring for HoodieRecord

--- hudi-common/src/main/java/org/apache/hudi/common/table/read/buffer/FileGroupRecordBuffer.java
+++ hudi-common/src/main/java/org/apache/hudi/common/table/read/buffer/FileGroupRecordBuffer.java
@@ -24,6 +24,7 @@ import org.apache.hudi.common.config.RecordMergeMode;
 import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.common.engine.HoodieReaderContext;
 import org.apache.hudi.common.model.HoodieRecordMerger;
+import org.apache.hudi.common.serialization.DefaultSerializer;
 import org.apache.hudi.common.table.HoodieTableMetaClient;
 import org.apache.hudi.common.table.PartialUpdateMode;
 import org.apache.hudi.common.table.log.KeySpec;
@@ -68,6 +69,7 @@ import static org.apache.hudi.common.table.log.block.HoodieLogBlock.HeaderMetada
 abstract class FileGroupRecordBuffer<T> implements HoodieFileGroupRecordBuffer<T> {
   protected final HoodieReaderContext<T> readerContext;
   protected final Schema readerSchema;
+  protected final Schema incomingRecordsSchema;
   protected final List<String> orderingFieldNames;
   protected final RecordMergeMode recordMergeMode;
   protected final Option<PartialUpdateMode> partialUpdateModeOpt;
@@ -96,10 +98,12 @@ abstract class FileGroupRecordBuffer<T> implements HoodieFileGroupRecordBuffer<T
                                   Option<PartialUpdateMode> partialUpdateModeOpt,
                                   TypedProperties props,
                                   List<String> orderingFieldNames,
-                                  UpdateProcessor<T> updateProcessor) {
+                                  UpdateProcessor<T> updateProcessor,
+                                  Option<Schema> incomingRecordsSchemaOpt) {
     this.readerContext = readerContext;
     this.updateProcessor = updateProcessor;
     this.readerSchema = AvroSchemaCache.intern(readerContext.getSchemaHandler().getRequiredSchema());
+    this.incomingRecordsSchema = incomingRecordsSchemaOpt.orElseGet(() -> readerSchema);
     this.recordMergeMode = recordMergeMode;
     this.partialUpdateModeOpt = partialUpdateModeOpt;
     this.recordMerger = readerContext.getRecordMerger();
@@ -129,7 +133,7 @@ abstract class FileGroupRecordBuffer<T> implements HoodieFileGroupRecordBuffer<T
     boolean isBitCaskDiskMapCompressionEnabled = props.getBoolean(DISK_MAP_BITCASK_COMPRESSION_ENABLED.key(),
         DISK_MAP_BITCASK_COMPRESSION_ENABLED.defaultValue());
     return new ExternalSpillableMap<>(maxMemorySizeInBytes, spillableMapBasePath, new DefaultSizeEstimator<>(),
-        readerContext.getRecordSizeEstimator(), diskMapType, readerContext.getRecordSerializer(), isBitCaskDiskMapCompressionEnabled, getClass().getSimpleName());
+        new DefaultSizeEstimator<>(), diskMapType, new DefaultSerializer<>(), isBitCaskDiskMapCompressionEnabled, getClass().getSimpleName());
   }
 
   @Override
