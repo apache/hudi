4b13dfc9ae9e9a357a4631d13c79db0ebcfd242f
Danny Chan <yuzhao.cyz@gmail.com>
2025-09-25 16:43:30 +0800

    test: optimize the debug loggings for Flink local test (#13991)

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/client/transaction/lock/LockManager.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/client/transaction/lock/LockManager.java
@@ -128,9 +128,8 @@ public class LockManager implements Serializable, AutoCloseable {
       if (ReflectionUtils.hasConstructor(writeConfig.getLockProviderClass(), metricsConstructorTypes)) {
         lockProvider = (LockProvider) ReflectionUtils.loadClass(writeConfig.getLockProviderClass(),
             metricsConstructorTypes, lockConfiguration, hadoopConf.get(), metrics);
-        LOG.debug("Successfully loaded LockProvider with HoodieLockMetrics support");
       } else {
-        LOG.debug("LockProvider does not support HoodieLockMetrics constructor, falling back to standard constructor");
+        LOG.debug("LockProvider does not support HoodieLockMetrics param in constructor, falling back to standard constructor");
         // Fallback to original constructor without metrics
         lockProvider = (LockProvider) ReflectionUtils.loadClass(writeConfig.getLockProviderClass(),
                 new Class<?>[] {LockConfiguration.class, Configuration.class},
