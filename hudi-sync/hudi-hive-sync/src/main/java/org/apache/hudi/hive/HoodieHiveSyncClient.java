4f7f007c038e33dcd06e934acc5427a6a19cf519
Aditya Goenka <ad1happy2go@gmail.com>
2025-10-07 11:55:13 +0530

    Support out of order schema evolution fix for Hudi 0.14.1

--- hudi-sync/hudi-hive-sync/src/main/java/org/apache/hudi/hive/HoodieHiveSyncClient.java
+++ hudi-sync/hudi-hive-sync/src/main/java/org/apache/hudi/hive/HoodieHiveSyncClient.java
@@ -34,6 +34,7 @@ import org.apache.hudi.hive.ddl.HiveQueryDDLExecutor;
 import org.apache.hudi.hive.ddl.HiveSyncMode;
 import org.apache.hudi.hive.ddl.JDBCExecutor;
 import org.apache.hudi.hive.util.IMetaStoreClientUtil;
+import org.apache.hudi.hive.util.PartitionFilterGenerator;
 import org.apache.hudi.sync.common.HoodieSyncClient;
 import org.apache.hudi.sync.common.model.FieldSchema;
 import org.apache.hudi.sync.common.model.Partition;
@@ -225,6 +226,40 @@ public class HoodieHiveSyncClient extends HoodieSyncClient {
     } catch (TException e) {
       throw new HoodieHiveSyncException("Failed to get partitions for table "
           + tableId(databaseName, tableName) + " with filter " + filter, e);
+    }
+  }
+
+  @Override
+  public String generatePushDownFilter(List<String> writtenPartitions, List<FieldSchema> partitionFields) {
+    return new PartitionFilterGenerator().generatePushDownFilter(writtenPartitions, partitionFields, config);
+  }
+
+  public void createOrReplaceTable(String tableName,
+                                   MessageType storageSchema,
+                                   String inputFormatClass,
+                                   String outputFormatClass,
+                                   String serdeClass,
+                                   Map<String, String> serdeProperties,
+                                   Map<String, String> tableProperties) {
+
+    if (!tableExists(tableName)) {
+      createTable(tableName, storageSchema, inputFormatClass, outputFormatClass, serdeClass, serdeProperties, tableProperties);
+      return;
+    }
+    try {
+      // create temp table
+      String tempTableName = generateTempTableName(tableName);
+      createTable(tempTableName, storageSchema, inputFormatClass, outputFormatClass, serdeClass, serdeProperties, tableProperties);
+
+      // if create table is successful, drop the actual table
+      // and rename temp table to actual table
+      dropTable(tableName);
+
+      Table table = client.getTable(databaseName, tempTableName);
+      table.setTableName(tableName);
+      client.alter_table(databaseName, tempTableName, table);
+    } catch (Exception ex) {
+      throw new HoodieHiveSyncException("failed to create table " + tableId(databaseName, tableName), ex);
     }
   }
 
@@ -428,5 +463,15 @@ public class HoodieHiveSyncClient extends HoodieSyncClient {
   @VisibleForTesting
   StorageDescriptor getMetastoreStorageDescriptor(String tableName) throws TException {
     return client.getTable(databaseName, tableName).getSd();
+  }
+
+  @Override
+  public void dropTable(String tableName) {
+    try {
+      client.dropTable(databaseName, tableName);
+      LOG.info("Successfully deleted table in Hive: {}.{}", databaseName, tableName);
+    } catch (Exception e) {
+      throw new HoodieHiveSyncException("Failed to delete the table " + tableId(databaseName, tableName), e);
+    }
   }
 }
