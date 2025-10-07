effb0553ec4f466226b269c85728f235bd12010a
Aditya Goenka <ad1happy2go@gmail.com>
2025-10-07 11:55:13 +0530

    Support out of order schema evolution fix for Hudi 0.14.1

--- hudi-aws/src/main/java/org/apache/hudi/config/GlueCatalogSyncClientConfig.java
+++ hudi-aws/src/main/java/org/apache/hudi/config/GlueCatalogSyncClientConfig.java
@@ -89,4 +89,12 @@ public class GlueCatalogSyncClientConfig extends HoodieConfig {
       .withDocumentation(String.join(" ", "Specify the partitions fields to index on aws glue. Separate the fields by semicolon.",
           "By default, when the feature is enabled, all the partition will be indexed.",
           "You can create up to three indexes, separate them by comma. Eg: col1;col2;col3,col2,col3"));
+
+  public static final ConfigProperty<Boolean> RECREATE_GLUE_TABLE_ON_ERROR = ConfigProperty
+      .key(GLUE_CLIENT_PROPERTY_PREFIX + "recreate_table_on_error")
+      .defaultValue(false)
+      .sinceVersion("0.14.0")
+      .markAdvanced()
+      .withDocumentation("Glue sync may fail if the Glue table exists with partitions differing from the Hoodie table or if schema evolution is not supported by Glue."
+          + "Enabling this configuration will drop and create the table to match the Hoodie config");
 }
