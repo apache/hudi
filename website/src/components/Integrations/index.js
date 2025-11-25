08b5bba4a521ed32a827f46d8a911ad39bd10e18
Shiyan Xu <2701446+xushiyan@users.noreply.github.com>
2025-11-24 21:42:55 -0600

    docs(site): update homepage integration list (#14345)

--- website/src/components/Integrations/index.js
+++ website/src/components/Integrations/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import {
-  batchAnalytics,
-  biAnalytics,
-  cloudStorage,
-  databasesData,
-  dataStream,
-  interactiveAnalytics,
-   lakehousePlateformData,
-  metastoreData, orchestrationData, streamAnalytics
+  dataProcessingItems,
+  rawDataItems,
+  fileFormatItems,
+  tableFormatItems,
+  databaseItems,
+  streamingItems,
+  datawarehouseItems,
+  interactiveAnalyticsItems,
+  lakeStorageItems,
+  dataCatalogItems, 
+  orchestrationItems, 
+  cdcItems,
+  streamingProcessingItems
 } from "./data";
 import Services from "./Services";
 import styles from "./styles.module.css";
@@ -21,33 +26,31 @@ return(
       <div className={styles.integrationContainer}>
         <div className={styles.serviceGroup}>
           <div className={styles.serviceLeft}>
-            <Services name={"Data Stream"} serviceData={dataStream}/>
-            <Services name={"Databases"} serviceData={databasesData}/>
-          </div>
-          <div className={styles.serviceLeft}>
-            <Services name={"Cloud Storage"} serviceData={cloudStorage}/>
-          </div>
-          </div>
-        <div className={styles.serviceGroup}>
-          <div className={styles.serviceLeft}>
-            <Services name={"Lakehouse Platform"} serviceData={lakehousePlateformData}/>
+            <Services name={"Data Streaming"} serviceData={streamingItems}/>
+            <Services name={"Databases"} serviceData={databaseItems}/>
+            <Services name={"CDC"} serviceData={cdcItems}/>
+            <Services name={"Raw Data"} serviceData={rawDataItems}/>
           </div>
           </div>
         <div className={styles.serviceGroup}>
           <div className={styles.serviceLeft}>
-            <Services name={"Metastore"} serviceData={metastoreData}/>
+            <Services name={"Lake Storage"} serviceData={lakeStorageItems}/>
+          </div>
+          </div>
+        <div className={styles.serviceGroup}>
+          <div className={styles.serviceLeft}>
+            <Services name={"Data Catalogs"} serviceData={dataCatalogItems}/>
           </div>
           </div>
 
           <div className={styles.serviceGroup}>
             <div className={styles.serviceLeft}>
-              <Services name={"BI Analytics"} serviceData={biAnalytics}/>
-              <Services name={"Interactive Analytics"} serviceData={interactiveAnalytics}/>
-              <Services name={"Batch Analyitics"} serviceData={batchAnalytics}/>
+              <Services name={"Data Warehouses"} serviceData={datawarehouseItems}/>
+              <Services name={"Interactive Analytics"} serviceData={interactiveAnalyticsItems}/>
             </div>
             <div className={styles.serviceLeft}>
-              <Services name={"Stream Analytics"} serviceData={streamAnalytics}/>
-              <Services name={"Orchestration"} serviceData={orchestrationData}/>
+              <Services name={"Data Processing"} serviceData={dataProcessingItems}/>
+              <Services name={"Orchestration"} serviceData={orchestrationItems}/>
             </div>
           </div>
         </div>
