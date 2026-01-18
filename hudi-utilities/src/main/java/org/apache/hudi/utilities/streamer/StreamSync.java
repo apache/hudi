c5f52e592ab9a9fce37947d7f8754f13138c2d8d
syalla <syalla@uber.com>
2026-01-18 23:27:35 +0000

    Fix checkstyle issues

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/streamer/StreamSync.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/streamer/StreamSync.java
@@ -136,7 +136,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
