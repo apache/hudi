bf4ec8d9f28ea71bf33410262c8ef31e61aabfd6
Zhenqiu Huang <huangzhenqiu0825@gmail.com>
2026-01-21 22:09:52 -0800

    feat: add basic hoodie source

--- hudi-flink-datasource/hudi-flink/src/test/java/org/apache/hudi/source/enumerator/TestHoodieContinuousSplitEnumerator.java
+++ hudi-flink-datasource/hudi-flink/src/test/java/org/apache/hudi/source/enumerator/TestHoodieContinuousSplitEnumerator.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -402,7 +402,7 @@ public class TestHoodieContinuousSplitEnumerator {
         org.apache.flink.table.types.logical.RowType rowType,
         String startInstant,
         long maxPendingSplits) {
-      super(conf, path, rowType, startInstant, "",0L, maxPendingSplits, false, false, false, false);
+      super(conf, path, rowType, startInstant, "",0L, maxPendingSplits, false, false, false, false, false);
     }
 
     public static Builder builder() {
