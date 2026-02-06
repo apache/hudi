fe05f59bd6f3f6c9de98f02b0d9653d2f0d68ef3
Vinish Reddy <vinishreddygunner17@gmail.com>
2026-02-06 14:08:55 -0800

    Address comments

--- hudi-client/hudi-client-common/src/test/java/org/apache/hudi/client/transaction/TestSimpleConcurrentFileWritesConflictResolutionStrategy.java
+++ hudi-client/hudi-client-common/src/test/java/org/apache/hudi/client/transaction/TestSimpleConcurrentFileWritesConflictResolutionStrategy.java
@@ -517,7 +517,7 @@ public class TestSimpleConcurrentFileWritesConflictResolutionStrategy extends Ho
 
   @Test
   public void testErrorMessageForConflictWithCompaction() throws Exception {
-    initMetaClient();
+    initMetaClient(true, HoodieTableType.MERGE_ON_READ);
     createCommit(WriteClientTestUtils.createNewInstantTime(), metaClient);
     HoodieActiveTimeline timeline = metaClient.getActiveTimeline();
     Option<HoodieInstant> lastSuccessfulInstant = timeline.getCommitsTimeline().filterCompletedInstants().lastInstant();
