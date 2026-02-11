4a83cfb1e0c4c3bb957632557a957e46d45aec16
nada-attia <nada.attia@uber.com>
2026-02-11 16:48:05 -0500

    Add config to guard TOUCH partition events during meta sync

--- hudi-sync/hudi-sync-common/src/main/java/org/apache/hudi/sync/common/HoodieSyncClient.java
+++ hudi-sync/hudi-sync-common/src/main/java/org/apache/hudi/sync/common/HoodieSyncClient.java
@@ -48,10 +48,10 @@ import java.util.Objects;
 import java.util.Set;
 
 import static org.apache.hudi.sync.common.HoodieSyncConfig.META_SYNC_BASE_PATH;
-import static org.apache.hudi.sync.common.HoodieSyncConfig.META_SYNC_CONDITIONAL_SYNC;
 import static org.apache.hudi.sync.common.HoodieSyncConfig.META_SYNC_DATABASE_NAME;
 import static org.apache.hudi.sync.common.HoodieSyncConfig.META_SYNC_PARTITION_EXTRACTOR_CLASS;
 import static org.apache.hudi.sync.common.HoodieSyncConfig.META_SYNC_TABLE_NAME;
+import static org.apache.hudi.sync.common.HoodieSyncConfig.META_SYNC_TOUCH_PARTITIONS_ENABLED;
 import static org.apache.hudi.sync.common.HoodieSyncConfig.META_SYNC_USE_FILE_LISTING_FROM_METADATA;
 
 @Slf4j
@@ -243,8 +243,8 @@ public abstract class HoodieSyncClient implements HoodieMetaSyncOperations, Auto
             events.add(PartitionEvent.newPartitionAddEvent(storagePartition));
           } else if (!paths.get(storageValue).equals(fullStoragePartitionPath)) {
             events.add(PartitionEvent.newPartitionUpdateEvent(storagePartition));
-          } else if (config.getBoolean(META_SYNC_CONDITIONAL_SYNC)) {
-            // Only produce TOUCH events when conditional sync is enabled
+          } else if (config.getBoolean(META_SYNC_TOUCH_PARTITIONS_ENABLED)) {
+            // Only produce TOUCH events when touch partitions is enabled
             events.add(PartitionEvent.newPartitionTouchEvent(storagePartition));
           }
         }
