03235b00010425418d063128badd73bb96a531cf
nada-attia <nada.attia@uber.com>
2026-04-01 11:15:15 -0400

    Refactor TestLogReaderUtils to use loops instead of copy-paste

--- hudi-client/hudi-spark-client/src/test/java/org/apache/hudi/common/table/log/TestLogReaderUtils.java
+++ hudi-client/hudi-spark-client/src/test/java/org/apache/hudi/common/table/log/TestLogReaderUtils.java
@@ -22,14 +22,13 @@ package org.apache.hudi.common.table.log;
 import org.apache.hudi.client.SparkRDDWriteClient;
 import org.apache.hudi.client.WriteStatus;
 import org.apache.hudi.client.timeline.versioning.v2.TimelineArchiverV2;
+import org.apache.hudi.common.config.HoodieMetadataConfig;
 import org.apache.hudi.common.fs.FSUtils;
 import org.apache.hudi.common.model.HoodieLogFile;
-import org.apache.hudi.common.model.HoodieRecord;
 import org.apache.hudi.common.model.HoodieTableType;
 import org.apache.hudi.common.table.HoodieTableMetaClient;
 import org.apache.hudi.common.table.view.HoodieTableFileSystemView;
 import org.apache.hudi.common.testutils.HoodieTestDataGenerator;
-import org.apache.hudi.common.config.HoodieMetadataConfig;
 import org.apache.hudi.config.HoodieArchivalConfig;
 import org.apache.hudi.config.HoodieCleanConfig;
 import org.apache.hudi.config.HoodieCompactionConfig;
@@ -79,85 +78,54 @@ public class TestLogReaderUtils extends SparkClientFunctionalTestHarness {
       // First commit - insert data
       String firstCommit = "001";
       WriteClientTestUtils.startCommitWithTime(client, firstCommit);
-      List<HoodieRecord> records1 = dataGen.generateInserts(firstCommit, 100);
-      JavaRDD<HoodieRecord> writeRecords1 = jsc().parallelize(records1, 1);
-      JavaRDD<WriteStatus> statusesRdd1 = client.insert(writeRecords1, firstCommit);
-      List<WriteStatus> statuses1 = statusesRdd1.collect();
-      assertNoWriteErrors(statuses1);
-      client.commit(firstCommit, statusesRdd1);
+      JavaRDD<WriteStatus> insertRdd = client.insert(jsc().parallelize(dataGen.generateInserts(firstCommit, 100), 1), firstCommit);
+      assertNoWriteErrors(insertRdd.collect());
+      client.commit(firstCommit, insertRdd);
 
-      // Second commit - update data to create log files
-      String secondCommit = "002";
-      WriteClientTestUtils.startCommitWithTime(client, secondCommit);
-      List<HoodieRecord> records2 = dataGen.generateUpdates(secondCommit, 50);
-      JavaRDD<HoodieRecord> writeRecords2 = jsc().parallelize(records2, 1);
-      JavaRDD<WriteStatus> statusesRdd2 = client.upsert(writeRecords2, secondCommit);
-      List<WriteStatus> statuses2 = statusesRdd2.collect();
-      assertNoWriteErrors(statuses2);
-      assertLogFilesProduced(statuses2);
-      client.commit(secondCommit, statusesRdd2);
+      // Second and third commits - upserts to create log files
+      String[] upsertCommits = {"002", "003"};
+      String secondCommit = upsertCommits[0];
+      String thirdCommit = upsertCommits[1];
+      for (String commitTime : upsertCommits) {
+        WriteClientTestUtils.startCommitWithTime(client, commitTime);
+        JavaRDD<WriteStatus> upsertRdd = client.upsert(jsc().parallelize(dataGen.generateUpdates(commitTime, 50), 1), commitTime);
+        List<WriteStatus> statuses = upsertRdd.collect();
+        assertNoWriteErrors(statuses);
+        assertLogFilesProduced(statuses);
+        client.commit(commitTime, upsertRdd);
+      }
 
-      // Third commit - more updates
-      String thirdCommit = "003";
-      WriteClientTestUtils.startCommitWithTime(client, thirdCommit);
-      List<HoodieRecord> records3 = dataGen.generateUpdates(thirdCommit, 30);
-      JavaRDD<HoodieRecord> writeRecords3 = jsc().parallelize(records3, 1);
-      JavaRDD<WriteStatus> statusesRdd3 = client.upsert(writeRecords3, thirdCommit);
-      List<WriteStatus> statuses3 = statusesRdd3.collect();
-      assertNoWriteErrors(statuses3);
-      assertLogFilesProduced(statuses3);
-      client.commit(thirdCommit, statusesRdd3);
-
-      // Reload metaClient to get latest timeline
       metaClient = HoodieTableMetaClient.reload(metaClient);
 
-      // Get file system view
       HoodieTableFileSystemView fsView = HoodieTableFileSystemView.fileListingBasedFileSystemView(
           context(),
           metaClient,
           metaClient.getActiveTimeline().filterCompletedInstants());
 
-      // Get all partitions
       List<String> partitions = Arrays.asList(dataGen.getPartitionPaths());
 
       // Test: Get all log files up to the second commit
       Map<HoodieLogFile, List<String>> logFilesWithMaxCommit = LogReaderUtils.getAllLogFilesWithMaxCommit(
-          metaClient,
-          fsView,
-          partitions,
-          secondCommit,
-          context());
+          metaClient, fsView, partitions, secondCommit, context());
 
-      // Verify results
       assertNotNull(logFilesWithMaxCommit);
       assertFalse(logFilesWithMaxCommit.isEmpty());
 
-      // For a MOR table with updates, we should have log files
-      // The method should filter out log files created after secondCommit
       for (Map.Entry<HoodieLogFile, List<String>> entry : logFilesWithMaxCommit.entrySet()) {
-        HoodieLogFile logFile = entry.getKey();
+        assertNotNull(entry.getKey());
         List<String> commitTimes = entry.getValue();
-
-        assertNotNull(logFile);
         assertFalse(commitTimes.isEmpty(), "Each log file should have at least one associated commit time");
-
-        // Verify all commit times are <= secondCommit
         for (String commitTime : commitTimes) {
           assertTrue(commitTime.compareTo(thirdCommit) < 0,
               "Commit time should be before third commit: " + commitTime);
         }
       }
 
-      // Test with third commit - should include more log files
+      // Test with third commit - should include same or more log files
       Map<HoodieLogFile, List<String>> allLogFiles = LogReaderUtils.getAllLogFilesWithMaxCommit(
-          metaClient,
-          fsView,
-          partitions,
-          thirdCommit,
-          context());
+          metaClient, fsView, partitions, thirdCommit, context());
 
       assertNotNull(allLogFiles);
-      // Should have same or more entries when including more commits
       assertTrue(allLogFiles.size() >= logFilesWithMaxCommit.size(),
           "Should have same or more log files when including third commit");
     }
@@ -172,13 +140,8 @@ public class TestLogReaderUtils extends SparkClientFunctionalTestHarness {
         metaClient,
         metaClient.getActiveTimeline().filterCompletedInstants());
 
-    // Test with empty partitions list
     Map<HoodieLogFile, List<String>> result = LogReaderUtils.getAllLogFilesWithMaxCommit(
-        metaClient,
-        fsView,
-        Arrays.asList(),
-        "000",
-        context());
+        metaClient, fsView, Arrays.asList(), "000", context());
 
     assertNotNull(result);
     assertTrue(result.isEmpty(), "Should return empty map for empty partitions");
@@ -188,7 +151,6 @@ public class TestLogReaderUtils extends SparkClientFunctionalTestHarness {
   public void testGetAllLogFilesWithArchivedCommit() throws Exception {
     HoodieTableMetaClient metaClient = getHoodieMetaClient(HoodieTableType.MERGE_ON_READ, new Properties());
 
-    // Configure archival to archive after 4 commits, keeping minimum 2
     HoodieWriteConfig config = HoodieWriteConfig.newBuilder()
         .withPath(basePath())
         .withSchema(TRIP_EXAMPLE_SCHEMA)
@@ -220,63 +182,39 @@ public class TestLogReaderUtils extends SparkClientFunctionalTestHarness {
       // First commit - insert data (creates base files)
       String firstCommit = "001";
       WriteClientTestUtils.startCommitWithTime(client, firstCommit);
-      List<HoodieRecord> records1 = dataGen.generateInserts(firstCommit, 100);
-      JavaRDD<HoodieRecord> writeRecords1 = jsc().parallelize(records1, 1);
-      JavaRDD<WriteStatus> statusesRdd1 = client.insert(writeRecords1, firstCommit);
-      List<WriteStatus> statuses1 = statusesRdd1.collect();
-      assertNoWriteErrors(statuses1);
-      client.commit(firstCommit, statusesRdd1);
+      JavaRDD<WriteStatus> insertRdd = client.insert(jsc().parallelize(dataGen.generateInserts(firstCommit, 100), 1), firstCommit);
+      assertNoWriteErrors(insertRdd.collect());
+      client.commit(firstCommit, insertRdd);
 
-      // Second commit - update data to create log files
+      // Second commit - upsert to create log files (this commit will be archived)
       String secondCommit = "002";
       WriteClientTestUtils.startCommitWithTime(client, secondCommit);
-      List<HoodieRecord> records2 = dataGen.generateUpdates(secondCommit, 50);
-      JavaRDD<HoodieRecord> writeRecords2 = jsc().parallelize(records2, 1);
-      JavaRDD<WriteStatus> statusesRdd2 = client.upsert(writeRecords2, secondCommit);
-      List<WriteStatus> statuses2 = statusesRdd2.collect();
-      assertNoWriteErrors(statuses2);
-      assertLogFilesProduced(statuses2);
-      client.commit(secondCommit, statusesRdd2);
+      JavaRDD<WriteStatus> secondRdd = client.upsert(jsc().parallelize(dataGen.generateUpdates(secondCommit, 50), 1), secondCommit);
+      List<WriteStatus> secondStatuses = secondRdd.collect();
+      assertNoWriteErrors(secondStatuses);
+      assertLogFilesProduced(secondStatuses);
+      client.commit(secondCommit, secondRdd);
 
-      // Third through sixth commits - more updates to trigger archival
-      String thirdCommit = "003";
-      WriteClientTestUtils.startCommitWithTime(client, thirdCommit);
-      JavaRDD<WriteStatus> statusesRdd3 = client.upsert(jsc().parallelize(dataGen.generateUpdates(thirdCommit, 30), 1), thirdCommit);
-      assertNoWriteErrors(statusesRdd3.collect());
-      client.commit(thirdCommit, statusesRdd3);
+      // Additional commits to trigger archival (003-006)
+      String[] additionalCommits = {"003", "004", "005", "006"};
+      for (String commitTime : additionalCommits) {
+        WriteClientTestUtils.startCommitWithTime(client, commitTime);
+        JavaRDD<WriteStatus> rdd = client.upsert(jsc().parallelize(dataGen.generateUpdates(commitTime, 20), 1), commitTime);
+        assertNoWriteErrors(rdd.collect());
+        client.commit(commitTime, rdd);
+      }
 
-      String fourthCommit = "004";
-      WriteClientTestUtils.startCommitWithTime(client, fourthCommit);
-      JavaRDD<WriteStatus> statusesRdd4 = client.upsert(jsc().parallelize(dataGen.generateUpdates(fourthCommit, 20), 1), fourthCommit);
-      assertNoWriteErrors(statusesRdd4.collect());
-      client.commit(fourthCommit, statusesRdd4);
+      String sixthCommit = additionalCommits[additionalCommits.length - 1];
 
-      String fifthCommit = "005";
-      WriteClientTestUtils.startCommitWithTime(client, fifthCommit);
-      JavaRDD<WriteStatus> statusesRdd5 = client.upsert(jsc().parallelize(dataGen.generateUpdates(fifthCommit, 20), 1), fifthCommit);
-      assertNoWriteErrors(statusesRdd5.collect());
-      client.commit(fifthCommit, statusesRdd5);
-
-      String sixthCommit = "006";
-      WriteClientTestUtils.startCommitWithTime(client, sixthCommit);
-      JavaRDD<WriteStatus> statusesRdd6 = client.upsert(jsc().parallelize(dataGen.generateUpdates(sixthCommit, 20), 1), sixthCommit);
-      assertNoWriteErrors(statusesRdd6.collect());
-      client.commit(sixthCommit, statusesRdd6);
-
-      // Reload metaClient and trigger archival
+      // Trigger archival
       metaClient = HoodieTableMetaClient.reload(metaClient);
       HoodieTable table = HoodieSparkTable.create(config, context(), metaClient);
-      TimelineArchiverV2 archiver = new TimelineArchiverV2(config, table);
-      archiver.archiveIfRequired(context());
-
-      // Reload metaClient after archival
+      new TimelineArchiverV2(config, table).archiveIfRequired(context());
       metaClient = HoodieTableMetaClient.reload(metaClient);
 
-      // Verify that the second commit (which produced log files) has been archived
       assertFalse(metaClient.getActiveTimeline().containsInstant(secondCommit),
           "Second commit should be archived");
 
-      // Get file system view using the active timeline
       HoodieTableFileSystemView fsView = HoodieTableFileSystemView.fileListingBasedFileSystemView(
           context(),
           metaClient,
@@ -284,20 +222,13 @@ public class TestLogReaderUtils extends SparkClientFunctionalTestHarness {
 
       List<String> partitions = Arrays.asList(dataGen.getPartitionPaths());
 
-      // Get all log files up to the sixth commit - should include log files from archived second commit
       Map<HoodieLogFile, List<String>> logFilesWithMaxCommit = LogReaderUtils.getAllLogFilesWithMaxCommit(
-          metaClient,
-          fsView,
-          partitions,
-          sixthCommit,
-          context());
+          metaClient, fsView, partitions, sixthCommit, context());
 
-      // Verify results
       assertNotNull(logFilesWithMaxCommit);
       assertFalse(logFilesWithMaxCommit.isEmpty(),
           "Should return log files even though some commits are archived");
 
-      // Verify that log files contain commit times from archived timeline
       boolean hasArchivedCommitTime = false;
       for (List<String> commitTimes : logFilesWithMaxCommit.values()) {
         assertFalse(commitTimes.isEmpty(), "Each log file should have at least one associated commit time");
