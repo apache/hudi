411ad9a3d86ae336a1c664ff7acc20bec278f5f0
sivabalan <n.siva.b@gmail.com>
2026-04-03 14:04:21 -0700

    Revert "Fixing spark3.5 profile build"
    This reverts commit dcf5fba0dfe542c124e71910afb64406775e377a.

--- hudi-spark-datasource/hudi-spark3.4.x/src/main/scala/org/apache/spark/sql/hudi/analysis/Spark34HoodiePruneFileSourcePartitions.scala
+++ hudi-spark-datasource/hudi-spark3.4.x/src/main/scala/org/apache/spark/sql/hudi/analysis/Spark34HoodiePruneFileSourcePartitions.scala
@@ -15,17 +15,14 @@
  * limitations under the License.
  */
 
-import org.apache.hudi.{HoodieBaseRelation, HoodieFileIndex}
 import org.apache.hudi.SparkAdapterSupport.sparkAdapter
-
-import Spark34HoodiePruneFileSourcePartitions.{exprUtils, getPartitionFiltersAndDataFilters, rebuildPhysicalOperation}
-import Spark34HoodiePruneFileSourcePartitions.HoodieRelationMatcher
+import org.apache.hudi.{HoodieBaseRelation, HoodieFileIndex}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.catalog.CatalogStatistics
 import org.apache.spark.sql.catalyst.expressions.{And, AttributeReference, AttributeSet, Expression, ExpressionSet, NamedExpression, PredicateHelper, SubqueryExpression}
 import org.apache.spark.sql.catalyst.planning.PhysicalOperation
-import org.apache.spark.sql.catalyst.plans.logical.{Filter, LeafNode, LogicalPlan, Project}
 import org.apache.spark.sql.catalyst.plans.logical.statsEstimation.FilterEstimation
+import org.apache.spark.sql.catalyst.plans.logical.{Filter, LeafNode, LogicalPlan, Project}
 import org.apache.spark.sql.catalyst.rules.Rule
 import org.apache.spark.sql.execution.datasources.{HadoopFsRelation, LogicalRelation}
 import org.apache.spark.sql.sources.BaseRelation
