6572fe07dd1074b09d55fb6ebe621b8174f15b41
Shuo Cheng <njucshuo@gmail.com>
2026-06-02 08:47:25 +0800

    fix(flink): Trigger a failover after pending instants recommitted for both global and partitioned RLI (#18793)
    (cherry picked from commit ed9ea0ead908e3765b6c938da121e96ace13ba38)

--- hudi-flink-datasource/hudi-flink/src/test/java/org/apache/hudi/sink/utils/StreamWriteFunctionWrapper.java
+++ hudi-flink-datasource/hudi-flink/src/test/java/org/apache/hudi/sink/utils/StreamWriteFunctionWrapper.java
@@ -368,13 +368,7 @@ public class StreamWriteFunctionWrapper<I> implements TestFunctionWrapper<I> {
   }
 
   public void coordinatorFails() throws Exception {
-    this.coordinator.close();
-    if (isStreamingWriteIndexEnabled) {
-      this.coordinator.setExecutor(new MockCoordinatorExecutor(coordinatorContext));
-    }
     resetCoordinatorToCheckpoint();
-    this.coordinator.start();
-    this.coordinator.setExecutor(new MockCoordinatorExecutor(coordinatorContext));
   }
 
   public void restartCoordinator() throws Exception {
