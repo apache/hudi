18a2eb849fea7babf2dc56656686ee35d244c393
Y Ethan Guo <ethan.guoyihua@gmail.com>
2026-07-04 17:03:33 -0700

    fix(spark): make export_instants desc ordering work (#19172)
    (cherry picked from commit 8347be48ce31ef3aee70964a9d0931e32b857136)

--- hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/spark/sql/hudi/procedure/TestExportInstantsProcedure.scala
+++ hudi-spark-datasource/hudi-spark/src/test/scala/org/apache/spark/sql/hudi/procedure/TestExportInstantsProcedure.scala
@@ -17,34 +17,116 @@
 
 package org.apache.spark.sql.hudi.procedure
 
+import org.apache.spark.sql.Row
+
+import java.io.File
+
 class TestExportInstantsProcedure extends HoodieSparkProcedureTestBase {
+
+  private def createCowTable(tableName: String, path: String): Unit = {
+    spark.sql(
+      s"""
+         |create table $tableName (
+         |  id int,
+         |  name string,
+         |  price double,
+         |  ts long
+         |) using hudi
+         | location '$path'
+         | tblproperties (
+         |  primaryKey = 'id',
+         |  orderingFields = 'ts'
+         | )
+       """.stripMargin)
+  }
+
+  private def newExportDir(tmp: File, name: String): File = {
+    val dir = new File(tmp, name)
+    assert(dir.mkdirs(), s"Failed to create export dir $dir")
+    dir
+  }
+
+  private def exportedCount(result: Array[Row]): Int = {
+    assertResult(1)(result.length)
+    val detail = result.head.getString(0)
+    val matched = "Exported (\\d+) Instants".r.findFirstMatchIn(detail)
+    assert(matched.isDefined, s"Unexpected export detail: $detail")
+    matched.get.group(1).toInt
+  }
 
   test("Test Call export_instants Procedure") {
     withTempDir { tmp =>
       val tableName = generateTableName
-      // create table
-      spark.sql(
-        s"""
-           |create table $tableName (
-           |  id int,
-           |  name string,
-           |  price double,
-           |  ts long
-           |) using hudi
-           | location '${tmp.getCanonicalPath}/$tableName'
-           | tblproperties (
-           |  primaryKey = 'id',
-           |  orderingFields = 'ts'
-           | )
-       """.stripMargin)
+      createCowTable(tableName, s"${tmp.getCanonicalPath}/$tableName")
 
-      // insert data to table
       spark.sql(s"insert into $tableName select 1, 'a1', 10, 1000")
 
-      val result = spark.sql(s"""call export_instants(table => '$tableName', local_folder => '${tmp.getCanonicalPath}/$tableName')""").limit(1).collect()
-      assertResult(1) {
-        result.length
-      }
+      val exportDir = newExportDir(tmp, "export_basic")
+      val result = spark.sql(
+        s"""call export_instants(table => '$tableName', local_folder => '${exportDir.getCanonicalPath}')""").collect()
+
+      // A single insert produces one exportable commit instant that is written to disk.
+      assertResult(1)(exportedCount(result))
+      assertResult(1)(exportDir.listFiles().count(_.getName.endsWith(".commit")))
+    }
+  }
+
+  test("Test Call export_instants Procedure with desc ordering") {
+    withTempDir { tmp =>
+      val tableName = generateTableName
+      createCowTable(tableName, s"${tmp.getCanonicalPath}/$tableName")
+
+      spark.sql(s"insert into $tableName select 1, 'a1', 10, 1000")
+      spark.sql(s"insert into $tableName select 2, 'a2', 20, 2000")
+      spark.sql(s"insert into $tableName select 3, 'a3', 30, 3000")
+
+      val exportDir = newExportDir(tmp, "export_desc")
+      val result = spark.sql(
+        s"""call export_instants(table => '$tableName',
+           | local_folder => '${exportDir.getCanonicalPath}', desc => true)""".stripMargin).collect()
+
+      // The desc branch reverses the active instants and exports all three commits to disk.
+      assertResult(3)(exportedCount(result))
+      assertResult(3)(exportDir.listFiles().count(_.getName.endsWith(".commit")))
+    }
+  }
+
+  test("Test Call export_instants Procedure filters by action") {
+    withTempDir { tmp =>
+      val tableName = generateTableName
+      createCowTable(tableName, s"${tmp.getCanonicalPath}/$tableName")
+
+      spark.sql(s"insert into $tableName select 1, 'a1', 10, 1000")
+      spark.sql(s"insert into $tableName select 2, 'a2', 20, 2000")
+
+      // Restricting to an action that is not present exports nothing.
+      val cleanDir = newExportDir(tmp, "export_clean_only")
+      val cleanResult = spark.sql(
+        s"""call export_instants(table => '$tableName',
+           | local_folder => '${cleanDir.getCanonicalPath}', actions => 'clean')""".stripMargin).collect()
+      assertResult(0)(exportedCount(cleanResult))
+      assertResult(0)(cleanDir.listFiles().count(_.getName.endsWith(".clean")))
+
+      // Restricting to the commit action exports exactly the commit instants.
+      val commitDir = newExportDir(tmp, "export_commit_only")
+      val commitResult = spark.sql(
+        s"""call export_instants(table => '$tableName',
+           | local_folder => '${commitDir.getCanonicalPath}', actions => 'commit')""".stripMargin).collect()
+      assertResult(2)(exportedCount(commitResult))
+      assertResult(2)(commitDir.listFiles().count(_.getName.endsWith(".commit")))
+    }
+  }
+
+  test("Test Call export_instants Procedure with an invalid local folder") {
+    withTempDir { tmp =>
+      val tableName = generateTableName
+      createCowTable(tableName, s"${tmp.getCanonicalPath}/$tableName")
+      spark.sql(s"insert into $tableName select 1, 'a1', 10, 1000")
+
+      val notADir = new File(tmp, "does_not_exist").getCanonicalPath
+      checkExceptionContain(
+        s"""call export_instants(table => '$tableName', local_folder => '$notADir')""")(
+        "is not a valid local directory")
     }
   }
 }
